1c2f642c4f7e3b955f7cff2a1fd0f6f102e62250
jehon <marielineet.jean@gmail.com>
2014-05-13 19:43:49 +0200

    Adding angularjs route

--- app/webroot/static/src/js/service_rest.js
+++ app/webroot/static/src/js/service_rest.js
@@ -6,11 +6,23 @@ cryptoApp.factory('service_rest', [ '$http', '$log' , function($http, $log) {
 		'isLoggedIn': function() {
 			return loggedIn;
 		},
-		'doLogin': function() {
+		'doLogin': function(username, password) {
+			var def = jQuery.Deferred();
+			if (loggedIn) {
+				def.resolve();
+			};
 			loggedIn = true;
+			def.resolve();
+			return def;
 		},
 		'doLogout': function() {
+			var def = jQuery.Deferred();
+			if (!loggedIn) {
+				def.resolve();
+			};
 			loggedIn = false;
+			def.resolve();
+			return def;
 		},
 	};
 }]);
