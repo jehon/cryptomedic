d8b58e9fb6cd4d32bfd5b9a5ca40211412641d3a
jehon <marielineet.jean@gmail.com>
2014-05-16 06:34:02 +0200

    removed unused static et testing

--- app/webroot/testing/tests_jehon.js
+++
@@ -1,8 +0,0 @@
-test("jehon library", function() {
-	ok(typeof(jehon) != "undefined", "Loading jehon library");
-	QUnit.assert.tableCell("1", "2", "table cell test");
-});
-
-function qbehavior_nextPage() {
-	return "/";
-}
