7c1f8a640c244f6045c9d9432864d30a9231d679
jehon <marielineet.jean@gmail.com>
2014-05-20 07:49:25 +0200

    Read work for normal case
    Link to be done

--- app/webroot/angular/app/php/templates.php
+++ app/webroot/angular/app/php/templates.php
@@ -1,6 +1,7 @@
 <?php
 
 require_once "../../../../../../maintenance.php";
+require_once(__DIR__ . "/../../../../Lib/cryptomedic.php");
 
 global $mysqli;
 $mysqli = new mysqli($config['database']['host'],
@@ -37,6 +38,43 @@ function _label($key) {
 	return $version["english"];
 }
 
+function label($key) {
+	echo "<label for='$key'>" . _label($key) . "</label>\n";
+}
+
+function value($key) {
+	if (array_key_exists('mode', $_REQUEST) && $_REQUEST['mode'] == "write") {
+		return write($key);
+	} else {
+		return read($key);
+	}
+}
+
+function read($key, $type = null) {
+	$struct = _parseKey($key);
+	if ($type == null) $type = $struct->myType; 
+	switch($type) {
+		case 'text':
+		case 'numeric':
+		case 'float':
+		case 'datetime':
+			echo "<span id='$key'>{{" . $struct->name . "}}</span>";
+			break;
+		case 'boolean':
+			echo "<span id='$key-true' ng-show='" . $struct->name . "'><img src='img/boolean-true.gif'></span>"
+					. "<span id='$key-true' ng-hide='" . $struct->name . "'><img src='img/boolean-false.gif'></span>";
+		case 'link':
+			//TODO
+		default:
+			echo "$key input";
+	}
+}
+
+function write($key, $allownull) {
+	$struct = _parseKey($key);
+	read($key);
+}
+
 function _parseKey($key) {
 	$data = explode(".", $key);
 	if (count($data) != 2) {
@@ -44,25 +82,81 @@ function _parseKey($key) {
 	}
 	$model = $data[0];
 	$field = $data[1];
-	require(__DIR__ . "/../../../../Lib/cryptomedic.php");
-}
 
-function label($key) {
-	echo "<label for='$key'>" . _label($key) . "</label>\n";
-}
-
-function value($key) {
-	if ($_REQUEST['mode'] == "write") {
-		return write($key);
-	} else {
-		return read($key);
+	global $mysqli;
+	$res = $mysqli->query("SELECT $field FROM patients LIMIT 1");
+	$structures = $res->fetch_fields();
+	$structure = $structures[0];
+	
+	$structure->myFlags = array();
+	$constants = get_defined_constants(true);
+	foreach ($constants['mysqli'] as $c => $n) {
+		if (preg_match('/MYSQLI_(.*)_FLAG$/', $c) && (($structure->flags & $n) > 0)) {
+			$structure->myFlags[$c] = 1; 
+		}
 	}
-}
-
-function read($key) {
-	$struct = _parseKey($key);
-}
-
-function write($key, $allownull) {
-	$struct = _parseKey($key);
+	
+	switch ($structure->type) {
+		case MYSQLI_TYPE_TINY:
+		case MYSQLI_TYPE_BIT:
+				$structure->myType = "boolean";
+				break;
+		case MYSQLI_TYPE_DECIMAL:
+		case MYSQLI_TYPE_NEWDECIMAL:
+		case MYSQLI_TYPE_SHORT:
+		case MYSQLI_TYPE_LONG:
+		case MYSQLI_TYPE_LONGLONG:
+		case MYSQLI_TYPE_INT24:
+				$structure->myType = "numeric";
+				break;
+		case MYSQLI_TYPE_FLOAT:
+		case MYSQLI_TYPE_DOUBLE:
+				$structure->myType = "float";
+				break;
+		case MYSQLI_TYPE_TIMESTAMP:
+		case MYSQLI_TYPE_DATETIME:
+				$structure->myType = "datetime";
+				break;
+		case MYSQLI_TYPE_VAR_STRING:
+		case MYSQLI_TYPE_STRING:
+		case MYSQLI_TYPE_CHAR:
+		case MYSQLI_TYPE_TINY_BLOB:
+		case MYSQLI_TYPE_MEDIUM_BLOB:
+		case MYSQLI_TYPE_LONG_BLOB:
+		case MYSQLI_TYPE_BLOB:
+			$structure->myType = "text";
+				break;
+		case MYSQLI_TYPE_TIME:
+		case MYSQLI_TYPE_DATE: 
+		case MYSQLI_TYPE_YEAR:
+		case MYSQLI_TYPE_NEWDATE:
+		case MYSQLI_TYPE_INTERVAL:
+		case MYSQLI_TYPE_ENUM:
+		case MYSQLI_TYPE_SET:
+		case MYSQLI_TYPE_GEOMETRY:
+		default:
+				// http://www.php.net/manual/en/mysqli.constants.php
+				var_dump($structure);
+				$constants = get_defined_constants(true);
+				foreach ($constants['mysqli'] as $c => $n) 
+					if (preg_match('/^MYSQLI_TYPE_(.*)/', $c, $m) && ($n == $structure->type))
+						var_dump($c); 
+						//$types[$n] = $m[1];
+						
+				throw new Exception("Unhandled type for field $field");
+	}
+	global $model_listing;
+	if (array_key_exists($key, $model_listing)) {
+		$structure->listing = $model_listing[$key];
+		if (array_key_exists('labels', $structure->listing) && ($structure->listing['labels'])) {
+			$list = $structure->listing;
+			unset($list['labels']);
+			$structure->listing = [];
+			foreach($list as $k => $v){
+				$structure->listing[$v] = _label($v);
+			}
+		}
+		$structure->myType = "link";
+	}
+	return $structure;
 }
