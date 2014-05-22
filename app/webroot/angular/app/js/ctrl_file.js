b5f280df0a403328c5aa1168dd4e4773954bf2d7
jehon <marielineet.jean@gmail.com>
2014-05-22 17:28:44 +0200

    Dynamic name, files with s everywhere

--- app/webroot/angular/app/js/ctrl_file.js
+++ app/webroot/angular/app/js/ctrl_file.js
@@ -1,7 +1,7 @@
 "use strict";
 
 cryptoApp.controller('ctrl_file', [ '$scope', '$location', 'service_rest', '$routeParams' , function($scope, $location, service_rest, $routeParams) { 
-	$scope.file = [];
+	$scope.files = [];
 	$scope.page = 0;
 	$scope.patientId = $routeParams['id'];
 	if (typeof($routeParams['page']) != 'undefined') {
@@ -9,19 +9,26 @@ cryptoApp.controller('ctrl_file', [ '$scope', '$location', 'service_rest', '$rou
 	}
 	
 	$scope.select = function(page) {
-		console.log(page);
 		$scope.page = page;
 	};
 	
 	$scope.selected = function(i) {
 		if (i == $scope.page) {
-			return "btn-error";
+			return "btn-warning";
 		}
 		return "";
 	};
 
 	$scope.name = function() {
-		return "patient";
+		if (parseInt($scope.page) == $scope.page) {
+			if ($scope.page < $scope.files.length) {
+				console.log($scope.files[$scope.page]);
+				return $scope.files[$scope.page]['type'].toLowerCase();
+			} else {
+				return "blank";
+			}
+		}
+		return $scope.page;
 	};
 	
 	var busyEnd = $scope.doBusy("Getting the file from the server");
