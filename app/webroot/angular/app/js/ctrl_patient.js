962fca179f406eaf348a42d32fde065ee02a7bb3
jehon <marielineet.jean@gmail.com>
2014-05-22 17:29:21 +0200

    calcul of stat_base_actualage

--- app/webroot/angular/app/js/ctrl_patient.js
+++ app/webroot/angular/app/js/ctrl_patient.js
@@ -1,5 +1,9 @@
 "use strict";
 
 cryptoApp.controller('ctrl_patient', [ '$scope', 'service_rest', function($scope, service_rest) {
-	
+	$scope.stats_base_actualage = "#Year of birth unknown#";
+	if ($scope.files[$scope.page]['Yearofbirth'] >= 1900) {
+		$scope.stats_base_actualage = (new Date().getFullYear() - $scope.files[$scope.page]['Yearofbirth']) + " years old today";
+    }
+
 }]);
