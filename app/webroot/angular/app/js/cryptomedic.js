5ebf926d5ce45e8ef226f5075950dae7d931a21f
jehon <marielineet.jean@gmail.com>
2014-05-23 08:09:02 +0200

    Since jehon.js is not necessary anymore, put here the console stub

--- app/webroot/angular/app/js/cryptomedic.js
+++ app/webroot/angular/app/js/cryptomedic.js
@@ -1,8 +1,29 @@
 "use strict";
 
+/**
+ * For this f*** old IE6-8
+ */
+if (typeof(console) === 'undefined') { console = {}; }
+if (typeof(console.log) !== 'function') { console.log = function() {}; }
+if (typeof(console.info) !== 'function') { console.info = console.log; }
+if (typeof(console.error) !== 'function') { console.error = console.log; }
+if (typeof(console.trace) !== 'function') { console.trace = console.log; }
+if (typeof(console.warn) !== 'function') { console.warn = console.log; }
+if (typeof(console.group) !== 'function') { console.group = function(group) { console.log("GROUP: " + group); }; }
+if (typeof(console.groupCollapsed) !== 'function') { console.groupCollapsed = console.group; }
+if (typeof(console.groupEnd) !== 'function') { console.groupEnd = function() { console.log("GROUP END"); } ; }
+
+if (window.location.search) {
+	if (window.location.search.search("_nocollapse") > 0) {
+		console.log("mode no-collapse");
+		console.groupCollapsed = console.group;
+	}
+}
+
 var cryptomedic = {};
 cryptomedic.settings = {};
 cryptomedic.structure = {};
+cryptomedic.prototypes = {};
 
 cryptomedic.model2controller = {
     "Bill": "bills", 
