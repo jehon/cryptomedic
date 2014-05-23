8d8876069e9d30088aaecd01d5edbf80e2e593bc
jehon <marielineet.jean@gmail.com>
2014-05-23 08:10:58 +0200

    added doc pointer

--- app/webroot/angular/app/js/ctrl_consultation.js
+++ app/webroot/angular/app/js/ctrl_consultation.js
@@ -1,5 +1,7 @@
 "use strict";
 
 cryptoApp.controller('ctrl_consultation', [ '$scope', 'service_rest', function($scope, service_rest) {
+	// TODO: calculate age:             dx = v.Date.substr(0, 4) - ajax.Yearofbirth;
+	// From application.js # 534
 	$scope.stats_base_age = 12;
 }]);
