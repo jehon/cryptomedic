3a3c35de29a997a664cfe3bf34e827d20e3bb685
jehon <marielineet.jean@gmail.com>
2014-05-26 07:13:12 +0200

    Factorize data load + UT ok

--- app/webroot/angular/app/js/model_data.js
+++ app/webroot/angular/app/js/model_data.js
@@ -4,8 +4,11 @@ cryptomedic.models = {};
 
 cryptomedic.models.Data = Class.extend({
 	init: function(data){
-//		this._super();
-		if (data != null) {
+//		this._super.init();
+		this.load(data);
+	},
+	load: function(data) {
+		if ((typeof(data) != undefined) && (data != null)) {
 			for(var i in data) {
 				this[i] = data[i];
 			}
--- app/webroot/angular/test/unit/test_model_data.js
+++ app/webroot/angular/test/unit/test_model_data.js
@@ -1,12 +1,29 @@
 "use strict";
 
 describe("Data", function() {
-	var data = new cryptomedic.models.Data();
-	it('should have inheritance ok', function() {
-		expect(data instanceof cryptomedic.models.Data).toBeTruthy();
+	describe("with empty loader", function() {
+		var data = new cryptomedic.models.Data();
+		it('should have inheritance ok', function() {
+			expect(data instanceof cryptomedic.models.Data).toBeTruthy();
+		});
+	
+		it('should have a getName function', function() {
+			expect(data.getName()).toBeTruthy('Data');
+		});
 	});
-
-	it('should have a getName function', function() {
-		expect(data.getName()).toBeTruthy('Data');
+		
+	describe("with data loading", function() {
+		var folder = new cryptomedic.models.Folder({
+			data1: "data1",
+			dataArray: [ 1, 2, 3]
+		});
+		it("should contain all datas", function() {
+			expect(folder.data1).toBe("data1");
+			expect(folder.dataArray).toContain(1);
+			expect(folder.dataArray).toContain(2);
+			expect(folder.dataArray).toContain(3);
+			expect(folder.dataArray).not.toContain(4);
+			expect(folder.anything).toBe(undefined);
+		});
 	});
 });
