0288fee87cca3040049cc3c55b0a1f7244db254e
jehon <marielineet.jean@gmail.com>
2014-05-26 06:56:37 +0200

    Adding dependancy on mocks

--- app/webroot/angular/test/karma.conf.js
+++ app/webroot/angular/test/karma.conf.js
@@ -1,33 +1,41 @@
-module.exports = function(config){
-  config.set({
+module.exports = function(config) {
+	config.set({
+		plugins : [ 
+		            'karma-chrome-launcher', 
+		            'karma-firefox-launcher',
+		            'karma-jasmine', 
+		            'karma-junit-reporter',
+		            //'karma-htmlfile-reporter' // html report
+		],
+		// https://www.npmjs.org/package/karma-htmlfile-reporter
+		reporters : [ 
+		              'progress', 
+//		              'html' 
+		             ],
 
-    basePath : '../',
+		basePath : '../',
 
-    files : [
-      'app/bower_components/angular/angular.js',
-      'app/bower_components/angular-route/angular-route.js',
-      'app/bower_components/angular-mocks/angular-mocks.js',
-      'app/js/**/*.js',
-      'test/unit/**/*.js'
-    ],
+		files : [ 
+		          'app/bower_components/angular/angular.js',
+		          'app/bower_components/angular-route/angular-route.js',
+		          'app/bower_components/angular-mocks/angular-mocks.js',
+		          'app/js/**/*.js', 'test/unit/**/*.js', 
+		          { pattern : 'test/mock/*.json', watched : true, served : true, included : false }
+		        ],
 
-    autoWatch : true,
+		autoWatch : true,
 
-    frameworks: ['jasmine'],
+		frameworks : [ 'jasmine' ],
 
-    browsers : ['Chrome'],
+		browsers : [ 'Chrome' ],
 
-    plugins : [
-            'karma-chrome-launcher',
-            'karma-firefox-launcher',
-            'karma-jasmine',
-            'karma-junit-reporter'
-            ],
-
-    junitReporter : {
-      outputFile: 'test_out/unit.xml',
-      suite: 'unit'
-    }
-
-  });
+		junitReporter : {
+			outputFile : 'test_out/unit.xml',
+			suite : 'unit'
+		},
+		
+//		htmlReporter : {
+//			outputFile : 'test_out/html_unit.html'
+//		}
+	});
 };
