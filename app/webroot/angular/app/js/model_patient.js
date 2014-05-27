c74db60ee188cc971c98b36ab3cc856fa5ced097
jehon <marielineet.jean@gmail.com>
2014-05-27 17:04:52 +0200

    Testing actualAge

--- app/webroot/angular/app/js/model_patient.js
+++ app/webroot/angular/app/js/model_patient.js
@@ -1,9 +1,10 @@
 "use strict";
 
 cryptomedic.models.Patient = cryptomedic.models.Data.extend({
-	actualAge: function() {
+	actualAge: function(date) {
+		if (typeof(date) == "undefined") date = new Date();
 		if (this.Yearofbirth >= 1900) {
-			return (new Date().getFullYear() - this.Yearofbirth) + " years old today";
+			return (date.getFullYear() - this.Yearofbirth) + " years old today";
 		} else {
 			return "#Year of birth unknown#";
 		} 
