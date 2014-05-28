7c400ef591c79820de8199ad86e29408715bfdb5
jehon <marielineet.jean@gmail.com>
2014-05-28 16:52:00 +0200

    Clean up duplicate tests

--- app/webroot/angular/test/unit/test_model_file.js
+++ app/webroot/angular/test/unit/test_model_file.js
@@ -8,26 +8,6 @@ describe("File", function() {
 		}, function() {
 			expect(folder instanceof cryptomedic.models.Folder).toBeTruthy();
 			folder.objectizeList();
-			expect(folder.file(1) instanceof cryptomedic.models.File).toBeTruthy();
-			expect(folder.file(1).id).toBe(695);
-			expect(folder.getPatient().Sex).toBe(207);
-			expect(folder.getPatient().Yearofbirth).toBe(1998);
-			expect(folder.getPatient().actualAge(new Date("2014-01-01"))).toBe("16 years old today");
-			
-			expect(folder.file(1).Date).toBe("2014-04-17");
-			expect(folder.file(1).ageAtConsultTime()).toBe(16);
-			expect(folder.file(1).ageAtConsultTimeStr()).toBe("16 years old at that time of consultation");
-			
-		}));
-	});
-
-	describe("with mock_patient_10.json", function() {
-		var folder = new cryptomedic.models.Folder();
-		it("should have correct properties", myDeferredTest(function () {
-			return folder.loadFrom("/base/test/mocks/mock_patient_10.json");
-		}, function() {
-			expect(folder instanceof cryptomedic.models.Folder).toBeTruthy();
-			folder.objectizeList();
 			expect(folder.getPatient().Sex).toBe(207);
 			expect(folder.getPatient().Yearofbirth).toBe(1998);
 			expect(folder.getPatient().actualAge(new Date("2014-01-01"))).toBe("16 years old today");
