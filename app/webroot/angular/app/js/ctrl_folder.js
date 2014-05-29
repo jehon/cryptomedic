12e2859473fe0f83eddefab02d3fad9749fddcad
jehon <marielineet.jean@gmail.com>
2014-05-29 21:02:30 +0200

    Clean up according to controller

--- app/webroot/angular/app/js/ctrl_folder.js
+++ app/webroot/angular/app/js/ctrl_folder.js
@@ -1,17 +1,20 @@
 "use strict";
 
-cryptoApp.controller('ctrl_folder', [ '$scope', '$location', 'service_rest', '$routeParams' , function($scope, $location, service_rest, $routeParams) { 
+cryptoApp.controller('ctrl_folder', [ '$scope', '$location', 'service_rest', '$routeParams' , function($scope, $location, service_rest, $routeParams) {
 	$scope.folder = new cryptomedic.models.Folder();
-	$scope.page = 0;
-	$scope.patientId = $routeParams['id'];
-	if (typeof($routeParams['page']) != 'undefined') {
-		$scope.page = $routeParams['page'];
-		if (parseInt($routeParams['page']) == $routeParams['page'])
-			$scope.page = parseInt($routeParams['page']); 
-	}
+	$scope.page = "summary";
+	$scope.pageIsFile = false;
+	$scope.patientId = parseInt($routeParams['id']);
 	
 	$scope.select = function(page) {
 		$scope.page = page;
+		$scope.pageIsFile = false;
+		if (parseInt($scope.page) == $scope.page) {
+			$scope.page = parseInt($scope.page);
+			if ($scope.page < $scope.folder.files.length)
+				$scope.pageIsFile = true;
+			console.log($scope.currentFile());
+		}
 	};
 	
 	$scope.selected = function(i) {
@@ -21,30 +24,46 @@ cryptoApp.controller('ctrl_folder', [ '$scope', '$location', 'service_rest', '$r
 		return "";
 	};
 
-	$scope.name = function() {
-		if (parseInt($scope.page) == $scope.page) {
-			if ($scope.page < $scope.folder.files.length) {
-				return $scope.folder.files[$scope.page]['type'].toLowerCase();
-			} else {
-				return "blank";
-			}
+	$scope.currentFile = function() {
+		if ($scope.pageIsFile) {
+			return $scope.folder.files[$scope.page];
 		}
+		console.log("default to patient");
+		return $scope.patient();
+	};
+	
+	$scope.patient = function() {
+		if ($scope.folder.files.count == 0) return null;
+		return $scope.folder.getPatient();
+	};
+	
+	$scope.name = function() {
+		if ($scope.pageIsFile) {
+			return $scope.folder.files[$scope.page]['type'].toLowerCase();
+		}
+		if (typeof($scope.page) == "number") return "blank";
 		return $scope.page;
 	};
 	
 	$scope.getCached = function(id) {
 		return service_rest.getCached(id);
 	};
+
+	if (typeof($routeParams['page']) != 'undefined') {
+		$scope.select($routeParams['page']);
+	}
 	
 	var busyEnd = $scope.doBusy("Getting the file from the server");
 	service_rest.getFile($scope.patientId)
 		.done(function(data) {
 			$scope.folder = data;
+			$scope.select($scope.page);
 			$scope.safeApply();
 		})
 		.fail(function(data) {
 			console.error(data);
 		}).always(function() {
+			$scope.$broadcast("refresh");
 			busyEnd();
 		});
 }]);
--- app/webroot/angular/app/partials/folder.php
+++ app/webroot/angular/app/partials/folder.php
@@ -4,23 +4,23 @@
 	<a ng-class="selected('graphics')" href="#/patient/{{patientId}}/graphics" class="btn btn-default" style="width: 100%">Graphics</a>
 	<span ng-repeat="f in folder.files">
 		<a href="#/patient/{{patientId}}/{{$index}}" class="btn btn-default" style="width: 100%" ng-class="selected($index)">
-			{{f.type}} [{{f.Date}}]
+			{{f.type}}<span ng-if="f.Date">[{{f.Date}}]</span>
 		</a>
 	</span>
 	<a ng-if="hasPermission('all.debug')" ng-class="selected('export')" href="#/patient/{{patientId}}/export" class="btn btn-default debug" style="width: 100%">Export</a>
 </div>
 <div class="col-md-10">
-	<div >
+	<div ng-if="patient()" ng-controller="ctrl_patient">
         Get a complete report:
         	<a target="_new" href="/amd/patients/view/<?php rawValue("Patient.id"); ?>.csv" >csv</a>&nbsp
         	<a target="_new" href="/amd/patients/view/<?php rawValue("Patient.id"); ?>.csv?fr=1" >french-csv</a>&nbsp
     		<a target="_new" href="/amd/patients/view/<?php rawValue("Patient.id"); ?>.xls" >xls</a>&nbsp
 	</div>
-	<div xng-if="isNumber(page)">
-		Id: {{folder.files[page].id}} #
-		Type: {{folder.files[page].type}} #
-		Modified on: {{folder.files[page].modified}} #
-		By: {{folder.files[page].lastuser}}
+	<div ng-if="pageIsFile">
+		Id: {{currentFile().id}} #
+		Type: {{currentFile().type}} #
+		Modified on: {{currentFile().modified}} #
+		By: {{currentFile().lastuser}}
 	</div>
 	<span ng-include="'partials/' + name() + '.php'">
 		debug: Content {{page}}
