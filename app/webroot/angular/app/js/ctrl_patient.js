ea06ee9dcb751d61b9f84469822a2db8ae94f4c5
jehon <marielineet.jean@gmail.com>
2014-05-29 21:02:48 +0200

    ctrl patient: current is the patient, not the current file

--- app/webroot/angular/app/js/ctrl_patient.js
+++ app/webroot/angular/app/js/ctrl_patient.js
@@ -1,4 +1,7 @@
 "use strict";
 
 cryptoApp.controller('ctrl_patient', [ '$scope', 'service_rest', function($scope, service_rest) {
+	$scope.currentFile = function() {
+		return $scope.folder.getPatient();
+	};
 }]);
