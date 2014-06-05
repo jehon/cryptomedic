078ec7cce79b5d671963da4f09b798a2dbd72d57
jehon <marielineet.jean@gmail.com>
2014-06-05 16:33:01 +0200

    Format

--- app/webroot/angular/test/karma.conf.js
+++ app/webroot/angular/test/karma.conf.js
@@ -22,7 +22,8 @@ module.exports = function(config) {
 		          'app/bower_components/angular/angular.js',
 		          'app/bower_components/angular-route/angular-route.js',
 		          'app/bower_components/angular-mocks/angular-mocks.js',
-		          'app/js/**/*.js', 'test/unit/**/*.js', 
+		          'app/js/**/*.js', 
+		          'test/unit/**/*.js', 
 		          { pattern : 'test/mocks/*.json', watched : true, served : true, included : false }
 		        ],
 
