627ab06ea322bab361a8ce75e307d2ea5ecdf88d
jehon <marielineet.jean@gmail.com>
2014-06-20 14:52:36 +0200

    Club foot ok

--- app/webroot/angular/app/partials/consult-introduction.php
+++ app/webroot/angular/app/partials/consult-introduction.php
@@ -3,9 +3,9 @@
 	<legend><?php (new t("NonricketConsult-GeneralData"))->label()->p(); ?></legend>
 	<table  class='colorize'>
 		<col width='30%' /><col width='*' /> 
-		<?php (new t("NonricketConsult.Date"))->label()->p(); ?>
-		<?php (new t("NonricketConsult.ExaminerName"))->label()->p(); ?>
-		<?php (new t("NonricketConsult.Center"))->label()->p(); ?>
+		<?php (new t("NonricketConsult.Date"))->tr()->p(); ?>
+		<?php (new t("NonricketConsult.ExaminerName"))->tr()->p(); ?>
+		<?php (new t("NonricketConsult.Center"))->tr()->p(); ?>
 		<tr modes='read'>
 			<td><?php (new t("Patient-Age"))->label()->p(); ?></td>
 			<td>{{ageAtConsultTimeStr()}}</td>
@@ -26,14 +26,14 @@
 			<td class='tdright'><?php (new t("NonricketConsult.Heightcm"))->value()->p(); ?></td>
 			<td>{{stats_ds_height() | mynumber:2:'ds' }}</div></td>
 		</tr><tr>
-			<td><?php (new t("NonricketConsultWeightHeightRatio"))->label()->p(); ?></td>
+			<td><?php (new t("NonricketConsult-WeightHeightRatio"))->label()->p(); ?></td>
 			<td class='tdright'>{{stats_wh() | mynumber:2 }}</td>
 			<td>{{stats_ds_weight_height() | mynumber:2:'ds' }}</td>
 		</tr><tr>
 			<td><?php (new t("NonricketConsult.bmi"))->label()->p(); ?></td>
 			<td>{{stats_bmi() | mynumber:2 }}</td>
 			<td>{{stats_ds_bmi() | mynumber:2:'ds' }}</td>
-		</tr>		<?php (new t("NonricketConsult.Brachialcircumferencecm"))->label()->p(); ?>
-
+		</tr>
+		<?php (new t("NonricketConsult.Brachialcircumferencecm"))->label()->p(); ?>
 	</table>
 </fieldset>
