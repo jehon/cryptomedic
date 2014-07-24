dac970ab81708ab358f058c77cb6ec8586468248
jehon <marielineet.jean@gmail.com>
2014-07-24 07:14:26 +0200

    mysqli

--- app/webroot/rest/config.php
+++ app/webroot/rest/config.php
@@ -7,7 +7,7 @@ $config = array(
 		"debug" => false,
 		"domain" => "domain",
 		"database" => array(
-				"uri" => "mysql://user:pwd@server/db?persist",
+				"uri" => "mysqli://user:pwd@server/db?persist",
 				"init" => "SET CHARACTER SET 'utf8'"
 			),
 		"publicURL" => array()
@@ -16,7 +16,7 @@ $config = array(
 if (file_exists(__DIR__ . DIRECTORY_SEPARATOR . "../../../../maintenance.php"))
 	require_once(__DIR__ . DIRECTORY_SEPARATOR . "../../../../maintenance.php");
 
-$config['database']['uri'] = "mysql://{$config['database']['login']}:{$config['database']['password']}"
+$config['database']['uri'] = "mysqli://{$config['database']['login']}:{$config['database']['password']}"
 	. "@{$config['database']['host']}/{$config['database']['schema']}";
 $config['database']['init'] = "SET CHARACTER SET 'utf8'";
 
