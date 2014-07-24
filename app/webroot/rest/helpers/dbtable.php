ace3e06f9e3cdc9b0da4cec780aab81cacc603d4
jehon <marielineet.jean@gmail.com>
2014-07-24 07:09:12 +0200

    Change pointer to adodb
    Change dieIfNecessary to posttreatment

--- app/webroot/rest/helpers/dbtable.php
+++ app/webroot/rest/helpers/dbtable.php
@@ -1,6 +1,6 @@
 <?php
 
-require_once(__DIR__ . "/../../adodb/adodb.inc.php");
+require_once(__DIR__ . "/../../new/libs/adodb/adodb.inc.php");
 
 class DBTable {
 	const PRIVATE_COLUMNS = "privateColumns";
@@ -21,9 +21,8 @@ class DBTable {
 		$this->db = ADONewConnection($config['uri']);
 		$this->db->SetFetchMode(ADODB_FETCH_ASSOC);
 		if (array_key_exists("init", $config)) {
-			//$this->db->Execute("SET CHARACTER SET 'utf8'");
 			$this->lastStatement = $config['init'];
-			$this->dieIfNecessary($this->db->Execute($config['init']), "Database init script error");
+			$this->myPostTreatment($this->db->Execute($config['init']), "Database init script error");
 		}
 
 		$tablesList = $this->server->getCache("tables", null);
@@ -38,6 +37,14 @@ class DBTable {
 		}
 	}
 
+	public function isColumn($col) {
+		if (!$this->table) throw new DBSystemError("No table in ". __METHOD__);
+		$i = array_search($col, $this->getColumns());
+		if ($i === false)
+			return false;
+		return $this->columns[$i];
+	}
+
 	public function escape($what) {
 		return $this->db->qstr($what, get_magic_quotes_gpc());
 	}
@@ -45,16 +52,8 @@ class DBTable {
 	public function getColumns() {
 		if (!$this->table) throw new DBSystemError("No table in ". __METHOD__);
 		if (!$this->columns) 
-			$this->columns = $this->dieIfNecessary($this->db->MetaColumnNames($this->table), "Getting columns names");
+			$this->columns = $this->myPostTreatment($this->db->MetaColumnNames($this->table), "Getting columns names");
 		return $this->columns;
-	}
-
-	public function isColumn($col) {
-		if (!$this->table) throw new DBSystemError("No table in ". __METHOD__);
-		$i = array_search($col, $this->getColumns());
-		if ($i === false)
-			return false;
-		return $this->columns[$i];
 	}
 
 	public function rowAll($where = array(), $limit = null) {
@@ -65,9 +64,8 @@ class DBTable {
 		$sql .= "`" . implode($cols, "`, `") . "`";
 		$sql .= "FROM `{$this->table}` ";
 
-		// We send back only what we can handle:
-		$whereClause = "(id > '') AND (modified is not null)";
-		// $whereClause = "(1 = 1)";
+		// We send back only what we can handle: 
+		$whereClause = "(id is not null) AND (modified is not null)";
 		
 		foreach($where as $p => $v) {
 			$np = $this->isColumn($p);
@@ -80,13 +78,13 @@ class DBTable {
 		if (is_numeric($limit))
 			$sql .= "LIMIT " . $limit;
 
-		$data = $this->dieIfNecessary($this->db->GetAll($sql), "rowAll");
+		$data = $this->myPostTreatment($this->db->GetAll($sql), "rowAll");
 		return $data;
 	}
 
 	public function rowGet($id) {
 		if (!$this->table) throw new DBSystemError("No table in ". __METHOD__);
-		$res = $this->preparedStatement("SELECT * FROM `{$this->table}` WHERE id= ?", array($id));
+		$res = $this->preparedStatement("SELECT * FROM `{$this->table}` WHERE `id` = ?", array($id));
 		foreach($this->options[self::PRIVATE_COLUMNS] as $c) {
 			unset($res[$c]);
 		}
@@ -95,14 +93,40 @@ class DBTable {
 
 	public function preparedStatement($statement, $data) {
 		$this->lastStatement = $statement;
-		$stmt = $this->dieIfNecessary($this->db->Prepare($statement), "preparing statement");
-		$res = $this->dieIfNecessary($this->db->Execute($stmt, $data), "executing statement");
+		$stmt = $this->db->Prepare($statement); // This could not fail!
+		$res = $this->myPostTreatment($this->db->Execute($stmt, $data), "executing statement");
 		return $res->getArray();
 	}
 
-	public function dieIfNecessary($result, $dbgMsg) {
-		if (($result === false) && ($this->db->ErrorMsg()))
+	public function execute($sql, $byId = false) {
+		$recordSet = $this->myPostTreatment($this->db->GetAll($sql), "executing sql");
+		if ($byId === false) return $recordSet;
+		$res = array();
+		foreach($recordSet as $v) {
+			$res[$v[$byId]] = $v;
+		}
+		return $res;
+	}
+ 
+	protected function myPostTreatment($result, $dbgMsg) {
+		if (($result === false) && ($this->db->ErrorMsg())) {
+			trace();
 			throw new DBSystemError("Invalid SQL: " . ($dbgMsg ? "[" . $dbgMsg . "]" : "") . ":" . $this->db->ErrorMsg());
+		}
 		return $result;
 	}
+
+	// private function myTreatNumbers($data) {
+	// 	if (is_string($data)) 
+	// 		if (filter_var($data, FILTER_VALIDATE_FLOAT))
+	// 			return filter_var($data, FILTER_VALIDATE_FLOAT);
+	// 	if (is_array($data)) {
+	// 		$d2 = array();
+	// 		foreach($data as $k => $v) {
+	// 			$d2[$k] = $this->myTreatNumber($v);
+	// 		} 
+	// 		return $d2;
+	// 	}
+	// 	return $data;
+	// }
 }
