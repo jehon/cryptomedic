5f81fe4820ded1566dd207f6f2b68fdf92a517a1
jehon <marielineet.jean@gmail.com>
2014-07-28 06:27:28 +0200

    Layout and conditionnal display

--- app/webroot/angular/app/partials/consult-introduction.php
+++ app/webroot/angular/app/partials/consult-introduction.php
@@ -5,7 +5,7 @@
 ?>
 <fieldset>
 	<legend><?php label("NonricketConsult-GeneralData"); ?></legend>
-	<table  class='colorize'>
+	<table>
 		<col width='30%' /><col width='*' /> 
 		<?php (new t("NonricketConsult.Date"))->tr()->p(); ?>
 		<?php (new t("NonricketConsult.ExaminerName"))->tr()->p(); ?>
@@ -19,26 +19,33 @@
 <br/>
 <fieldset>
 	<legend><?php label("NonricketConsult-NutritionalData"); ?></legend>
-	<table class='colorize'>
+	<table>
 		<col width='30%' /><col width='*' style='text-align: right' /><col width='20%' /> 
-		<tr><td/><td/><td><?php label("Consultation-Deviation"); ?></td></tr>
+		<tr>
+			<td/><td/>
+			<td class='notModeWrite'><?php label("Consultation-Deviation"); ?></td>
+		</tr>
 		<tr ng-class='{ emptyValue: !currentFile().Weightkg}'>
 			<td><?php label("Consultation.Weightkg"); ?></td>
 			<td class='tdright'><?php (new t("NonricketConsult.Weightkg"))->value()->p(); ?></td>
-			<td><span catch-it ng-model="folder" tryit="currentFile().ds_weight()">{{result | number:2 }} ds</span></td>
+			<td class='notModeWrite'><span catch-it ng-model="folder" tryit="currentFile().ds_weight()">{{result | number:2 }} ds</span></td>
 		</tr><tr ng-class='{ emptyValue: !currentFile().Heightcm}'>
 			<td><?php label("Consultation.Heightcm"); ?></td>
 			<td class='tdright'><?php (new t("NonricketConsult.Heightcm"))->value()->p(); ?></td>
-			<td><span catch-it ng-model="folder" tryit="currentFile().ds_height()">{{result | number:2 }} ds</span></td>
-		</tr><tr ng-class='{ emptyValue: !currentFile().Heightcm || !currentFile().Weightkg}'>
+			<td class='notModeWrite'><span catch-it ng-model="folder" tryit="currentFile().ds_height()">{{result | number:2 }} ds</span></td>
+		</tr><tr ng-class='{ emptyValue: !currentFile().Heightcm || !currentFile().Weightkg}'
+				class='notModeWrite'
+				>
 			<td><?php label("Consultation-WeightHeightRatio"); ?></td>
 			<td class='tdright'><span catch-it ng-model="folder" tryit="currentFile().wh()">{{result | number:2 }}</span></td>
-			<td><span catch-it ng-model="folder" tryit="currentFile().ds_weight_height()">{{result | number:2 }} ds</span></td>
-		</tr><tr ng-class='{ emptyValue: !currentFile().Heightcm || !currentFile().Weightkg}'>
+			<td class='notModeWrite'><span catch-it ng-model="folder" tryit="currentFile().ds_weight_height()">{{result | number:2 }} ds</span></td>
+		</tr><tr ng-class='{ emptyValue: !currentFile().Heightcm || !currentFile().Weightkg}'
+				class='notModeWrite'
+				>
 			<td><?php label("Consultation.bmi"); ?></td>
 			<td class='tdright'><span catch-it ng-model="folder" tryit="currentFile().bmi()">{{result | number:2 }}</span></td>
-			<td><span catch-it ng-model="folder" tryit="currentFile().ds_bmi()">{{result | number:2 }} ds</span></td>
+			<td class='notModeWrite'><span catch-it ng-model="folder" tryit="currentFile().ds_bmi()">{{result | number:2 }} ds</span></td>
 		</tr>
-		<?php label("Consultation.Brachialcircumferencecm"); ?>
+		<?php (new t("NonricketConsult.Brachialcircumferencecm"))->tr()->p(); ?>
 	</table>
 </fieldset>
