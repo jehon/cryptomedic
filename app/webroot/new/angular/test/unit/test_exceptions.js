c0eda1b40b2fc1cdf44b2a09f466eb8d9910762d
jehon <marielineet.jean@gmail.com>
2014-08-07 16:36:40 +0200

    Adding tests for ApplicationMessage

---
+++ app/webroot/new/angular/test/unit/test_exceptions.js
@@ -0,0 +1,10 @@
+'use strict';
+
+describe('ApplicationException', function() {
+	it('should inherit from Error', function() {
+		var ae = new ApplicationException("my message");
+		
+		expect(ae instanceof Error).toBeTruthy();
+		expect(ae.getMessage()).toBe("my message");
+	});
+});
