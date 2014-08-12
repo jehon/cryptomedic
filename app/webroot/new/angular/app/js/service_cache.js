b1590c58a86fce745b61648a588feefa61aba84b
jehon <marielineet.jean@gmail.com>
2014-08-12 16:48:15 +0200

    cache perish

--- app/webroot/new/angular/app/js/service_cache.js
+++ app/webroot/new/angular/app/js/service_cache.js
@@ -1,25 +1,37 @@
 
-
 function perishableCache(n, perish) {
 	var cache = {};
 	var timestamps = {};
-	
+
+	function perish(id) {
+		delete(cache[id]);
+		delete(timestamps[id]);
+	}
+
+	function checkId(id) {
+		var lim = new Date();
+		lim.setMinutes(lim.getMinutes() - 1);
+		if (timestamps[id] < lim) {
+			console.warn("AutoPerish " + id);
+			perish(id);
+		}
+	}
+
 	return {
-		'isCached': function(id) {
+		'perish': perish,
+		'isCached': function (id) {
+			checkId(id);
 			return (typeof(cache[id]) != "undefined");
 		},
 		'get': function(id) {
-			if (this.isCached(id)) 
+			if (this.isCached(id)) {
 				return angular.copy(cache[id]);
+			}
 			return null;
-		},
-		'perish': function(id) {
-			if (this.isCached(id))
-				delete(cache[id]);
-			return true;
 		},
 		'set': function(id, data) {
 			cache[id] = angular.copy(data);
+			timestamps[id] = new Date();
 			return data;
 		}
 	};
