fd4016a16b6420dcaac70624052bdcb5a28fa27c
jehon <marielineet.jean@gmail.com>
2014-09-15 21:07:43 +0200

    For autodeploy in phprest

--- autodeploy.php
+++ autodeploy.php
@@ -1,18 +1,10 @@
 <?php
 
-$deploys['cryptomedic'] = array(
+$deploy = array(
     'root' => __DIR__,
     'owner' => 'jehon',
     'repository' => 'cryptomedic',
     'DatabaseHistoryPath' => __DIR__ . "/database_scripts",
-    'DatabaseDevPath' => __DIR__ . "/dev.sql"
+    'DatabaseDevPath' => __DIR__ . "/dev.sql",
+    'restoreDB' =>  "C:\Users\jehon\Downloads\amd_chakaria.sql"
 );
-
-require_once("appConfiguration.php");
-$deploys['cryptomedic']['database'] = $config['database'];
-
-$deploys['cryptomedic-test'] = $config['deploys']['cryptomedic'];
-$deploys['cryptomedic-test']['root'] = $config['deploys']['cryptomedic']['root'] . "-test";
-$deploys['cryptomedic-test']['restoreDB'] = "C:\Users\jehon\Downloads\amd_chakaria.sql";
-
-if ($deploy_locally) unset($deploys['cryptomedic']);
