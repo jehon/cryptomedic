ecfda5a18384a5d399f1d641052457deea00689c
jehon <marielineet.jean@gmail.com>
2014-10-02 07:32:15 +0200

    restoreDB: require array as input

--- autodeploy.php
+++ autodeploy.php
@@ -1,10 +1,12 @@
 <?php
 
 $deploy = array(
-    'root' => __DIR__,
     'owner' => 'jehon',
     'repository' => 'cryptomedic',
     'DatabaseHistoryPath' => __DIR__ . "/database_scripts",
     'DatabaseDevSQL' => __DIR__ . "/dev.sql",
-    'restoreDB' =>  "C:\Users\jehon\Downloads\amd_chakaria.sql"
+    'restoreDB' =>  array(
+    	"prod2acc" => "C:\Users\jehon\Downloads\amd_chakaria.sql",
+    	"end2end_testing" => __DIR__ . "/configs/tests/e2e.sql"
+    )
 );
