acb9aec56dab4575f3d017c53c9b1a587d4a7371
jehon <marielineet.jean@gmail.com>
2014-11-04 06:52:42 +0100

    Use the nice tableRoute to manage files
    managing physical files does not work

--- configs/routes/file.php
+++ configs/routes/file.php
@@ -2,86 +2,105 @@
 
 require(__DIR__ . "/helpers/getFolder.php");
 
+class RouteFile extends RouteDBTable {
+	public function collectionIndex() {
+		$this->notAllowed();
+	}
+
+	public function collectionDelete() {
+		$this->notAllowed();
+	}
+
+	public function collectionModify() {
+		$this->notAllowed();
+	}
+
+	public function collectionCustom($method) {
+		$this->notAllowed();
+	}
+
+	public function elementCreate($data) {
+		$data['lastuser'] = $this->server->getSession(Server::LOGIN_USERNAME);
+
+		$idfolder = $data["patient_id"];
+		$fid = parent::elementCreate($data);
+
+		// Send back the folder
+		$data = getFolder($idfolder);
+		$data["newKey"] = $fid;
+		return $data;
+	}
+	
+	public function elementRead($id) {
+		$this->notAllowed();
+	}
+	
+	public function elementUpdate($id, $data) {
+		$data['lastuser'] = $this->server->getSession(Server::LOGIN_USERNAME);
+
+		// Modify the file
+		$res = parent::elementUpdate($id, $data);
+
+		// Get the folder id
+		$nrec = $this->dbTable->rowGet($id);
+
+		// Send back the folder
+		if (array_key_exists("patient_id", $nrec))
+			return getFolder($nrec["patient_id"]);
+		else
+			return getFolder($nrec["id"]);
+
+	}
+	
+	public function elementDelete($id) {
+		// Get the folder id (if it is a file -> will need the patient_id:
+		$nrec = $this->dbTable->rowGet($id);
+
+		// if (array_key_exists("file", $nrec) && $nrec["file"]) {
+		// 	if (!array_key_exists($type, $config) || !array_key_exists('upload', $config[$type]) || !$config[$type]['upload']) {
+		// 		throw new StorageDeleteError("File storage not defined");
+		// 	}
+		// 	$tfile = $config[$type]['upload'] . DIRECTORY_SEPARATOR . $nrec["file"];
+		// 	if (file_exists($tfile)) {
+		// 		if (!unlink($tfile)) {
+		// 			throw new StorageDeleteError("Could not delete the file");
+		// 		}
+		// 	}
+		// }
+
+		// Delete the record
+		parent::elementDelete($id);
+
+		if ($this->table == "patients") {
+			return array();
+		} else {			
+			// Send back the folder
+			return getFolder($nrec["patient_id"]);
+		}
+	}
+
+	public function elementCustom($method, $id) {
+		if ($method == "UNLINK") {
+			// Unlock the file
+			$this->server->getDatabase()->exec("UPDATE {$this->table} SET modified = NOW(), lastuser = :lastuser WHERE id = :id", 
+				array("lastuser" => $this->server->getSession(Server::LOGIN_USERNAME), "id" => $id));
+
+			// Get the folder id:
+			$nrec = $this->dbTable->rowGet($id);
+
+			// Send back the folder
+			return getFolder($nrec["patient_id"]);
+		} else {
+			parent::elementcustom($method, $id);
+		}
+	}
+}
+
+// TODOJH: restrict more ?
 if (!$request->routeIsEnded()) {
 	$type = $request->routeConsumeNext();
 	$type = type2db($type);
-	$typeDB = $server->getDatabase()->getTable($type);
 
-	if (!$request->routeIsEnded()) {
-		$id = $request->routeConsumeNext();
-
-		// Unlock file
-		if ($request->getMethod() == "UNLINK") {
-			// Unlock the file
-			$server->getDatabase()->exec("UPDATE $type SET modified = NOW(), lastuser = :lastuser WHERE id = :id", array("lastuser" => $server->getSession(Server::LOGIN_USERNAME), "id" => $id));
-
-			// Get the folder id:
-			$nrec = $typeDB->rowGet($id);
-
-			// Send back the folder
-			$response->ok(getFolder($nrec["patient_id"]));
-		}
-
-		// DELETE
-		if ($request->getMethod() == Request::DELETE) {
-			// Get the folder id:
-			$nrec = $typeDB->rowGet($id);
-
-			if (array_key_exists("file", $nrec) && $nrec["file"]) {
-				if (!array_key_exists($type, $config) || !array_key_exists('upload', $config[$type]) || !$config[$type]['upload']) {
-					throw new StorageDeleteError("File storage not defined");
-				}
-				$tfile = $config[$type]['upload'] . DIRECTORY_SEPARATOR . $nrec["file"];
-				if (file_exists($tfile)) {
-					if (!unlink($tfile)) {
-						throw new StorageDeleteError("Could not delete the file");
-					}
-				}
-			}
-
-			// Delete the record
-			$server->getDatabase()->exec("DELETE FROM $type WHERE id = :id", array("id" => $id));
-
-			if ($type == "patients") {
-				$response->ok();
-			} else {			
-				// Send back the folder
-				$response->ok(getFolder($nrec["patient_id"]));
-			}
-		}
-
-		// UPDATE
-		if ($request->getMethod() == Request::UPDATE) {
-			$data = $request->getPost();
-			// $data['lastuser'] = $server->getSession(Server::LOGIN_USERNAME);
-
-			// Modify the file
-			$typeDB->rowUpdate($data);
-
-			// Get the folder id
-			$nrec = $typeDB->rowGet($id);
-
-			// Send back the folder
-			if (array_key_exists("patient_id", $nrec))
-				$response->ok(getFolder($nrec["patient_id"]));
-			else
-				$response->ok(getFolder($nrec["id"]));
-		}
-	} else {
-
-		// Create
-		if ($request->getMethod() == Request::CREATE) {
-			$data = $request->getPost();
-			// $data['lastuser'] = $server->getSession(Server::LOGIN_USERNAME);
-
-			$idfolder = $request->getPost("patient_id");
-			$fid = $typeDB->rowCreate($data);
-
-			$data = getFolder($idfolder);
-			$data["newKey"] = $fid;
-
-			// Send back the folder
-			$response->ok($data);
-		}
-	}
+	$tdb = new RouteFile($request, $response, $server, $type);
+	$tdb->routeAndRespond();
 }
