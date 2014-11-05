9d82a3bbf12de50584cf44becb2d31c8260e3700
jehon <marielineet.jean@gmail.com>
2014-11-05 21:28:22 +0100

    first version (no filter, not all data's)

--- configs/routes/reports/daily.php
+++ configs/routes/reports/daily.php
@@ -4,6 +4,135 @@ require_once(__DIR__ . "/../helpers/getFolder.php");
 require_once(__DIR__ . "/../../amd_listings.php");
 require_once(__DIR__ . "/../helpers/references.php");
 
+// TODOJH: manage parameters
+$when = $request->getParameter("when", "todojh");
+$who = $request->getParameter("who", "todojh");
+$where = $request->getParameter("where", "todojh");
+
+?>
+<html>
+<div>
+    <form>
+        <label>Who ?</label><input name='who' value='<? echo $who; ?>'><br>
+        <label>Where ?</label><input name='where' value='<? echo $where; ?>'><br>
+        <label>When ?</label><input name='when' value='<? echo $when; ?>'><br>
+        <input type='submit' value='Calculate'>
+    </form>
+</div>
+<div>
+    <table>
+        <thead>
+            <tr>
+                <th>SARPV - AMD - KDM</th>
+            </tr>
+            <tr>
+                <th colspan=5>Name of project: Ricktes in cox's Bazar</th>
+                <th>When</th>
+                <th><? echo $when; ?></th>
+                <th></th>
+                <th></th>
+                <th colspan=4>Levels of the social level</th>
+            </tr>
+            <tr>
+                <th colspan=5>SARPV, CHAKARIA DISABILITY CENTER, CHAKARIA, COX'S BAZAR</th>
+                <th>Who</th>
+                <th><? echo $who; ?></th>
+                <th></th>
+                <th></th>
+                <th>1</th>
+                <th>2</th>
+                <th>3</th>
+                <th>4</th>
+            </tr>
+            <tr>
+                <th colspan=5>Daily report of <? echo $when; ?></th>
+                <th>Where</th>
+                <th><? echo unreference("Bill", "Center", $where); ?></th>
+                <th></th>
+                <th></th>
+                <th>todojh</th>
+                <th>todojh</th>
+                <th>todojh</th>
+                <th>todojh</th>
+            </tr>
+            <tr>
+                <th colspan="4"></th>
+                <th colspan="5">Identity</th>
+                <th colspan="4">SEL</th>
+                <th colspan="3">Medical</th>
+                <th colspan="6">Price</th>
+            </tr>
+            <tr>
+                <th>N#</th>
+                <th>Date</th>
+                <th>Physio</th>
+                <th>Place</th>
+                <th>Record n#</th>
+
+                <th>Patient name</th>
+                <th>Age</th>
+                <th>M/F</th>
+                <th>New/Old</th>
+                <th>Tk income</th>
+                <th>Nb pers</th>
+                <th>Tk per pers</th>
+                <th>SL</th>
+
+                <th>Diagno</th>
+                <th>Act</th>
+                <th>Trt</th>
+
+                <th>Consult</th>
+                <th>Medicine</th>
+                <th>Others</th>
+                <th>Full</th>
+                <th>Asked</th>
+                <th>Payed</th>
+            </tr>
+        </thead>
+        <tbody>
+            <?php
+                $result = $database->query("SELECT * FROM bills 
+                    JOIN patients ON bills.patient_id = patients.id 
+                    LIMIT 100");
+                foreach($result as $i => $v) {
+                    ?>
+                        <tr>
+                            <td><?php echo $i; ?></td>
+                            <td><?php echo $v['Date']; ?></td>
+                            <td><?php echo $v['ExaminerName']; ?></td>
+                            <td><?php echo unreference("Bills", "Center", $v['Center']); ?></td>
+                            <td><?php echo $v['entryyear'] . "-" . $v['entryorder']; ?></td>
+
+                            <td><?php echo $v['Firstname'] . " " . $v['Lastname']; ?></td>
+                            <td><?php echo (Date('Y') - $v['Yearofbirth']); ?></td>
+                            <td><?php echo unreference("Patient", "Sex", $v['Sex']); ?></td>
+                            <td><?php echo "new/old"; ?></td>
+                            <td><?php echo $v['Familysalaryinamonth']; ?></td>
+                            <td><?php echo $v['Numberofhouseholdmembers']; ?></td>
+                            <td><?php echo ($v['Numberofhouseholdmembers'] > 0 ? $v['Familysalaryinamonth'] / $v['Numberofhouseholdmembers'] : "?"); ?></td>
+                            <td><?php echo $v['Sociallevel']; ?></td>
+
+                            <td><?php echo "todojh"; ?>diagno</td>
+                            <td><?php echo "todojh"; ?>actc</td>
+                            <td><?php echo "todojh"; ?>treatment</td>
+
+                            <td><?php echo "todojh"; ?>total consult</td>
+                            <td><?php echo "todojh"; ?>total medecine</td>
+                            <td><?php echo "todojh"; ?>total other</td>
+                            <td><?php echo $v['total_real']; ?></td>
+                            <td><?php echo $v['total_asked']; ?></td>
+                            <td><?php echo $v['total_paid']; ?></td>
+                            <td><?php //var_dump($v); ?></td>
+                        </tr>
+                    <?php
+                }
+
+
+            ?>
+        </tbody>
+    </table>
+<?
 $response->data = array();
 $response->formParameters = array(
         "when" => array(
@@ -21,73 +150,3 @@ $response->formParameters = array(
             "list" => buildLinkedList($amd_listing['Centers'])
             ),
     );
-
-// TODOJH: set parameters
-$day = $request->getParameter("when", "todojh");
-$who = $request->getParameter("who", "todojh");
-$where = $request->getParameter("where", "todojh");
-
-$response->addLine();
-$response->addCell("SARPV - AMD - KDM");
-
-$response->addLine();
-$response->addCell("Name of project: Ricktes in cox's Bazar", [ "colspan" => 5 ]);
-$response->addCell("When");
-$response->addCell($day);
-$response->addCell("");
-
-$response->addLine();
-$response->addCell("SARPV, CHAKARIA DISABILITY CENTER, CHAKARIA, COX'S BAZAR", [ "colspan" => 5 ]);
-$response->addCell("Who");
-$response->addCell($who);
-$response->addCell("");
-$response->addCell("");
-$response->addCell("Levels of the social level", [ "colspan" => 4 ]);
-
-$response->addLine();
-$response->addCell("Daily report of " . $day, [ "colspan" => 5 ]);
-$response->addCell("Where");
-$response->addCell(unreference("Bill", "Center", $where));
-$response->addCell("");
-$response->addCell("");
-$response->addCell("todojh");        
-$response->addCell("todojh");        
-$response->addCell("todojh");        
-$response->addCell("todojh");        
-
-$response->addLine();
-$response->addCell("");
-$response->addCell("");
-$response->addCell("");
-$response->addCell("");
-$response->addCell("Identity", [ "colspan" => 5 ]);
-$response->addCell("SEL", [ "colspan" => 4 ]);
-$response->addCell("Medical", [ "colspan" => 3 ]);
-$response->addCell("Price", [ "colspan" => 6 ]);
-
-$response->addLine();
-$response->addCell("N#");
-$response->addCell("Date");
-$response->addCell("Physio");
-$response->addCell("Place");
-$response->addCell("Record n#");
-
-$response->addCell("Patient name");
-$response->addCell("Age");
-$response->addCell("M/F");
-$response->addCell("New/Old");
-$response->addCell("Tk income");
-$response->addCell("Nb pers");
-$response->addCell("Tk per pers");
-$response->addCell("SL");
-
-$response->addCell("Diagno");
-$response->addCell("Act");
-$response->addCell("Trt");
-
-$response->addCell("Consult");
-$response->addCell("Medicine");
-$response->addCell("Others");
-$response->addCell("Full");
-$response->addCell("Asked");
-$response->addCell("Payed");
