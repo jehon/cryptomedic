9b8f2c0ab97b9fc8ac6e10b44e5e7f8065c8e552
jehon <marielineet.jean@gmail.com>
2014-11-06 17:11:34 +0100

    parameters in reports

--- angular/app/js/ctrl_reports.js
+++ angular/app/js/ctrl_reports.js
@@ -2,5 +2,43 @@
 
 mainApp.controller('ctrl_reports', [ '$scope', '$routeParams', function($scope, $routeParams) {
 	var report = $routeParams['report'];
+	$scope.values = {
+		'examinerName': '',
+		'date': new Date(),
+		'center': 992
+	};
+
+	var reports = {
+		'daily': [ "center", "examinerName", "date" ]
+	}
+
+	$scope.getReport = function() {
+		if (report) {
+			return report;
+		}
+		return false;
+	}
+
+	$scope.isParam = function(name) {
+		return reports[report].indexOf(name) > -1;
+	}
+
+
+	$scope.refresh = function() {
+		console.log("refresh");
+		var res = report + ".html?";
+		console.log($scope.values);
+		angular.forEach(reports[report], function(value) {
+			if (typeof($scope.values[value]) != 'undefined') {
+				var v = $scope.values[value];
+				if (v instanceof Date) {
+					v = v.toISOString();
+				}
+				res += value + "=" + v + "&";
+			}
+		});
+		console.log(res);
+		$scope.url = res;
+	}
 
 }]);
