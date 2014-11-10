8e1ca6f30e33c88093da116ef644e31215004bcd
jehon <marielineet.jean@gmail.com>
2014-11-10 20:56:37 +0100

    adding first patient reporting

--- configs/templates/patient-reporting.php
+++ configs/templates/patient-reporting.php
@@ -12,3 +12,13 @@
         <li><a target="_new" href="/rest/reports/patients/{{id()}}.xls" >excel file</a></li>
     </ul>
 </div>
+<table class='reporting'>
+	<tbody>
+		<tr>
+			<td class='subheader' ng-repeat='(f, v) in folder.mainFile'>{{f}}</td>
+		</tr>
+		<tr>
+			<td ng-repeat='(f, v) in folder.mainFile'>{{v}}</td>
+		</tr>
+	</tbody>
+</table>
