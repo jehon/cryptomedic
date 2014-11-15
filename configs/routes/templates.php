0699630923132cf0dd2dd98ba516c0a6c9cb57b8
jehon <marielineet.jean@gmail.com>
2014-11-15 19:03:25 +0100

    enhance template debug infos

--- configs/routes/templates.php
+++ configs/routes/templates.php
@@ -6,7 +6,7 @@ if (($request->getMethod() == Request::READ) && !$request->routeIsEnded()) {
 	debugHeader($file, "X-TEMPLATE-ASKED");
 	if (file_exists($file)) {
 		include_once($file);
-		echo "<div class='debug_infos'>" . date("Y-m-d h:M:s") . "</div>";
+		echo "<div class='debug_infos'>template " . basename($file) . "@" . date("Y-m-d h:M:s") . "</div>";
 		$response->ok();
 	}
 }
