dc224782e1b6051e1ab3b33d5946a5a8134cfbf7
jehon <marielineet.jean@gmail.com>
2014-11-16 15:02:21 +0100

    Détecter si les cookies sont bien activés
    Fix #21

--- app/js/application.js
+++ app/js/application.js
@@ -442,3 +442,17 @@ function debug_showLabels() {
 server.setSettings = function(data) {
 	server.settings = objectify(data);
 }
+
+function are_cookies_enabled() {
+    var cookieEnabled = (navigator.cookieEnabled) ? true : false;
+
+    if (typeof navigator.cookieEnabled == "undefined" && !cookieEnabled)
+    { 
+        document.cookie="testcookie";
+        cookieEnabled = (document.cookie.indexOf("testcookie") != -1) ? true : false;
+    }
+    return (cookieEnabled);
+}
+if (!are_cookies_enabled()) {
+	alert("Your cookie are disabled. Please enable them.\nVos cookies sont désactivés. Merci de les activer.");
+}
