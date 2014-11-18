269fd567e2e60a09d385d9dbd29ec495c593d501
jehon <marielineet.jean@gmail.com>
2014-11-18 07:28:25 +0100

    test compile template server side

---
+++ unused/compileTemplates.php
@@ -0,0 +1,44 @@
+<?php
+// TODOJH: enable this
+$response = new Response($request);
+
+if (!$request->isServedLocally()) {
+   throw new Error("Only available locally");
+}
+
+// Check if a downloaded is old
+$html_dir = __DIR__ . "/../tmp/templates-html/";
+if (!is_dir($html_dir)) {
+    mkdir($html_dir);
+}
+
+$js_dir = __DIR__ . "/../tmp/templates-js/";
+if (!is_dir($html_dir)) {
+    mkdir($html_dir);
+}
+
+foreach(MyFile::myglob(__DIR__ . "/../configs/templates/*.php") as $source) {
+    $name = pathinfo($source, PATHINFO_FILENAME);
+    $target = $html_dir . $name . ".html";
+
+    if (!is_file($target) || (filemtime($target) < filemtime($source))) {
+        ob_start();
+        require_once($source);
+        $res = ob_get_contents();
+        ob_end_clean();
+        file_put_contents($target, $res);
+        // die("should upgrade $source");
+    }
+
+    // download
+    // Check if a compiled is old
+    // Run npm to compile
+    // $v8 = new V8Js();
+}
+
+foreach(MyFile::myglob($html_dir . "/*.html") as $source) {
+    $name = pathinfo($source, PATHINFO_FILENAME);
+    $target = $js_dir . $name . ".html";
+    if (!is_file($target) || (filemtime($target) < filemtime($source))) {
+    }    
+}
