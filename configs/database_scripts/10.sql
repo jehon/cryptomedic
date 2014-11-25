05e10a57a3d8d3ca6cd46538d75903afa99006a1
jehon <marielineet.jean@gmail.com>
2014-11-25 07:36:14 +0100

    Add technical user

---
+++ configs/database_scripts/10.sql
@@ -0,0 +1,3 @@
+
+INSERT INTO `amd_chakaria`.`users` (`id`, `username`, `name`, `password`, `email`, `notes`, `group`, `created`, `modified`, `lastuser`, `last_login`) 
+VALUES (NULL, 'mep', 'Mise en production automatique', 'c2eb5c0c3d80f1117e55c4a598ca1e05a043841d', NULL, 'Fait par Jean Honlet', 'technical', CURRENT_TIMESTAMP, CURRENT_TIMESTAMP, 'jehon', NULL);
