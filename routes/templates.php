bad767462c4d50d73d9546d165e867e7449242d7
jehon <marielineet.jean@gmail.com>
2014-11-26 16:40:12 +0100

    Templates are lowercase

--- routes/templates.php
+++ routes/templates.php
@@ -2,9 +2,10 @@
 
 if (($request->getMethod() == Request::READ) && !$request->routeIsEnded()) {
 	$basename = basename($request->routeConsumeNext());
-	$file = __DIR__ . DIRECTORY_SEPARATOR . ".." . DIRECTORY_SEPARATOR . "templates" . DIRECTORY_SEPARATOR . $basename .".php";
+	$file = __DIR__ . DIRECTORY_SEPARATOR . ".." . DIRECTORY_SEPARATOR . "templates" . DIRECTORY_SEPARATOR . strtolower($basename) .".php";
 	debugHeader($file, "X-TEMPLATE-ASKED");
 	if (file_exists($file)) {
+		debugHeader($file, "X-TEMPLATE-FOUND");
 		include_once($file);
 		echo "<div class='debug_infos'>template " . basename($file) . "?mode=" . $request->getParameter("mode", "?") . "@" . date("Y-m-d h:M:s") . "</div>";
 		if ($request->getSystemParameter('unused', false)) {
