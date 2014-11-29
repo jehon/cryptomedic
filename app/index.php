49077b7a53cf22ce250b12b4f47af03024ae808c
jehon <marielineet.jean@gmail.com>
2014-11-29 21:11:56 +0100

    enable index into application.php

--- app/index.php
+++ app/index.php
@@ -1,6 +1,8 @@
 <!DOCTYPE html>
 <?php 
-	require __DIR__ . "/../../rest/php/core.php";
+	require_once __DIR__ . "/../../rest/php/core.php";
+	Script::$rootPath = __DIR__;
+	
 	$server = new Server();
 	$request = new Request($server);
 
@@ -26,7 +28,6 @@
 		</script>
 	<![endif]-->
 <?php 
-	Script::$rootPath = __DIR__;
 	// jquery
 	(new Script("bower_components/jquery/dist/jquery.min.js"))->dependFile()->toPrint();
 	(new Script("bower_components/jquery-ui/jquery-ui.min.js"))->dependFile()->toPrint();
