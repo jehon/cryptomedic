641655d7566c7de5a149d61155c76a5f230b972c
Jean Honlet <marielineet.jean@gmail.com>
2014-12-30 09:03:46 +0100

    Fix social level in summary

--- templates/bill-summary.php
+++ templates/bill-summary.php
@@ -5,7 +5,7 @@
 <table style='width: 100%'>
 	<colgroup span="2" width="50%"></colgroup>
 	<tbody>
-		<?php (new t("Bill.socialLevel"))->tr("Social Level")->p(); ?>
+		<?php (new t("Bill.Sociallevel"))->tr("Social Level")->p(); ?>
 		<?php (new t("Bill.total_asked"))->tr("Total asked")->p(); ?>
 		<?php (new t("Bill.total_paid"))->tr("Total paid")->p(); ?>
 	</tbody>
