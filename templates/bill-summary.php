c04ea1ae99325978a0bb6ab42327ddd2d97556de
Jean Honlet <marielineet.jean@gmail.com>
2015-01-08 17:11:14 +0100

    Add operation in bill summary

--- templates/bill-summary.php
+++ templates/bill-summary.php
@@ -8,5 +8,12 @@
 		<?php (new t("Bill.Sociallevel"))->tr("Social Level")->p(); ?>
 		<?php (new t("Bill.total_asked"))->tr("Total asked")->p(); ?>
 		<?php (new t("Bill.total_paid"))->tr("Total paid")->p(); ?>
+		<?php 
+			foreach(Bill::$categories as $cat) {
+				foreach(Bill::getFielsList($cat) as $item) {
+					(new t("Bill." . $item))->tr(str_replace("_", " ", substr($item, strpos($item, '_') + 1)))->p();		
+				}
+			}
+		?>
 	</tbody>
 </table>
