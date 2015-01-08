66d068e2b7f48e28a2b49002cb711e0da8aedc22
Jean Honlet <marielineet.jean@gmail.com>
2015-01-08 17:42:54 +0100

    Fix: remove demiglio from club foot summary

--- templates/clubfoot-summary.php
+++ templates/clubfoot-summary.php
@@ -5,7 +5,6 @@
 <table style='width: 100%'>
 	<colgroup span="2" width="50%"></colgroup>
 	<tbody>
-		<?php (new t("ClubFoot.DIMEGLIO"))->tr()->p(); ?>
 		<?php (new t("ClubFoot.Treatment"))->tr()->p(); ?>
 	</tbody>
 </table>
