bbfea2395f9254a33f48eedf9c98b3f99911c4da
Jean Honlet <marielineet.jean@gmail.com>
2015-01-14 07:39:01 +0100

    [technical] show unused lists in templates/unused mode

--- routes/templates.php
+++ routes/templates.php
@@ -11,6 +11,16 @@ if (($request->getMethod() == Request::READ) && !$request->routeIsEnded()) {
 		echo "<div class='debug_infos'>template " . basename($file) . "?mode=" . $request->getParameter("mode", "?") . "@" . date("Y-m-d h:i:s") . "</div>";
 		if ($request->getSystemParameter('unused', false)) {
 			t::showUnused($request->getSystemParameter('unused'));
+			$t = $request->getSystemParameter('unused');
+			$m = References::db2model($t);
+			foreach(References::$model_listing as $l => $content) {
+				if (substr($l, 0, strlen($m)) == $m) {
+					$f = substr($l, strlen($m) + 1);
+					if (!$server->getDatabase()->getTable($t)->isColumn($f)) {
+						var_dump("List without database behind: $f");
+					} 
+				}
+			}
 		}
 		$response->ok();
 	}
