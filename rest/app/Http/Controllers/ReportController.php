f9db562b2766881fef960b352c993b737bcffe5f
Jean Honlet <marielineet.jean@gmail.com>
2015-05-04 21:08:46 +0200

    route related migrated, route consultation working with bind parameters

--- rest/app/Http/Controllers/ReportController.php
+++ rest/app/Http/Controllers/ReportController.php
@@ -9,35 +9,37 @@ require_once(__DIR__ . "/../../../../php/references.php");
 use \References;
 
 class ReportController extends Controller {
-	protected function consultations_getSqlConsult($label, $table) {
+	protected function consultations_getSqlConsult($label, $table, &$params) {
 		$sql = "SELECT \"$label\" as c_type, c.id as c_id, c.Date as c_Date, c.NextCenter as c_Center, c.NextAppointment as c_nextAppointment, c.patient_id as patient_id FROM $table as c ";
 		$sql .= " WHERE (1 = 1)";
 		
 		if (Request::input("day", false)) {
-			$sql .= " AND (Nextappointment = '" . DB::escape(Request::input("day", false)) . "') ";
+			$sql .= " AND (Nextappointment = ?)";
+			$params[] = Request::input("day");
 		}		
 		
 		if (Request::input("center", false)) {
-			$sql .= " AND (NextCenter= '" . DB::escape(Request::input("center", false)) . "') ";
+			$sql .= " AND (NextCenter= ?) ";
+			$params[] = Request::input("center");
 		}		
 		return $sql;
 	}
 		
 	public function consultations() {
-		// day=2014-01-29
+		$params = array();
 		$sql = "SELECT cc.*, patients.*
 		FROM "
 				. "(("
-						. $this->consultations_getSqlConsult("ricket", "ricket_consults")
+						. $this->consultations_getSqlConsult("ricket", "ricket_consults", $params)
 						. 	") UNION ("
-						. $this->consultations_getSqlConsult("clubfoot", "club_foots")
+						. $this->consultations_getSqlConsult("clubfoot", "club_foots", $params)
 						. 	") UNION ("
-						. $this->consultations_getSqlConsult("non-ricket", "nonricket_consults")
+						. $this->consultations_getSqlConsult("non-ricket", "nonricket_consults", $params)
 				.")) AS cc "
 				. " JOIN patients ON (cc.patient_id = patients.id) ";
 
 		$sql .= " LIMIT 100";
-		$listing = DB::select($sql);
+		$listing = DB::select($sql, $params);
 		return response()->jsonOrJSONP($listing);
 	}	
 	
