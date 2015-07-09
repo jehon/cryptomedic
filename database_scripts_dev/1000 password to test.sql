1abbe3370a691c37854893b7151df083dd276db4
Jean Honlet <marielineet.jean@gmail.com>
2015-07-09 22:34:03 +0200

    passwords set to "test" is old_password
    will change later (when old password will not be used anymore)

--- database_scripts_dev/1000 password to test.sql
+++ database_scripts_dev/1000 password to test.sql
@@ -1,2 +1,3 @@
 -- set password to 'test'
-UPDATE `users` SET password = '83998c0388c12fe57c1e75d4c50c904fe774a8ec' WHERE password is not null;
+UPDATE `users` SET old_password = '83998c0388c12fe57c1e75d4c50c904fe774a8ec' WHERE old_password is not null;
+
