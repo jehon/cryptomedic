955f1118e66cd7702e0fd992334eec57cbdfaee4
Jean Honlet <marielineet.jean@gmail.com>
2015-08-01 22:34:38 +0200

    prepare removing old rest system

--- database_scripts/dev_only/1001 remove created and modified.sql
+++ database_scripts/dev_only/1001 remove created and modified.sql
@@ -1,42 +1,42 @@
 
-ALTER TABLE `bills`
-  DROP `modified`,
-  DROP `created`;
+UPDATE bills SET created_at = created  WHERE created_at IS NULL or created_at = 0;
+UPDATE bills SET updated_at = modified WHERE updated_at IS NULL or updated_at = 0;
+ALTER TABLE `bills`  DROP `modified`,  DROP `created`;
   
-ALTER TABLE `club_foots`
-  DROP `modified`,
-  DROP `created`;
+UPDATE `club_foots` SET created_at = created  WHERE created_at IS NULL or created_at = 0;
+UPDATE `club_foots` SET updated_at = modified WHERE updated_at IS NULL or updated_at = 0;
+ALTER TABLE `club_foots` DROP `modified`, DROP `created`;
 
-ALTER TABLE `deleted`
-  DROP `modified`;
+UPDATE `deleted` SET updated_at = modified WHERE updated_at IS NULL or updated_at = 0;
+ALTER TABLE `deleted` DROP `modified`;
   
-ALTER TABLE `nonricket_consults`
-  DROP `modified`,
-  DROP `created`;
+UPDATE `nonricket_consults` SET created_at = created  WHERE created_at IS NULL or created_at = 0;
+UPDATE `nonricket_consults` SET updated_at = modified WHERE updated_at IS NULL or updated_at = 0;
+ALTER TABLE `nonricket_consults` DROP `modified`, DROP `created`;
 
-ALTER TABLE `patients`
-  DROP `modified`,
-  DROP `created`;
+UPDATE `patients` SET created_at = created  WHERE created_at IS NULL or created_at = 0;
+UPDATE `patients` SET updated_at = modified WHERE updated_at IS NULL or updated_at = 0;
+ALTER TABLE `patients` DROP `modified`, DROP `created`;
 
-ALTER TABLE `pictures`
-  DROP `modified`,
-  DROP `created`;
+UPDATE `pictures` SET created_at = created  WHERE created_at IS NULL or created_at = 0;
+UPDATE `pictures` SET updated_at = modified WHERE updated_at IS NULL or updated_at = 0;
+ALTER TABLE `pictures` DROP `modified`, DROP `created`;
 
-ALTER TABLE `prices`
-  DROP `modified`,
-  DROP `created`;
+UPDATE `prices` SET created_at = created  WHERE created_at IS NULL or created_at = 0;
+UPDATE `prices` SET updated_at = modified WHERE updated_at IS NULL or updated_at = 0;
+ALTER TABLE `prices` DROP `modified`, DROP `created`;
 
-ALTER TABLE `settings`
-  DROP `modified`;
+UPDATE `settings` SET updated_at = modified WHERE updated_at IS NULL or updated_at = 0;
+ALTER TABLE `settings` DROP `modified`;
 
-ALTER TABLE `surgeries`
-  DROP `modified`,
-  DROP `created`;
+UPDATE `surgeries` SET created_at = created  WHERE created_at IS NULL or created_at = 0;
+UPDATE `surgeries` SET updated_at = modified WHERE updated_at IS NULL or updated_at = 0;
+ALTER TABLE `surgeries` DROP `modified`, DROP `created`;
 
-ALTER TABLE `ricket_consults`
-  DROP `modified`,
-  DROP `created`;
+UPDATE `ricket_consults` SET created_at = created  WHERE created_at IS NULL or created_at = 0;
+UPDATE `ricket_consults` SET updated_at = modified WHERE updated_at IS NULL or updated_at = 0;
+ALTER TABLE `ricket_consults` DROP `modified`, DROP `created`;
 
-ALTER TABLE `users`
-  DROP `modified`,
-  DROP `created`;
+UPDATE `users` SET created_at = created  WHERE created_at IS NULL or created_at = 0;
+UPDATE `users` SET updated_at = modified WHERE updated_at IS NULL or updated_at = 0;
+ALTER TABLE `users` DROP `modified`, DROP `created`;
