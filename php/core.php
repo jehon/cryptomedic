7f59cd2d87a253dd58ff588b16e824d02b7922bd
Jean Honlet <marielineet.jean@gmail.com>
2015-08-05 08:08:28 +0200

    adding myCleanValue function

--- php/core.php
+++ php/core.php
@@ -5,20 +5,25 @@
 // 	$server = Server::getInstance();
 	
 	// Application autoload classes
-	spl_autoload_register(function ($class) {
-		$file = __DIR__ . "/" . strtolower($class) . ".php";
-		if (file_exists($file)) {
-			include_once($file);
-			return true;
-		}
-		return false;
-	});
-
-	
-	function getVersion() {
-		$file = __DIR__ . "/../../cryptomedic.version";
-		if (file_exists($file)) {
-			return trim(file_get_contents($file));
-		}
-		return "undefined";
+spl_autoload_register(function ($class) {
+	$file = __DIR__ . "/" . strtolower($class) . ".php";
+	if (file_exists($file)) {
+		include_once($file);
+		return true;
 	}
+	return false;
+});
+
+
+function getVersion() {
+	$file = __DIR__ . "/../../cryptomedic.version";
+	if (file_exists($file)) {
+		return trim(file_get_contents($file));
+	}
+	return "undefined";
+}
+
+function myCleanValue($c) {
+	return str_replace(["'", " ", "\""], "", $c);
+}
+	
