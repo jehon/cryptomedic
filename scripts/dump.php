47b876f07ec93f96ad95beefb583735154a66363
Jean Honlet <marielineet.jean@gmail.com>
2015-08-19 07:30:24 +0200

    moving to scripts

--- scripts/dump.php
+++ scripts/dump.php
@@ -13,12 +13,13 @@ $mysqldump = "mysqldump --user=" . getGlobalConfig("databaseUsername")
 			
 $target = __DIR__ . "/../database_scripts/structure/structure.sql";
 unlink($target);
-var_dump(exec("$mysqldump --no-data --databases amd_chakaria > '" . $target ."'"));
+var_dump(exec("$mysqldump --databases amd_chakaria --no-data > '" . $target ."'"));
+var_dump(exec("$mysqldump --databases amd_chakaria --tables settings >> '" . $target ."'"));
 
-// var_dump(exec("$mysqldump amd_chakaria --tables > '" . __DIR__ . "/../database_scripts/structure/0 structure.sql'"));
+var_dump(exec("$mysqldump amd_chakaria --tables > '" . __DIR__ . "/../database_scripts/structure/0 structure.sql'"));
 
 // TODO: How to commit only one file, whatever the state of the repository?
-var_dump(exec("git commit -m 'updating the structure' '$target'"));
+// var_dump(exec("git commit -m 'updating the structure' '$target'"));
 
 var_dump("done");
 
