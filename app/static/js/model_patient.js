7edc4ee817e63e0ea048804c0fcbf6c75cc537f6
Jean Honlet <marielineet.jean@gmail.com>
2015-09-29 14:37:01 +0200

    Clean up of old calcul in comment

--- app/static/js/model_patient.js
+++ app/static/js/model_patient.js
@@ -13,30 +13,7 @@ application.models.Patient = application.models.Data.extend({
 	    	if (this.Sex == "Male") return "m";
 	    	if (this.Sex == "Female") return "f";
 	    	return null;
-//		return this.Sex == 207 ? "m" : "f"; 
 	},
-//	'ratioSalary': function() {
-//		if (!this.isNotZero('Numberofhouseholdmembers')) throw new DataMissingException("Numberofhouseholdmembers");
-//		return Math.ceil(this.Familysalaryinamonth / this.Numberofhouseholdmembers);
-//	},
-//	'calculateSocialLevel': function() {
-//		/**
-//			From TC:
-//		 	Level 0 is when the familial ration is < 300
-//			Level 1 is when the familial ration is 300<  FR < 500
-//			Level 2 is when the familial ration is 500< FR < 1500
-//			Level 3 is when the familial ration is 1500< FR < 3000
-//			Level 4 is when the familial ration is 3000< FR  
-//		 * 
-//		 */
-//		
-//		if (typeof(this.ratioSalary()) == "string") throw new DataMissingException("Numberofhouseholdmembers");
-//		if (this.ratioSalary() <= 300) 	return 0;
-//		if (this.ratioSalary() <= 500)	return 1;
-//		if (this.ratioSalary() <= 1500)	return 2;
-//		if (this.ratioSalary() <= 3000)	return 3;
-//		return 4;
-//	},
 	'validate': function(res) {
 		res = this._super(res);
 
