295ddee18dd196b7979f8481155c857dc3541138
Jean Honlet <marielineet.jean@gmail.com>
2015-09-29 07:05:47 +0200

    new maintenance routes

---
+++ maintenance/bug_submit.php
@@ -0,0 +1,10 @@
+<?php
+
+// 	require_once(__DIR__ . "/vendor/autoload.php");
+	
+	require(__DIR__ . "/../config.php");
+	
+	require_once(__DIR__ . "/../vendor/jehon/maintenance/bin/try_catch.php");
+	
+	require_once(__DIR__ . "/../vendor/jehon/maintenance/bin/bug_submit.php");
+	
---
+++ maintenance/deploy.php
@@ -0,0 +1,11 @@
+<?php
+
+// 	require_once(__DIR__ . "/vendor/autoload.php");
+	
+	require(__DIR__ . "/../config.php");
+	
+	require_once(__DIR__ . "/../vendor/jehon/maintenance/bin/require_session.php");
+	require_once(__DIR__ . "/../vendor/jehon/maintenance/bin/try_catch.php");
+	
+	require_once(__DIR__ . "/../vendor/jehon/maintenance/bin/deploy.php");
+	
