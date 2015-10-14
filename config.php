6091e192ca935f48ddb94901fec37fa91ea5b997
Jean Honlet <marielineet.jean@gmail.com>
2015-10-14 16:18:28 +0200

    trying to fix problem with travis

--- config.php
+++ config.php
@@ -6,7 +6,7 @@ if (!defined("secretFile")) {
 }
 
 if (file_exists(constant("secretFile"))) {
-	require_once(constant("secretFile"));	
+	require_once(constant("secretFile"));
 }
 
 global $localhost;
@@ -18,17 +18,17 @@ $cli = $_SERVER && !array_key_exists('HTTP_HOST', $_SERVER);
 function getGlobalConfig($key) {
 	if (function_exists("getSecret")) {
 		$res = getSecret($key, false);
-		if ($res !== null) { 
-			return $res; 
+		if ($res !== null) {
+			return $res;
 		}
 	}
-	
+
 	switch ($key) {
 		case 'debug':
 			global $localhost;
 			return $localhost;
 		case 'laravelRandomString':
-			return "123";	
+			return "123";
 		case 'databaseName':
 			return "amd_chakaria";
 		case 'databaseUsername':
@@ -50,7 +50,7 @@ $myconfig['database'] = array(
 		'database' => "mysql:host=localhost;dbname=amd_chakaria",
 		'username' => 'amd_chakaria',
 		'password' => getGlobalConfig("databasePassword"),
-		'options' => [ PDO::MYSQL_ATTR_INIT_COMMAND  => "SET CHARACTER SET 'utf8'" ],
+		'options' => [ PDO::MYSQL_ATTR_INIT_COMMAND  => "SET CHARACTER SET 'utf8'; SET time_zone = '+00:00'" ],
 		'patches' => array()
 );
 
