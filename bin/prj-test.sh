db6ca01f08a95ee29ecde03be54a1925d29fe727
Jean Honlet <marielineet.jean@gmail.com>
2015-10-18 15:55:45 +0200

    should work with failing

--- bin/prj-test.sh
+++ bin/prj-test.sh
@@ -17,5 +17,16 @@ echo "Project directory is $PRJ_DIR"
 cd "$PRJ_DIR"
 
 for V in api/* ; do
+  echo -e "\e[1m\e[45mBuilding version $V\e[0m"
   cd "$PRJ_DIR/$V" && vendor/bin/phpunit
+  RES=$?
+  if cd "$PRJ_DIR/$V" && vendor/bin/phpunit; then
+    echo "ok"
+  else
+    echo "failed"
+    return 1;
+  fi
+  echo "Result: $RES"
 done
+
+echo -e "\e[1m\e[45mAll test terminated $V\e[0m"
