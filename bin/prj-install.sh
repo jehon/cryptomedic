27f7e8912c4fe8ae9a208ff075113b68f5945e48
Jean Honlet <marielineet.jean@gmail.com>
2015-10-20 22:07:14 +0200

    Fix name in string (copy-paste)

--- bin/prj-install.sh
+++ bin/prj-install.sh
@@ -16,9 +16,9 @@ fi
 
 echo -e "\e[1m\e[45mComposer install $V\e[0m"
 if composer.phar install; then
-    echo -e "\e[0;45mNPM install: \e[1;42msuccess.\e[0m"
+    echo -e "\e[0;45mComposer install: \e[1;42msuccess.\e[0m"
   else
-    echo -e "\e[0;45mNPM install: \e[1;41mfailed\e[0m"
+    echo -e "\e[0;45mComposer install: \e[1;41mfailed\e[0m"
     exit 1;
 fi
 
