7beca9b1f32667c3e3d69de5a7e3f00c7c76c146
Jean Honlet <marielineet.jean@gmail.com>
2015-10-21 22:09:46 +0200

    Enable other browser and configure them

--- cache/generator.php
+++ cache/generator.php
@@ -2,7 +2,7 @@
 try {
 	global $generator;
 	$generator = array();
-	
+
 	require_once(dirname(__DIR__) . DIRECTORY_SEPARATOR . "config.php");
 
 	$generator['database'] = array(
@@ -12,12 +12,12 @@ try {
 			'pdo_password' => getGlobalConfig("databasePassword"),
 			'init' => "SET CHARACTER SET 'utf8'",
 	);
-	
+
 	$generator['target'] = $_REQUEST['target'];
 	$generator['path'] = explode("/", $generator['target']);
 	$generator['runtime'] = pathinfo($generator['path'][0], PATHINFO_FILENAME) . ".php";
 	$generator['dest'] = __DIR__ . "/../cache/" . $generator['target'];
-	
+
 	foreach($generator['path'] as $p) {
 		if ($p == "..") {
 			throw new Exception("I say: invalid request");
@@ -28,9 +28,9 @@ try {
 	}
 
 	// TODO CHECK: If we had some parameters, do not save it to file?
-	
+
 	ob_start();
-	
+
 	include(__DIR__ . DIRECTORY_SEPARATOR . $generator['runtime']);
 
 	umask(02);
@@ -39,12 +39,12 @@ try {
 // 		echo "Paramters are not allowed: " . implode($_REQUEST);
 // 		die("File will not be saved");
 // 	}
-	
+
 	// create path if necessary - wrong permissions!
 // 	if (!file_exists(dirname($generator['dest']))) {
 // 		mkdir(dirname($generator['dest']), 0775, true);
 // 	}
-	
+
 // 	$generator['fdest'] = fopen($generator['dest'], "w");
 // 	ftruncate($generator['fdest'], 0);
 // 	fwrite($generator['fdest'], ob_get_contents());
