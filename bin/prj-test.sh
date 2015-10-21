8c7a9a9ea9872f8b067e690bea9ac363a0c6ee32
Jean Honlet <marielineet.jean@gmail.com>
2015-10-21 07:49:51 +0200

    Add nightwatch testing capacity

--- bin/prj-test.sh
+++ bin/prj-test.sh
@@ -6,6 +6,8 @@ PRJ_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 PRJ_DIR=$(dirname "$PRJ_DIR")
 echo "Project directory is $PRJ_DIR"
 
+cd "$PRJ_DIR"
+
 test_dir() {
   # $1 = the name of the test
 
@@ -16,6 +18,11 @@ test_dir() {
     $PRJ_DIR/vendor/bin/phpunit \
         --coverage-html   "$PRJ_DIR/tmp/$N" \
         --coverage-xml    "$PRJ_DIR/tmp/$N"
+  fi
+
+  if [ -r nightwatch.json ]; then
+    echo -e "\e[0;45m[\e[1;45m$N/nightwatch\e[0;45m] Testing $1\e[0m"
+    node $PRJ_DIR/node_modules/.bin/nightwatch
   fi
 }
 
