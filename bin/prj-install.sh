85241b4832a11a3b4052c653f838b3c37075bd03
Jean Honlet <marielineet.jean@gmail.com>
2015-10-21 06:43:16 +0200

    Use set -e to handle errors
    The script is far more lighter.

--- bin/prj-install.sh
+++ bin/prj-install.sh
@@ -1,25 +1,17 @@
 #!/bin/bash
 
+set -e
+
 PRJ_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 PRJ_DIR=$(dirname "$PRJ_DIR")
 echo "Project directory is $PRJ_DIR"
 
 cd "$PRJ_DIR"
 
-echo -e "\e[1m\e[45mNPM install $V\e[0m"
-if npm install; then
-    echo -e "\e[0;45mNPM install: \e[1;42msuccess.\e[0m"
-  else
-    echo -e "\e[0;45mNPM install: \e[1;41mfailed\e[0m"
-    exit 1;
-fi
+echo -e "\e[1m\e[45mNPM install\e[0m"
+npm install
 
-echo -e "\e[1m\e[45mComposer install $V\e[0m"
-if composer install; then
-    echo -e "\e[0;45mComposer install: \e[1;42msuccess.\e[0m"
-  else
-    echo -e "\e[0;45mComposer install: \e[1;41mfailed\e[0m"
-    exit 1;
-fi
+echo -e "\e[1m\e[45mComposer install\e[0m"
+composer install
 
-echo -e "\e[1m\e[45mAll test terminated $V\e[0m"
+echo -e "\e[1m\e[45mTerminated ok.\e[0m"
--- bin/prj-test.sh
+++ bin/prj-test.sh
@@ -1,43 +1,35 @@
 #!/bin/bash
 
+set -e
+
 PRJ_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 PRJ_DIR=$(dirname "$PRJ_DIR")
 echo "Project directory is $PRJ_DIR"
 
-test_phpunit() {
-  # $1 = technical name of the test
-  # $2 = the name of the test
-  if [ ! -r phpunit.xml ]; then
-    return 0
-  fi
+test_dir() {
+  # $1 = the name of the test
+
   N=`pwd`
   N=`basename "$N"`
-  echo name $N
-
-  echo -e "\e[0;45mTesting \e[1;45m$2\e[0;45m [phpunit/$1]\e[0m"
-#      --coverage-clover "$PRJ_DIR/tmp/$1.clover" \
-  if $PRJ_DIR/vendor/bin/phpunit \
-      --coverage-html   "$PRJ_DIR/tmp/$1" \
-      --coverage-xml    "$PRJ_DIR/tmp/$1" \
-      ; then
-    echo -e "\e[0;45mTesting \e[1;45m$2\e[0;45m [phpunit/$1]: \e[1;42msuccess.\e[0m"
-  else
-    echo -e "\e[0;45mTesting \e[1;45m$2\e[0;45m [phpunit/$1]: \e[1;41mfailed\e[0m"
-    exit 1;
+  if [ -r phpunit.xml ]; then
+    echo -e "\e[0;45m[\e[1;45m$N/phpunit\e[0;45m] Testing $1\e[0m"
+    $PRJ_DIR/vendor/bin/phpunit \
+        --coverage-html   "$PRJ_DIR/tmp/$N" \
+        --coverage-xml    "$PRJ_DIR/tmp/$N"
   fi
-
 }
 
 for V in "$PRJ_DIR"/api/* ; do
   N=`basename "$V"`
-  cd "$V" && test_phpunit  "$N" "version api/$N"
+  cd "$V" && test_dir "version api/$N"
 done
 
 for T in "$PRJ_DIR"/tests/* ; do
-#  if [ -r "$T/phpunit.xml" ]; then
-      N=`basename "$T"`
-      cd "$T" && test_phpunit "$N" "custom test $N"
-#  fi
+  N=`basename "$T"`
+  TITLE="Custom test $N"
+  if [ -d "$T" ]; then
+    cd "$T" && test_dir $TITLE
+  fi
 done
 
-echo -e "\e[1m\e[45mAll test terminated $V\e[0m"
+echo -e "\e[1m\e[45mTerminated ok.\e[0m"
