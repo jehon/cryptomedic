979846e2d7f5cbc3abb6b242a56c11a2028d1ec0
Jean Honlet <marielineet.jean@gmail.com>
2015-10-21 23:03:06 +0200

    Fix route /app/ not working
    And some other mapping

--- tests/router.php
+++ tests/router.php
@@ -1,5 +1,6 @@
 <?php
 $uri = $_SERVER["REQUEST_URI"];
+$uri = parse_url($uri, PHP_URL_PATH);
 
 if (substr($uri, 0, strlen("/cryptomedic")) != "/cryptomedic") {
   die("Not found");
@@ -9,7 +10,7 @@ $uri = preg_replace("%/cryptomedic/%", "/", $uri);
 // Route the cryptomedic URIs:
 
 if (substr($uri, 0, 6) == "/api/v") {
-  preg_match("%^/api/v(.*)/%", $uri, $version);
+  preg_match("%^/api/v([^/]*)/%", $uri, $version);
   require __DIR__ . "/../" . $version[0] . "/public/index.php";
   return true;
 }
@@ -20,15 +21,24 @@ if (substr($uri, 0, 7) == "/cache/") {
   return true;
 }
 
-if (substr($uri, 0, 14) == "/app/index.php") {
+if ((substr($uri, 0, 14) == "/app/index.php") || ($uri == "/app/")) {
   require __DIR__ . "/../app/index.php";
   return true;
 }
 
 $file = __DIR__ . "/../" . $uri;
 if (file_exists($file)) {
-  $finfo = finfo_open(FILEINFO_MIME_TYPE);
-  header("Content-type: " . finfo_file($finfo, $file));
+  switch(pathinfo($file, PATHINFO_EXTENSION)) {
+    case 'css':
+      header("Content-type: text/css");
+      break;
+    case 'js':
+      header("Content-type: text/script");
+      break;
+    default:
+      header("Content-type: " . finfo_file(finfo_open(FILEINFO_MIME_TYPE), $file));
+  }
+
   readfile($file);
   return true;
 }
