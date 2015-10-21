48cace08edc133c671195341c74a514b031a7acb
Jean Honlet <marielineet.jean@gmail.com>
2015-10-21 08:13:54 +0200

    Update data for test

--- tests/end2end/tests/reports/consultations.js
+++ tests/end2end/tests/reports/consultations.js
@@ -3,10 +3,10 @@ module.exports = {
 	'reportConsultation': function(client) {
 	    client
 	    	.page.cryptomedic().authenticate("readonly")
-	    	.page.cryptomedic().report("consultations", { "day": "2014-12-31" })
-		.assert.myAssertCell("#report_table table", 1, 1, "clubfoot")
+	    	.page.cryptomedic().report("consultations", { "day": "2015-04-28" })
+		.assert.myAssertCell("#report_table table", 1, 1, "ricket")
 		.assert.myAssertCell("=", "=", 2, "Chakaria")
-		.assert.myAssertCell("=", "=", 3, "2014-80095")
+		.assert.myAssertCell("=", "=", 3, "2001-1")
 		.end();
 	}
 }
