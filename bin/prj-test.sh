6c799d205fcb803ef35b7028c70fa9285d77d0f0
Jean Honlet <marielineet.jean@gmail.com>
2015-10-24 20:51:19 +0200

    Enable testing firefox and chrome

--- bin/prj-test.sh
+++ bin/prj-test.sh
@@ -34,7 +34,7 @@ test_dir() {
     if [ "$FRONT" ]; then
       node $PRJ_DIR/node_modules/.bin/nightwatch "$@"
     else
-      node $PRJ_DIR/node_modules/.bin/nightwatch "$@"
+      node $PRJ_DIR/node_modules/.bin/nightwatch -e default,chrome "$@"
     fi
 
     echo -e "\e[0;45m[\e[1;45m$N/nightwatch\e[0;45m] Stoping server @$PHPSERV\e[0m"
