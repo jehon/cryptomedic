d65fe3e156775bf746a4ea0a8e587d796fe43537
Jean Honlet <marielineet.jean@gmail.com>
2015-10-28 22:26:18 +0100

    Add a upload script for travis

---
+++ maintenance/travis.php
@@ -0,0 +1,13 @@
+<?php
+
+require_once(__DIR__ . "/../vendor/autoload.php");
+require_once(__DIR__ . "/../vendor/jehon/maintenance/lib/getParameter.php");
+
+require(__DIR__ . "/../config.php");
+
+use Jehon\Maintenance\Lib\getParameter;
+
+\Jehon\Maintenance\TryCatch::run();
+\Jehon\Maintenance\SessionProtect::run(getGlobalConfig("travis.upload"));
+
+\Jehon\Maintenance\SaveToFile::run(__DIR__ . "/../../travis/" . filter_input(INPUT_POST, "travis", FILTER_SANITIZE_STRING));
