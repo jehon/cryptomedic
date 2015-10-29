156803d36c7fffd9b50f2584f75ea2c258843ee1
Jean Honlet <marielineet.jean@gmail.com>
2015-10-29 07:33:28 +0100

    Clean some up

--- tests/end2end/pages/cryptomedic.js
+++ tests/end2end/pages/cryptomedic.js
@@ -90,8 +90,6 @@ module.exports = (function() {
         .setValue('input[ng-model="entryorder"]', entryorder)
         .waitForElementVisible('[ng-click="checkReference()"]', timeout)
         .myClick('[ng-click="checkReference()"]')
-        // .waitForElementVisible('#folder_id', timeout)
-        // .assert.containsText("#folder_id", id)
         .waitForElementVisible('#Patient_entryyear', timeout)
         .assert.containsText("#Patient_entryyear", entryyear)
         .waitForElementVisible('#Patient_entryorder', timeout)
