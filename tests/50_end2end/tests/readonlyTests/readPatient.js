93753e13f962f3b84ef58524670a331712cad669
Jean Honlet <marielineet.jean@gmail.com>
2015-11-14 22:55:58 +0100

    Add some file testing in patients

--- tests/50_end2end/tests/readonlyTests/readPatient.js
+++ tests/50_end2end/tests/readonlyTests/readPatient.js
@@ -28,12 +28,28 @@ module.exports = {
       .assert.containsText("#ds_bmi", "0.0 ds")
 
       .page.cryptomedic().selectFile("Picture", 2)
-      .assert.containsText("#Date", "2014-11-04")
+      .assert.containsText("#Picture_Date", "2014-11-04")
       .assert.containsText("#Picture_OriginalName", "03.09 a.JPG")
-      .assert.containsText("#Picture_File", "10_2014-11-06_15-32-45.JPG")
+      .assert.containsText("#Picture_file", "10_2014-11-06_15-32-45.JPG")
       .assert.containsText("#Patient_entryyear", "2000")
       .assert.containsText("#Patient_entryorder", "1")
 
+      .page.cryptomedic().selectFile("Bill", 1)
+      // TODO: check bill
+
+      .page.cryptomedic().goPatient(2014, 105)
+      .assert.containsText("#Patient_Upazilla", "Ukhia")
+      .assert.containsText("#Patient_Union_", "Jalia palong")
+      .assert.containsText("#Patient_Telephone", "1813247984")
+
+      .page.cryptomedic().selectFile("ClubFoot", 1)
+      .assert.containsText("#ageAtConsultationTime", "2 years old at consultation time")
+      .assert.containsText("#ClubFoot_Treatment", "DB splint")
+      // TODO: adapt the data and check them
+
+      .page.cryptomedic().goPatient(2001, 1)
+      .page.cryptomedic().selectFile("RicketConsult", 3)
+      // TODO: adapt the data and check them
 
       .end();
   },
