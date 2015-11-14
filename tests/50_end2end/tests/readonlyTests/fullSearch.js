b0643a252cb92e9a7f89018008f792eb171ea32b
Jean Honlet <marielineet.jean@gmail.com>
2015-11-14 22:55:42 +0100

    Remove comments

--- tests/50_end2end/tests/readonlyTests/fullSearch.js
+++ tests/50_end2end/tests/readonlyTests/fullSearch.js
@@ -28,52 +28,11 @@ module.exports = {
     testSearch(client, { "#Patient_Lastname": "OSMAN" }, [ "2014-103" ]);
     testSearch(client, { "#Patient_Lastname": "osman" }, [ "2014-103" ])
     testSearch(client, { "#Patient_Yearofbirth": 2002 }, [ "2014-107" ])
-
     testSearch(client, { "#Patient_Lastname": "Islam" }, [ "2000-1" ])
     testSearch(client, { "#Patient_Lastname": "OSM" }, [ "2014-103" ])
     testSearch(client, { "#Patient_Telephone": "1813247984" }, [ "2014-105" ])
     testSearch(client, { "#Patient_pathology_Clubfoot": true }, [ "2014-107", "2014-103", "2014-104", "2014-105", "2000-1" ])
+
+    // TODO: search by sex
   }
-  // 'searchPatient2000': function(client) {
-  //   testSearch(client, { "#Patient_entryyear": "2000" }, [ "2000-1" ])
-  //     .myClick("#search_results tbody tr:nth-child(1) td:nth-child(1) img")
-  //     .assert.myAssertHashIs("#/folder/1")
-  //     .end();
-  // },
-  // 'searchPatient2001': function(client) {
-  //   testSearch(client, { "#Patient_entryyear": "2001" }, [ "2001-1", "2001-4" ])
-  //     .end();
-  // },
-  // 'searchPatientOrder': function(client) {
-  //   testSearch(client, { "#Patient_entryorder": "104" }, [ "2014-104" ])
-  //     .end();
-  // },
-  // 'searchPatientByName': function(client) {
-  //   testSearch(client, { "#Patient_Lastname": "OSMAN" }, [ "2014-103" ])
-  //     .end();
-  // },
-  // 'searchPatientByNameIncorrectCase': function(client) {
-  //   testSearch(client, { "#Patient_Lastname": "osman" }, [ "2014-103" ])
-  //     .end();
-  // },
-  // 'searchPatientByYearOfBirth': function(client) {
-  //   testSearch(client, { "#Patient_Yearofbirth": 2002 }, [ "2014-107" ])
-  //     .end();
-  // },
-  // 'searchPatientByLastName': function(client) {
-  //   testSearch(client, { "#Patient_Lastname": "Islam" }, [ "2000-1" ])
-  //     .end();
-  // },
-  // 'searchPatientByNamePartial': function(client) {
-  //   testSearch(client, { "#Patient_Lastname": "OSM" }, [ "2014-103" ])
-  //     .end();
-  // },
-  // 'searchPatientByTelephone': function(client) {
-  //   testSearch(client, { "#Patient_Telephone": "1813247984" }, [ "2014-105" ])
-  //     .end();
-  // },
-  // 'searchPatientCP': function(client) {
-  //   testSearch(client, { "#Patient_pathology_Clubfoot": true }, [ "2014-107", "2014-103", "2014-104", "2014-105", "2000-1" ])
-  //     .end();
-  // }
 };
