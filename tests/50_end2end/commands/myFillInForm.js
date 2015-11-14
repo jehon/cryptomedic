8dd2431d33f2fbb2e89589de2495347dd7e1f77e
Jean Honlet <marielineet.jean@gmail.com>
2015-11-14 15:34:58 +0100

    Add a fill in form function

---
+++ tests/50_end2end/commands/myFillInForm.js
@@ -0,0 +1,21 @@
+exports.command = function(selector, fields, button) {
+  var self = this;
+  this.waitForElementVisible(selector, 1000);
+  for(f in fields) {
+    this
+      .assert.visible(f);
+    if (fields[f] === true) {
+      this.
+        click(f);
+    } else {
+      this
+        .clearValue(f)
+        .setValue(f, fields[f]);
+    }
+  }
+  this
+    .pause(100)
+    .myClick(button);
+
+  return this; // allows the command to be chained.
+};
