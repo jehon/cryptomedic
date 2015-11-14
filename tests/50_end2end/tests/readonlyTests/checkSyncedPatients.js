04c1e8eb8942731d48df6b32b7ad5b03632d2876
Jean Honlet <marielineet.jean@gmail.com>
2015-11-14 22:56:08 +0100

    Regroup file testing

--- tests/50_end2end/tests/readonlyTests/checkSyncedPatients.js
+++ tests/50_end2end/tests/readonlyTests/checkSyncedPatients.js
@@ -1,58 +1,70 @@
 module.exports = {
-  'view1': function(client) {
+  'viewAllSyncedPatients': function(client) {
       client
         .page.cryptomedic().authenticate("readonly")
         .page.cryptomedic().goPatient(2000, 1)
-        .end();
-  },
-
-  'view2': function(client) {
-      client
-        .page.cryptomedic().authenticate("readonly")
         .page.cryptomedic().goPatient(2014, 107)
-        .end();
-  },
-
-  'view3': function(client) {
-      client
-        .page.cryptomedic().authenticate("readonly")
         .page.cryptomedic().goPatient(2014, 103)
-        .end();
-  },
-
-  'view4': function(client) {
-      client
-        .page.cryptomedic().authenticate("readonly")
         .page.cryptomedic().goPatient(2014, 104)
-        .end();
-  },
-
-  'view4': function(client) {
-      client
-        .page.cryptomedic().authenticate("readonly")
         .page.cryptomedic().goPatient(2014, 105)
-        .end();
-  },
-
-  'view5': function(client) {
-      client
-        .page.cryptomedic().authenticate("readonly")
-        .page.cryptomedic().goPatient(2014, 105)
-        .end();
-  },
-
-  'view6': function(client) {
-      client
-        .page.cryptomedic().authenticate("readonly")
         .page.cryptomedic().goPatient(2001, 1)
-        .end();
-  },
-
-  'view7': function(client) {
-      client
-        .page.cryptomedic().authenticate("readonly")
         .page.cryptomedic().goPatient(2001, 4)
         .end();
-  },
+  }
+  // 'view1': function(client) {
+  //     client
+  //       .page.cryptomedic().authenticate("readonly")
+  //       .page.cryptomedic().goPatient(2000, 1)
+  //       .end();
+  // },
+
+  // 'view2': function(client) {
+  //     client
+  //       .page.cryptomedic().authenticate("readonly")
+  //       .page.cryptomedic().goPatient(2014, 107)
+  //       .end();
+  // },
+
+  // 'view3': function(client) {
+  //     client
+  //       .page.cryptomedic().authenticate("readonly")
+  //       .page.cryptomedic().goPatient(2014, 103)
+  //       .end();
+  // },
+
+  // 'view4': function(client) {
+  //     client
+  //       .page.cryptomedic().authenticate("readonly")
+  //       .page.cryptomedic().goPatient(2014, 104)
+  //       .end();
+  // },
+
+  // 'view4': function(client) {
+  //     client
+  //       .page.cryptomedic().authenticate("readonly")
+  //       .page.cryptomedic().goPatient(2014, 105)
+  //       .end();
+  // },
+
+  // 'view5': function(client) {
+  //     client
+  //       .page.cryptomedic().authenticate("readonly")
+  //       .page.cryptomedic().goPatient(2014, 105)
+  //       .end();
+  // },
+
+  // 'view6': function(client) {
+  //     client
+  //       .page.cryptomedic().authenticate("readonly")
+  //       .page.cryptomedic().goPatient(2001, 1)
+  //       .end();
+  // },
+
+  // 'view7': function(client) {
+  //     client
+  //       .page.cryptomedic().authenticate("readonly")
+  //       .page.cryptomedic().goPatient(2001, 4)
+  //       .end();
+  // },
 
 };
