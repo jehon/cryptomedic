d3b3d686e1d1dec5448c8f98c42cc3d20793ecee
Jean Honlet <marielineet.jean@gmail.com>
2015-11-24 22:36:15 +0100

    Remove getVersion

--- php/core.php
+++ php/core.php
@@ -1,9 +1,9 @@
 <?php
 // 	require_once(__DIR__ . "/../../rest/php/core.php");
-	
+
 // 	Server::setOption(Server::OPTION_NO_SESSION);
 // 	$server = Server::getInstance();
-	
+
 	// Application autoload classes
 spl_autoload_register(function ($class) {
 	$file = __DIR__ . "/" . strtolower($class) . ".php";
@@ -15,15 +15,15 @@ spl_autoload_register(function ($class) {
 });
 
 
-function getVersion() {
-	$file = __DIR__ . "/../../cryptomedic.version";
-	if (file_exists($file)) {
-		return trim(file_get_contents($file));
-	}
-	return "undefined";
-}
+// function getVersion() {
+// 	$file = __DIR__ . "/../../cryptomedic.version";
+// 	if (file_exists($file)) {
+// 		return trim(file_get_contents($file));
+// 	}
+// 	return "undefined";
+// }
 
 function myCleanValue($c) {
 	return str_replace(["'", " ", "\""], "", $c);
 }
-	
+
