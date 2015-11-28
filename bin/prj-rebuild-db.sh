4e80c632caa8b5ff7b9122fedc5ba3da034d867b
Jean Honlet <marielineet.jean@gmail.com>
2015-11-28 22:18:20 +0100

    Add a script to rebuild the database

--- bin/prj-rebuild-db.sh
+++ bin/prj-rebuild-db.sh
@@ -1,1 +1,29 @@
-php api/v1.0/tests/bootstrap.php
+
+php <<-PHP
+  <?php
+
+  // Set up the database
+
+  // require_once(__DIR__ . "/bootstrap/autoload.php");
+
+  require_once(__DIR__ . "/vendor/autoload.php");
+
+  global \$myconfig;
+  require_once(__DIR__ . "/config.php");
+
+  \Jehon\Maintenance\Database::run(
+      [
+          __DIR__ . "/conf/database_scripts/dev_only/reset.sql",
+          __DIR__ . "/conf/database_scripts/dev_only/testing.sql",
+          __DIR__ . "/conf/database_scripts",
+          __DIR__ . "/conf/database_scripts/dev_only"
+      ],
+      \$myconfig['database']['database'],
+      \$myconfig['database']['username'],
+      \$myconfig['database']['password'],
+      \$myconfig['database']['options']
+  );
+
+PHP
+
+#php api/v1.0/tests/bootstrap.php
