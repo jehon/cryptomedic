6f67ef8513ad779e644ad20f2f9afd28c81dd2a8
Jean Honlet <marielineet.jean@gmail.com>
2015-11-28 16:21:45 +0100

    Test bug reporting to github

--- maintenance/bug_submit.php
+++ maintenance/bug_submit.php
@@ -1,17 +1,28 @@
 <?php
 
 	require_once(__DIR__ . "/../vendor/autoload.php");
-	
+
 	require(__DIR__ . "/../config.php");
-	
+
 	\Jehon\Maintenance\TryCatch::run();
-	
+
 	$db = new \Jehon\Maintenance\Database(
 			$myconfig['database']['database'],
 			$myconfig['database']['username'],
 			$myconfig['database']['password'],
 			$myconfig['database']['options']
 	);
-	
+
 	\Jehon\Maintenance\BugReporting::record($db, "http://www.cryptomedic.org/cryptomedic/maintenance/bug_view.php");
-	
+
+	// $client = new Github\Client();
+	// // See https://github.com/KnpLabs/php-github-api/blob/master/doc/security.md
+	// var_dump("authenticate");
+	// $client->authenticate($myconfig['github']['token'], "", Github\Client::AUTH_HTTP_TOKEN);
+
+	// var_dump("submit bug");
+ //  $client->api('issue')->create('jehon', 'database_patch', array('title' => 'The issue title', 'body' => 'The issue body'));
+
+	// var_dump("get list");
+	// $issues = $client->api('issue')->all("jehon", "database_patch", array('state' => 'open'));
+	// var_dump($issues);
