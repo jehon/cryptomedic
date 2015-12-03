d8626c1ab29a35386c614494fb25ec2c18d26be1
Jean Honlet <marielineet.jean@gmail.com>
2015-12-03 15:50:13 +0600

    Clean up

--- cache/templates/test.php
+++ cache/templates/test.php
@@ -6,7 +6,7 @@ if (!isset($_SERVER['HTTP_HOST']) || strcasecmp(substr($_SERVER['HTTP_HOST'], 0,
 			$res = true;
 			foreach(glob("$f/*") as $i) {
 				$res = testOne($i) && $res;
-			}	
+			}
 			return $res;
 		}
 		$l = "../" . str_replace(".php", ".html", substr($f, 2));
@@ -14,17 +14,10 @@ if (!isset($_SERVER['HTTP_HOST']) || strcasecmp(substr($_SERVER['HTTP_HOST'], 0,
 			echo "<a href='$l'>";
 		}
 		echo $f;
-// 		$res = file_get_contents("http://localhost/cryptomedic/templates/$f");
-// 		if ($res === false) {
-// 			echo "FAILED";
-// 		} else {
-// 			echo "ok";
-// 		}
 		if (isset($_SERVER['HTTP_HOST'])) {
 			echo "</a><br>";
 		}
 		echo "\n";
-// 		return $res !== false;
 	}
 
 	return testOne(".");
