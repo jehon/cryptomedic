2ee0a3a1b00cc2d2f09772ad1a206c9c3e56b4b0
Jean Honlet <marielineet.jean@gmail.com>
2015-12-05 21:39:58 +0600

    Update references to examiner names

---
+++ conf/database_scripts/50 update examiner names.sql
@@ -0,0 +1,28 @@
+
+UPDATE appointment SET ExaminerName = 'Dr Monir' WHERE ExaminerName = 'Monir';
+UPDATE appointment SET ExaminerName = 'Dr Taslim' WHERE ExaminerName = 'Taslim';
+UPDATE appointment SET ExaminerName = 'Prietush' WHERE ExaminerName = 'Prytosh';
+
+UPDATE bills SET ExaminerName = 'Dr Monir' WHERE ExaminerName = 'Monir';
+UPDATE bills SET ExaminerName = 'Dr Taslim' WHERE ExaminerName = 'Taslim';
+UPDATE bills SET ExaminerName = 'Prietush' WHERE ExaminerName = 'Prytosh';
+
+UPDATE club_foots SET ExaminerName = 'Dr Monir' WHERE ExaminerName = 'Monir';
+UPDATE club_foots SET ExaminerName = 'Dr Taslim' WHERE ExaminerName = 'Taslim';
+UPDATE club_foots SET ExaminerName = 'Prietush' WHERE ExaminerName = 'Prytosh';
+
+UPDATE nonricket_consults SET ExaminerName = 'Dr Monir' WHERE ExaminerName = 'Monir';
+UPDATE nonricket_consults SET ExaminerName = 'Dr Taslim' WHERE ExaminerName = 'Taslim';
+UPDATE nonricket_consults SET ExaminerName = 'Prietush' WHERE ExaminerName = 'Prytosh';
+
+UPDATE pictures SET ExaminerName = 'Dr Monir' WHERE ExaminerName = 'Monir';
+UPDATE pictures SET ExaminerName = 'Dr Taslim' WHERE ExaminerName = 'Taslim';
+UPDATE pictures SET ExaminerName = 'Prietush' WHERE ExaminerName = 'Prytosh';
+
+UPDATE ricket_consults SET ExaminerName = 'Dr Monir' WHERE ExaminerName = 'Monir';
+UPDATE ricket_consults SET ExaminerName = 'Dr Taslim' WHERE ExaminerName = 'Taslim';
+UPDATE ricket_consults SET ExaminerName = 'Prietush' WHERE ExaminerName = 'Prytosh';
+
+UPDATE surgeries SET ExaminerName = 'Dr Monir' WHERE ExaminerName = 'Monir';
+UPDATE surgeries SET ExaminerName = 'Dr Taslim' WHERE ExaminerName = 'Taslim';
+UPDATE surgeries SET ExaminerName = 'Prietush' WHERE ExaminerName = 'Prytosh';
