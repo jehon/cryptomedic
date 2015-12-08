d2517dbf23c3d7c2e200a55cea23a64e4c397ea6
Jean Honlet <marielineet.jean@gmail.com>
2015-12-08 12:36:34 +0600

    Render better the next appointment

--- cache/templates/fiches/partials/patient-related.php
+++ cache/templates/fiches/partials/patient-related.php
@@ -4,7 +4,8 @@
 ?>
 <fieldset class='related'>
 	<legend>Related Patient</legend>
-		<table>
+	<table>
+		<tbody>
 			<tr>
 				<td rowspan='100' align='left' width='100px'>
 					<img src="static/img/patient.gif">
@@ -28,20 +29,27 @@
 			<?php (new t("Patient.Sex", $opt))->tr("Sex")->p(); ?>
 
       <tr>
-        <td colspan=2 class='notModeWrite text-center'>
-        	<span ng-if="nextAppointment()">
-        		<div class='alert alert-info'>
-	        		Next appointment: {{nextAppointment()}}
-	        	</div>
-        	</span>
-        	<span ng-if="!nextAppointment()">
-        		<div class='alert alert-danger'>
-        			No next appointment planned.
-	          	<a class='btn btn-default' ng-href="#/folder/{{patient_id}}/file/Appointment">Add an appointment</a>
-        		</div>
-        	</span>
-        </td>
       </tr>
+		</tbody>
+	</table>
+</fieldset>
+<fieldset class='related'>
+	<legend>Agenda</legend>
+	<table>
+		<tbody>
+			<tr>
+				<td rowspan='100' align='left' width='100px'>
+					<img src="static/img/consultOfDay.gif" width='75px'>
+				</td>
+        <td class='notModeWrite text-center'>
+        	<div ng-if="nextAppointment()" class='alert alert-info' style='margin-bottom: 0px'>
+        		Next appointment: {{nextAppointment()}}
+        	</div>
+        	<div ng-if="!nextAppointment()" class='alert alert-danger' style='margin-bottom: 0px'>
+      			No next appointment planned.
+          	<a class='btn btn-default' ng-href="#/folder/{{patient_id}}/file/Appointment">Add an appointment</a>
+        	</div>
+        </td>
 		</tbody>
 	</table>
 </fieldset>
