5547fcd3e31027e926fdf25302af92fe15bd3bb4
Jean Honlet <marielineet.jean@gmail.com>
2015-12-09 18:03:02 +0600

    Test logout

---
+++ tests/50_end2end/tests/ro/login_logout.js
@@ -0,0 +1,9 @@
+module.exports = {
+  'authenticate_multiple': function(client) {
+    client
+      .page.cryptomedic().authenticate("readonly")
+      .myClick('#logout')
+      .page.cryptomedic().authenticate("readonly")
+      .end();
+  },
+}
