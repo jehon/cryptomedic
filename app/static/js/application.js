f945e1aa58577363628b7acf4959f062e71be3b3
Jean Honlet <marielineet.jean@gmail.com>
2015-12-09 16:23:19 +0600

    Add month numbers

--- app/static/js/application.js
+++ app/static/js/application.js
@@ -166,7 +166,8 @@ var mainApp = angular.module('app_main', [ 'ngRoute' ])
     jQuery(elem).datepicker({
       dateFormat: "yy-mm-dd",
       changeMonth: true,
-      changeYear: true
+      changeYear: true,
+      monthNamesShort: [ "1 Jan", "2 Feb", "3 Mar", "4 Apr", "5 May", "6 Jun", "7 Jul", "8 Aug", "9 Sep", "10 Oct", "11 Nov", "12 Dec" ]
     });
   }
 })
