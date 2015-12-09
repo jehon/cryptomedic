42f82270a44ccb164e5ec8c04db0fd4a6d90aeaa
Jean Honlet <marielineet.jean@gmail.com>
2015-12-09 14:56:42 +0600

    Fix date calculation not working

--- app/static/js/ctrl_file_appointment.js
+++ app/static/js/ctrl_file_appointment.js
@@ -2,9 +2,8 @@
 
 mainApp.controller('ctrl_file_appointment', [ '$scope', function($scope) {
   $scope.nextMonth = function(months) {
-    console.info($scope.currentFile());
-    d = new Date();
-    nd = new Date(d.getFullYear(), d.getMonth() + months, d.getDay());
+    var d = new Date();
+    var nd = new Date(d.getFullYear(), d.getMonth() + months, d.getDate(), 0, 0, 0);
 
     $scope.currentFile().Nextappointment = date2CanonicString(nd, true);
   }
