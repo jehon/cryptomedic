5446a084c8b29e9a4a8e41efcaf1f01eccd29883
Jean Honlet <marielineet.jean@gmail.com>
2015-12-09 13:06:06 +0600

    Rename ready into busy

--- app/reducers/stateReducers.js
+++ app/reducers/stateReducers.js
@@ -8,29 +8,29 @@ catalog._define('STATE_CLEAR');
 export default function(state, action) {
   if (!state || action.type == catalog.STATE_CLEAR) {
     state = {
-      ready: 0,
+      busy: 0,
       messages: "",
       max: 0
     };
   }
   if (action.type == catalog.STATE_BUSY) {
     return {
-      ready: state.ready + 1,
-      messages: state.messages + "<br>" + action.payload,
+      busy: state.busy + 1,
+      messages: (state.messages + "<br>" + action.payload).trim(),
       max: state.max + 1
     };
   };
 
   if (action.type == catalog.STATE_READY) {
-    if (state.ready <= 1) {
+    if (state.busy <= 1) {
       return {
-        ready: 0,
+        busy: 0,
         messages: "",
         max: 0
       }
     }
     return {
-      ready: state.ready - 1,
+      busy: state.busy - 1,
       messages: state.messages,
       max: state.max
     };
--- tests/20_js/appState/state.js
+++ tests/20_js/appState/state.js
@@ -1,55 +1,55 @@
 
 describe("appState", function() {
   it("should handle busy / ready", function() {
-    expect(appState().store.getState().state.ready).toBe(0);
+    expect(appState().store.getState().state.busy).toBe(0);
     expect(appState().store.getState().state.max).toBe(0);
     expect(appState().store.getState().state.messages).toBe("");
 
     appState().actions.state.busy("test");
-    expect(appState().store.getState().state.ready).toBe(1);
+    expect(appState().store.getState().state.busy).toBe(1);
     expect(appState().store.getState().state.max).toBe(1);
 
     appState().actions.state.busy("test");
-    expect(appState().store.getState().state.ready).toBe(2);
+    expect(appState().store.getState().state.busy).toBe(2);
     expect(appState().store.getState().state.max).toBe(2);
 
     appState().actions.state.busy("test");
-    expect(appState().store.getState().state.ready).toBe(3);
+    expect(appState().store.getState().state.busy).toBe(3);
     expect(appState().store.getState().state.max).toBe(3);
 
     appState().actions.state.ready();
-    expect(appState().store.getState().state.ready).toBe(2);
+    expect(appState().store.getState().state.busy).toBe(2);
     expect(appState().store.getState().state.max).toBe(3);
 
     appState().actions.state.ready();
-    expect(appState().store.getState().state.ready).toBe(1);
+    expect(appState().store.getState().state.busy).toBe(1);
     expect(appState().store.getState().state.max).toBe(3);
 
     appState().actions.state.busy("test");
-    expect(appState().store.getState().state.ready).toBe(2);
+    expect(appState().store.getState().state.busy).toBe(2);
     expect(appState().store.getState().state.max).toBe(4);
 
     appState().actions.state.ready();
-    expect(appState().store.getState().state.ready).toBe(1);
+    expect(appState().store.getState().state.busy).toBe(1);
     expect(appState().store.getState().state.max).toBe(4);
 
     appState().actions.state.ready();
-    expect(appState().store.getState().state.ready).toBe(0);
+    expect(appState().store.getState().state.busy).toBe(0);
     expect(appState().store.getState().state.max).toBe(0);
     expect(appState().store.getState().state.messages).toBe("");
   });
 
   it("should handle clear", function() {
-    expect(appState().store.getState().state.ready).toBe(0);
+    expect(appState().store.getState().state.busy).toBe(0);
     expect(appState().store.getState().state.max).toBe(0);
     expect(appState().store.getState().state.messages).toBe("");
 
     appState().actions.state.busy("test");
-    expect(appState().store.getState().state.ready).toBe(1);
+    expect(appState().store.getState().state.busy).toBe(1);
     expect(appState().store.getState().state.max).toBe(1);
 
     appState().actions.state.clear();
-    expect(appState().store.getState().state.ready).toBe(0);
+    expect(appState().store.getState().state.busy).toBe(0);
     expect(appState().store.getState().state.max).toBe(0);
     expect(appState().store.getState().state.messages).toBe("");
   });
