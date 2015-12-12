cde51da44350dfc8a8e838bae1a7e2373778659b
Jean Honlet <marielineet.jean@gmail.com>
2015-12-12 22:21:54 +0600

    Enable dev server

--- app/reducers/store.js
+++ app/reducers/store.js
@@ -1,12 +1,60 @@
 
-import { createStore } from 'redux';
-import { combineReducers } from 'redux';
+import { combineReducers, compose, createStore, applyMiddleware } from 'redux';
+import thunkMiddleware from 'redux-thunk';
+import createLogger from 'redux-logger';
+// import { persistStore, autoRehydrate } from 'redux-persist';
+// import { devTools, persistState } from 'redux-devtools';
+// import { DevTools, DebugPanel, LogMonitor } from 'redux-devtools/lib/react';
 
 import connection from 'reducers/connectionReducers';
 import state from 'reducers/stateReducers';
 import database from 'reducers/databaseReducers';
 
-let store = createStore(combineReducers({
+
+const loggerMiddleware = createLogger({
+  level: 'info',
+  collapsed: true,
+});
+
+let finalCreateStore = compose(
+  //enables middleware:
+  applyMiddleware(
+    thunkMiddleware,
+    loggerMiddleware
+  )
+  ,
+  // Provides support for DevTools:
+  // devTools(),
+  // Lets you write ?debug_session=<name> in address bar to persist sessions
+  // persistState(window.location.href.match(/[?&]debug_session=([^&]+)\b/))
+)
+(createStore);
+
+// function configureStore(initialState) {
+//   const store = autoRehydrate()(finalCreateStore)(
+//       combineReducers({
+//         connection,
+//         state,
+//         database
+//       })
+//     );
+//   if (module.hot) {
+//     // Enable Webpack hot module replacement for reducers
+//     module.hot.accept('../reducers', () => {
+//       const nextRootReducer = require('../reducers');
+//       store.replaceReducer(nextRootReducer);
+//     });
+//   }
+//   return store;
+// }
+// const store = configureStore();
+
+// persistStore(store, {whitelist: []}, () => {
+//   store.dispatch(customRehydrate());
+// });
+
+
+let store = finalCreateStore(combineReducers({
   connection,
   state,
   database
--- webpack.dev.js
+++ webpack.dev.js
@@ -7,6 +7,6 @@ var base_config = require('./webpack.js');
 // base_config.entry.push('webpack-dev-server/client?/');
 // base_config.entry.push('webpack/hot/only-dev-server');
 // base_config.module.loaders[0].loaders.push('react-hot');
-// base_config.plugins.push(new webpack.HotModuleReplacementPlugin());
+base_config.plugins.push(new webpack.HotModuleReplacementPlugin());
 
 module.exports = base_config;
