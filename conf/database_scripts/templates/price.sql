a99a6c8dfaff5e4f042c745ce6377507a581be99
Jean Honlet <marielineet.jean@gmail.com>
2015-12-13 20:47:32 +0600

    Update price template for laravel system

--- conf/database_scripts/templates/price.sql
+++ conf/database_scripts/templates/price.sql
@@ -4,7 +4,7 @@ CREATE TEMPORARY TABLE tmptable_1 SELECT * FROM prices WHERE dateto IS NULL ORDE
 
 -- get a new id
 UPDATE tmptable_1 SET id = (SELECT max(id) FROM prices) + 1;
-	
+
 -- set the date_from (new price) and date_to (old price)
 UPDATE tmptable_1 SET datefrom = NOW();
 UPDATE prices SET dateto = NOW() WHERE dateto IS NULL;
@@ -15,23 +15,18 @@ INSERT INTO prices SELECT * FROM tmptable_1;
 -- drop the temporary table
 DROP TEMPORARY TABLE IF EXISTS tmptable_1;
 
-UPDATE prices SET created = NOW(), modified = NOW() WHERE dateto is NULL;
+UPDATE prices SET created_at = NOW(), updated_at = NOW() WHERE dateto is NULL;
 
 -- add fields
-ALTER TABLE `bills` 
-	ADD `other_physiotherapy_child` INT(11) NOT NULL DEFAULT '0' AFTER `other_group_physiotherapy`, 
-	ADD `other_physiotherapy_adult` INT(11) NOT NULL DEFAULT '0' AFTER `other_physiotherapy_child`;
+ALTER TABLE `bills`
+	ADD `other_physiotherapy_child` INT(11) NOT NULL DEFAULT '0' AFTER `other_group_physiotherapy`;
 
 ALTER TABLE `prices`
-	ADD `other_physiotherapy_child` INT(11) NOT NULL DEFAULT '-1' AFTER `other_group_physiotherapy`, 
-	ADD `other_physiotherapy_adult` INT(11) NOT NULL DEFAULT '-1' AFTER `other_physiotherapy_child`;
+	ADD `other_physiotherapy_child` INT(11) NOT NULL DEFAULT '-1' AFTER `other_group_physiotherapy`;
 
 -- modification to prices elements
 UPDATE `prices`
-	SET 
-		other_physiotherapy_child = 100, 
-		other_physiotherapy_adult = 200,
-		other_physiotherapy = -1,
-		other_group_physiotherapy = -1
+	SET
+		other_physiotherapy_child = 100;
 	WHERE dateto is NULL;
 
