29df722eb5ae3ccf78bff51a053f460da73a46b1
Jean Honlet <marielineet.jean@gmail.com>
2015-12-14 22:01:18 +0600

    Add token

--- maintenance/deploy.php
+++ maintenance/deploy.php
@@ -7,7 +7,7 @@ require_once(__DIR__ . "/../config.php");
 \Jehon\Maintenance\TryCatch::run();
 \Jehon\Maintenance\SessionProtect::run(getGlobalConfig("maintenance.code"), getGlobalConfig("maintenance.token"));
 
-\Jehon\Maintenance\Deploy::run(dirname(__DIR__), "jehon", "cryptomedic");
+\Jehon\Maintenance\Deploy::run(dirname(__DIR__), "jehon", "cryptomedic", getGlobalConfig("github.token")));
 
 echo "<h3>Custom: patch db</h3>";
 \Jehon\Maintenance\Lib\stepByStep(function() {
