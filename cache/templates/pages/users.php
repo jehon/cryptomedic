25f579d94f3a391a3f5c06f86eba46abf57c1419
Jean Honlet <marielineet.jean@gmail.com>
2015-12-16 21:54:51 +0600

    Enhance user editing page

--- cache/templates/pages/users.php
+++ cache/templates/pages/users.php
@@ -24,49 +24,49 @@
   </table>
 </div>
 
-<form>
+<form name='user_edit_form'>
   <div ng-if='edit'>
     <h3>Edit user '{{edit.username}}'</h3>
-    <table class='table table-hover table-bordered tablesorter'>
-      <tr>
-        <td>Id</td>
-        <td>{{edit.id}}</td>
-      </tr>
-      <tr>
-        <td>Username</td>
-        <td>
-          <input id='newcode' ng-model="edit.username" class="form-control" placeholder="enter new password here" required autofocus>
-        </td>
-      </tr>
-      <tr>
-        <td>Name</td>
-        <td>
-          <input id='newcode' ng-model="edit.name" class="form-control" placeholder="enter new password here" required autofocus>
-        </td>
-      </tr>
-      <tr>
-        <td>Email</td>
-        <td>
-          <input id='newcode' ng-model="edit.email" class="form-control" placeholder="enter new password here" required autofocus>
-        </td>
-      </tr>
-      <tr>
-        <td>Notes</td>
-        <td>
-          <input id='newcode' ng-model="edit.notes" class="form-control" placeholder="enter new password here" required autofocus>
-        </td>
-      </tr>
-    </table>
     <span ng-if='!password'>
+      <table class='table table-hover table-bordered tablesorter'>
+        <tr>
+          <td>Id</td>
+          <td>{{edit.id}}</td>
+        </tr>
+        <tr>
+          <td>Username</td>
+          <td>
+            <input id='newcode' ng-model="edit.username" class="form-control" required autofocus>
+          </td>
+        </tr>
+        <tr>
+          <td>Name</td>
+          <td>
+            <input id='newcode' ng-model="edit.name" class="form-control" required>
+          </td>
+        </tr>
+        <tr>
+          <td>Email</td>
+          <td>
+            <input id='newcode' ng-model="edit.email" class="form-control">
+          </td>
+        </tr>
+        <tr>
+          <td>Notes</td>
+          <td>
+            <input id='newcode' ng-model="edit.notes" class="form-control">
+          </td>
+        </tr>
+      </table>
       <span class='btn btn-default' ng-click='doCancel()'>cancel</span>
-      <span class='btn btn-info' ng-click='doSave()'>save</span>
+      <span ng-disabled="user_edit_form.$invalid" class='btn btn-info' ng-click='doSave()'>save</span>
       <span class='btn btn-warning' ng-click='doShowPassword()'>set password</span>
     </span>
 
     <div ng-if='password'>
       <table class='table table-hover table-bordered tablesorter'>
         <tr>
-          <td>New password</td>
+          <td>New password for {{edit.name}}</td>
           <td>
             <input id='newcode' ng-model="pwd.newcode" class="form-control" placeholder="enter new password here" required autofocus>
           </td>
