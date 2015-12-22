be35bda3e206fb18f25c62c279916e833564e606
Jean Honlet <marielineet.jean@gmail.com>
2015-12-22 15:08:34 +0100

    Remove jquery from bug reporting

--- app/static/js/model_patient.js
+++ app/static/js/model_patient.js
@@ -7,21 +7,4 @@ application.models.Patient = application.models.Data.extend({
 	    	if (this.Sex == "Female") return "f";
 	    	return null;
 	},
-	// 'validate': function(res) {
-	// 	res = this._super(res);
-
-		// if (!this.pathology_Clubfoot
-		// 	&& !this.pathology_Ricket
-		// 	&& !this.pathology_Adult
-		// 	&& !this.pathology_CP
-		// 	&& !this.pathology_Polio
-		// 	&& !this.pathology_Burn
-		// 	&& !this.pathology_Congenital
-		// 	&& !this.pathology_other) {
-
-		// 	res.noPathology = true;
-		// }
-
-	// 	return res;
-	// }
 });
