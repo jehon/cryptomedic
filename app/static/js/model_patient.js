aa85e7a5e087bd580664123a4ea65157e7a3d60f
Jean Honlet <marielineet.jean@gmail.com>
2015-12-26 12:29:56 +0100

    Format

--- app/static/js/model_patient.js
+++ app/static/js/model_patient.js
@@ -2,9 +2,9 @@
 
 application.models.Patient = application.models.Data.extend({
 	'sexStr': function() {
-	    	if (!this.isNotZero('Sex')) return null;
-	    	if (this.Sex == "Male") return "m";
-	    	if (this.Sex == "Female") return "f";
-	    	return null;
+  	if (!this.isNotZero('Sex')) return null;
+  	if (this.Sex == "Male") return "m";
+  	if (this.Sex == "Female") return "f";
+  	return null;
 	},
 });
