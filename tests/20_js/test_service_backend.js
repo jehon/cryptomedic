5c28f50ca4eadc5c67fe11524d321357a4996e17
Jean Honlet <marielineet.jean@gmail.com>
2015-12-29 16:22:46 +0100

    Test nullify

---
+++ tests/20_js/test_service_backend.js
@@ -0,0 +1,29 @@
+"use strict";
+
+
+describe("service_backend", function() {
+	describe("nullify", function() {
+		var now2 = new Date(2015, 6, 1);
+		// Same
+		it("should handle null", function() {
+			expect(nullify(null)).toBe(null);
+		});
+
+		it("should handle 'null'", function() {
+			expect(nullify("null")).toBe(null);
+		});
+
+		it("should handle '?'", function() {
+			expect(nullify("?")).toBe(null);
+		});
+
+		it("should handle anything else", function() {
+			expect(nullify(123)).toBe(123);
+		});
+
+		it("should handle anything else", function() {
+			expect(nullify("123")).toBe("123");
+		});
+
+	});
+});
