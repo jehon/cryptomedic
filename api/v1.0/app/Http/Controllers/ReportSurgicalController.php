43ab2d51d92891bc534384c2c1b93b47ac304716
Jean Honlet <jehon@users.noreply.github.com>
2015-12-31 22:00:34 +0100

    Change align

--- api/v1.0/app/Http/Controllers/ReportSurgicalController.php
+++ api/v1.0/app/Http/Controllers/ReportSurgicalController.php
@@ -10,68 +10,68 @@ use App\Bill;
 use \References;
 
 class ReportSurgicalController extends ReportController {
-	public function index($when) {
-		$this->getReportParams("when", $when);
+  public function index($when) {
+    $this->getReportParams("when", $when);
 
-		$this->result['list'] = DB::select("SELECT
-				bills.id as bid,
-				patients.id as pid,
-				bills.Date as Date,
-				CONCAT(patients.entryyear, '-', patients.entryorder) as patient_reference,
-				patients.Name as patient_name,
-				patients.yearofbirth,
-				patients.Sex,
-				bills.sl_familySalary,
-				bills.sl_numberOfHouseholdMembers,
-				bills.Sociallevel,
-				patients.Pathology,
-				" . Bill::getSQLAct() . " as act,
-				" . Bill::getSQLTreatment() . " as treatment,
-				" . Bill::getSQLFieldsSum(Bill::CAT_CONSULT) . " AS price_consult,
-	            " . Bill::getSQLFieldsSum(Bill::CAT_MEDECINE) . " AS price_medecine,
-				" . Bill::getSQLFieldsSum(Bill::CAT_WORKSHOP) . " AS price_workshop,
-	            " . Bill::getSQLFieldsSum(Bill::CAT_SURGICAL) . " AS price_surgical,
-	            " . Bill::getSQLFieldsSum(Bill::CAT_OTHER) . " AS price_other,
-	            bills.total_real as total_real,
-	            bills.total_asked as total_asked,
-	            bills.total_paid as total_paid,
-				exists(select * from bills as b2 where b2.patient_id = bills.patient_id and b2.Date < :whenFrom12) as oldPatient,
-				TConsults.Date AS last_seen,
-				TConsults.TreatmentEvaluation AS last_treat_result,
-				TConsults.TreatmentFinished AS last_treat_finished
-			FROM bills
+    $this->result['list'] = DB::select("SELECT
+        bills.id as bid,
+        patients.id as pid,
+        bills.Date as Date,
+        CONCAT(patients.entryyear, '-', patients.entryorder) as patient_reference,
+        patients.Name as patient_name,
+        patients.yearofbirth,
+        patients.Sex,
+        bills.sl_familySalary,
+        bills.sl_numberOfHouseholdMembers,
+        bills.Sociallevel,
+        patients.Pathology,
+        " . Bill::getSQLAct() . " as act,
+        " . Bill::getSQLTreatment() . " as treatment,
+        " . Bill::getSQLFieldsSum(Bill::CAT_CONSULT) . " AS price_consult,
+              " . Bill::getSQLFieldsSum(Bill::CAT_MEDECINE) . " AS price_medecine,
+        " . Bill::getSQLFieldsSum(Bill::CAT_WORKSHOP) . " AS price_workshop,
+              " . Bill::getSQLFieldsSum(Bill::CAT_SURGICAL) . " AS price_surgical,
+              " . Bill::getSQLFieldsSum(Bill::CAT_OTHER) . " AS price_other,
+              bills.total_real as total_real,
+              bills.total_asked as total_asked,
+              bills.total_paid as total_paid,
+        exists(select * from bills as b2 where b2.patient_id = bills.patient_id and b2.Date < :whenFrom12) as oldPatient,
+        TConsults.Date AS last_seen,
+        TConsults.TreatmentEvaluation AS last_treat_result,
+        TConsults.TreatmentFinished AS last_treat_finished
+      FROM bills
           JOIN patients ON bills.patient_id = patients.id
           JOIN prices ON bills.price_id = prices.id
-					JOIN (
-						(
-							(SELECT id, patient_id, Date, TreatmentEvaluation, TreatmentFinished FROM ricket_consults)
-							UNION
-							(SELECT id, patient_id, Date, TreatmentEvaluation, TreatmentFinished FROM club_foots)
-							UNION
-							(SELECT id, patient_id, Date, TreatmentEvaluation, TreatmentFinished FROM other_consults)
-						) ORDER BY Date DESC LIMIT 1
-					) AS TConsults ON (TConsults.patient_id = patients.id)
+          JOIN (
+            (
+              (SELECT id, patient_id, Date, TreatmentEvaluation, TreatmentFinished FROM ricket_consults)
+              UNION
+              (SELECT id, patient_id, Date, TreatmentEvaluation, TreatmentFinished FROM club_foots)
+              UNION
+              (SELECT id, patient_id, Date, TreatmentEvaluation, TreatmentFinished FROM other_consults)
+            ) ORDER BY Date DESC LIMIT 1
+          ) AS TConsults ON (TConsults.patient_id = patients.id)
       WHERE (1 = 1)
-				AND " . $this->getReportParamFilter("when", "bills.Date") . "
-				AND " . Bill::getSQLFieldsSum(Bill::CAT_SURGICAL) . " > 0
-		  ORDER BY bills.id",
-			// ORDER BY bills.Date ASC, patients.entryyear ASC, patients.entryorder ASC, bills.id ASC
-			$this->sqlBindParams + [ "whenFrom12" => $this->internalWhenFrom ]
+        AND " . $this->getReportParamFilter("when", "bills.Date") . "
+        AND " . Bill::getSQLFieldsSum(Bill::CAT_SURGICAL) . " > 0
+      ORDER BY bills.id",
+      // ORDER BY bills.Date ASC, patients.entryyear ASC, patients.entryorder ASC, bills.id ASC
+      $this->sqlBindParams + [ "whenFrom12" => $this->internalWhenFrom ]
 
-				// (select consults.TreatmentEvaluation from consults where consults.patient_id = bills.patient_id ORDER BY consults.Date DESC LIMIT 1) as last_treat_result,
-				// (select consults.TreatmentFinished   from consults where consults.patient_id = bills.patient_id ORDER BY consults.Date DESC LIMIT 1) as last_treat_ended
+        // (select consults.TreatmentEvaluation from consults where consults.patient_id = bills.patient_id ORDER BY consults.Date DESC LIMIT 1) as last_treat_result,
+        // (select consults.TreatmentFinished   from consults where consults.patient_id = bills.patient_id ORDER BY consults.Date DESC LIMIT 1) as last_treat_ended
 
-		);
+    );
 
-		$this->result['totals'] = array();
-		foreach($this->result['list'] as $e) {
-			foreach($e as $k => $v) {
-				if (!array_key_exists($k, $this->result['totals'])) {
-					$this->result['totals'][$k] = 0;
-				}
-				$this->result['totals'][$k] += $v;
-			}
-		}
-		return response()->jsonOrJSONP($this->result);
-	}
+    $this->result['totals'] = array();
+    foreach($this->result['list'] as $e) {
+      foreach($e as $k => $v) {
+        if (!array_key_exists($k, $this->result['totals'])) {
+          $this->result['totals'][$k] = 0;
+        }
+        $this->result['totals'][$k] += $v;
+      }
+    }
+    return response()->jsonOrJSONP($this->result);
+  }
 }
