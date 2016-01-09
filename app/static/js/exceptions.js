6709edf0e9f3a254a82cbe2ad040bae978e44c5a
Jean Honlet <marielineet.jean@gmail.com>
2016-01-09 21:34:31 +0100

    Concentrate exceptions into application.js

--- app/static/js/exceptions.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-
-function DataMissingException(data) {
-    this.message = "Missing "  + (data || "some data");
-    this.data = data;
-}
-DataMissingException.prototype = new ApplicationException();
