97974242a07c19d8c008afad2a8d9b3ae0d8f3c3
Jean Honlet <jehon@users.noreply.github.com>
2016-01-09 21:35:44 +0100

    Merge cryptomedic.js into application.js

--- app/static/js/application.js
+++ app/static/js/application.js
@@ -475,6 +475,40 @@ mainApp.controller('ctrl', [ '$scope', '$location', '$sce', function($scope, $lo
 
   $scope.doCheckLogin();
 }]);
+
+
+
+var cryptomedic = {};
+{
+    var path = location.pathname.split("/");
+    cryptomedic.flavor = "/" + path[1];
+}
+cryptomedic.templateRoot = cryptomedic.flavor + "/cache/templates";
+cryptomedic.settings = {};
+
+mainApp.config([ '$routeProvider', function($routeProvider) {
+    $routeProvider
+    .when('/home', {
+        templateUrl: cryptomedic.templateRoot + '/pages/home.html',
+        controller: 'ctrl_home'
+    }).when('/login', {
+        templateUrl: cryptomedic.templateRoot + '/pages/login.html',
+        controller: 'ctrl_login',
+    }).when('/folder/:patient_id/:page?/:subtype?/:subid?/:mode?', {
+        templateUrl: cryptomedic.templateRoot + '/pages/folder.php',
+        controller: 'ctrl_folder',
+    }).when('/search', {
+        templateUrl: cryptomedic.templateRoot + '/pages/search.php',
+        controller: 'ctrl_search',
+    }).when('/reports/:report?', {
+        templateUrl: cryptomedic.templateRoot + '/pages/reports.php',
+        controller: 'ctrl_reports',
+    }).when('/users', {
+        templateUrl: cryptomedic.templateRoot + '/pages/users.html',
+        controller: 'ctrl_users',
+    }).otherwise({ 'redirectTo': '/home'});
+}]);
+
 function DataMissingException(data) {
     this.message = "Missing "  + (data || "some data");
     this.data = data;
