e65ad5bdd8df7d44f57da0175a13386876a15ddd
Jean Honlet <marielineet.jean@gmail.com>
2016-01-10 12:02:19 +0100

    Revert "Use sexStr in calculations"
    This reverts commit 51e207886fe46fcfb67cfcd0215529ae5de14900.

--- app/static/js/ctrl_graphic.js
+++ app/static/js/ctrl_graphic.js
@@ -10,7 +10,7 @@ mainApp.controller('ctrl_graphic', [ '$scope', '$element', function($scope, $ele
 
   $scope.getImageName = function() {
     if (x == null) return "";
-    if (typeof(calculations.sexStr($scope.folder.getMainFile())) == "undefined") return "";
+    if (typeof($scope.folder.getMainFile().sexStr) == "undefined") return "";
     var name = "";
     if (x == "ageAtConsultTime") {
       if (y == "Heightcm") name += "height";
@@ -19,7 +19,7 @@ mainApp.controller('ctrl_graphic', [ '$scope', '$element', function($scope, $ele
     } else {
      name += "wh";
     }
-    name += "-" + calculations.sexStr($scope.folder.getMainFile());
+    name += "-" + $scope.folder.getMainFile().sexStr();
     return name;
   }
 
@@ -29,7 +29,7 @@ mainApp.controller('ctrl_graphic', [ '$scope', '$element', function($scope, $ele
   }
 
   var imgDimension = function(what) {
-    return amd_stats.dimensions[x + "_" + y + "_" + calculations.sexStr($scope.folder.getMainFile())][what];
+    return amd_stats.dimensions[x + "_" + y + "_" + $scope.folder.getMainFile().sexStr()][what];
   }
 
   $scope.getValidity = function($index) {
