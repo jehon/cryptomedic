dc1d241a9f838483e9cf2cea710a20686f8f3bd7
Jean Honlet <jehon@users.noreply.github.com>
2016-01-18 20:49:35 +0100

    Fix default values in creating the bill

--- app/static/js/ctrl_folder.js
+++ app/static/js/ctrl_folder.js
@@ -46,7 +46,7 @@ mainApp.controller("ctrl_folder", [ "$scope", "$location", "$routeParams" , func
 
     if ($scope.page == "file") {
       if ($scope.mode == "add") {
-        cachedCurrentFile = appState().helpers.create($scope.subtype, {}, $scope.folder);
+        cachedCurrentFile = appState().helpers.create($scope.subtype, null, $scope.folder);
         cachedCurrentFile.patient_id = $scope.patient_id;
       } else {
         for(var i in $scope.folder.getSubFiles()) {
