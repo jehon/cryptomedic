77df2d855c208c7e3c4a7abe8e527e98415fc769
Jean Honlet <jehon@users.noreply.github.com>
2016-01-21 06:45:19 +0100

    Clean up
    And allow the script to run from any place.

--- bin/prj-rebuild-db.sh
+++ bin/prj-rebuild-db.sh
@@ -1,33 +1,4 @@
 
-php maintenance/patch_db.php
-
-exit 0
-
-php <<-PHP
-  <?php
-
-  // Set up the database
-
-  // require_once(__DIR__ . "/bootstrap/autoload.php");
-
-  require_once(__DIR__ . "/vendor/autoload.php");
-
-  global \$myconfig;
-  require_once(__DIR__ . "/config.php");
-
-  \Jehon\Maintenance\Database::run(
-      [
-          __DIR__ . "/conf/database_scripts/dev_only/reset.sql",
-          __DIR__ . "/conf/database_scripts/dev_only/testing.sql",
-          __DIR__ . "/conf/database_scripts",
-          __DIR__ . "/conf/database_scripts/dev_only"
-      ],
-      \$myconfig['database']['database'],
-      \$myconfig['database']['username'],
-      \$myconfig['database']['password'],
-      \$myconfig['database']['options']
-  );
-
-PHP
-
-#php api/v1.0/tests/bootstrap.php
+SCRIPTDIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
+echo "Script dir: $SCRIPTDIR"
+php "$SCRIPTDIR/../maintenance/patch_db.php"
