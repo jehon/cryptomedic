9a42b83b48500a9a6da1ca53e1ad1fac86b1cd88
Jean Honlet <jehon@users.noreply.github.com>
2016-01-22 21:57:34 +0100

    Build up a webapp
    But not tested if it works or not...

--- bin/package.js
+++ bin/package.js
@@ -5,16 +5,54 @@
 // https://www.npmjs.com/package/jszip
 
 var fs = require('fs');
-
-
 var archiver = require('archiver');
-var archive = archiver.create('zip', {});
-var output = fs.createWriteStream(__dirname + '/../package.zip');
+require('es6-object-assign').polyfill();
 
-archive.on('error', function(err){
-  throw err;
-});
 
-archive.pipe(output);
-archive.directory(__dirname + '/../build/', '/');
-archive.finalize();
+function create(path, manifest) {
+  var archive = archiver.create('zip', {});
+  var output = fs.createWriteStream(path +  'package.zip');
+
+  archive.on('error', function(err){
+    throw err;
+  });
+
+  archive.pipe(output);
+  archive.directory(__dirname + '/../build/', '/build/');
+
+  archive.append(JSON.stringify(manifest), { name: 'manifest.webapp'});
+  archive.finalize();
+
+  var minimanifest = {
+    name          : manifest.name,
+    package_path  : manifest.package_path,
+    developer     : manifest.developer
+  };
+  fs.writeFileSync(path + 'manifest.webapp', JSON.stringify(minimanifest));
+}
+
+
+var manifest = {
+  name           : 'Cryptomedic',
+  description    : 'Cryptomedic patient management',
+  launch_path    : '/build/index.html',
+  icons          : {
+    '512'        : '/icon-512.png',
+    '128'        : '/icon-128.png'
+  },
+  package_path   : 'http://www.cryptomedic.org/cryptomedic/package.zip',
+  developer      : {
+    name         : 'Jean Honlet',
+    url          : 'https://github.com/jehon/'
+  },
+  default_locale : 'en',
+  chrome         : { 'navigation'                    : true }
+};
+
+create(__dirname + '/../', manifest);
+create(__dirname + '/../tmp/', Object.assign({},
+  manifest,
+  {
+    package_path: 'http://localhost/cryptomedic/tmp/package.zip'
+  })
+);
