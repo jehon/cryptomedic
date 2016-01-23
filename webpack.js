6be8f80abcca860e6658fb76cd8d750c6882aae1
Jean Honlet <marielineet.jean@gmail.com>
2016-01-23 20:23:25 +0100

    Update build

--- webpack.js
+++ webpack.js
@@ -1,13 +1,13 @@
-"use strict";
+'use strict';
 
-require("es6-promise").polyfill();
+require('es6-promise').polyfill();
 
-var path = require("path");
-var webpack = require("webpack");
-var glob = require("glob");
-var HtmlWebpackPlugin = require("html-webpack-plugin");
-var scriptLoader = require("script-loader");
-var recursiveReadSync = require("recursive-readdir-sync");
+var path = require('path');
+var webpack = require('webpack');
+var glob = require('glob');
+var HtmlWebpackPlugin = require('html-webpack-plugin');
+var scriptLoader = require('script-loader');
+var recursiveReadSync = require('recursive-readdir-sync');
 
 // https://github.com/petehunt/webpack-howto
 
@@ -19,59 +19,55 @@ var recursiveReadSync = require("recursive-readdir-sync");
 // @see https://github.com/webpack/webpack-with-common-libs/blob/master/webpack.config.js
 // @see http://mts.io/2015/04/08/webpack-shims-polyfills/
 
-// test: jQuery("#busy").modal("show"); jQuery("#busy").datepicker()
+// test: jQuery('#busy').modal('show'); jQuery('#busy').datepicker()
 
 var config = {
-  entry: []
+  entry: [ ]
     .concat([
-      //'./app/bower_components/jquery-ui/themes/ui-lightness/jquery-ui.min.css',
-      //'./app/bower_components/bootstrap/dist/css/bootstrap.min.css'
-      "./node_modules/bootstrap/dist/css/bootstrap.css",
-      "./node_modules/jquery-ui/themes/ui-lightness/jquery-ui.css"
-
+      './node_modules/bootstrap/dist/css/bootstrap.css',
+      './node_modules/jquery-ui/themes/ui-lightness/jquery-ui.css'
     ])
-    .concat(glob.sync("./app/static/css/*.css"))
+    .concat(glob.sync('./app/css/*.css'))
     .concat([
-      "expose?jQuery!./node_modules/jquery/dist/jquery.js",
-      "./node_modules/jquery-ui/datepicker.js",
-      "./node_modules/angular/angular.js",
-      "./node_modules/angular-route/angular-route.min.js",
-      "./node_modules/whatwg-fetch/fetch.js",
+      'expose?jQuery!./node_modules/jquery/dist/jquery.js',
+      './node_modules/jquery-ui/datepicker.js',
+      './node_modules/angular/angular.js',
+      './node_modules/angular-route/angular-route.min.js',
+      './node_modules/whatwg-fetch/fetch.js',
+      './node_modules/html2canvas/dist/html2canvas.js',
 
-      "script!./node_modules/excellentexport/excellentexport.min.js",
-      "script!./node_modules/dexie/dist/latest/Dexie.min.js",
-      "./node_modules/bootstrap/dist/js/bootstrap.min.js",
+      'script!./node_modules/excellentexport/excellentexport.min.js',
+      'script!./node_modules/dexie/dist/latest/Dexie.min.js',
+      './node_modules/bootstrap/dist/js/bootstrap.min.js',
 
-      // './app/static/js/bugreporting.js',
-      // './app/static/js/html2canvas.js',
+      'script!./app/static/js/bugreporting.js',
 
-      // './app/static/js/application.js',
-      // './app/static/js/database.js',
-      // './app/static/js/cryptomedic.js',
-      // './app/static/worker/worker.js',
+      // 'script!./app/static/js/application.js',
+      // 'script!./app/static/js/database.js',
+      // 'script!./app/static/js/cryptomedic.js',
     ])
-    // .concat(glob.sync('./app/static/js/service_*.js'))
-    // .concat(glob.sync('./app/static/js/ctrl_*.js'))
+    // .concat(glob.sync('script!./app/static/js/service_*.js'))
+    // .concat(glob.sync('script!./app/static/js/ctrl_*.js'))
 
     // Last one, since it will define what is exported:
-    .concat([ "./app/status.js" ])
+    .concat([ './app/status.js' ])
     ,
   resolve: {
-    extensions: ["", ".js", ".jsx"],
-    modulesDirectories: [ "node_modules", "app" ],
+    extensions: ['', '.js', '.jsx'],
+    modulesDirectories: [ 'node_modules', 'app' ],
     alias: {
-      "jquery": "jquery"
+      'jquery': 'jquery'
     }
   },
   output: {
-    path: path.join(__dirname, "build/"),
+    path: path.join(__dirname, 'build/'),
     // filename: 'bundle.js',
-    filename: "bundle-[hash].js",
-    chunkFilename: "[id].[hash].bundle.js",
+    filename: 'bundle-[hash].js',
+    chunkFilename: '[id].[hash].bundle.js',
 
     // @see https://webpack.github.io/docs/library-and-externals.html
-    libraryTarget: "var",
-    library: "appState"
+    libraryTarget: 'var',
+    library: 'appState'
   },
   module: {
     loaders: [
@@ -79,60 +75,57 @@ var config = {
         test: /\.jsx?$/,
         exclude: /(node_modules)/,
         loaders: [
-          "babel-loader?sourceMaps=inline&optional=runtime"
+          'babel-loader?sourceMaps=inline&optional=runtime'
         ]
       },
       {
         test: /\.html$/,
         loaders: [
-          "ng-cache?-removeEmptyAttributes&prefix=" + path.dirname(__dirname) + ":/**"
+          'ng-cache?-removeEmptyAttributes&prefix=' + path.dirname(__dirname) + ':/**'
         ]
       },
       {
         test: /\.php$/,
         loaders: [
-        //module=app_main&
-          "ng-cache?-removeEmptyAttributes&prefix=" + path.dirname(__dirname) + ":/**",
-          // 'ng-cache?module=app_main-removeEmptyAttributes&prefix=' + __dirname + ':/' + path.basename(__dirname) + '/cache/**',
+          'ng-cache?-removeEmptyAttributes&prefix=' + path.dirname(__dirname) + ':/**',
           // 'html-minify',
-          "php-loader"
+          'php-loader'
         ]
       },
       {
-        // https://webpack.github.io/docs/stylesheets.html
         test: /\.css$/,
-        loader: "style-loader!css-loader"
+        loader: 'style-loader!css-loader'
       },
       {
-        test: /\.svg($|\?)|\.gif$|.png$/,
+        test: /\.svg($|\?)|\.jp(e)?g|\.gif$|.png$/,
         loaders: [
-          "file-loader?name=img/[name].[ext]"
+          'file-loader?name=img/[name].[ext]'
         ]
       },
       {
         test: /\.woff($|\?)|\.woff2($|\?)|\.ttf($|\?)|\.eot($|\?)/,
         loaders: [
-          "file-loader?name=fonts/[name].[ext]"
+          'file-loader?name=fonts/[name].[ext]'
         ]
       }
     ]
   },
   plugins: [
     new HtmlWebpackPlugin({
-      template: "app/index.html", // Load a custom template
+      template: 'app/index.html', // Load a custom template
       inject: false // Inject all scripts into the body
     }),
     new webpack.ProvidePlugin({
-      "angular": "angular"
+      'angular': 'angular'
     })
   ]
 };
 
-var templates = recursiveReadSync("./cache/templates");
+var templates = recursiveReadSync('./cache/templates');
 for(var i in templates) {
-  templates[i] = "./" + templates[i];
+  templates[i] = './' + templates[i];
 }
 config.entry = config.entry.concat(templates);
-config.entry = config.entry.concat([ "./app/status.js" ]);
+config.entry = config.entry.concat([ './app/status.js' ]);
 
 module.exports = config;
