5170af852f2616ad465795cedd6ccc84325c1cb2
Jean Honlet <jehon@users.noreply.github.com>
2016-01-26 20:56:19 +0100

    End trying to make mozilla web app

--- bin/package.js
+++ bin/package.js
@@ -9,7 +9,9 @@ var fse = require('fs-extra');
 var archiver = require('archiver');
 require('es6-object-assign').polyfill();
 
-function create(path, manifest) {
+var version = (new Date()).getTime();
+
+function create(path, flavor, manifest) {
   /**
    * Function to create a package based on 'build' directory and a given @param manifest
    * into @param path
@@ -20,18 +22,21 @@ function create(path, manifest) {
     throw err;
   });
 
-  archive.directory(__dirname + '/../build/', '/');
-  archive.pipe(fs.createWriteStream(path +  'package.zip'));
+  archive.directory(__dirname + '/../build/', '/build/');
+  archive.pipe(fs.createWriteStream(path +  'package' + flavor + '.zip'));
 
+  archive.file(__dirname + '/../index.html', { name: '/index.html' });
   archive.append(JSON.stringify(manifest), { name: 'manifest.webapp' });
   archive.finalize();
 
   var minimanifest = {
     name          : manifest.name,
     package_path  : manifest.package_path,
-    developer     : manifest.developer
+    developer     : manifest.developer,
+    version       : manifest.version,
+    icons         : manifest.icons
   };
-  fs.writeFileSync(path + 'manifest.webapp', JSON.stringify(minimanifest));
+  fs.writeFileSync(path + 'manifest' + flavor + '.webapp', JSON.stringify(minimanifest));
 }
 
 // Add the not integrated sources
@@ -47,7 +52,7 @@ var manifest = {
     '512'        : '/icon-512.png',
     '128'        : '/icon-128.png'
   },
-  package_path   : 'http://www.cryptomedic.org/cryptomedic/build/package.zip',
+  package_path   : 'http://www.cryptomedic.org/cryptomedic/package.zip',
   developer      : {
     name         : 'Jean Honlet',
     url          : 'https://github.com/jehon/'
@@ -55,17 +60,28 @@ var manifest = {
   default_locale : 'en',
   chrome         : {
     navigation   : true
-  }
+  },
+  version        : version
 };
 
+
+fse.removeSync(__dirname + '/../build/package.zip');
+fse.removeSync(__dirname + '/../build/manifest.webapp');
+fse.removeSync(__dirname + '/../tmp/package.zip');
+fse.removeSync(__dirname + '/../tmp/manifest.webapp');
+fse.removeSync(__dirname + '/../package.zip');
+fse.removeSync(__dirname + '/../manifest.webapp');
+fse.removeSync(__dirname + '/../package-dev.zip');
+fse.removeSync(__dirname + '/../manifest-dev.webapp');
+
 // Make the two instances
-create(__dirname + '/../tmp/', Object.assign({},
+create(__dirname + '/../', '-dev', Object.assign({},
   manifest,
   {
-    name           : 'DEV VERSION - ' + manifest.name + ' - DEV VERSION',
-    package_path   : 'http://localhost/cryptomedic/tmp/package.zip'
+    name           : manifest.name + ' - DEV',
+    package_path   : 'http://localhost/cryptomedic/package-dev.zip',
   })
 );
 // Make the "build" instance. Since it it stored in "build", it has to
 // be created after the tmp one...
-create(__dirname + '/../build/', manifest);
+create(__dirname + '/../', '', manifest);
