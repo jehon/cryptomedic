4cb67e1c02bda814d9fae02b3f37be57d16fe7df
Jean Honlet <marielineet.jean@gmail.com>
2016-01-28 07:15:57 +0100

    Make a try towards offline application
    This is not correctly working, but I make a remember point here.

--- app/status.js
+++ app/status.js
@@ -10,6 +10,14 @@ import objectify from 'helpers/objectify';
 import create from 'helpers/create';
 import date2CanonicString from 'helpers/date2CanonicString';
 
+var offline = require('offline-plugin/runtime');
+if (location.pathname.split('/')[2] == 'offline') {
+  console.log('SW-OFFLINE: Detecting offline mode, activating plugin');
+  offline.install(null,
+    function(data) { console.log('SW-OFFLINE activated:', data); },
+    function(data) { console.error('SW-OFFLINE problem:', data); }
+  );
+}
 
 export default function() {
   return {
