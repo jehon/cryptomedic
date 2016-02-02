762a1f24b472ca35c80085b6862e24d28256c3ba
Jean Honlet <jehon@users.noreply.github.com>
2016-02-02 21:24:53 +0100

    Add controller for computers listing

---
+++ api/v1.0/app/Http/Controllers/ReportComputersController.php
@@ -0,0 +1,21 @@
+<?php namespace App\Http\Controllers;
+
+use App\Http\Controllers\Controller;
+use DB;
+use Illuminate\Http\Response;
+use Illuminate\Support\Facades\Request;
+use App\Bill;
+
+#require_once(__DIR__ . "/../../../../php/references.php");
+use \References;
+
+class ReportComputersController extends ReportController {
+  public function index() {
+    $this->result['list'] = DB::select("SELECT * FROM sync_computers WHERE
+      updated_at IS NOT NULL
+      AND updated_at < DATE_ADD(NOW(), INTERVAL - 6 MONTH)
+    ");
+
+    return response()->jsonOrJSONP($this->result);
+  }
+}
