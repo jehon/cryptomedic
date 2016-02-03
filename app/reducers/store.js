fd7f9879fe00d1b9898aa40fb382019ab7b6ee45
Jean Honlet <jehon@users.noreply.github.com>
2016-02-03 21:46:12 +0100

    Clean up

--- app/reducers/store.js
+++ app/reducers/store.js
@@ -1,9 +1,7 @@
 
-import { combineReducers, compose, createStore, applyMiddleware } from 'redux';
+import { combineReducers, compose, createStore } from 'redux';
 import { persistStore, autoRehydrate }                            from 'redux-persist';
 // import thunkMiddleware                                         from 'redux-thunk';
-// import createLogger                                            from 'redux-logger';
-// import { devTools, persistState }                              from 'redux-devtools';
 // import { DevTools, DebugPanel, LogMonitor }                    from 'redux-devtools/lib/react';
 
 import prefs                                                      from 'reducers/prefsReducers';
@@ -12,50 +10,9 @@ import state                                                      from 'reducers
 import database                                                   from 'reducers/databaseReducers';
 import log                                                        from 'reducers/logReducers';
 
-// Manage persistence
-
-// const loggerMiddleware = createLogger({
-//   level: 'info',
-//   collapsed: true,
-// });
-
 if (typeof(window.__karma__) == 'undefined') {
   // disable logger?
 }
-
-// let finalCreateStore = (autoRehydrate)compose(
-  //enables middleware:
-  // applyMiddleware(
-    // thunkMiddleware,
-    // loggerMiddleware
-  // )
-  // ,
-  // Provides support for DevTools:
-  // devTools(),
-  // Lets you write ?debug_session=<name> in address bar to persist sessions
-  // persistState(window.location.href.match(/[?&]debug_session=([^&]+)\b/))
-// )(createStore);
-
-// function configureStore(initialState) {
-//   const store = autoRehydrate()(finalCreateStore)(
-//       combineReducers({
-//         connection,
-//         state,
-//         database
-//       })
-//     );
-//   if (module.hot) {
-//     // Enable Webpack hot module replacement for reducers
-//     module.hot.accept('../reducers', () => {
-//       const nextRootReducer = require('../reducers');
-//       store.replaceReducer(nextRootReducer);
-//     });
-//   }
-//   return store;
-// }
-// const store = configureStore();
-
-
 
 let store = autoRehydrate()(createStore)
   (combineReducers({
