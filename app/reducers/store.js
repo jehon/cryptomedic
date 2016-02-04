e49ba73f929738730ea198b8ac0f890380fab6b2
Jean Honlet <marielineet.jean@gmail.com>
2016-02-04 21:32:31 +0100

    Put log reducer into main

--- app/reducers/store.js
+++ app/reducers/store.js
@@ -1,28 +1,41 @@
 
 import { combineReducers, compose, createStore } from 'redux';
-import { persistStore, autoRehydrate }                            from 'redux-persist';
 // import thunkMiddleware                                         from 'redux-thunk';
 // import { DevTools, DebugPanel, LogMonitor }                    from 'redux-devtools/lib/react';
 
 import prefs                                                      from 'reducers/prefsReducers';
 import connection                                                 from 'reducers/connectionReducers';
-import state                                                      from 'reducers/stateReducers';
+import stateReducers                                              from 'reducers/stateReducers';
 import database                                                   from 'reducers/databaseReducers';
-import log                                                        from 'reducers/logReducers';
+import catalog                                                    from 'reducers/catalog';
 
-if (typeof(window.__karma__) == 'undefined') {
   // disable logger?
+function transition_log(state, action) {
+  if (typeof(window.__karma__) == 'undefined') {
+    console.log('Action: ', action.type, ' with ', action.payload);
+  }
+  return state;
 }
 
-let store = autoRehydrate()(createStore)
-  (combineReducers({
-    prefs,
-    connection,
-    state,
-    database,
-    log
-  }));
+let store = createStore(
+  function(state, action) {
+    // Log
+    state = transition_log(state, action);
 
-persistStore(store, { whitelist: [ 'prefs' ] });
+    // Work the state
+    state = combineReducers({
+      prefs,
+      connection,
+      state: stateReducers,
+      database,
+    })(state, action);
+    return state;
+  }
+);
+
+if (sessionStorage.cryptomedicPrefs) {
+  console.log('reload with ', JSON.parse(sessionStorage.cryptomedicPrefs));
+  store.dispatch({ type: catalog.PREFS_REHYDRATE, payload: JSON.parse(sessionStorage.cryptomedicPrefs) });
+}
 
 export default store;
--- tests/20_js/test_jasmine.js
+++ tests/20_js/test_jasmine.js
@@ -1,22 +1,22 @@
-"use strict";
+'use strict';
 
-describe("Jasmine", function() {
-	  it("should work as \"expect\"", function() {
-		  var a = true;
-		  expect(a).toBe(true);
-		  expect(a).toEqual(true);
-	});
+describe('Jasmine', function() {
+  it('should work as \'expect\'', function() {
+    var a = true;
+    expect(a).toBe(true);
+    expect(a).toEqual(true);
+  });
 
-	// it('prints jasmine version', function() {
-	// 	expect(jasmine.version).toMatch("2.4.1");
-	// });
+  // it('prints jasmine version', function() {
+  //  expect(jasmine.version).toMatch('2.4.1');
+  // });
 
-	  it("manage exceptions", function() {
-		  expect(function() { throw "test"; }).toThrow();
-		  expect(function() { throw "test"; }).toThrow("test");
-		  expect(function() { throw new Error("test"); }).toThrow();
-		  expect(function() { throw new Error("test"); }).toThrow(new Error("test"));
-		  expect(function() { throw new DataMissingException("test"); }).toThrow();
-		  expect(function() { throw new DataMissingException("test"); }).toThrow(new DataMissingException("test"));
-	});
+  it('manage exceptions', function() {
+    expect(function() { throw 'test'; }).toThrow();
+    expect(function() { throw 'test'; }).toThrow('test');
+    expect(function() { throw new Error('test'); }).toThrow();
+    expect(function() { throw new Error('test'); }).toThrow(new Error('test'));
+    expect(function() { throw new DataMissingException('test'); }).toThrow();
+    expect(function() { throw new DataMissingException('test'); }).toThrow(new DataMissingException('test'));
+  });
 });
