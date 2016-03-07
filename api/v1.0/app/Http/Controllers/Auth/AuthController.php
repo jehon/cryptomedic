4325f100e5ba21a71393cec38da677496f9b35de
Jean Honlet <marielineet.jean@gmail.com>
2016-03-07 09:46:49 +0100

    Format

--- api/v1.0/app/Http/Controllers/Auth/AuthController.php
+++ api/v1.0/app/Http/Controllers/Auth/AuthController.php
@@ -16,20 +16,20 @@ use \References;
 
 class AuthController extends Controller {
 
-	/*
-	|--------------------------------------------------------------------------
-	| Registration & Login Controller
-	|--------------------------------------------------------------------------
-	|
-	| This controller handles the registration of new users, as well as the
-	| authentication of existing users. By default, this controller uses
-	| a simple trait to add these behaviors. Why don't you explore it?
-	|
-	*/
+  /*
+  |--------------------------------------------------------------------------
+  | Registration & Login Controller
+  |--------------------------------------------------------------------------
+  |
+  | This controller handles the registration of new users, as well as the
+  | authentication of existing users. By default, this controller uses
+  | a simple trait to add these behaviors. Why don't you explore it?
+  |
+  */
 
-	use AuthenticatesAndRegistersUsers;
+  use AuthenticatesAndRegistersUsers;
 
-	static protected $permissions = [];
+  static protected $permissions = [];
 
   static public function hasPermission($header) {
       $user = Auth::user();
@@ -61,93 +61,93 @@ class AuthController extends Controller {
       self::$permissions[$profile][$header] = $value;
   }
 
-	/**
-	 * Create a new authentication controller instance.
-	 *
-	 * @param  \Illuminate\Contracts\Auth\Guard  $auth
-	 * @param  \Illuminate\Contracts\Auth\Registrar  $registrar
-	 * @return void
-	 */
-	// public function __construct()
-	// {
-	// 	// If activating this, you will be redirected to home on new login attempt
-	// 	// $this->middleware('guest', ['except' => 'getLogout']);
-	// }
+  /**
+   * Create a new authentication controller instance.
+   *
+   * @param  \Illuminate\Contracts\Auth\Guard  $auth
+   * @param  \Illuminate\Contracts\Auth\Registrar  $registrar
+   * @return void
+   */
+  // public function __construct()
+  // {
+  //  // If activating this, you will be redirected to home on new login attempt
+  //  // $this->middleware('guest', ['except' => 'getLogout']);
+  // }
 
-	public function getSettings() {
+  public function getSettings() {
     // sleep(3);
-		if (!Auth::user()) {
-			abort(401);
-		}
-		$data = array();
-		$data['username'] = Auth::user()->username;
-		$data['group'] = Auth::user()->group;
-		$data['name'] = Auth::user()->name;
+    if (!Auth::user()) {
+      abort(401);
+    }
+    $data = array();
+    $data['username'] = Auth::user()->username;
+    $data['group'] = Auth::user()->group;
+    $data['name'] = Auth::user()->name;
 
-		$listing = DB::table('prices')->orderBy('id', 'ASC')->get();
-		$data['prices'] = array();
-		foreach($listing as $v) {
-			$data['prices'][$v->id] = $v;
-		}
+    $listing = DB::table('prices')->orderBy('id', 'ASC')->get();
+    $data['prices'] = array();
+    foreach($listing as $v) {
+      $data['prices'][$v->id] = $v;
+    }
 
-		$data['codes'] = References::$codes;
+    $data['codes'] = References::$codes;
     $data['associations'] = References::$associations;
-		$data['authorized'] = self::$permissions[$data['group']];
+    $data['authorized'] = self::$permissions[$data['group']];
 
-		// Update last_login timestamp
-		$user = Auth::user();
-		$user->last_login = new \DateTime();
-		$user->save();
+    // Update last_login timestamp
+    $user = Auth::user();
+    $user->last_login = new \DateTime();
+    $user->save();
 
-		if (Request::input("computerId", false)) {
-			// Record the computer Id into database and session
-			$computerId = Request::input("computerId");
-			$computer = SyncComputer::firstOrNew([ "computer_id" => $computerId ]);
-			$computer->useragent = $_SERVER['HTTP_USER_AGENT'];
-			$computer->cryptomedic_version = Request::input("appVersion", "");
-			if (strpos($computer->user_list, Auth::user()->username) === false) {
-				$computer->user_list .= ',' . Auth::user()->username;
-			}
-			$computer->save();
-			session()->put('computerId', $computerId);
-		}
+    if (Request::input("computerId", false)) {
+      // Record the computer Id into database and session
+      $computerId = Request::input("computerId");
+      $computer = SyncComputer::firstOrNew([ "computer_id" => $computerId ]);
+      $computer->useragent = $_SERVER['HTTP_USER_AGENT'];
+      $computer->cryptomedic_version = Request::input("appVersion", "");
+      if (strpos($computer->user_list, Auth::user()->username) === false) {
+        $computer->user_list .= ',' . Auth::user()->username;
+      }
+      $computer->save();
+      session()->put('computerId', $computerId);
+    }
 
-		/*
-		 * TODO: Define a security key
-		 *
-		 * - The security key should be unique by [ computerId ]
-		 * - How and when should we deprecate a key?
-		 * 		- two keys: old and new -> when we receive data signed with "new" key, old is deprecated
-		 * - What to sign, and how to sign it?
-		 * 		- date of modification
-		 *      - type of modification
-		 *      - user who made it (for security checks)
-		 *      - data
-		 *      - folderId (for tracking)
-		 */
+    /*
+     * TODO: Define a security key
+     *
+     * - The security key should be unique by [ computerId ]
+     * - How and when should we deprecate a key?
+     *    - two keys: old and new -> when we receive data signed with "new" key, old is deprecated
+     * - What to sign, and how to sign it?
+     *    - date of modification
+     *      - type of modification
+     *      - user who made it (for security checks)
+     *      - data
+     *      - folderId (for tracking)
+     */
 
-		return response()->jsonOrJSONP($data);
-	}
+    return response()->jsonOrJSONP($data);
+  }
 
-	public function postMylogin() {
-		$credentials = Request::only('username', 'password');
-		if (\getenv('BYPASS_AUTHENTICATION')) {
-			$user = User::where("username", $credentials['username'])->first();
-			Auth::login($user);
-			return $this->getSettings();
-		}
+  public function postMylogin() {
+    $credentials = Request::only('username', 'password');
+    if (\getenv('BYPASS_AUTHENTICATION')) {
+      $user = User::where("username", $credentials['username'])->first();
+      Auth::login($user);
+      return $this->getSettings();
+    }
 
-		if (Auth::attempt($credentials))
-		{
-			return $this->getSettings();
-		}
-		return abort(406, "Invalid credentials");
-	}
+    if (Auth::attempt($credentials))
+    {
+      return $this->getSettings();
+    }
+    return abort(406, "Invalid credentials");
+  }
 
-	public function getLogout() {
-		Auth::logout();
-		return response()->jsonOrJSONP(null);
-	}
+  public function getLogout() {
+    Auth::logout();
+    return response()->jsonOrJSONP(null);
+  }
 
   public function matrix() {
       $profiles = array_keys(self::$permissions);
@@ -230,8 +230,8 @@ class Role {
 
   (new Role("readonly"))
     ->givePermission("folder.read")
-  	->givePermission("reports.execute")
-  	;
+    ->givePermission("reports.execute")
+    ;
 
   (new Role("cdc", "readonly"))
     ->givePermission("folder.edit")
@@ -245,12 +245,12 @@ class Role {
     ;
 
   (new Role("manager", "cdc"))
-  	->givePermission("folder.unlock")
-  	->givePermission("users.manage")
-  	;
+    ->givePermission("folder.unlock")
+    ->givePermission("users.manage")
+    ;
 
   (new Role("admin", "manager"))
     ->givePermission("admin.securityMatrix")
     ->givePermission("admin.computers")
-  	;
+    ;
 }
