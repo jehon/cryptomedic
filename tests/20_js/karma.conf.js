bbfe6a6fd249f60f8c68da9c187440a112514013
Jean Honlet <jehon@users.noreply.github.com>
2016-03-08 18:00:00 +0100

    format

--- tests/20_js/karma.conf.js
+++ tests/20_js/karma.conf.js
@@ -1,61 +1,61 @@
 module.exports = function(config) {
   var configuration = {
     plugins : [
-      "karma-chrome-launcher",
-      "karma-firefox-launcher",
-      "karma-jasmine",
-      "karma-junit-reporter",
-      "karma-coverage"
+      'karma-chrome-launcher',
+      'karma-firefox-launcher',
+      'karma-jasmine',
+      'karma-junit-reporter',
+      'karma-coverage'
       // 'karma-html-reporter'
     ],
     reporters : [
-      "progress",
-      "coverage"
+      'progress',
+      'coverage'
       // 'html'
     ],
 
-    basePath : "../../",
+    basePath : '../../',
 
     files : [
-      "tests/20_js/kickoff.js",
+      'tests/20_js/kickoff.js',
       // Work only if one bundle is present:
-      "build/bundle-*.js",
-      "app/static/js/application.js",
-      "app/static/js/calculations.js",
-      "app/static/js/database.js",
-      "app/static/js/myfetch.js",
-      "app/static/js/service*.js",
-      "tests/20_js/**/*.js",
-      { pattern : "tests/20_js/mocks/*.json", watched : true, served : true, included : false }
+      'build/bundle-*.js',
+      'app/static/js/application.js',
+      'app/static/js/calculations.js',
+      'app/static/js/database.js',
+      // 'app/static/js/myfetch.js',
+      'app/static/js/service*.js',
+      'tests/20_js/**/*.js',
+      { pattern : 'tests/20_js/mocks/*.json', watched : true, served : true, included : false }
     ],
 
     autoWatch : true,
 
-    frameworks : [ "jasmine" ],
+    frameworks : [ 'jasmine' ],
 
-//    browsers: [ 'Chrome', "Firefox" ],
-    browsers: [ "Firefox" ],
+//    browsers: [ 'Chrome', 'Firefox' ],
+    browsers: [ 'Firefox' ],
 
     customLaunchers: {
       // http://stackoverflow.com/questions/19255976/how-to-make-travis-execute-angular-tests-on-chrome-please-set-env-variable-chr
       Chrome_travis_ci: {
-        base: "Chrome",
-        flags: ["--no-sandbox"]
+        base: 'Chrome',
+        flags: ['--no-sandbox']
       }
     },
 
     junitReporter : {
-      outputFile : "tmp/js/unit.xml",
-      suite : "unit"
+      outputFile : 'tmp/js/unit.xml',
+      suite : 'unit'
     },
 
     preprocessors: {
-      "app/js/*.js": [ "coverage" ]
+      'app/js/*.js': [ 'coverage' ]
     },
 
     coverageReporter: {
-      type : "html",
-      dir : "tmp/js/"
+      type : 'html',
+      dir : 'tmp/js/'
     }
 
     // htmlReporter: {
@@ -66,9 +66,9 @@ module.exports = function(config) {
 
   if(process.env.TRAVIS){
     // configuration.browsers = [ 'Firefox', 'Chrome_travis_ci' ];
-    configuration.browsers = [ "Firefox" ];
+    configuration.browsers = [ 'Firefox' ];
   } else {
-    configuration.browsers = [ "Firefox", "Chrome" ];
+    configuration.browsers = [ 'Firefox', 'Chrome' ];
   }
 
   config.set(configuration);
