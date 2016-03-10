db558493750e3c3311527f118d64e049a4fe665d
Jean Honlet <marielineet.jean@gmail.com>
2016-03-10 09:34:31 +0100

    Use imports

--- app/actions/selectFile.js
+++ app/actions/selectFile.js
@@ -1,15 +1,17 @@
 
 import myFetch from 'helpers/myFetch';
+import objectify from 'helpers/objectify';
+import create from 'helpers/create';
 
 export default function selectFile(id, type = 'folder') {
   // If not final then go to the server anyway...
   // return db.getFolder(id).catch(function(error) {
   //   console.log('Getting the folder live: #' + id);
   return myFetch(type + '/' + id)
-    .then(appState().helpers.objectify)
+    .then(objectify)
 
     // TODO: Generalize this:
-    .then(function(data) { return appState().helpers.create('Folder', data); })
+    .then(function(data) { return create('Folder', data); })
 
     .catch()
     ;
