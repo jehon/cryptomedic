37e76958185a8fecb1a1e6115eb0e93105dd39f6
Jean Honlet <marielineet.jean@gmail.com>
2016-03-11 08:54:00 +0100

    Format, start removing server.settings

--- app/helpers/date2CanonicString.js
+++ app/helpers/date2CanonicString.js
@@ -1,14 +1,14 @@
 
 function date2CanonicString(d, dateOnly) {
-  if (d == null) return "0000-00-00 00:00:00 GMT+0000";
+  if (d == null) return '0000-00-00 00:00:00 GMT+0000';
 
   var ts = - (new Date()).getTimezoneOffset()/60 * 100;
 
   var dateStr = d.getFullYear() +
-      "-" +
-      ("00" + (d.getMonth() + 1)).substr(-2) +
-      "-" +
-      ("00" + (d.getDate())).substr(-2);
+      '-' +
+      ('00' + (d.getMonth() + 1)).substr(-2) +
+      '-' +
+      ('00' + (d.getDate())).substr(-2);
 
   if (dateOnly) return dateStr;
 
@@ -17,14 +17,14 @@ function date2CanonicString(d, dateOnly) {
     return dateStr;
   }
 
-  return dateStr + " " +
-      ("00" + d.getHours()).substr(-2) +
-      ":" +
-      ("00" + d.getMinutes()).substr(-2) +
-      ":" +
-      ("00" + d.getSeconds()).substr(-2) +
-      " GMT" + (ts < 0 ? "-" : "+") +
-      ("0000" + Math.abs(ts)).substr(-4);
+  return dateStr + ' ' +
+      ('00' + d.getHours()).substr(-2) +
+      ':' +
+      ('00' + d.getMinutes()).substr(-2) +
+      ':' +
+      ('00' + d.getSeconds()).substr(-2) +
+      ' GMT' + (ts < 0 ? '-' : '+') +
+      ('0000' + Math.abs(ts)).substr(-4);
 }
 
 export default date2CanonicString;
