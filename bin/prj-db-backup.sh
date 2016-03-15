9070026e66a191fbdce578e628814f875ac3af66
Jean Honlet <jehon@users.noreply.github.com>
2016-03-15 06:36:09 +0100

    Add help

--- bin/prj-db-backup.sh
+++ bin/prj-db-backup.sh
@@ -1,9 +1,22 @@
 #!/bin/bash
 
+PRJ_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+PRJ_DIR=$(dirname "$PRJ_DIR")
+
+if [ "$1" = "help" ]; then
+  cat <<-EOL
+  Backup the actual dev database into backups/dev/
+EOL
+  exit 0
+fi
+
+set -e
+
+ROOT="$PRJ_DIR/backups/dev/"
+mkdir -p "$ROOT"
+
 TS=`/bin/date "+%Y-%m-%d-%H.%M.%S"`
 
-ROOT="/vagrant/tmp"
-mkdir -p "$ROOT"
 FILENAME="backup-$TS"
 if [ "$1" != "" ]; then
   FILENAME="$FILENAME-$1"
