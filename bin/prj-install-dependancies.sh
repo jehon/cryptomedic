7c877f86dc4a707bfb9fc874e0a587dc79b5f9e2
Jean Honlet <marielineet.jean@gmail.com>
2016-03-15 06:49:19 +0100

    Move getting composer in prj-install-dependancies

--- bin/prj-install-dependancies.sh
+++ bin/prj-install-dependancies.sh
@@ -7,8 +7,9 @@ if [ "$1" = "help" ]; then
   cat <<-EOL
     Install dependancies according to
     - package.json
-    - composer.json (install composer if necessary)
-    - if a "GITHUB_OAUTH_TOKEN" is set in env, configure it into composer cache
+    - composer.json
+      * install composer if necessary
+      * if a "GITHUB_OAUTH_TOKEN" is set in env, configure it into composer cache
 EOL
   exit 0
 fi
@@ -21,8 +22,15 @@ if [ -e package.json ]; then
 fi
 
 if [ -e composer.json ]; then
+  # Get composer
+  if [ ! -x /usr/local/bin/composer.phar ]; then
+    echo -e "\e[1m\e[45mGetting the composer\e[0m"
+    curl -sS https://getcomposer.org/installer | php -- --install-dir=/usr/local/bin
+  fi
+
+  # Configure the OAUTH
   if [ -n "$GITHUB_OAUTH_TOKEN" ]; then
-    echo "setting composer oauth"
+    echo -e "\e[1m\e[45mSetting the OAUTH token\e[0m"
     composer config github-oauth.github.com "${GITHUB_OAUTH_TOKEN}"
     echo "result: $?"
   else
