7dfcfeaa58f1ef2a7e9aa0b364c6b44c3d6839c2
Jean Honlet <jehon@users.noreply.github.com>
2016-03-15 22:19:37 +0100

    Change getGlobalConfig into $myconfig

--- maintenance/debug.php
+++ maintenance/debug.php
@@ -5,6 +5,6 @@ require_once(__DIR__ . "/../vendor/autoload.php");
 require(__DIR__ . "/../config.php");
 
 \Jehon\Maintenance\TryCatch::run();
-\Jehon\Maintenance\SessionProtect::run(getGlobalConfig("maintenance.code"), getGlobalConfig("maintenance.token"));
+\Jehon\Maintenance\SessionProtect::run($myconfig["maintenance"]["code"], $myconfig["maintenance"]["token"]);
 
 \Jehon\Maintenance\Debug::run();
--- maintenance/logs.php
+++ maintenance/logs.php
@@ -5,7 +5,7 @@ require_once(__DIR__ . "/../vendor/autoload.php");
 require(__DIR__ . "/../config.php");
 
 \Jehon\Maintenance\TryCatch::run();
-\Jehon\Maintenance\SessionProtect::run(getGlobalConfig("maintenance.code"), getGlobalConfig("maintenance.token"));
+\Jehon\Maintenance\SessionProtect::run($myconfig["maintenance"]["code"], $myconfig["maintenance"]["token"]);
 
 \Jehon\Maintenance\Logs::run([
 	__DIR__ . "/../rest/storage/logs",
