456e82d6053bbc9d2b36b2a50185f6e76ba3f3a4
Jean Honlet <jehon@users.noreply.github.com>
2016-03-20 21:25:32 +0100

    Moving cache into www laravel

---
+++ www/api/v1.0/app/Http/Controllers/Cache/templates/fiches/surgery.php
@@ -0,0 +1,31 @@
+<?php
+  require_once(__DIR__ . "/../../t.php");
+
+	t::setDefaultOption("baseExpression", "currentFile().");
+?>
+<div class='container-fluid'>
+	<div class='row'>
+		<div class="col-md-6">
+			<fieldSet>
+				<legend>Surgical Report</legend>
+				<table>
+					<?php (new t("Surgery.Date"))->tr("Operation Date")->p(); ?>
+					<?php (new t("Surgery.ReportDiagnostic"))->tr()->p(); ?>
+					<?php (new t("Surgery.ReportSurgeon"))->tr()->p(); ?>
+					<?php (new t("Surgery.ReportSideR"))->tr()->p(); ?>
+					<?php (new t("Surgery.ReportSideL"))->tr()->p(); ?>
+					<?php (new t("Surgery.report_procedure"))->tr()->p(); ?>
+				</table>
+			</fieldSet>
+			<fieldSet>
+				<legend>Hospitalisation Follow-Up</legend>
+				<table>
+					<?php (new t("Surgery.FollowUpComplication"))->tr()->p(); ?>
+				</table>
+			</fieldSet>
+		</div>
+		<div class="col-md-6">
+			<?php require(__DIR__ . "/../../helpers/patient-related.php"); ?>
+		</div>
+	</div>
+</div>
---
+++ www/api/v1.0/app/Http/Controllers/Cache/templates/summary/bill.php
@@ -0,0 +1,27 @@
+<?php
+	namespace App;
+  require_once(__DIR__ . "/../../t.php");
+	require_once(__DIR__ . "/../../../www/api/v1.0/app/Bill.php");
+
+	use \App\Bill;
+	use \References;
+	use \t;
+
+	t::setDefaultOption("baseExpression", "folder.getSubFile(\$index).");
+	t::setDefaultOption("readOnly");
+?>
+<table style='width: 100%'>
+	<colgroup span="2" width="50%"></colgroup>
+	<tbody>
+		<?php (new t("Bill.Sociallevel"))->tr("Social Level")->p(); ?>
+		<?php (new t("Bill.total_asked"))->tr("Total asked")->p(); ?>
+		<?php (new t("Bill.total_paid"))->tr("Total paid")->p(); ?>
+		<?php
+			foreach(Bill::$categories as $cat) {
+				foreach(Bill::getFieldsList($cat, t::getColumnsOfTable('bills')) as $item) {
+					(new t("Bill." . $item))->tr(str_replace("_", " ", substr($item, strpos($item, '_') + 1)))->p();
+				}
+			}
+		?>
+	</tbody>
+</table>
