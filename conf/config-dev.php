2b0475d1cc6f06129da391d541e436cba14975e1
Jean Honlet <marielineet.jean@gmail.com>
2016-03-30 12:23:22 +0200

    Fix argc unknown

--- conf/config-dev.php
+++ conf/config-dev.php
@@ -23,18 +23,20 @@ if (file_exists(__DIR__ . 'config-custom.php')) {
   require_once(__DIR__ . 'config-custom.php');
 }
 
-if ($argc == 2) {
-  $path = $argv[1];
-  global $myconfig;
-  $array = $myconfig;
+if (isset($argc)) {
+  if ($argc == 2) {
+    $path = $argv[1];
+    global $myconfig;
+    $array = $myconfig;
 
-  $keys = explode('.', $path);
-  foreach ($keys as $key) {
-    if (isset($array[$key])) {
-      $array = $array[$key];
-    } else {
-      throw new Exception("Path not found: " . $path);
+    $keys = explode('.', $path);
+    foreach ($keys as $key) {
+      if (isset($array[$key])) {
+        $array = $array[$key];
+      } else {
+        throw new Exception("Path not found: " . $path);
+      }
     }
+    echo $array;
   }
-  echo $array;
 }
