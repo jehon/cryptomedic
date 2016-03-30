28d8a23fc35020b839347ace9580a29391e87ce3
Jean Honlet <marielineet.jean@gmail.com>
2016-03-30 21:59:27 +0200

    Remove launching php server
    We are using apache in vagrant now.

--- bin/prj-test.sh
+++ bin/prj-test.sh
@@ -4,7 +4,6 @@ set -e
 
 PRJ_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 PRJ_DIR=$(dirname "$PRJ_DIR")
-echo "Project directory is $PRJ_DIR"
 PHPUNIT="$PRJ_DIR/vendor/bin/phpunit"
 
 cd "$PRJ_DIR"
@@ -26,16 +25,7 @@ test_dir() {
 
   if [ -r nightwatch.json ]; then
     echo -e "\e[0;45m[\e[1;45m$N/nightwatch\e[0;45m] Testing $L\e[0m"
-    echo -e "\e[0;45m[\e[1;45m$N/nightwatch\e[0;45m] Starting server\e[0m"
-    cd $PRJ_DIR
-    BYPASS_AUTHENTICATION=1 php -S localhost:1234 tests/router.php -t . &
-    PHPSERV=$!
-    cd - > /dev/null
-    echo -e "\e[0;45m[\e[1;45m$N/nightwatch\e[0;45m] Starting Test\e[0m"
     node $PRJ_DIR/node_modules/.bin/nightwatch -e default "$@"
-
-    echo -e "\e[0;45m[\e[1;45m$N/nightwatch\e[0;45m] Stoping server @$PHPSERV\e[0m"
-    kill -s SIGINT $PHPSERV
   fi
 
   if [ -r karma.conf.js ]; then
@@ -87,11 +77,6 @@ else
       cd "$T" && test_dir "$TITLE"
     fi
   done
-
-  if [ -r "$PRJ_dIR/tmp/router.log" ]; then
-    echo -e "\e[0;45mRouter.log\e[0m"
-    cat "$PRJ_dIR/tmp/router.log" | sort | uniq
-  fi
 fi
 
 echo -e "\e[1m\e[45mTerminated ok.\e[0m"
