1c7478f849a0682804c0dbd49dee492e5d87a80c
Jean Honlet <marielineet.jean@gmail.com>
2016-03-31 13:22:43 +0200

    Mount instead of link

--- bin/prj-configure-live.sh
+++ bin/prj-configure-live.sh
@@ -10,16 +10,14 @@ set -e
 chmod +x "$SCRIPT_DIR"/*
 "$SCRIPT_DIR"/prj-configure-base.sh "$@"
 
-echo "** Install current /var/www/html **"
-if [ -r /var/www/html ]; then
-  if [ ! -L /var/www/html ]; then
-    mv --force /var/www/html /var/www/html.bak
-  fi
-fi
-# Override existing link, even if it is a directory
-ln --symbolic --force --no-dereference "$PRJ_DIR/www" /var/www/html
+echo "** Mount current /var/www/html **"
+#ln --symbolic --force --no-dereference "$PRJ_DIR/www" /var/www/html
+cat /etc/fstab | grep -v "/var/www/html" > /etc/fstab
+echo -e "$PRJ_DIR/www\t/var/www/html\tnone\tbind\t0\t0" >> /etc/fstab
+mount /var/www/html
 
 # Run project custom files
 if [ -x $SCRIPT_DIR/prj-configure-live-custom.sh ]; then
   $SCRIPT_DIR/prj-configure-live-custom.sh
 fi
+
