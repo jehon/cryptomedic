2e3de319201d302a3eb666fe5bbd55ae222ed1cb
Jean Honlet <jehon@users.noreply.github.com>
2016-03-31 18:00:00 +0200

    Fix fstab disappearing

--- bin/prj-configure-live.sh
+++ bin/prj-configure-live.sh
@@ -12,12 +12,13 @@ chmod +x "$SCRIPT_DIR"/*
 
 echo "** Mount current /var/www/html **"
 #ln --symbolic --force --no-dereference "$PRJ_DIR/www" /var/www/html
-cat /etc/fstab | grep -v "/var/www/html" > /etc/fstab
-echo -e "$PRJ_DIR/www\t/var/www/html\tnone\tbind\t0\t0" >> /etc/fstab
+cat /etc/fstab | grep -v "/var/www/html" > /etc/fstab.new
+echo -e "$PRJ_DIR/www\t/var/www/html\tnone\tbind\t0\t0" >> /etc/fstab.new
+mv /etc/fstab /etc/fstab.prj-install.bak
+mv /etc/fstab.new /etc/fstab
 mount /var/www/html
 
 # Run project custom files
 if [ -x $SCRIPT_DIR/prj-configure-live-custom.sh ]; then
   $SCRIPT_DIR/prj-configure-live-custom.sh
 fi
-
