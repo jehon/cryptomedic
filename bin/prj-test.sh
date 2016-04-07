2c54bb20e6ba869cc90f8c62de3fed259b23865a
Jean Honlet <jehon@users.noreply.github.com>
2016-04-07 07:31:15 +0200

    Fix build path

--- bin/prj-test.sh
+++ bin/prj-test.sh
@@ -95,7 +95,7 @@ else
   "$PRJ_DIR/bin/prj-db-reset.php"
 
   echo -e "\e[0;45mRebuild for production\e[0m"
-  find "$PRJ_DIR/build/" -mindepth 1 -delete
+  find "$PRJ_DIR/www/build/" -mindepth 1 -delete
   npm run build
 
   for V in "$PRJ_DIR"/www/api/* ; do
