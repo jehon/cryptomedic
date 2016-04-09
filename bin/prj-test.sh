d26a0010724bec8562a727c938e869f83d6fde7f
Jean Honlet <jehon@users.noreply.github.com>
2016-04-09 15:08:23 +0200

    Fix DEBUG and xvfb-run

--- bin/prj-test.sh
+++ bin/prj-test.sh
@@ -24,9 +24,9 @@ XVFB='xvfb-run'
 
 mxvfb() {
   if [ "$DEBUG" != "" ]; then
-    xvfb-run --auto-servernum --server-args='-screen 0 1024x768x24 -ac +extension GLX +render -noreset' "$@"
-  else
     "$@"
+  else
+    xvfb-run --auto-servernum --server-args='-screen 0 1024x768x24 -ac +extension GLX +render -noreset' "$@"
   fi
 }
 
