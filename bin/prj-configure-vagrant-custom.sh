d80a8e7719a86cef424514bb824c87580271e777
Jean Honlet <jehon@users.noreply.github.com>
2016-04-16 21:40:42 +0200

    Go into live folder

--- bin/prj-configure-vagrant-custom.sh
+++ bin/prj-configure-vagrant-custom.sh
@@ -2,11 +2,11 @@
 
 set -e
 
-mkdir -p /var/www/uploadedPictures
+mkdir -p /var/www/live/uploadedPictures
 
-cp /vagrant/resources/1.jpg /var/www/uploadedPictures/98141_2014-06-01_16-51-42.JPG
-cp /vagrant/resources/1.jpg /var/www/uploadedPictures/10_2014-11-06_15-32-45.JPG
-cp /vagrant/resources/1.jpg /var/www/uploadedPictures/3288.JPG
-cp /vagrant/resources/1.jpg /var/www/uploadedPictures/98146_2015-04-28_3952.jpg
+cp /vagrant/resources/1.jpg /var/www/live/uploadedPictures/98141_2014-06-01_16-51-42.JPG
+cp /vagrant/resources/1.jpg /var/www/live/uploadedPictures/10_2014-11-06_15-32-45.JPG
+cp /vagrant/resources/1.jpg /var/www/live/uploadedPictures/3288.JPG
+cp /vagrant/resources/1.jpg /var/www/live/uploadedPictures/98146_2015-04-28_3952.jpg
 
-chown -R www-data /var/www/uploadedPictures
+chown -R www-data /var/www/live/uploadedPictures
