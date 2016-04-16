0bf4574120cd603e2ed7d89907ec28670382606e
Jean Honlet <marielineet.jean@gmail.com>
2016-04-16 21:40:49 +0200

    Use picture path

--- www/templates/templates/picture_summary.php
+++ www/templates/templates/picture_summary.php
@@ -6,7 +6,7 @@
 	<colgroup span="2" width="50%"></colgroup>
 	<tbody>
         <tr>
-        	<td><img width='50px' ng-src="/uploadedPictures/{{folder.getSubFile($index).file}}"></td>
+        	<td><img width='50px' ng-src="/api/v1.0/picture/{{folder.getSubFile($index).id}}"></td>
 			<td><?php (new t("Picture.comment"))->read()->p(); ?></td>
 		</tr>
 	</tbody>
