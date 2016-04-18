2551104143a5066f176020f68407bf6f0311f279
Jean Honlet <jehon@users.noreply.github.com>
2016-04-18 21:34:05 +0200

    Simplify logs sometimes

--- bin/prj-logs.sh
+++ bin/prj-logs.sh
@@ -12,19 +12,22 @@ EOL
   exit 0
 fi
 
-touch /tmp/emails.txt || true
-touch /var/log/apache2/error.log || true
-touch /var/log/apache2/access.log || true
-touch /var/log/apache2/other_vhosts_access.log || true
-touch /tmp/php_errors.log || true
+touch /tmp/emails.txt                          2>/dev/null || true
+touch /var/log/apache2/error.log               2>/dev/null || true
+touch /var/log/apache2/access.log              2>/dev/null || true
+touch /var/log/apache2/other_vhosts_access.log 2>/dev/null || true
+touch /tmp/php_errors.log                      2>/dev/null || true
+chmod a+rw /tmp/email.txt /tmp/php_errors.log  2>/dev/null || true
 
-chmod a+rw /tmp/email.txt /tmp/php_errors.log || true
+if [ "$1" = "extended" ]; then
+  EXTENDED=-c  -cS apache --label "[http]"   -I /var/log/apache2/access.log
+fi
 
 multitail --mark-interval 60 \
   -c  -ci red    --label "[hter]"   -i /var/log/apache2/error.log \
-  -c  -cS apache --label "[http]"   -I /var/log/apache2/access.log \
   -d  -ci cyan   --label "[@   ]"   -I /tmp/emails.txt \
   -d  -ci yellow --label "[php ]"   -I /tmp/php_errors.log \
+  $EXTENDED
   "$@"
 
 #  -c  -cS apache --label "[ssl ]"   -I /var/log/apache2/other_vhosts_access.log \
