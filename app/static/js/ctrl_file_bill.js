6efdaa0997fc71a3036f56a223ffd361049d1957
Jean Honlet <jehon@users.noreply.github.com>
2016-04-21 07:02:46 +0200

    Format

--- app/static/js/ctrl_file_bill.js
+++ app/static/js/ctrl_file_bill.js
@@ -1,6 +1,6 @@
-"use strict";
+'use strict';
 
-mainApp.controller("ctrl_file_bill", [ "$scope", function($scope) {
+mainApp.controller('ctrl_file_bill', [ '$scope', function($scope) {
   $scope.$watch(function() {
     return server.settings;
   }, function() {
@@ -8,7 +8,7 @@ mainApp.controller("ctrl_file_bill", [ "$scope", function($scope) {
     $scope.safeApply();
   });
 
-  $scope.$watch("currentFile().Date", function() {
+  $scope.$watch('currentFile().Date', function() {
     if ($scope.currentFile() && $scope.currentFile().calculatePriceId) {
       $scope.currentFile().calculatePriceId();
       $scope.safeApply();
@@ -17,11 +17,11 @@ mainApp.controller("ctrl_file_bill", [ "$scope", function($scope) {
     }
   });
 
-  $scope.$watch("currentFile().sl_numberOfHouseholdMembers", function() {
+  $scope.$watch('currentFile().sl_numberOfHouseholdMembers', function() {
     $scope.currentFile().ratioSalary();
   });
 
-  $scope.$watch("currentFile().sl_familySalary", function() {
+  $scope.$watch('currentFile().sl_familySalary', function() {
     $scope.currentFile().ratioSalary();
   });
 }]);
