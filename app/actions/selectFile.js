0ade8bed1e79128ea61d5db2f712bc8d19de9f0a
Jean Honlet <marielineet.jean@gmail.com>
2016-04-21 07:03:41 +0200

    Lake action work

--- app/actions/selectFile.js
+++ app/actions/selectFile.js
@@ -2,6 +2,8 @@
 import myFetch from 'helpers/myFetch';
 import objectify from 'helpers/objectify';
 import create from 'helpers/create';
+import dispatch from 'reducers/dispatch';
+import catalog from 'reducers/catalog';
 
 export default function selectFile(id, type = 'folder') {
   // If not final then go to the server anyway...
@@ -11,9 +13,13 @@ export default function selectFile(id, type = 'folder') {
     .then(objectify)
 
     // TODO: Generalize this:
-    .then(function(data) { return create('Folder', data); })
-
+    .then(function(data) {
+      return create('Folder', data);
+    })
+    .then(function(data) {
+      dispatch(catalog.GUI_SET_SELECT_FILE, data);
+      return data;
+    })
     .catch()
     ;
-  // });
 }
