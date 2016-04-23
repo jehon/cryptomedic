44126a383224a9e8a4954b36dad395b09fea3f12
Jean Honlet <marielineet.jean@gmail.com>
2016-04-23 19:49:46 +0200

    Remove unecessary log

--- www/static/js/application.js
+++ www/static/js/application.js
@@ -4,7 +4,7 @@ var application = {};
 var server = {};
 
 function template(...names) {
-  console.log('requesting template: ' + names.join('_'));
+  // console.log('requesting template: ' + names.join('_'));
   return '/api/v1.0/templates/' + names.join('_') + '.php';
 }
 
