68433c0221d2b45dc15899e124e62ebd9e610577
Jean Honlet <marielineet.jean@gmail.com>
2016-04-25 11:52:34 +0200

    Use arrow notation

--- app/helpers/myFrontFetch.js
+++ app/helpers/myFrontFetch.js
@@ -6,7 +6,7 @@ import myFetch  from 'helpers/myFetch';
 
 export default function myFrontFetch(url, init, data) {
   return myFetch(url, init, data).then(
-    function(json) {
+    (json) => {
       dispatch(catalog.CONNECTION_SUCCESS);
       if (json._offline) {
       //   return database.storeRecord({ record: json })
@@ -17,27 +17,27 @@ export default function myFrontFetch(url, init, data) {
       } else {
         return json;
       }
-    }, function(httpErrorCode) {
-    console.log('httperrorcode', httpErrorCode);
-    switch(httpErrorCode) {
-      case 401: // unauthorized
-        dispatch(catalog.CONNECTION_EXPIRED);
-        location.hash = '#/login';
-        break;
-      case 403: // forbidden
-        dispatch(catalog.CONNECTION_FAILED);
-        break;
-      case 404: // not found
-        dispatch(catalog.CONNECTION_NOT_FOUND);
-        break;
-      case 500: // internal server error
-        dispatch(catalog.CONNECTION_SERVER_ERROR);
-        break;
-      default:
-        dispatch(catalog.CONNECTION_SERVER_ERROR);
-        break;
+    },
+    (httpErrorCode) => {
+      switch(httpErrorCode) {
+        case 401: // unauthorized
+          dispatch(catalog.CONNECTION_EXPIRED);
+          location.hash = '#/login';
+          break;
+        case 403: // forbidden
+          dispatch(catalog.CONNECTION_FAILED);
+          break;
+        case 404: // not found
+          dispatch(catalog.CONNECTION_NOT_FOUND);
+          break;
+        case 500: // internal server error
+          dispatch(catalog.CONNECTION_SERVER_ERROR);
+          break;
+        default:
+          dispatch(catalog.CONNECTION_SERVER_ERROR);
+          break;
+      }
+      return Promise.reject('myFrontFetch error: ' + httpErrorCode);
     }
-    return Promise.reject('myFrontFetch error: ' + httpErrorCode);
-  }
   );
 }
