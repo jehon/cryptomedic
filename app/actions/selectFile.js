c04f0ce34e7c2ac2ec485f69ee3b301fff5b0efd
Jean Honlet <jehon@users.noreply.github.com>
2016-04-25 18:00:00 +0200

    Use arrow notation

--- app/actions/selectFile.js
+++ app/actions/selectFile.js
@@ -16,17 +16,10 @@ export default function selectFile(id, type = 'folder') {
   //   console.log('Getting the folder live: #' + id);
   return myFrontFetch(type + '/' + id)
     .then(objectify)
-
-    // TODO: Generalize this:
-    .then(function(data) {
-      return create('Folder', data);
-    })
-    .then(function(data) {
-      dispatch(catalog.FOLDER_SELECT, data);
-      return data;
-    })
-    .catch(function(data) {
-      console.log('in error in selectFile');
+    .then((data) => { return create('Folder', data); })
+    .then((data) => { return dispatch(catalog.FOLDER_SELECT, data); })
+    .catch((data) => {
+      console.log('in error in selectFile', data);
       dispatch(catalog.CONNECTION_NOT_FOUND, data);
       return null;
     })
