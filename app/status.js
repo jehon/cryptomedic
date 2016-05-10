2355dcf4fb6ac540e9ae86f216bafb3152bf57eb
Jean Honlet <jehon@users.noreply.github.com>
2016-05-10 18:00:00 +0200

    Add db to status transitionnal object

--- app/status.js
+++ app/status.js
@@ -4,19 +4,17 @@ import catalog              from 'reducers/catalog';
 import dispatch             from 'reducers/dispatch';
 import store                from 'reducers/store';
 
-// import objectify            from 'helpers/objectify';
 import create               from 'helpers/create';
 import date2CanonicString   from 'helpers/date2CanonicString';
-// import database             from 'helpers/database';
+import Database             from 'helpers/database';
 import myFetch              from 'helpers/myFetch';
 import service_backend_fn   from 'helpers/service_backend';
-// import { nullify }          from 'helpers/service_backend';
 
 import selectFile           from 'actions/selectFile';
 import { DataMissingException } from 'helpers/exceptions';
 import { ApplicationException } from 'helpers/exceptions';
 
-var offline = require('offline-plugin/runtime');
+let offline = require('offline-plugin/runtime');
 if (location.pathname.split('/')[1] == 'offline') {
   console.log('SW-OFFLINE: Detecting offline mode, activating plugin');
   offline.install(null,
@@ -24,6 +22,8 @@ if (location.pathname.split('/')[1] == 'offline') {
     function(data) { console.error('SW-OFFLINE: problem', data); }
   );
 }
+
+let db = new Database();
 
 export default function() {
   return {
@@ -37,9 +37,9 @@ export default function() {
       // objectify,
       create,
       date2CanonicString,
-      // database,
       service_backend_fn,
       myFetch,
+      db,
       DataMissingException,
       ApplicationException
       // nullify
