81abbedc9c0c8269c0d93c192d60009f4b3f4327
Jean Honlet <marielineet.jean@gmail.com>
2016-05-19 07:02:16 +0200

    Clean up

--- www/api/v1.0/app/Http/Controllers/Controller.php
+++ www/api/v1.0/app/Http/Controllers/Controller.php
@@ -1,7 +1,5 @@
 <?php namespace App\Http\Controllers;
 
-// Migration 5.1 -> 5.2
-// use Illuminate\Foundation\Bus\DispatchesCommands;
 use Illuminate\Foundation\Bus\DispatchesJobs;
 use Illuminate\Routing\Controller as BaseController;
 use Illuminate\Foundation\Validation\ValidatesRequests;
--- www/api/v1.0/config/app.php
+++ www/api/v1.0/config/app.php
@@ -156,8 +156,6 @@ return [
 		 * Application Service Providers...
 		 */
 		'App\Providers\AppServiceProvider',
-		// 'App\Providers\BusServiceProvider',
-		// 'App\Providers\ConfigServiceProvider',
 		'App\Providers\EventServiceProvider',
 		'App\Providers\RouteServiceProvider',
 
