45b0c30aa596a91d9e3d886a5497c175f451e8f8
Jean Honlet <jehon@users.noreply.github.com>
2016-07-16 22:02:30 +0200

    Regroup configurations

--- bin/prj-configure-vagrant.sh
+++ bin/prj-configure-vagrant.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 
-# *** Depend on DEV ***
-
 SCRIPT_DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
 PRJ_DIR=$(dirname "$SCRIPT_DIR")
 echo "SCRIPT: $SCRIPT_DIR"
@@ -10,17 +8,7 @@ echo "PRJ:    $PRJ_DIR"
 # Stop on error
 set -e
 
-# Add the prj-build path into the server path
-cat <<PROFILE > /etc/profile.d/vagrant-append-project.sh
-#!/bin/bash
-# Export will make this inheritable
-export PATH=$PATH:$SCRIPT_DIR
-export DISPLAY=":99.0"
-
-export PRJ_DIR=$PRJ_DIR
-export SCRIPT_DIR=$SCRIPT_DIR
-
-# Set the locale
+# Set the host locale
 # http://serverfault.com/questions/362903/how-do-you-set-a-locale-non-interactively-on-debian-ubuntu
 if [ "\$LC_ALL" != "" ]; then
   if locale -a | grep "\$LC_ALL" > /dev/null; then
@@ -33,17 +21,29 @@ else
   echo "No locale found in LC_ALL"
 fi
 
-cd /vagrant
+# Add the prj-build path into the server path
+cat <<PROFILE > /etc/profile.d/vagrant-append-project.sh
+#!/bin/bash
+# Export will make this inheritable
+export PATH=$PATH:$SCRIPT_DIR
+export DISPLAY=":99.0"
+
+export PRJ_DIR=$PRJ_DIR
+export SCRIPT_DIR=$SCRIPT_DIR
+
+# Fix permissions on the various files
+chmod +x "$SCRIPT_DIR"/*
+
+cd /$SCRIPT_DIR
+
+#prj-logs.sh
 PROFILE
 chmod 777 /etc/profile.d/vagrant-append-project.sh
 
-INDEX_CREATED=""
-if [ ! -r "/var/www/html/index.cgi" ]; then
-  touch /var/www/html/index.cgi
-  INDEX_CREATED=/var/www/html/index.cgi
-fi
+# Manage user rights
+usermod -a -G adm vagrant
 
-if ([ "$1" == "" ] || [ "$1" = "install" ]); then
+if ([ "$1" != "offline" ]); then
   # Manage packages
   if test "`find /root/last_apt_get_update -mtime -1 2>/dev/null`"; then
     echo "Last apt-get update less than 1 day ago. Skipping it." >&2
@@ -52,35 +52,51 @@ if ([ "$1" == "" ] || [ "$1" = "install" ]); then
   else
     apt-get -y update
     touch /root/last_apt_get_update
+
+    # Developpement packages
+    DEBIAN_FRONTEND=noninteractive apt-get install --yes --force-yes apache2 \
+      php5-xdebug     \
+      firefox         \
+      xvfb            \
+      phpmyadmin      \
+      default-jre     \
+      mysql-client    \
+      build-essential \
+      libapache2-mod-php5 php5-cli php5-mysql php5-mcrypt php5-curl \
+      mysql-server    \
+      multitail       \
+      crudini         \
+      curl            \
+      git             \
+    # end
+
+    # Install nodejs 5.* ==> usefull????
+    curl -sL https://deb.nodesource.com/setup_5.x | bash -
+    apt-get install -y nodejs
+
+    echo "Install terminated"
   fi
-
-  echo "Install terminated"
 fi
 
-if [ "$INDEX_CREATED" != "" ]; then
-  echo "Removing place holder index.cgi"
-  rm "$INDEX_CREATED"
+# Install project custom debs
+if [ "$(ls -A /vagrant/conf/repo/*.deb)" ]; then
+  echo "Installing custom debs"
+  dpkg -i /vagrant/conf/repo/*.deb
+  apt-get install -f
 fi
 
-# Run the dev configuration
-"$SCRIPT_DIR"/prj-configure-dev.sh "$@"
+# Enable php5-mcrypt
+php5enmod mcrypt || true
 
-# Manage user rights
-usermod -a -G adm vagrant
+# Enable SSL
+a2enmod  rewrite ssl || true
+a2ensite default-ssl || true
 
 # Put various configs file in place (cp because needed before vagrant mount)
-cp --force $PRJ_DIR/conf/apache-config.conf         /etc/apache2/conf-enabled/apache-config.conf
-cp --force $PRJ_DIR/conf/apache-server-10080.conf   /etc/apache2/sites-enabled/apache-server-10080.conf
-cp --force $PRJ_DIR/conf/phpmyadmin.site.conf       /etc/apache2/sites-enabled/phpmyadmin.conf
-cp --force $PRJ_DIR/conf/phpmyadmin.inc.php         /etc/phpmyadmin/conf.d/phpmyadmin.inc.php
+rsync -a $PRJ_DIR/conf/root/ /
 
 # Configure phpmyadmin
 cat /usr/share/doc/phpmyadmin/examples/create_tables.sql.gz | gunzip | mysql
-
-cat <<EOF > /etc/php5/apache2/conf.d/99-vagrant.ini
-xdebug.max_nesting_level = 256
-EOF
-
 
 # Enable xvfb
 
@@ -94,20 +110,20 @@ sed -i -e "s:;sendmail_path =:sendmail_path = \"$SCRIPT_DIR/prj-fake-sendmail.sh
 touch /tmp/emails.txt
 chmod a+rwx /tmp/emails.txt
 
-sed -i -e "s:;error_log = php_errors.log:error_log = /tmp/php_errors.log:g" /etc/php5/apache2/php.ini
+#sed -i -e "s:;error_log = php_errors.log:error_log = /tmp/php_errors.log:g" /etc/php5/apache2/php.ini
 touch /tmp/php_errors.log
 chmod a+rwx /tmp/php_errors.log
 
 # Add some swap
 # See @https://jeqo.github.io/blog/devops/vagrant-quickstart/
-if [ ! -r "/etc/fstab/swapfile" ]; then
+if [ ! -r "/swapfile" ]; then
   echo 'swapfile not found. Adding swapfile.'
   #dd if=/dev/zero of=/swapfile bs=1024 count=524288
   fallocate -l 1GiB /swapfile
   chmod 600 /swapfile
   mkswap /swapfile
   swapon /swapfile
-  echo '/swapfile none swap defaults 0 0' > /etc/fstab.d/swapfile
+  #echo '/swapfile none swap defaults 0 0' > /etc/fstab.d/swapfile
 fi
 
 # Run project custom files
@@ -116,8 +132,35 @@ if [ -x $SCRIPT_DIR/prj-configure-vagrant-custom.sh ]; then
 fi
 
 # Create live folder
-mkdir -p /var/www/live
-chown -R www-data /var/www/live
+#mkdir -p /var/www/live
+#chown -R www-data /var/www/live
+
+if [ "$1" != "offline" ]; then
+  $SCRIPT_DIR/prj-install-dependancies.sh
+
+  if [ -e "$PRJ_DIR"/composer.json ] && [ ! -x /usr/local/bin/composer.phar ]; then
+    echo "** Getting the composer **"
+    curl -sS https://getcomposer.org/installer | php -- --install-dir=/usr/local/bin
+  fi
+fi
+
+$SCRIPT_DIR/prj-db-reset.php
+
+# This file is not necessary on vagrant boot
+echo "** Install new config.php"
+ln -s --force $PRJ_DIR/conf/config-dev.php /var/www/config.php
+sudo chmod a+r /var/www/config.php
+
+if [ -r "$PRJ_DIR/conf/config-custom.php" ]; then
+  echo "** Install new config-custom.php"
+  sudo ln $PRJ_DIR/conf/config-custom.php /var/www/config-custom.php
+  sudo chmod a+r /var/www/config-custom.php
+fi
+
+# Run project custom files
+if [ -x $SCRIPT_DIR/prj-configure-base-custom.sh ]; then
+  $SCRIPT_DIR/prj-configure-base-custom.sh
+fi
 
 /etc/init.d/apache2 restart
 
