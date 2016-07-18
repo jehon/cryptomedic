5b0f6a68dc6ceda4b0fa6e9a88429cba1a75864a
Jean Honlet <marielineet.jean@gmail.com>
2016-07-18 12:10:43 +0200

    Fix config location

--- bin/dev-db-createNewTestingSqlDump.sh
+++ bin/dev-db-createNewTestingSqlDump.sh
@@ -3,7 +3,7 @@
 PRJ_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 PRJ_DIR=$(dirname "$PRJ_DIR")
 
-DB=`php $PRJ_DIR/conf/config-dev.php 'database.schema'`
+DB=`php $PRJ_DIR/config.php 'database.schema'`
 if [ -z "$DB" ]; then
   echo "Missing $DB in config.php"
   exit 1
