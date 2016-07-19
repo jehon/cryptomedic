6e43716327f0762b1d6d7e5c8f80c247d9fa7389
Jean Honlet <marielineet.jean@gmail.com>
2016-07-19 17:09:39 +0200

    Intermediary version of new bug_submit system

--- www/maintenance/bug_submit.php
+++ www/maintenance/bug_submit.php
@@ -2,52 +2,56 @@
 
 	require_once(__DIR__ . "/../vendor/autoload.php");
 
-	require(__DIR__ . "/../config.php");
-  global $myconfig;
+	// require(__DIR__ . "/../config.php");
+  // global $myconfig;
 
-	\Jehon\Maintenance\TryCatch::run();
+	// \Jehon\Maintenance\TryCatch::run();
 
-	$db = new \Jehon\Maintenance\Database(
-	 	"mysql:host=" . $myconfig['database']['host'] . ";dbname=" . $myconfig['database']['schema'],
-		$myconfig['database']['username'],
-		$myconfig['database']['password'],
-		[ PDO::MYSQL_ATTR_INIT_COMMAND  => "SET CHARACTER SET 'utf8'" ]
-	);
+	// $db = new \Jehon\Maintenance\Database(
+	//  	"mysql:host=" . $myconfig['database']['host'] . ";dbname=" . $myconfig['database']['schema'],
+	// 	$myconfig['database']['username'],
+	// 	$myconfig['database']['password'],
+	// 	[ PDO::MYSQL_ATTR_INIT_COMMAND  => "SET CHARACTER SET 'utf8'" ]
+	// );
 
-	\Jehon\Maintenance\BugReporting::record($db, "http://www.cryptomedic.org/cryptomedic/maintenance/bug_view.php");
+	// \Jehon\Maintenance\BugReporting::record($db, "http://www.cryptomedic.org/cryptomedic/maintenance/bug_view.php");
 
-// require 'PHPMailerAutoload.php';
+	require 'PHPMailerAutoload.php';
 
-// $mail = new PHPMailer;
+	$mail = new PHPMailer();
 
-// //$mail->SMTPDebug = 3;                               // Enable verbose debug output
+	// Set PHPMailer to use the sendmail transport
+	$mail->isSendmail();
+	// //$mail->SMTPDebug = 3;                               // Enable verbose debug output
 
-// $mail->isSMTP();                                      // Set mailer to use SMTP
-// $mail->Host = 'smtp1.example.com;smtp2.example.com';  // Specify main and backup SMTP servers
-// $mail->SMTPAuth = true;                               // Enable SMTP authentication
-// $mail->Username = 'user@example.com';                 // SMTP username
-// $mail->Password = 'secret';                           // SMTP password
-// $mail->SMTPSecure = 'tls';                            // Enable TLS encryption, `ssl` also accepted
-// $mail->Port = 587;                                    // TCP port to connect to
+	// $mail->isSMTP();                                      // Set mailer to use SMTP
+	// $mail->Host = 'smtp1.example.com;smtp2.example.com';  // Specify main and backup SMTP servers
+	// $mail->SMTPAuth = true;                               // Enable SMTP authentication
+	// $mail->Username = 'user@example.com';                 // SMTP username
+	// $mail->Password = 'secret';                           // SMTP password
+	// $mail->SMTPSecure = 'tls';                            // Enable TLS encryption, `ssl` also accepted
+	// $mail->Port = 587;                                    // TCP port to connect to
 
-// $mail->setFrom('from@example.com', 'Mailer');
-// $mail->addAddress('joe@example.net', 'Joe User');     // Add a recipient
-// $mail->addAddress('ellen@example.com');               // Name is optional
-// $mail->addReplyTo('info@example.com', 'Information');
-// $mail->addCC('cc@example.com');
-// $mail->addBCC('bcc@example.com');
+	$mail->setFrom('noreply@cryptomedic.org', 'Bug report');
+	$mail->addAddress('marielineetjean+cryptomedic@gmail.com', 'Cryptomedic Maintainer');
+	// $mail->addReplyTo('info@example.com', 'Information');
+	// $mail->addCC('cc@example.com');
 
-// $mail->addAttachment('/var/tmp/file.tar.gz');         // Add attachments
-// $mail->addAttachment('/tmp/image.jpg', 'new.jpg');    // Optional name
-// $mail->isHTML(true);                                  // Set email format to HTML
+	// $mail->addAttachment('/var/tmp/file.tar.gz');         // Add attachments
+	// $mail->addAttachment('/tmp/image.jpg', 'new.jpg');    // Optional name
+	$mail->isHTML(true);                                  // Set email format to HTML
 
-// $mail->Subject = 'Here is the subject';
-// $mail->Body    = 'This is the HTML message body <b>in bold!</b>';
-// $mail->AltBody = 'This is the body in plain text for non-HTML mail clients';
+	$mail->Subject = 'Bug report';
+	$mail->Body    = 'This is the HTML message body <b>in bold!</b>'
+  								// . 'Your <b>HTML</b> with an embedded Image: <img src="cid:my-attach"> Here is an image!'
+  								;
 
-// if(!$mail->send()) {
-//     echo 'Message could not be sent.';
-//     echo 'Mailer Error: ' . $mail->ErrorInfo;
-// } else {
-//     echo 'Message has been sent';
-// }
+  // $mail->AddEmbeddedImage("rocks.png", "my-attach", "rocks.png");
+  // $mail->AddAttachment('something.zip'); // this is a regular attachment (Not inline)
+
+	// if(!$mail->send()) {
+	//     echo 'Message could not be sent.';
+	//     echo 'Mailer Error: ' . $mail->ErrorInfo;
+	// } else {
+	//     echo 'Message has been sent';
+	// }
