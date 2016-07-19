8068a066706889e286f3989f93c89c380b5f946a
Jean Honlet <jehon@users.noreply.github.com>
2016-07-19 18:00:00 +0200

    Thrust config.php to throw exception if config is not found

--- bin/dev-db-reset.sh
+++ bin/dev-db-reset.sh
@@ -8,21 +8,6 @@ set -e
 DBROOT=`php $PRJ_DIR/config.php 'database.rootpwd'`
 DBNAME=`php $PRJ_DIR/config.php 'database.schema'`
 DBUSER=`php $PRJ_DIR/config.php 'database.username'`
-if [ -z "$DBROOT" ]; then
-  echo "Missing database.rootpwd in config.php"
-  exit 1
-fi
-
-if [ -z "$DBNAME" ]; then
-  echo "Missing database.schema in config.php"
-  exit 1
-fi
-
-if [ -z "$DBUSER" ]; then
-  echo "Missing database.username in config.php"
-  exit 1
-fi
-
 BASE="$PRJ_DIR"/conf/database/base.sql
 
 if [ ! -r "$BASE" ]; then
