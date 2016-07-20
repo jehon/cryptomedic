db1e5514acd96df899b7f2ba3d56fe9af43f06c5
Jean Honlet <marielineet.jean@gmail.com>
2016-07-20 11:51:06 +0200

    Move versions files correctly

--- conf/database/always-dev/rebuild view.sql
+++ conf/database/always-dev/rebuild view.sql
@@ -31,9 +31,3 @@ CREATE OR REPLACE
       FROM other_consults
     )
   ;
-
-DELETE FROM sync_computers
-  WHERE
-    updated_at IS NOT NULL
-    AND updated_at < DATE_ADD(NOW(), INTERVAL - 6 MONTH)
-  ;
