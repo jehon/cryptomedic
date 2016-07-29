69575ae1be3638a9effb04b2246cc985f3d87154
Jean Honlet <marielineet.jean@gmail.com>
2016-07-29 21:33:14 +0200

    Enable selecting folder but always goes live

--- app/actions/selectFile.js
+++ app/actions/selectFile.js
@@ -2,9 +2,29 @@
 import myFrontFetch     from 'helpers/myFrontFetch';
 import objectify        from 'helpers/objectify';
 import create           from 'helpers/create';
+import Database         from 'helpers/database';
 import dispatch         from 'reducers/dispatch';
 import catalog          from 'reducers/catalog';
-import store            from 'reducers/store';
+// import store            from 'reducers/store';
+
+let db = new Database();
+
+function getLive(id) {
+  console.log('Getting the folder live: #' + id);
+  dispatch(catalog.FOLDER_SELECT, id);
+  return myFrontFetch({ url: 'folder/' + id })
+    // Store the received record into the database
+    .then((json) => (new Database()).storeRecord({ record: json}, false) )
+    .then(objectify)
+    .then((data) => { return create('Folder', data); })
+    .then((data) => { return dispatch(catalog.FOLDER_UPDATE_FROM_SERVER, data); })
+    .catch((data) => {
+      console.log('in error in selectFile', data);
+      dispatch(catalog.CONNECTION_NOT_FOUND, data);
+      return null;
+    })
+    ;
+}
 
 export default function selectFile(id, type = 'folder') {
   // if (id == store.getState().folder.selected_id) {
@@ -12,16 +32,8 @@ export default function selectFile(id, type = 'folder') {
   // }
 
   // If not final then go to the server anyway...
-  // return db.getFolder(id).catch(function(error) {
-  //   console.log('Getting the folder live: #' + id);
-  return myFrontFetch({ url: type + '/' + id })
-    .then(objectify)
-    .then((data) => { return create('Folder', data); })
-    .then((data) => { return dispatch(catalog.FOLDER_SELECT, data); })
-    .catch((data) => {
-      console.log('in error in selectFile', data);
-      dispatch(catalog.CONNECTION_NOT_FOUND, data);
-      return null;
-    })
-    ;
+  return db.getFolder(id)
+  .then(data => dispatch(catalog.FOLDER_UPDATE_FROM_DB, data))
+  .then(() => getLive(id), () => getLive(id))
+  ;
 }
