27faa26cead02d5441e220411a11786c07c98dc6
Jean Honlet <jehon@users.noreply.github.com>
2016-07-31 22:07:19 +0200

    Use relative path for test includes

--- tests/20_js/test_build_record.js
+++ tests/20_js/test_build_record.js
@@ -1,5 +1,5 @@
 
-import { buildRecord } from 'thelpers';
+import { buildRecord } from './thelpers';
 
 describe('BuildRecord', function() {
   it('should update default values with modifiers', function() {
