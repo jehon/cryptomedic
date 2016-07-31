8d7c84b300f0d3d8a3707a494733763a6255ce59
Jean Honlet <marielineet.jean@gmail.com>
2016-07-31 09:57:11 +0200

    Make test_model_file work again

--- tests/20_js/test_model_file.js
+++ tests/20_js/test_model_file.js
@@ -1,18 +1,22 @@
-import { loadMock } from 'thelpers';
+import { loadMock }  from 'thelpers';
 import { DataMissingException } from 'helpers/exceptions';
-import create       from 'helpers/create';
+import File          from 'models/File';
+import Folder        from 'models/Folder';
+import Patient       from 'models/Patient';
+import ClubFoot      from 'models/ClubFoot';
+import RicketConsult from 'models/RicketConsult';
 
 describe('File', function() {
   var ricketConsult_8819 = 6;
   var clubFoot_695 = 4;
 
-  describe('with mock_patient_10.json', function() {
+  describe('with mock_patient_10', function() {
     it('should have correct properties', function(done) {
       // Go through the rest_service !!!
-      loadMock('mock_patient_10.json', 'Folder').then(function(folder) {
-        expect(folder instanceof create('Folder').constructor).toBeTruthy('Object is not a Folder');
+      loadMock('mock_patient_10', 'Folder').then(function(folder) {
+        expect(folder instanceof Folder).toBeTruthy('Object is not a Folder');
 
-        expect(folder.getMainFile() instanceof create('Patient').constructor).toBeTruthy('Object is not a patient');
+        expect(folder.getMainFile() instanceof Patient).toBeTruthy('Object is not a patient');
         expect(folder.getMainFile().Sex).toBe('Male');
         expect(folder.getMainFile().Yearofbirth).toBe(1998);
         expect(folder.getMainFile().actualAge(new Date('2014-01-01'))).toBe('16y0m');
@@ -21,30 +25,33 @@ describe('File', function() {
 
         var i;
         i = ricketConsult_8819;
-        expect(folder.getSubFile(i) instanceof create('File').constructor).toBeTruthy();
-        expect(folder.getSubFile(i) instanceof create('RicketConsult').constructor).toBeTruthy();
-        expect(folder.getSubFile(i).id).toBe(8819);
-        expect(folder.getSubFile(i).getPatient() instanceof create('Patient').constructor).toBeTruthy();
+
+        expect(folder.getSubFile(i)).toEqual(jasmine.any(File));
+        expect(folder.getSubFile(i)).toEqual(jasmine.any(RicketConsult));
+        expect(folder.getSubFile(i)).toEqual(jasmine.anything({ id: 8819 }));
+        expect(folder.getSubFile(i).getPatient()).toEqual(jasmine.any(Patient));
         expect(folder.getSubFile(i).Date).toEqual('2014-01-04');
         expect(folder.getSubFile(i).ageAtConsultTime(true)).toBe('16y0m');
 
         i = clubFoot_695;
-        expect(folder.getSubFile(i) instanceof create('File').constructor).toBeTruthy();
-        expect(folder.getSubFile(i) instanceof create('ClubFoot').constructor).toBeTruthy();
-        expect(folder.getSubFile(i).id).toBe(695);
-        expect(folder.getSubFile(i).getPatient() instanceof create('Patient').constructor).toBeTruthy();
+        expect(folder.getSubFile(i)).toEqual(jasmine.any(File));
+        expect(folder.getSubFile(i)).toEqual(jasmine.any(ClubFoot));
+        expect(folder.getSubFile(i)).toEqual(jasmine.anything({ id: 659 }));
+        expect(folder.getSubFile(i).getPatient()).toEqual(jasmine.any(Patient));
         expect(folder.getSubFile(i).Date).toEqual('2014-04-17');
         expect(folder.getSubFile(i).ageAtConsultTime()).toBe(16.25);
 
         done();
       });
     });
+
     describe('with ricketConsult_8819', function() {
       it('should have correct statistics', function(done) {
-        loadMock('mock_patient_10.json', 'Folder').then(function(folder) {
+        loadMock('mock_patient_10', 'Folder').then(function(folder) {
           var i = ricketConsult_8819;
 
-          expect(folder.getSubFile(i).id).toBe(8819);
+          expect(folder.getSubFile(i)).toEqual(jasmine.any(RicketConsult));
+          expect(folder.getSubFile(i)).toEqual(jasmine.anything({ id: 8819 }));
           expect(folder.getSubFile(i).ageAtConsultTime()).toBeCloseTo(16, 2);
           expect(folder.getSubFile(i).bmi()).toBeCloseTo(30.57, 1);
           expect(folder.getSubFile(i).wh()).toBeCloseTo(0.3363, 3);
@@ -66,9 +73,9 @@ describe('File', function() {
 
   describe('with patient with sex', function() {
     it('should throw error everytime', function(done) {
-      var folder = create('Folder');
-      folder.mainFile = create('Patient', { 'Sex': 'Male' });
-      var o = create('File', {}, folder);
+      var folder = new Folder();
+      folder.mainFile = new Patient({ 'Sex': 'Male' });
+      var o = new File({}, folder);
 
       expect(function() { o.ageAtConsultTime(); }).toThrow(new DataMissingException('Date'));
       expect(function() { o.bmi(); }).toThrow(new DataMissingException('Height'));
@@ -81,23 +88,4 @@ describe('File', function() {
       done();
     });
   });
-
-  // describe('with empty object', function() {
-  //   it('should throw error everytime', function(done) {
-  //     var o = create('File', {}, create('Folder'));
-
-  //     console.log('get patient:', this.getPatient());
-
-  //     expect(function() { o.ageAtConsultTime(); }).toThrow(new DataMissingException('Date'));
-  //     expect(function() { o.bmi(); }).toThrow(new DataMissingException('Height'));
-  //     expect(function() { o.wh(); }).toThrow(new DataMissingException('Height'));
-  //     expect(function() { o.ds_height(); }).toThrow(new DataMissingException('sex'));
-  //     expect(function() { o.ds_weight(); }).toThrow(new DataMissingException('sex'));
-  //     expect(function() { o.ds_weight_height(); }).toThrow(new DataMissingException('sex'));
-  //     expect(function() { o.ds_bmi(); }).toThrow(new DataMissingException('sex'));
-
-  //     done();
-  //   });
-  // });
-
 });
