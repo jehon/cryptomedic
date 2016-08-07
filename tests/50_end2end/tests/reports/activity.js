846417b79ef8cbecc2b58a1ca5f68c40a0b98d75
Jean Honlet <marielineet.jean@gmail.com>
2016-08-07 23:33:33 +0200

    Define and use a table iterator class

--- tests/50_end2end/tests/reports/activity.js
+++ tests/50_end2end/tests/reports/activity.js
@@ -1,75 +1,88 @@
 module.exports = {
   'tags': [ 'readonly', 'reports' ],
   'reportMonthlyActivity': function(client) {
+    var report_table = client.page.cryptomedic().tableIterator('#report_table table');
     client
       .page.cryptomedic().authenticate('readonly')
       .page.cryptomedic().report('monthlyActivity', { 'month': '2014-05' })
-        .assert.myAssertCell('#report_table table', 1, 1, '1')
-        .assert.myAssertCell('#report_table table', 1, 2, '2014-05')
-        .assert.myAssertCell('Ershad')
-        .assert.myAssertCell('RA') // Ramu (codage)
-        .assert.myAssertCell('2014-103')
-        .assert.myAssertCell('OSMAN')
-        .assert.myAssertCell('5y4m')
-        .assert.myAssertCell('Male')
-        .assert.myAssertCell('Old')
-        .assert.myAssertCell('4500')
-        .assert.myAssertCell('9')
-        .assert.myAssertCell('500')
-        .assert.myAssertCell('2')
-        .assert.myAssertCell('CF')
-        .assert.myAssertCell('CsP')
-        .assert.myAssertCell('Plast')
-        .assert.myAssertCell('100')
-        .assert.myAssertCell('0')
-        .assert.myAssertCell('0')
-        .assert.myAssertCell('0')
-        .assert.myAssertCell('400')
-        .assert.myAssertCell('500')
-        .assert.myAssertCell('200')
-        .assert.myAssertCell('0')
+      ;
+    report_table.assert('1')
+        .nextCol().assert('2014-05')
+        .nextCol().assert('Ershad')
+        .nextCol().assert('RA') // Ramu (codage)
+        .nextCol().assert('2014-103')
+        .nextCol().assert('OSMAN')
+        .nextCol().assert('5y4m')
+        .nextCol().assert('Male')
+        .nextCol().assert('Old')
+        .nextCol().assert('4500')
+        .nextCol().assert('9')
+        .nextCol().assert('500')
+        .nextCol().assert('2')
+        .nextCol().assert('CF')
+        .nextCol().assert('CsP')
+        .nextCol().assert('Plast')
+        .nextCol().assert('100')
+        .nextCol().assert('0')
+        .nextCol().assert('0')
+        .nextCol().assert('0')
+        .nextCol().assert('400')
+        .nextCol().assert('500')
+        .nextCol().assert('200')
+        .nextCol().assert('0')
 
         // New / Old patient in same month
-        .assert.myAssertCell('#report_table table', 2, 5, '2014-107')
-        .assert.myAssertCell('#report_table table', '=', '+4', 'New')
-        .assert.myAssertCell('#report_table table', '+1', 5, '2014-107')
-        .assert.myAssertCell('#report_table table', '=', '+4', 'Old')
+        .row(2).col(5).assert('2014-107')
+        .nextCol(4).assert('New')
 
-        .assert.myAssertCell('=', 'last', 17, '400')
-        .assert.myAssertCell('=', 'last', 'last', '300')
-        .end();
+        .nextRow(1).col(5).assert('2014-107')
+        .nextCol(4).assert('Old')
+
+        // .nextCol().assert('=', 'last', 17, '400')
+        // .nextCol().assert('=', 'last', 'last', '300')
+
+        .row('last').col(17).assert('400')
+        .row('last').col('last').assert('300')
+        ;
+    client
+      .end();
   },
 
   'reportDailyActivity': function(client) {
+    var report_table = client.page.cryptomedic().tableIterator('#report_table table');
     client
       .page.cryptomedic().authenticate('readonly')
       .page.cryptomedic().report('dailyActivity', { 'day': '2014-05-20' })
-        .assert.myAssertCell('#report_table table', 1, 2, '2014-05-20')
-        .assert.myAssertCell('Ershad')
-        .assert.myAssertCell('RA') // Ramu (codage)
-        .assert.myAssertCell('2014-103')
-        .assert.myAssertCell('OSMAN')
-        .assert.myAssertCell('5y4m')
-        .assert.myAssertCell('Male')
-        .assert.myAssertCell('Old')
-        .assert.myAssertCell('4500')
-        .assert.myAssertCell('9')
-        .assert.myAssertCell('500')
-        .assert.myAssertCell('2')
-        .assert.myAssertCell('CF')
-        .assert.myAssertCell('CsP')
-        .assert.myAssertCell('Plast')
-        .assert.myAssertCell('100')
-        .assert.myAssertCell('0')
-        .assert.myAssertCell('0')
-        .assert.myAssertCell('0')
-        .assert.myAssertCell('400')
-        .assert.myAssertCell('500')
-        .assert.myAssertCell('200')
-        .assert.myAssertCell('0')
+      ;
+    report_table.assert('1')
+        .nextCol().assert('2014-05-20')
+        .nextCol().assert('Ershad')
+        .nextCol().assert('RA') // Ramu (codage)
+        .nextCol().assert('2014-103')
+        .nextCol().assert('OSMAN')
+        .nextCol().assert('5y4m')
+        .nextCol().assert('Male')
+        .nextCol().assert('Old')
+        .nextCol().assert('4500')
+        .nextCol().assert('9')
+        .nextCol().assert('500')
+        .nextCol().assert('2')
+        .nextCol().assert('CF')
+        .nextCol().assert('CsP')
+        .nextCol().assert('Plast')
+        .nextCol().assert('100')
+        .nextCol().assert('0')
+        .nextCol().assert('0')
+        .nextCol().assert('0')
+        .nextCol().assert('400')
+        .nextCol().assert('500')
+        .nextCol().assert('200')
+        .nextCol().assert('0')
 
-        .assert.myAssertCell('=', 'last', 17, '200')
-        .assert.myAssertCell('=', 'last', 'last', '300')
-        .end();
+        .row('last').col(17).assert('200')
+        .row('last').col('last').assert('300')
+        ;
+    client
+      .end();
   }
 };
