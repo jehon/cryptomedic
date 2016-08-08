02be15870850377ad1c91dd943ff8d6d59908c05
Jean Honlet <jehon@users.noreply.github.com>
2016-08-08 18:00:00 +0200

    Make the tests works and use the new architecture

--- tests/50_end2end/tests/reports/surgical.js
+++ tests/50_end2end/tests/reports/surgical.js
@@ -1,11 +1,16 @@
 module.exports = {
   "tags": [ "readonly", "reports" ],
   "reportSurgical": function(client) {
+    var report_table = client.page.cryptomedic().tableIterator('#report_table table');
     client
-        .page.cryptomedic().authenticate("readonly")
-        .page.cryptomedic().report("surgical", { "month": "2014-01" })
-    .assert.myAssertCell("#report_table table", 1, 4, "Ukhia")
-    .assert.myAssertCell("2014-104")
-    .end();
+      .page.cryptomedic().authenticate("readonly")
+      .page.cryptomedic().report("surgical", { "month": "2014-01" })
+      ;
+    report_table
+      .col(4).assert('Ukhia')
+      .nextCol().assert('2014-104')
+      ;
+    client
+      .end();
   }
 };
