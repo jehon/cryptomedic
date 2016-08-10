4cab62027acd149c17e2cf2507afb323550396c0
Jean Honlet <marielineet.jean@gmail.com>
2016-08-10 08:19:14 +0200

    Add a FRONT to bypass prj-xvfb...

--- bin/prj-xvfb-run.sh
+++ bin/prj-xvfb-run.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
 
-xvfb-run --auto-servernum --server-args='-screen 0 1024x768x24 -ac +extension GLX +render -noreset' "$@"
+if [ "$FRONT" = "1" ]; then
+  "$@"
+else
+  xvfb-run --auto-servernum --server-args='-screen 0 1024x768x24 -ac +extension GLX +render -noreset' "$@"
+fi
