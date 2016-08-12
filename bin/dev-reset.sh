9e83074135c10f9e30a90e51b5d49122674d93ee
Jean Honlet <jehon@users.noreply.github.com>
2016-08-12 23:02:44 +0200

    Reset the database even if base.sql is not found

--- bin/dev-reset.sh
+++ bin/dev-reset.sh
@@ -19,38 +19,37 @@ else
   mkdir -p "$PRJ_DIR/tmp"
 fi
 
+#MYSQL="mysql --database=$DBNAME -u root --quick"
+MYSQL="mysql -u root --quick"
+
+if [ -z "$DBROOT" ]; then
+  PWD=""
+else
+  # Add a password to connect to the database
+  MYSQL=$MYSQL --password=$DBROOT
+fi
+
+echo "* Resetting the database $DBNAME"
+$MYSQL  --database=mysql <<-EOC
+  DROP SCHEMA IF EXISTS $DBNAME;
+  CREATE SCHEMA $DBNAME;
+  USE $DBNAME;
+  GRANT ALL PRIVILEGES ON $DBNAME   TO $DBUSER IDENTIFIED BY '$DBPASS';
+  GRANT ALL PRIVILEGES ON $DBNAME.* TO $DBUSER IDENTIFIED BY '$DBPASS';
+EOC
+
 if [ ! -r "$BASE" ]; then
   echo "No '$BASE' base sql script found"
 else
-  #MYSQL="mysql --database=$DBNAME -u root --quick"
-  MYSQL="mysql -u root --quick"
-
-  if [ -z "$DBROOT" ]; then
-    PWD=""
-  else
-    # Add a password to connect to the database
-    MYSQL=$MYSQL --password=$DBROOT
-  fi
-
-  echo "* Resetting the database $DBNAME"
-  $MYSQL  --database=mysql <<-EOC
-    DROP SCHEMA IF EXISTS $DBNAME;
-    CREATE SCHEMA $DBNAME;
-    USE $DBNAME;
-    GRANT ALL PRIVILEGES ON $DBNAME   TO $DBUSER IDENTIFIED BY '$DBPASS';
-    GRANT ALL PRIVILEGES ON $DBNAME.* TO $DBUSER IDENTIFIED BY '$DBPASS';
-EOC
-
   echo "* Loading $BASE into $DBNAME"
   cat "$PRJ_DIR/conf/database/base.sql" | $MYSQL  --database=$DBNAME
-
-  echo "* Upgrading $DBNAME"
-  $PRJ_DIR/bin/prj-db-upgrade
-
-  echo "* Applying dev hooks to $DBNAME"
-  $PRJ_DIR/bin/prj-db-upgrade "$PRJ_DIR/conf/database/always-dev/"
 fi
 
+echo "* Upgrading $DBNAME"
+$PRJ_DIR/bin/prj-db-upgrade
+
+echo "* Applying dev hooks to $DBNAME"
+$PRJ_DIR/bin/prj-db-upgrade "$PRJ_DIR/conf/database/always-dev/"
 
 echo "* Reset the live folder from live-for-test"
 rsync                       \
@@ -67,3 +66,4 @@ if [ -x "$PRJ_DIR/dev-reset-custom.sh" ]; then
 fi
 
 run-parts --regex="^[a-z0-9_.]+$" --report $PRJ_DIR/bin/dev-reset.d
+
