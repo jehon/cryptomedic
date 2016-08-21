f7e0fd5d840edb4f96acbdcb115617e23f801cff
Jean Honlet <jehon@users.noreply.github.com>
2016-08-21 08:21:32 +0200

    Remove after check and make after fill in works

--- tests/50_end2end/commands/myForm.js
+++ tests/50_end2end/commands/myForm.js
@@ -1,26 +1,18 @@
-exports.command = function(selector, fields, button, afterFillIn, afterCheck) {
+exports.command = function(selector, fields, button, afterFillIn) {
   this.
     myFormFillIn(selector, fields);
 
   if (typeof(afterFillIn) == 'function') {
-    afterFillIn.bind(this, this);
+    afterFillIn.call(this, this);
   }
 
-  if (typeof(button) == 'function') {
-    button.bind(this, this);
-  } else {
-    // Submit the form:
-    this
-      .pause(100)
-      .myClick(button);
-  }
+  // Submit the form:
+  this
+    .pause(100)
+    .myClick(button);
 
   this
     .myFormCheck(selector, fields);
-
-  if (typeof(afterCkeck) == 'function') {
-    afterCheck.bind(this, this);
-  }
 
   return this;
 };
