d9f606d4eb9b26b3bdcc4373b4f2efcc7e4aab28
Jean Honlet <marielineet.jean@gmail.com>
2016-08-21 22:02:46 +0200

    Add id for testing

--- www/templates/templates/bill_fiche.php
+++ www/templates/templates/bill_fiche.php
@@ -89,7 +89,7 @@
           <?php (new t("Bill.sl_numberOfHouseholdMembers"))->tr("Number of Houslehold Members")->p(); ?>
           <tr>
             <td>Salary ratio</td>
-            <td><span catch-it ng-model="folder" tryit="currentFile().ratioSalary()">{{ currentFile().ratioSalary() | number:0 }}</span></td>
+            <td><span id='salary_ratio' catch-it ng-model="folder" tryit="currentFile().ratioSalary()">{{ currentFile().ratioSalary() | number:0 }}</span></td>
           </tr>
           <?php (new t("Bill.Sociallevel"))->id("calculated_social_level")->readOnly()->tr("Calculated Social Level")->p(); ?>
         </table>
