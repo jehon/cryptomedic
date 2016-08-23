4cbd34c248dbf2e4250c44d0f3d95202bc05d25a
Jean Honlet <marielineet.jean@gmail.com>
2016-08-23 22:00:01 +0200

    Reformat

--- app/status.js
+++ app/status.js
@@ -1,15 +1,15 @@
 'use strict';
 
-import catalog              from 'reducers/catalog';
-import dispatch             from 'reducers/dispatch';
-import store                from 'reducers/store';
+import catalog                  from 'reducers/catalog';
+import dispatch                 from 'reducers/dispatch';
+import store                    from 'reducers/store';
 
-import date2CanonicString   from 'helpers/date2CanonicString';
-import Database             from 'helpers/database';
-import myFetch              from 'helpers/myFetch';
-import service_backend_fn   from 'helpers/service_backend';
+import date2CanonicString       from 'helpers/date2CanonicString';
+import Database                 from 'helpers/database';
+import myFetch                  from 'helpers/myFetch';
+import service_backend_fn       from 'helpers/service_backend';
 
-import selectFile           from 'actions/selectFile';
+import selectFile               from 'actions/selectFile';
 import { DataMissingException } from 'helpers/exceptions';
 import { ApplicationException } from 'helpers/exceptions';
 
