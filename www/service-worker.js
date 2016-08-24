17d85800f5105f24e7f1c17bcfa2b94ceb914740
Jean Honlet <jehon@users.noreply.github.com>
2016-08-24 22:59:05 +0200

    Clean up

--- www/service-worker.js
+++ www/service-worker.js
@@ -30,8 +30,6 @@ var cacheName = 'sw-precache-v2-sw-precache-' + (self.registration ? self.regist
 
 var ignoreUrlParametersMatching = [/^utm_/];
 
-
-
 var addDirectoryIndex = function (originalUrl, index) {
     var url = new URL(originalUrl);
     if (url.pathname.slice(-1) === '/') {
