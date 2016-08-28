4f4db93977279499e63e9a43abac892e6deecf76
Jean Honlet <jehon@users.noreply.github.com>
2016-08-28 13:05:39 +0200

    Add build script

---
+++ bin/prj-build.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# Stop on error
+set -e
+
+SCRIPT_DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
+PRJ_DIR=$(dirname "$SCRIPT_DIR")
+
+cd "$PRJ_DIR"
+
+# Run project custom files
+run-parts --report $PRJ_DIR/bin/prj-install-dependancies.d
