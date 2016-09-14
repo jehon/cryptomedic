5f49fd9864eed3302171a4c6a13b9c7f410df66d
Jean Honlet <marielineet.jean@gmail.com>
2016-09-14 21:12:04 +0200

    Test user with wrong credentials

--- tests/50_end2end/tests/ro/login_logout.js
+++ tests/50_end2end/tests/ro/login_logout.js
@@ -1,4 +1,10 @@
 module.exports = {
+  "wrong_password": function(client) {
+    client
+      .page.cryptomedic().authenticate_fillIn("user_that_does_not_exists_at_all")
+      .waitForElementPresent("#login_error");
+  },
+
   "authenticate_multiple": function(client) {
     client
       .page.cryptomedic().authenticate("readonly")
