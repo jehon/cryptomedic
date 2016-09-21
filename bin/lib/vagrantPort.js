83c2c8b706f28b0684dcd883407b47dd3cfff40c
Jean Honlet <jehon@users.noreply.github.com>
2016-09-21 08:29:26 +0200

    Enhance message display

--- bin/lib/vagrantPort.js
+++ bin/lib/vagrantPort.js
@@ -1,20 +1,27 @@
 
 // **** Detect the remote port ****
 var remoteTarget;
-var port = 80;
-try {
-  var vport = require('child_process').execSync('vagrant port --guest ' + port, { cwd: __dirname }).toString();
-
-  if (!isNaN(vport)) {
-    port = vport.trim();
-    console.info("Detected vagrant port: -" + port + "-");
-  } else {
-    console.warn("Vagrant didn't send a valid port: ", vport);
-  }
-} catch (e) {
-  console.warn("Error running vagrant, but continuing ");
-  // console.error(e);
+if (typeof(port) == "undefined") {
+  port = 80;
 }
+{
+  var iport = port;
+  try {
+    var vport = require('child_process').execSync('vagrant port --guest ' + port, { cwd: __dirname }).toString();
 
-// module.exports.port = port;
-module.exports  = 'http://localhost:' + port + '/';
+    if (!isNaN(vport)) {
+      port = vport.trim();
+      console.info("Detected vagrant port: -" + port + "-");
+    } else {
+      console.warn("Vagrant didn't send a valid port: ", vport);
+    }
+  } catch (e) {
+    console.warn("Error running vagrant, but continuing ");
+    // console.error(e);
+  }
+
+  // module.exports.port = port;
+  module.exports  = 'http://localhost:' + port + '/';
+
+  console.log("Mapping found: ", iport, " => ", port);
+}
