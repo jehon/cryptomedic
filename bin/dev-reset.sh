e6283a7baa2dc0f7e905105100c8981d339d3d53
Jean Honlet <jehon@users.noreply.github.com>
2016-09-28 21:47:56 +0200

    Split into single files

--- bin/dev-reset.sh
+++ bin/dev-reset.sh
@@ -6,23 +6,10 @@ set -e
 SCRIPT_DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
 PRJ_DIR=$(dirname "$SCRIPT_DIR")
 
-# Give it to any sub-scripts
-export PRJ_DIR
 
 DBROOTUSER=`php $PRJ_DIR/config.php 'database.rootuser'`
 DBROOTPASS=`php $PRJ_DIR/config.php 'database.rootpwd'`
-DBNAME=`php $PRJ_DIR/config.php 'database.schema'`
-DBUSER=`php $PRJ_DIR/config.php 'database.username'`
-DBPASS=`php $PRJ_DIR/config.php 'database.password'`
-BASE="$PRJ_DIR"/conf/database/base.sql
 
-
-echo "* Cleaning tmp"
-if [ -d "$PRJ_DIR/tmp" ]; then
-  find "$PRJ_DIR/tmp/" -mindepth 1 ! -name '.gitkeep' -delete
-else
-  mkdir -p "$PRJ_DIR/tmp"
-fi
 
 #MYSQL="mysql --database=$DBNAME -u root --quick"
 MYSQL="mysql --quick"
@@ -37,27 +24,9 @@ if [ ! -z "$DBROOTPASS" ]; then
   MYSQL="$MYSQL --password=$DBROOTPASS"
 fi
 
-echo "* Resetting the database $DBNAME"
-$MYSQL  --database=mysql <<-EOC
-  DROP SCHEMA IF EXISTS $DBNAME;
-  CREATE SCHEMA $DBNAME;
-  USE $DBNAME;
-  GRANT ALL PRIVILEGES ON $DBNAME   TO $DBUSER IDENTIFIED BY '$DBPASS';
-  GRANT ALL PRIVILEGES ON $DBNAME.* TO $DBUSER IDENTIFIED BY '$DBPASS';
-EOC
-
-if [ ! -r "$BASE" ]; then
-  echo "No '$BASE' base sql script found"
-else
-  echo "* Loading $BASE into $DBNAME"
-  cat "$PRJ_DIR/conf/database/base.sql" | $MYSQL  --database=$DBNAME
-fi
-
-echo "* Upgrading $DBNAME"
-$PRJ_DIR/bin/prj-db-upgrade
-
-echo "* Applying dev hooks to $DBNAME"
-$PRJ_DIR/bin/prj-db-upgrade "$PRJ_DIR/conf/database/always-dev/"
+# Give it to any sub-scripts
+export PRJ_DIR
+export $MYSQL
 
 # Run project custom files
 run-parts --exit-on-error --report $PRJ_DIR/bin/dev-reset.d
