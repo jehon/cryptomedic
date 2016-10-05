d1e9b72a60cbc30b6d4c29d326a7689c39582b0e
Jean Honlet <marielineet.jean@gmail.com>
2016-10-05 07:06:39 +0200

    Clean up handler

--- www/api/v1.0/app/Exceptions/Handler.php
+++ www/api/v1.0/app/Exceptions/Handler.php
@@ -18,55 +18,14 @@ class Handler extends ExceptionHandler
      *
      * @var array
      */
-    protected $dontReport = [
-        \Illuminate\Auth\AuthenticationException::class,
-        \Illuminate\Auth\Access\AuthorizationException::class,
-        \Symfony\Component\HttpKernel\Exception\HttpException::class,
-        \Illuminate\Database\Eloquent\ModelNotFoundException::class,
-        \Illuminate\Session\TokenMismatchException::class,
-        \Illuminate\Validation\ValidationException::class,
-    ];
-
-    /**
-     * Report or log an exception.
-     *
-     * This is a great spot to send exceptions to Sentry, Bugsnag, etc.
-     *
-     * @param  \Exception  $exception
-     * @return void
-     */
-    public function report(Exception $exception)
-    {
-        parent::report($exception);
-    }
-
-    /**
-     * Render an exception into an HTTP response.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  \Exception  $exception
-     * @return \Illuminate\Http\Response
-     */
-    public function render($request, Exception $exception)
-    {
-        return parent::render($request, $exception);
-    }
-
-    /**
-     * Convert an authentication exception into an unauthenticated response.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  \Illuminate\Auth\AuthenticationException  $exception
-     * @return \Illuminate\Http\Response
-     */
-    protected function unauthenticated($request, AuthenticationException $exception)
-    {
-        if ($request->expectsJson()) {
-            return response()->json(['error' => 'Unauthenticated.'], 401);
-        }
-
-        return redirect()->guest('login');
-    }
+    // protected $dontReport = [
+    //     \Illuminate\Auth\AuthenticationException::class,
+    //     \Illuminate\Auth\Access\AuthorizationException::class,
+    //     \Symfony\Component\HttpKernel\Exception\HttpException::class,
+    //     \Illuminate\Database\Eloquent\ModelNotFoundException::class,
+    //     \Illuminate\Session\TokenMismatchException::class,
+    //     \Illuminate\Validation\ValidationException::class,
+    // ];
 
     protected function convertExceptionToResponse(Exception $e)
     {
@@ -80,11 +39,9 @@ class Handler extends ExceptionHandler
         // $handler = new SymfonyExceptionHandler(config('app.debug'));
 
         // $html = $handler->getHtml($e);
-
-
+        // 500 <= $e->getStatusCode();
         return SymfonyResponse::create($html, $e->getStatusCode(), $e->getHeaders());
     }
-
 }
 
 
