b3249eae22ceb11e978c1e51e156e4b585e09e85
Jean Honlet <jehon@users.noreply.github.com>
2016-10-06 21:00:18 +0200

    Migrate activity test

--- www/api/v1.0/tests/ReportActivityMonthlyTest.php
+++ www/api/v1.0/tests/ReportActivityMonthlyTest.php
@@ -1,12 +1,14 @@
 <?php
 
 require_once("RouteReferenceTestCase.php");
+require_once("ReportActivityDailyTest.php");
 
 class ReportActivityMonthlyTest extends ReportActivityDailyTest {
 
 	public function setUp($url = null, $params = array()) {
 		parent::setUp();
-		$this->setUrl("reports/monthlyActivity");
+		$this->setUrl("reports/activity/month");
+    $this->timingParam = [ 'month' => self::$nmonth ];
     $this->type = self::$MONTHLY;
 	}
 }
