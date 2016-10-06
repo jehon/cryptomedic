6db9a3c1f7b7b9235cc7bf54ffb9cd59261c0659
Jean Honlet <jehon@users.noreply.github.com>
2016-10-06 18:00:00 +0200

    Adapt to getData structure

--- www/api/v1.0/tests/ReportSurgicalTest.php
+++ www/api/v1.0/tests/ReportSurgicalTest.php
@@ -4,6 +4,7 @@ require_once("RouteReferenceTestCase.php");
 
 class ReportSurgicalTest extends RouteReferenceTestCase {
 	static public $month = "2014-01";
+	static public $year = "2014";
 
 	public function setUp($url = null, $params = array()) {
 		parent::setUp();
@@ -57,4 +58,20 @@ class ReportSurgicalTest extends RouteReferenceTestCase {
 			$this->assertEquals($v->Center, "Ukhia");
 		}
 	}
+
+	// public function testsByYear() {
+	// 	$this->setParams([ "year" => self::$year, "center" => "" ]);
+	// 	$this->myAssertUnauthorized();
+
+	// 	$this->myAssertResponseForReference("readonly");
+	// 	$this->myAssertResponseForReference("cdc");
+	// 	$this->myAssertResponseForReference("manager");
+	// 	$this->myAssertResponseForReference("admin");
+
+	// 	$json = $this->myAssertJSON("admin");
+	// 	$this->assertEquals(1, count($json->list));
+	// 	foreach($json->list as $k => $v) {
+	// 		$this->assertEquals(substr($v->Date, 0, 7), self::$month);
+	// 	}
+	// }
 }
