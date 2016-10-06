6e48c6fcccb38d973ba462104cfb7aff8d720da1
Jean Honlet <jehon@users.noreply.github.com>
2016-10-06 20:36:18 +0200

    Migrate it, but it is not tested or used already

--- www/api/v1.0/app/Http/Controllers/ReportComputersController.php
+++ www/api/v1.0/app/Http/Controllers/ReportComputersController.php
@@ -1,21 +1,10 @@
 <?php namespace App\Http\Controllers;
 
-use App\Http\Controllers\Controller;
-use DB;
-use Illuminate\Http\Response;
-use Illuminate\Support\Facades\Request;
-use App\Bill;
-
-#require_once(__DIR__ . "/../../../../php/references.php");
-use App\References;
-
 class ReportComputersController extends ReportController {
-  public function index() {
-    $this->result['list'] = DB::select("SELECT * FROM sync_computers WHERE
+  public function buildData() {
+    $this->result['list'] = $this->runSqlWithNamedParameter("SELECT * FROM sync_computers WHERE
       updated_at IS NOT NULL
       AND updated_at < DATE_ADD(NOW(), INTERVAL - 6 MONTH)
     ");
-
-    return response()->jsonOrJSONP($this->result);
   }
 }
