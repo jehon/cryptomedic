d6fb08560e3890ace6920e81aafeb6ad10518810
Jean Honlet <jehon@users.noreply.github.com>
2016-10-07 18:00:00 +0200

    Make report controller working

--- www/api/v1.0/app/Http/Controllers/ReportConsultationsController.php
+++ www/api/v1.0/app/Http/Controllers/ReportConsultationsController.php
@@ -7,32 +7,23 @@ use DB;
 use Illuminate\Http\Response;
 use Illuminate\Support\Facades\Request;
 
-#require_once(__DIR__ . "/../../../../php/references.php");
 use App\References;
 
 class ReportConsultationsController extends ReportController {
-  /* Consultations */
-  // protected function consultations_getSqlConsult($label, $table) {
-  //  $sql = ;
-  //  $sql .= " WHERE (1 = 1) " .
-  //    "AND " . $this->getReportParamFilter("day", "Nextappointment", true) .
-  //    "AND " . $this->getReportParamFilter("center", "NextCenter");
-  //  $sql .= " ORDER BY c.id";
-  //  return $sql;
-  // }
 
-  public function index() {
-    $sql = "SELECT patients.*, appointments.id as c_id, appointments.Date as c_Date, appointments.NextCenter as c_Center, "
+  public function buildData() {
+    $this->result['list'] = $this->runSqlWithNamedParameter(
+      "SELECT patients.*, appointments.id as c_id, appointments.Date as c_Date, appointments.NextCenter as c_Center, "
         . " appointments.NextAppointment as c_nextAppointment, appointments.patient_id as patient_id "
         . " FROM appointments "
         . " JOIN patients ON (appointments.patient_id = patients.id) "
         . " WHERE (1 = 1) "
-        . "AND " . $this->getReportParamFilter("day", "Nextappointment", true)
-        . "AND " . $this->getReportParamFilter("center", "NextCenter");
+        // . " AND " . $this->getParamAsSqlFilter("when", "day")
+        . " AND " . $this->getParamAsSqlFilter("day", "Nextappointment")
+        . " AND " . $this->getParamAsSqlFilter("center", "NextCenter")
 
-    $sql .= " ORDER BY appointments.Date, patients.id ";
-    $sql .= "LIMIT 100";
-    $this->result['list'] = DB::select($sql, $this->sqlBindParams);
-    return response()->jsonOrJSONP($this->result);
+        . " ORDER BY appointments.Date, patients.id "
+        . "LIMIT 100"
+      );
   }
 }
