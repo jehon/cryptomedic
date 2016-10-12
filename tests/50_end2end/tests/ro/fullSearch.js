825a2b254889d0d23236642746df2406ab959d7d
Jean Honlet <jehon@users.noreply.github.com>
2016-10-12 21:38:17 +0200

    Split test and make it work again

--- tests/50_end2end/tests/ro/fullSearch.js
+++ tests/50_end2end/tests/ro/fullSearch.js
@@ -26,16 +26,36 @@ module.exports = {
       .page.cryptomedic().authenticate("readonly")
       .myClick("#menu_search")
       ;
+  },
+  "byEntryYear": function(client) {
     testSearch(client, { "#Patient_entryyear": "2000" }, [ "2000-1" ]);
+  },
+
+  "byEntryYear2": function(client) {
     testSearch(client, { "#Patient_entryyear": "2001" }, [ "2001-1", "2001-4" ]);
+  },
+
+  "byEntryOrder": function(client) {
     testSearch(client, { "#Patient_entryorder": "104" }, [ "2014-104" ]);
+  },
+
+  "byNameUp": function(client) {
     testSearch(client, { "#Patient_Name": "OSMAN" }, [ "2014-103" ]);
     testSearch(client, { "#Patient_Name": "osman" }, [ "2014-103" ]);
-    testSearch(client, { "#Patient_Yearofbirth": 2002 }, [ "2014-107" ]);
     testSearch(client, { "#Patient_Name": "Islam" }, [ "2000-1" ]);
     testSearch(client, { "#Patient_Name": "OSM" }, [ "2014-103" ]);
+  },
+
+  "byYearOfBirth": function(client) {
+    testSearch(client, { "#Patient_Yearofbirth": 2002 }, [ "2014-107" ]);
+  },
+
+  "byPhone": function(client) {
     testSearch(client, { "#Patient_Telephone": "1813247984" }, [ "2014-105" ]);
-    testSearch(client, { "select#Patient_Pathology": 2 }, [ "2014-107", "2014-103", "2014-104", "2014-105", "2000-1" ]);
+  },
+
+  "byPathology": function(client) {
+    testSearch(client, { "select#Patient_Pathology": 'ClubFoot' }, [ "2014-107", "2014-103", "2014-104", "2014-105", "2000-1" ]);
 
     // TODO: search by sex
   }
