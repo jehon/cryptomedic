799a173d7de93a0a6222ed3dbbd5d3499dd2ddd3
Jean Honlet <jehon@users.noreply.github.com>
2016-10-17 21:48:27 +0200

    Fix maintenance patch_db script

--- www/maintenance/patch_db.php
+++ www/maintenance/patch_db.php
@@ -1,32 +1,23 @@
 <?php
 
-require_once(__DIR__ . "/../vendor/autoload.php");
-require_once(__DIR__ . "/../vendor/jehon/maintenance/lib/is_served_locally.php");
+require_once(__DIR__ . "/../../config.php");
+require_once(__DIR__ . "/../bin/lib/Database.php");
 
-require_once(__DIR__ . "/../config.php");
 global $myconfig;
 
-\Jehon\Maintenance\TryCatch::run();
-\Jehon\Maintenance\SessionProtect::run($myconfig["maintenance"]["code"], $myconfig["maintenance"]["token"]);
+if (!$myconfig['security']['admin']) {
+  die("No security.admin configured");
+}
+
+if ($_REQUEST['pwd'] != $myconfig['security']['admin']) {
+  die("No correct pwd given");
+}
 
 $db = new \Jehon\Maintenance\Database(
-    "mysql:host=" . $myconfig['database']['host'] . ";dbname=" . $myconfig['database']['schema'],
-		$myconfig['database']['username'],
-		$myconfig['database']['password'],
-		[ PDO::MYSQL_ATTR_INIT_COMMAND  => "SET CHARACTER SET 'utf8'" ]
-	);
+    "mysql:dbname={$myconfig['database']['schema']};host={$myconfig['database']['host']}",
+    $myconfig['database']['rootuser'],
+    $myconfig['database']['rootpwd']
+  );
 
-echo "<pre>";
-
-if (\Jehon\Maintenance\Lib\isServedLocally()) {
-	$db->runOne(__DIR__ . "/../conf/database/dev/reset.sql");
-  $db->runOne(__DIR__ . "/../conf/database/dev/testing.sql");
-	// $db->runOne("/home/jehon/amd_chakaria.sql");
-}
-
-$db->runOne(__DIR__ . "/../conf/database");
-$db->runOne(__DIR__ . "/../conf/database/always.sql");
-
-if (\Jehon\Maintenance\Lib\isServedLocally()) {
-	$db->runOne(__DIR__ . "/../conf/database/dev");
-}
+$db->runDirectory(__DIR__ . "/../conf/database/versions/");
+$db->runDirectory(__DIR__ . "/../conf/database/always/");
