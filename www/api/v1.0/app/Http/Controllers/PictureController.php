2d48d2efccb7c3440622c59863b24f5445855fe9
Jean Honlet <marielineet.jean@gmail.com>
2016-10-17 20:19:52 +0200

    Inline myglob

--- www/api/v1.0/app/Http/Controllers/PictureController.php
+++ www/api/v1.0/app/Http/Controllers/PictureController.php
@@ -9,7 +9,39 @@ use Illuminate\Support\Facades\Input;
 
 use App\Picture;
 
-require_once(__DIR__ . "/../../../../../../bin/lib/Database.php");
+function myglob($glob, $recursive = false) {
+  $pattern = basename($glob);
+  $path = dirname($glob);
+  if ($path == DIRECTORY_SEPARATOR) return array(".");
+
+  if (!is_readable($path)) {
+    throw new Exception("Path is not readable $path");
+  }
+
+  $handle = opendir($path);
+  if ($handle === false) {
+    return array();
+  }
+  $list = array();
+  while (false !== ($file = readdir($handle))) {
+    if ($file == ".") {
+      continue;
+    }
+    if ($file == "..") {
+      continue;
+    }
+    if (is_file(dirname($glob) . DIRECTORY_SEPARATOR . $file) && fnmatch($pattern, $file)) {
+      $list[] = $path . DIRECTORY_SEPARATOR . $file;
+    }
+    if (is_dir(dirname($glob) . DIRECTORY_SEPARATOR . $file) && $recursive) {
+      $res = myglob(dirname($glob) . DIRECTORY_SEPARATOR . $file . DIRECTORY_SEPARATOR . basename($glob), $recursive);
+      $list = array_merge($list, $res);
+    }
+  }
+  closedir($handle);
+  natsort($list);
+  return $list;
+}
 
 class PictureController extends Controller {
 
@@ -29,7 +61,7 @@ class PictureController extends Controller {
 
     echo "\n";
     echo "Check files present\n";
-    foreach(\Jehon\Maintenance\myglob(Picture::getPhysicalRoot() . "/*") as $file) {
+    foreach(myglob(Picture::getPhysicalRoot() . "/*") as $file) {
       $file = substr($file, strlen(Picture::getPhysicalRoot()));
       if (!Picture::getPictureĈountByPhysicalPath($file)) {
         echo "$file\n";
