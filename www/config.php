6fccecd48bcbb6e59ce99bb1d8904020fe9cf46a
Jean Honlet <marielineet.jean@gmail.com>
2016-10-19 20:47:31 +0200

    Remove intermediary config.php wich is not necessary anymore

--- www/config.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-if (file_exists(__DIR__ . "/../config.php")) {
-  require(__DIR__ . "/../config.php");
-} elseif (file_exists(__DIR__ . "/live/config.php")) {
-  require(__DIR__ . "/live/config.php");
-} else {
-  require(__DIR__ . "/../conf/config-dev.php");
-}
