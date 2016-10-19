24d5ecb442ef88cf9463706ea86306fe3dc8b8f9
Jean Honlet <marielineet.jean@gmail.com>
2016-10-19 20:47:47 +0200

    Configure ftp access

--- config-site.php
+++ config-site.php
@@ -4,8 +4,12 @@ global $myconfig;
 
 $myconfig['environment'] = 'dev';
 
+
+
 $myconfig['deployment']['prod'] = [
   'ftp_host' => 'ftp.cryptomedic.org',
+  'ftp_user' => $mysecrets['cryptomedic.ftp.username'],
+  'ftp_pass' => $mysecrets['cryptomedic.ftp.password'],
   'packages' => [
     [
       'extra_cmd_line' => ''
