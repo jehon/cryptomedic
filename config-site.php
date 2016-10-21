ecea79dfccf96c4205b6ebdf273abb43858f8ce8
Jean Honlet <jehon@users.noreply.github.com>
2016-10-21 20:13:03 +0200

    Import only if secrets is found

--- config-site.php
+++ config-site.php
@@ -5,14 +5,16 @@ global $myconfig;
 $myconfig['environment'] = 'dev';
 
 
-
-$myconfig['deployment']['prod'] = [
-  'ftp_host' => 'ftp.cryptomedic.org',
-  'ftp_user' => $mysecrets['cryptomedic.ftp.username'],
-  'ftp_pass' => $mysecrets['cryptomedic.ftp.password'],
-  'packages' => [
-    [
-      'extra_cmd_line' => ''
+if (file_exists(__DIR__ . "/../secrets.php")) {
+  require_once(__DIR__ . "/../secrets.php");
+  $myconfig['deployment']['prod'] = [
+    'ftp_host' => 'ftp.cryptomedic.org',
+    'ftp_user' => $mysecrets['cryptomedic.ftp.username'],
+    'ftp_pass' => $mysecrets['cryptomedic.ftp.password'],
+    'packages' => [
+      [
+        'extra_cmd_line' => '--exclude www/api/v1.0/storage/framework/'
+      ],
     ],
   ]
 ];
