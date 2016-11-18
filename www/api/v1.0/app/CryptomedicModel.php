473691deb53ad2d55086494d454639cbaf8d8a66
Jean Honlet <marielineet.jean@gmail.com>
2016-11-18 22:25:37 +0100

    Remove dead code

--- www/api/v1.0/app/CryptomedicModel.php
+++ www/api/v1.0/app/CryptomedicModel.php
@@ -77,39 +77,4 @@ class CryptomedicModel extends Model {
 		// Let's go delete it...
 		return parent::delete();
 	}
-
-// TODO: optimistic locking of files
-// 	protected function performUpdate(Builder $query, array $options = [])
-// 	{
-// 		// ???
-// 		// $query->where('updated_at', $this->updated_at);
-// 		// parent->performUpdate($query, $options);
-// 		//
-// 		// but in that case, we have no result???
-
-
-// 		$dirty = $this->getDirty();
-// 		if (count($dirty) > 0) {
-// 			if ($this->fireModelEvent('updating') === false) {
-// 				return false;
-// 			}
-
-// 			if ($this->timestamps) {
-// 				$this->updateTimestamps();
-// 			}
-
-// 			$dirty = $this->getDirty();
-
-// 			if (count($dirty) > 0) {
-// 				$query->where('updated_at', $this->updated_at);
-
-// 				if (!$this->setKeysForSaveQuery($query)->update($dirty)) {
-// 					throw new \RuntimeException('Row is dirty');
-// 				}
-// 				$this->fireModelEvent('updated', false);
-// 			}
-// 		}
-
-// 		return true;
-// 	}
 }
