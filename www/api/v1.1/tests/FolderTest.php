83e9dc82fc355ba90db60d6aa49564c9d1a6cf07
Jean Honlet <jehon@users.noreply.github.com>
2016-12-06 21:51:04 +0100

    Migrate FolderTest

--- www/api/v1.1/tests/FolderTest.php
+++ www/api/v1.1/tests/FolderTest.php
@@ -1,120 +1,166 @@
 <?php
 
-require_once("RouteReferenceTestCase.php");
+require_once("SyncableTestCase.php");
 
-class FolderTest extends RouteReferenceTestCase {
+class FolderTest extends SyncableTestCase {
 
 	public function setUp($url = null, $params = array()) {
 		parent::setUp("folder/1");
 	}
 
 	public function testsUnauthenticated() {
-		$this->setUrl("folder/1");
-		$this->myAssertUnauthorized();
+    $response = $this->myRunAssertQuery(
+        $this->getNewRequestOptionsBuilder()
+          ->setUrl("folder/1")
+        	->asUnauthenticated()
+          ->setExpected(401)
+          ->asText()
+      );
 
-		$this->setUrl("folder");
-		$this->myAssertUnauthorized();
+    $response = $this->myRunAssertQuery(
+        $this->getNewRequestOptionsBuilder()
+          ->setUrl("folder/")
+        	->asUnauthenticated()
+          ->setExpected(401)
+          ->asText()
+      );
 	}
 
 	public function test1() {
-		$this->setUrl("folder/1");
-		$this->myAssertResponseForReference("readonly");
-		$this->myAssertResponseForReference("cdc");
-		$this->myAssertResponseForReference("manager");
-		$this->myAssertResponseForReference("admin");
+    $opt = $this->getNewRequestOptionsBuilder()
+      ->setUrl("folder/1");
+
+    $json = $this->myRunAssertQuery(
+    		$opt->clone()->setUser("readonly")
+      );
+
+    $json = $this->myRunAssertQuery(
+    		$opt->clone()->setUser("cdc")
+      );
+
+    $json = $this->myRunAssertQuery(
+    		$opt->clone()->setUser("manager")
+      );
+
+    $json = $this->myRunAssertQuery(
+    		$opt->clone()->setUser("admin")
+      );
 	}
 
 	public function testSearchAllowed() {
-		$this->setUrl("folder?Name=md&entryyear=2009&pathology_Ricket=1&Telephone=1");
-		$this->myAssertResponseForReference("readonly");
-		$this->myAssertResponseForReference("cdc");
-		$this->myAssertResponseForReference("manager");
-		$this->myAssertResponseForReference("admin");
+    $json = $this->myRunAssertQuery(
+        $this->getNewRequestOptionsBuilder()
+          ->setUrl("folder?Name=md&entryyear=2009&pathology_Ricket=1&Telephone=1")
+      );
 	}
 
 	public function testSearchEntryYear() {
-		$this->setUrl("folder?entryyear=2010");
-		$json = $this->myAssertJSON("readonly");
+    $json = $this->myRunAsserQueryWithoutOffline(
+        $this->getNewRequestOptionsBuilder()
+          ->setUrl("folder?entryyear=2010")
+      );
 		foreach($json as $k => $v) {
 			$this->assertEquals(2010, $v->entryyear);
 		}
 	}
 
 	public function testSearchEntryOrder() {
-		$this->setUrl("folder?entryorder=10");
-		$json = $this->myAssertJSON("readonly");
+    $json = $this->myRunAsserQueryWithoutOffline(
+        $this->getNewRequestOptionsBuilder()
+          ->setUrl("folder?entryorder=10")
+      );
 		foreach($json as $k => $v) {
 			$this->assertEquals(10, $v->entryorder);
 		}
 	}
 
 	public function testSearchName() {
-		$this->setUrl("folder?Name=md");
-		$json = $this->myAssertJSON("readonly");
+    $json = $this->myRunAsserQueryWithoutOffline(
+        $this->getNewRequestOptionsBuilder()
+          ->setUrl("folder?Name=md")
+      );
 		foreach($json as $k => $v) {
 			$this->assertTrue(stripos($v->Name, 'md') !== false, "Name " . $v->Name . " does not match criteria");
 		}
 	}
 
 	public function testSearchNameWithJ() {
-		$this->setUrl("folder?Name=j");
-		$json = $this->myAssertJSON("readonly");
+    $json = $this->myRunAsserQueryWithoutOffline(
+        $this->getNewRequestOptionsBuilder()
+          ->setUrl("folder?Name=j")
+      );
 		foreach($json as $k => $v) {
 			$this->assertTrue(stripos($v->Name, 'j') !== false || stripos($v->Name, 'z') !== false, "Name " . $v->Name . " does not match criteria");
 		}
 	}
 
 	public function testSearchSexBoy() {
-		$this->setUrl("folder?Sex=206");
-		$json = $this->myAssertJSON("readonly");
+    $json = $this->myRunAsserQueryWithoutOffline(
+        $this->getNewRequestOptionsBuilder()
+          ->setUrl("folder?Sex=206")
+      );
+
 		foreach($json as $k => $v) {
 			$this->assertEquals(206, $v->Sex);
 		}
 	}
 
 	public function testSearchSexGirl() {
-		$this->setUrl("folder?Sex=207");
-		$json = $this->myAssertJSON("readonly");
+    $json = $this->myRunAsserQueryWithoutOffline(
+        $this->getNewRequestOptionsBuilder()
+          ->setUrl("folder?Sex=207")
+      );
+
 		foreach($json as $k => $v) {
 			$this->assertEquals(207, $v->Sex);
 		}
 	}
 
 	public function testSearchYearofbirth() {
-		$this->setUrl("folder?Yearofbirth=2000");
-		$json = $this->myAssertJSON("readonly");
+    $json = $this->myRunAsserQueryWithoutOffline(
+        $this->getNewRequestOptionsBuilder()
+          ->setUrl("folder?Yearofbirth=2000")
+      );
 		foreach($json as $k => $v) {
 			$this->assertEquals(2000, $v->Yearofbirth);
 		}
 	}
 
 	public function testSearchTelephone() {
-		$this->setUrl("folder?Telephone=2");
-		$json = $this->myAssertJSON("readonly");
+    $json = $this->myRunAsserQueryWithoutOffline(
+        $this->getNewRequestOptionsBuilder()
+          ->setUrl("folder?Telephone=2")
+      );
 		foreach($json as $k => $v) {
 			$this->assertTrue(strpos($v->Telephone, "2") !== false);
 		}
 	}
 
 	public function testSearchRicket() {
-		$this->setUrl("folder?Pathology=Ricket");
-		$json = $this->myAssertJSON("readonly");
+    $json = $this->myRunAsserQueryWithoutOffline(
+        $this->getNewRequestOptionsBuilder()
+          ->setUrl("folder?Pathology=Ricket")
+      );
 		foreach($json as $k => $v) {
 			$this->assertEquals("Ricket", $v->Pathology);
 		}
 	}
 
 	public function testSearchClubFoot() {
-		$this->setUrl("folder?Pathology=ClubFoot");
-		$json = $this->myAssertJSON("readonly");
+    $json = $this->myRunAsserQueryWithoutOffline(
+        $this->getNewRequestOptionsBuilder()
+          ->setUrl("folder?Pathology=ClubFoot")
+      );
 		foreach($json as $k => $v) {
 			$this->assertEquals("ClubFoot", $v->Pathology);
 		}
 	}
 
 	public function testSearchOther() {
-		$this->setUrl("folder?Pathology=Other");
-		$json = $this->myAssertJSON("readonly");
+    $json = $this->myRunAsserQueryWithoutOffline(
+        $this->getNewRequestOptionsBuilder()
+          ->setUrl("folder?Pathology=Other")
+      );
 		foreach($json as $k => $v) {
 			$this->assertEquals("Other", $v->Pathology);
 		}
