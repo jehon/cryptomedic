7769173749e24add4eb142645ee45f77af2f50b7
Jean Honlet <jehon@users.noreply.github.com>
2016-12-15 22:41:42 +0100

    Test non existing codage

--- tests/20_js/test_codage.js
+++ tests/20_js/test_codage.js
@@ -11,10 +11,17 @@ describe('test jh-codage', function() {
   };
 
   testWithComponent("jh-codage", "<jh-codage value='original'></jh-codage>", function(el, done) {
-    // console.log("Running tests");
     expect(el).not.toBeNull();
-    expect(el.$$('#translated').textContent).toEqual('codage');
     expect(el.$$('#original')  .textContent).toEqual('original');
+    expect(el.$$('#translating').attributes.hidden).toBeUndefined();
+    expect(el.$$('#translated').textContent).toEqual('codage');
+    done();
+  });
+
+  testWithComponent("jh-codage", "<jh-codage value='anything'></jh-codage>", function(el, done) {
+    expect(el).not.toBeNull();
+    expect(el.$$('#original')  .textContent).toEqual('anything');
+    expect(el.$$('#translating').attributes.hidden).not.toBeNull();
     done();
   });
 });
