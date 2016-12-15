a6e2451b0ebf88f7bfb222c0d4fb8f095d2e2e5f
Jean Honlet <jehon@users.noreply.github.com>
2016-12-15 22:32:20 +0100

    Make the test pass

--- tests/20_js/test_codage.js
+++ tests/20_js/test_codage.js
@@ -2,20 +2,19 @@
 /* global testWithComponent */
 
 describe('test jh-codage', function() {
-  it("Check component instantiation.", function(done) {
-    window.cryptomedic = {
-      serverSettings: {
-        codes: {
-          'original': 'codage'
-        }
+  window.cryptomedic = {
+    serverSettings: {
+      codes: {
+        'original': 'codage'
       }
-    };
+    }
+  };
 
-    testWithComponent("<jh-codage value='original'></jh-codage>", done, function(el, done) {
-      expect(el.querySelector('jh-codage')).not.toBeNull();
-      expect(el.querySelector('jh-codage').$$('#translated').textContent).toEqual('codage');
-      expect(el.querySelector('jh-codage').$$('#original')  .textContent).toEqual('original');
-      done();
-    });
+  testWithComponent("jh-codage", "<jh-codage value='original'></jh-codage>", function(el, done) {
+    // console.log("Running tests");
+    expect(el).not.toBeNull();
+    expect(el.$$('#translated').textContent).toEqual('codage');
+    expect(el.$$('#original')  .textContent).toEqual('original');
+    done();
   });
 });
