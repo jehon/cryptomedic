45d4c292c028bc0aeb436a2abdbb9a088d76e2b3
Jean Honlet <jehon@users.noreply.github.com>
2016-12-21 21:34:53 +0100

    Split database into database and databaseNew
    To avoid conflict in old and new structure

--- www/static/objects/database.js
+++ www/static/objects/database.js
@@ -1,3 +1,5 @@
+/* global Dexie */
+/* exported Database */
 
 // Shared database db object...
 
@@ -12,103 +14,11 @@ let Database = (function() {
 
         db = new Dexie('cryptomedic');
 
-        db.version(1).stores({
-          patients: '++id'
-        });
-
-        db.version(2).stores({
-          patients: '++id,[mainFile.entryyear+mainFile.entryorder]'
-        });
-
-        db.version(3).stores({
-          patients: '++id'
-        });
-
-        db.version(4).stores({
-          // @see
-          // db.relations.where('[userId1+userId2]').equals([2,3]).or('[userId1+userId2]').equals([3,2])
-          // - will give you all the relations that user 1 has to user 2 or user 2
-          // has to user 1.
-          patients: '++id,[mainFile.entryyear+mainFile.entryorder]'
-        });
-
-        db.version(5).upgrade(function(trans) {
-          trans.patients.toCollection().modify((p) => {
-            if (typeof(p.id) == 'number') {
-              delete this.value;
-            }
-            p.id = '' + p.id;
-          });
-        });
-
-        db.version(6).stores({
-          patients: '++id,[mainFile.entryyear+mainFile.entryorder]',
-          settings: 'key'
-        });
-
-        db.version(7).stores({
-          patients: '++id,[mainFile.entryyear+mainFile.entryorder]',
-          settings: 'key',
-          Patient:       'id,[entryyear+entryorder]',
-          Appointment:   'id,patient_id,Date,Nextappointment,NextCenter,[Nextappointment+NextCenter]',
-          Bill:          'id,patient_id,Date',
-          ClubFoot:      'id,patient_id,Date',
-          OtherConsult:  'id,patient_id,Date',
-          Payment:       'id,bill_id,   Date',
-          Picture:       'id,patient_id,Date',
-          RicketConsult: 'id,patient_id,Date',
-          Surgery:       'id,patient_id,Date',
-        });
-
-        // db.version(8).upgrade(function(trans) {
-        //   trans.patients.toCollection().modify((p) => {
-
-        //   });
-        // });
+        // Structure is managed in DatabaseNew.js
 
         db.open();
       }
-
-      // @See https://github.com/dfahlander/db.js/wiki/Table.mapToClass()
-      if (typeof(Patient) != "undefined") {
-        db.Patient      .mapToClass(Patient);
-        db.Appointment  .mapToClass(Appointment);
-        db.Bill         .mapToClass(Bill);
-        db.ClubFoot     .mapToClass(ClubFoot);
-        db.OtherConsult .mapToClass(OtherConsult);
-        db.Payment      .mapToClass(Payment);
-        db.Picture      .mapToClass(Picture);
-        db.RicketConsult.mapToClass(RicketConsult);
-        db.Surgery      .mapToClass(Surgery);
-      }
     }
-
-    // ------------------ Business functions v2 ------------------------------
-    /**
-     *
-     * Store a record in the correct database
-     *
-     */
-    storeInDB(type, record) {
-      // console.log("storeInDB: ", type, record.id);
-      return db[type].put(record);
-    }
-
-    deleteInDB(type, id) {
-      // console.log("deleteInDB: ", type, id);
-      return db[type].delete(id);
-    }
-
-    checkpointInDB(checkpoint = false) {
-      if (checkpoint) {
-        // console.log("checkpointInDB: ", checkpoint);
-        if (localStorage.syncCheckpoint < checkpoint) {
-          localStorage.syncCheckpoint = checkpoint;
-        }
-      }
-      return checkpoint;
-    }
-
 
     // ------------------ Business functions ------------------------------
     /**
