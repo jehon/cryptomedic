59a1a110c7dfa21498b49c76f1efb528d71b4736
Jean Honlet <jehon@users.noreply.github.com>
2017-01-21 21:49:41 +0100

    End removing redux

--- app/mainApp.js
+++ app/mainApp.js
@@ -2,9 +2,18 @@
 
 import angular                  from 'angular';
 import                               'angular-route';
-import store                    from 'reducers/store';
-import dispatch                 from 'reducers/dispatch';
-import catalog                  from 'reducers/catalog';
+import activateCache            from 'service-worker-registration';
+
+if (location.protocol == 'https:') {
+  if (location.pathname.split('/')[1] != 'online') {
+    console.info('[SW] Detection: offline mode, activating plugin');
+    activateCache();
+  } else {
+    console.info('[SW] Detection: online mode');
+  }
+} else {
+  console.info('[SW] Detection: https not detcted, online mode');
+}
 
 var application = {};
 
@@ -24,15 +33,15 @@ var mainApp = angular.module('app_main', [ 'ngRoute' ])
       return '' + (Math.round(text * 100 * Math.pow(10, rnd)) / Math.pow(10, rnd)) + '%';
     };
   })
-  .filter('nl2br', [ '$sce', function($sce) {
-    return function(text) {
-      var t = text;
-      while (t.search('\n') >= 0) {
-        t = t.replace('\n', '<br>');
-      }
-      return $sce.trustAsHtml(t);
-    };
-  }])
+  // .filter('nl2br', [ '$sce', function($sce) {
+  //   return function(text) {
+  //     var t = text;
+  //     while (t.search('\n') >= 0) {
+  //       t = t.replace('\n', '<br>');
+  //     }
+  //     return $sce.trustAsHtml(t);
+  //   };
+  // }])
   .directive('catchIt', function() {
     // http://tutorials.jenkov.com/angularjs/custom-directives.html#compile-and-link
     // http://stackoverflow.com/a/15298620
@@ -101,21 +110,21 @@ var mainApp = angular.module('app_main', [ 'ngRoute' ])
         // var transcludeScope = $scope.$parent.$new();
 
           $element[0].onchange = function() {
-            var busy = $scope.doBusy('Reducing the picture');
+            // var busy = $scope.doBusy('Reducing the picture');
 
           // http://hacks.mozilla.org/2011/01/how-to-develop-a-html5-image-uploader/
             var file = $element[0].files[0];
             if (!file.type.match(/image.*/)) {
               console.error('Not a picture?');
               alert('Are you sure it is a picture? If it is a picture, please send it by email to marielineet.jean@gmail.com to debug the application. Thank you');
-              busy();
+              // busy();
             }
 
             var img = document.createElement('img');
             var reader = new FileReader();
             reader.onerror = function(e) {
               console.error(e);
-              busy();
+              // busy();
             };
 
             reader.onload = function(e) {
@@ -151,7 +160,7 @@ var mainApp = angular.module('app_main', [ 'ngRoute' ])
                 $scope.currentFile().fileContent = dataURI;
                 $scope.currentFile().OriginalName = file.name;
                 $scope.$emit('revalidate');
-                busy();
+                // busy();
               };
             };
             reader.readAsDataURL(file);
@@ -186,15 +195,6 @@ mainApp.controller('ctrl', [ '$scope', function($scope) {
   $scope.calculations = calculations;
   $scope.template     = template;
 
-
-  $scope.appStateStore = store.getState();
-  store.subscribe(function() {
-    console.log('scope appState updated', store.getState());
-    $scope.appStateStore = store.getState();
-
-    $scope.safeApply();
-  });
-
   $scope.safeApply = function (fn) {
     if (this.$root && (this.$root.$$phase == '$apply' || this.$root.$$phase == '$digest')) {
       if (fn && (typeof(fn) === 'function')) {
@@ -212,50 +212,10 @@ mainApp.controller('ctrl', [ '$scope', function($scope) {
   $scope.sync = false;
   $scope.connected = false;
 
-  $scope.doBusy = function(msg) {
-    // TODO: remove this...
-    dispatch(catalog.STATE_BUSY, msg);
-    return function() {
-      dispatch(catalog.STATE_READY);
-    };
-  };
-
-  $scope.endBusy = dispatch.bind(this, catalog.STATE_BUSY);
-
   $scope.username = '';
   $scope.password = '';
 
   $scope.$on('$routeChangeError', function() { console.error('error in routes', arguments); });
-
-  $scope.messages = [];
-  var interval = 0;
-  $scope.$on('message', function(event, data) {
-    if (!data.level) {
-      data.level = 'success';
-    }
-    if (!data.text) {
-      data.text = 'Error!';
-    }
-    if (!data.seconds) {
-      data.seconds = 8;
-    }
-    var t = new Date();
-    data.timeout = t.setSeconds(t.getSeconds() + data.seconds);
-    $scope.messages.push(data);
-    if (interval == 0) {
-      interval = setInterval(function() {
-        var now = new Date();
-        $scope.messages = $scope.messages.filter(function(value) {
-          return (value.timeout >= now);
-        });
-        if ($scope.messages.length == 0) {
-          clearInterval(interval);
-          interval = 0;
-        }
-        $scope.safeApply();
-      }, 1000);
-    }
-  });
 }]);
 
 mainApp.controller('ctrl_allGraphics',      ctrl_allGraphics);
@@ -291,7 +251,6 @@ mainApp.config([ '$routeProvider', function($routeProvider) {
       controller: 'ctrl_users',
     }).otherwise({ 'redirectTo': '/home'});
 }]);
-
 
 window.cryptomedic = {
   serverSettings: {}
