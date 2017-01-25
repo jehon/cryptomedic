d0bd16d377c68c01215d0f6012d2281511ab9095
Jean Honlet <marielineetjean@gmail.com>
2017-01-25 22:48:27 +0100

    Export service-worker-registration out of webpack

--- app/mainApp.js
+++ app/mainApp.js
@@ -2,18 +2,6 @@
 
 import angular                  from 'angular';
 import                               'angular-route';
-import activateCache            from 'service-worker-registration';
-
-if (location.protocol == 'https:') {
-  if (location.pathname.split('/')[1] != 'online') {
-    console.info('[SW] Detection: offline mode, activating plugin');
-    activateCache();
-  } else {
-    console.info('[SW] Detection: online mode');
-  }
-} else {
-  console.info('[SW] Detection: https not detcted, online mode');
-}
 
 var application = {};
 
