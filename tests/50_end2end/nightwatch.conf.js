ddfabd3a76dd831d93bb1274b0ab6df46e8de8ba
Jean Honlet <jehon@users.noreply.github.com>
2017-01-26 20:16:19 +0100

    Fix new launch url

--- tests/50_end2end/nightwatch.conf.js
+++ tests/50_end2end/nightwatch.conf.js
@@ -30,7 +30,7 @@ module.exports = {
   },
   "test_settings" : {
     "default" : {
-      "launch_url" : remoteTarget + "/build/index.html",
+      "launch_url" : remoteTarget + "/static/index.html",
       "selenium_port"  : 4444,
       "selenium_host"  : "127.0.0.1",
       "silent": true,
