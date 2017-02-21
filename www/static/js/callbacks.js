768a91b12757ae73db0b601c649303cc1d899731
Jean Honlet <jehon@users.noreply.github.com>
2017-02-21 20:27:03 +0100

    Keep the last value in memory
    - Fire immediately any added callback
    - Centralize the initialization of the value

--- www/static/js/callbacks.js
+++ www/static/js/callbacks.js
@@ -1,12 +1,15 @@
 /* global jQuery */
 /* exported userCb, syncRemainingCb */
-let userCb = jQuery.Callbacks();
+let userCb = jQuery.Callbacks('memory');
 // Debug
 userCb.add((data) => {
   console.log("User callback: ", data);
 });
 
-let syncRemainingCb = jQuery.Callbacks();
+let syncRemainingCb = jQuery.Callbacks('memory');
+
+// By default, we don't know if there is data to sync
+syncRemainingCb.fire("?");
 
 // // Debug
 // syncRemainingCb.add((data) => {
