c88d1917736b04a05ea24e9520172283a4c90134
Jean Honlet <jehon@users.noreply.github.com>
2017-02-21 21:46:10 +0100

    Add extension on server-side

--- www/static/js/template.js
+++ www/static/js/template.js
@@ -1,6 +1,7 @@
 /* global API_VERSION */
 
+/* exported template */
 function template(...names) {
   // console.log('requesting template: ' + names.join('_'));
-  return '/api/' + API_VERSION + '/templates/' + names.join('_') + '.php';
+  return '/api/' + API_VERSION + '/templates/' + names.join('_');
 }
