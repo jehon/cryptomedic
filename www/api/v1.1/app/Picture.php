a994bf4acbe5579290b42bd1ce7a3e55d6177e82
Jean Honlet <marielineetjean@gmail.com>
2017-03-08 20:03:06 +0100

    Factorize calculating physical path based on mime type

--- www/api/v1.1/app/Picture.php
+++ www/api/v1.1/app/Picture.php
@@ -49,6 +49,29 @@ class Picture extends CryptomedicModel {
     return $dir . $this->file;
 	}
 
+	public function calculateTargetName($mimetype) {
+		switch ($mimetype) {
+			case "image/png":
+				$ext = "png";
+				break;
+			case "image/jpeg":
+				$ext = "jpg";
+				break;
+			default:
+				throw new \Error("Invalid mimetype: " . $mimetype);
+		}
+
+		$this->file = "{$this->patient_id}_"
+			. ($this->Date == null ? "undated" : $this->Date)
+			. "_{$this->id}.{$ext}";
+
+		if (file_exists($this->getPhysicalPath())) {
+			abort(500, "Moving uploaded file to " . $this->getPhysicalPath() . ": already exists");
+		}
+
+		return $this->getPhysicalPath();
+	}
+
 	public static function create(array $attributes = array()) {
 		$model = parent::create($attributes);
 		if (!$model->id) {
@@ -65,27 +88,14 @@ class Picture extends CryptomedicModel {
 			$v = substr($dataURI, strlen("data:"));
 			$mimetype = substr($v, 0, strpos($v, ";"));
 			$content64 = substr($v, strpos($v, ",") + 1);
-
-			switch ($mimetype) {
-				case "image/png":
-					$ext = "png";
-					break;
-				case "image/jpeg":
-					$ext = "jpg";
-					break;
-				default:
-					throw new StorageCreateError("Invalid extension");
-			}
-			$model->file = "{$model->patient_id}_"
-						. ($model->Date == null ? "undated" : $model->Date)
-						. "_{$model->id}.{$ext}";
 			$contentRaw = base64_decode($content64);
+
+			$model->calculateTargetName($mimetype);
+
 			if (!$contentRaw) {
 				abort(500, "Received data is empty");
 			}
-			if (file_exists($model->getPhysicalPath())) {
-				abort(500, "Moving uploaded file to " . $model->getPhysicalPath() . ": already exists");
-			}
+
 			if (!file_put_contents($model->getPhysicalPath(), $contentRaw)) {
 				abort(500, "Storing uploaded file to " . $model->getPhysicalPath());
 			}
