38835c956e64b6196d835906aa51b14ffb28342a
Jean Honlet <marielineetjean@gmail.com>
2017-03-13 21:01:57 +0100

    Make test works
    Using TDD :-)

--- tests/unitjs/test_jh-select.js
+++ tests/unitjs/test_jh-select.js
@@ -2,41 +2,187 @@
 /* global testComponent */
 
 describe('test jh-select', function() {
-  it("should radio correctly", function(done) {
-    let list = [ 'truc' , 'brol' , 'machin', 'chose' ];
-    testComponent("<jh-select value='truc' list='" + JSON.stringify(list) + "'></jh-select>").then(el => {
-      expect(el).not.toBeNull();
-      expect(el.$$('input[type=radio]')).not.toBeNull();
-      expect(el.$$('select').clientHeight).toBe(0);
-      expect(el.$$('input[type=radio][value=\'truc\']:checked')).not.toBeNull();
-      expect(el.value).toBe("truc");
+  let listRadio  = [ 'truc' , 'brol' , 'machin', 'chose' ];
+  let listSelect = [ 'truc' , 'brol' , 'machin', 'chose', 'bazar', 'ça', 'là' ];
 
-      el.value = 'brol';
-      expect(el.$$('input[type=radio][value="truc"]:checked')).toBeNull();
-      expect(el.$$('input[type=radio][value="brol"]:checked')).not.toBeNull();
-      expect(el.value).toBe("brol");
+  let checkRadio = (el, v) => {
+    // Are we set?
+    expect(el).not.toBeNull();
+    expect(el.$$('input[type=radio]')).not.toBeNull();
+    expect(el.$$('select')).toBeNull();
+    expect(el.getAttribute('mode')).toBe("radio");
+
+    // Check the value
+    expect(el.$$('input[type=radio][value=' + v + ']').getAttribute('checked')).toBeTruthy();
+    expect(el.value).toBe(v);
+  }
+
+  let checkRadioNull = (el) => {
+    // Are we set?
+    expect(el).not.toBeNull();
+
+    // Check the mode
+    expect(el.$$('input[type=radio]')).not.toBeNull();
+    expect(el.$$('select')).toBeNull();
+    expect(el.getAttribute('mode')).toBe("radio");
+
+    // Check the value
+    expect(el.$$('input[type=radio][null]').getAttribute('checked')).toBeTruthy();
+    expect(el.value).toBe(null);
+  }
+
+  let checkSelect = (el, v) => {
+    // Are we set?
+    expect(el).not.toBeNull();
+
+    // Check the mode
+    expect(el.$$('input[type=radio]')).toBeNull();
+    expect(el.$$('select')).not.toBeNull();
+    expect(el.getAttribute('mode')).toBe("select");
+
+    // Check the value
+    expect(el.$$('select').value).toBe(v);
+    expect(el.value).toBe(v);
+  }
+
+  let checkSelectNull = (el) => {
+    // Are we set?
+    expect(el).not.toBeNull();
+
+    // Check the mode
+    expect(el.$$('input[type=radio]')).toBeNull();
+    expect(el.$$('select')).not.toBeNull();
+    expect(el.getAttribute('mode')).toBe("select");
+
+    // Check the value
+    expect(el.$$('select').value).toBe('');
+    // expect(el.$$("option[null]").getAttribute('selected')).toBe('selected');
+    expect(el.value).toBe(null);
+  }
+
+  it("should show RADIO when the list is < 5 items", function(done) {
+    testComponent("<jh-select value='machin' list='" + JSON.stringify(listRadio) + "'></jh-select>").then(el => {
+
+      checkRadio(el, 'machin');
 
       el.testDone();
       done();
     });
-  });
+  })
 
+  it("should show SELECT when the list is > 5 items", function(done) {
+    testComponent("<jh-select value='machin' list='" + JSON.stringify(listSelect) + "'></jh-select>").then(el => {
 
-  it("should select correctly", function(done) {
-    let list = [ 'truc' , 'brol' , 'machin', 'chose', 'bazar', 'ça', 'là' ];
-    testComponent("<jh-select value='truc' list='" + JSON.stringify(list) + "'></jh-select>").then(el => {
-      expect(el).not.toBeNull();
-      expect(el.$$('input[type=radio]').clientHeight).toBe(0);
-      expect(el.$$('select').clientHeight).not.toBe(0);
-      expect(el.$$('select').value).toBe('truc');
-      expect(el.value).toBe("truc");
-
-      el.value = 'brol';
-      expect(el.$$('select').value).toBe('brol');
-      expect(el.value).toBe("brol");
+      checkSelect(el, 'machin');
 
       el.testDone();
       done();
     });
-  });
+  })
+
+
+  it("should handle RADIO nullable", function(done) {
+    testComponent("<jh-select nullable value='machin' list='" + JSON.stringify(listRadio) + "'></jh-select>").then(el => {
+      expect(el).not.toBeNull();
+      expect(el.$$('input[type=radio][null]')).not.toBeNull();
+      el.testDone();
+      done();
+    });
+  })
+
+  it("should handle SELECT nullable", function(done) {
+    testComponent("<jh-select nullable value='machin' list='" + JSON.stringify(listSelect) + "'></jh-select>").then(el => {
+      expect(el).not.toBeNull();
+      expect(el.$$('select option[value=""]')).not.toBeNull();
+      el.testDone();
+      done();
+    });
+  })
+
+  it("should handle RADIO with null", function(done) {
+    testComponent("<jh-select nullable value='' list='" + JSON.stringify(listRadio) + "'></jh-select>").then(el => {
+
+      checkRadioNull(el);
+
+      el.testDone();
+      done();
+    });
+  })
+
+  it("should handle SELECT with null", function(done) {
+    testComponent("<jh-select nullable value='' list='" + JSON.stringify(listSelect) + "'></jh-select>").then(el => {
+
+      checkSelectNull(el);
+
+      el.testDone();
+      done();
+    });
+  })
+
+  // Test changes in value
+  it("should handle RADIO value change", function(done) {
+    testComponent("<jh-select nullable value='machin' list='" + JSON.stringify(listRadio) + "'></jh-select>").then(el => {
+
+      checkRadio(el, 'machin');
+      el.value = 'truc';
+      checkRadio(el, 'truc');
+
+      el.testDone();
+      done();
+    });
+  })
+
+  it("should handle SELECT value change", function(done) {
+    testComponent("<jh-select nullable value='machin' list='" + JSON.stringify(listSelect) + "'></jh-select>").then(el => {
+
+      checkSelect(el, 'machin');
+      el.value = 'truc';
+      checkSelect(el, 'truc');
+
+      el.testDone();
+      done();
+    });
+  })
+
+  // Test changes in html elements
+  it("should handle RADIO html change", function(done) {
+    testComponent("<jh-select nullable value='machin' list='" + JSON.stringify(listRadio) + "'></jh-select>").then(el => {
+
+      checkRadio(el, 'machin');
+      el.$$('input[type=radio][value=truc]').checked = true;
+      el.updateValueFromRadio();
+      checkRadio(el, 'truc');
+
+      el.testDone();
+      done();
+    });
+  })
+
+  it("should handle SELECT html change", function(done) {
+    testComponent("<jh-select nullable value='machin' list='" + JSON.stringify(listSelect) + "'></jh-select>").then(el => {
+
+      checkSelect(el, 'machin');
+
+      el.$$('select').value = 'truc';
+      // Change events are not triggered by javascript, so we act ourself...
+      el.updateValueFromSelect();
+      checkSelect(el, 'truc');
+      el.testDone();
+      done();
+    });
+  })
+
+  // Test click on span for radio
+  it("should handle RADIO Span click", function(done) {
+    testComponent("<jh-select nullable value='machin' list='" + JSON.stringify(listRadio) + "'></jh-select>").then(el => {
+
+      checkRadio(el, 'machin');
+      // Simulate the click
+      el.updateValueFromSpan({ currentTarget: { to: 'truc' }});
+      checkRadio(el, 'truc');
+
+      el.testDone();
+      done();
+    });
+  })
 });
