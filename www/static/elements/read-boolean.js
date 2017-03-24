bb1bc026662626c1387528cac0704d3d0f59d16d
Jean Honlet <marielineetjean@gmail.com>
2017-03-24 21:52:06 +0100

    Add tested read-boolean

--- www/static/elements/read-boolean.js
+++ www/static/elements/read-boolean.js
@@ -1,6 +1,34 @@
 
+let selfURL = document.currentScript.src
+    .replace(/\/[^/]*\/?$/, '');
 
 class ReadBoolean extends HTMLElement {
+  constructor() {
+    super();
+
+    // Create a shadow root
+    this.attachShadow({mode: 'open'});
+    this.shadowRoot.innerHTML = `
+        <span>read-boolean+</span>
+        <img>
+        <span>read-boolean-</span>
+      `;
+    this.adapt();
+  }
+
+  static get observedAttributes() { return ['value']; }
+
+  attributeChangedCallback(attributeName, oldValue, newValue, namespace) {
+    console.log("attributeChangedCallback", attributeName, newValue);
+    if (attributeName == 'value') {
+      this.adapt();
+    }
+  }
+
+  adapt() {
+    let val = JSON.parse(this.getAttribute("value"));
+    this.shadowRoot.querySelector('img').setAttribute('src', `${selfURL}/resources/boolean-${val?'true':'false'}.gif`)
+  }
 
 }
 
