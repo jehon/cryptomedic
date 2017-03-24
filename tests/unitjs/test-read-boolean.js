43d715145dd80e2283c50f158db3ee39a5713fff
Jean Honlet <jehon@users.noreply.github.com>
2017-03-24 21:52:06 +0100

    Add tested read-boolean

--- tests/unitjs/test-read-boolean.js
+++ tests/unitjs/test-read-boolean.js
@@ -8,5 +8,33 @@ describe('test-read-boolean', function() {
       el.testDone();
       done();
     });
-  })
+  });
+
+  it("should show false when no value is specified", function(done) {
+    testComponent("<read-boolean></read-boolean>").then(el => {
+      expect(el.shadowRoot.querySelector("img").getAttribute('src')).toMatch(/-false.gif/i);
+      el.testDone();
+      done();
+    });
+  });
+
+  it("should show true only when attribute value=true is specified", function(done) {
+    testComponent("<read-boolean value='true'></read-boolean>").then(el => {
+      expect(el.shadowRoot.querySelector("img").getAttribute('src')).toMatch(/-true.gif/i);
+      el.testDone();
+      done();
+    });
+  });
+
+  it("should update when value is changed", function(done) {
+    testComponent("<read-boolean></read-boolean>").then(el => {
+      expect(el.shadowRoot.querySelector("img").getAttribute('src')).toMatch(/-false.gif/i);
+      el.setAttribute('value', 'true');
+      setTimeout(() => {
+        expect(el.shadowRoot.querySelector("img").getAttribute('src')).toMatch(/-true.gif/i);
+        el.testDone();
+        done();
+      }, 10)
+    });
+  });
 });
