94c5c6c84a4d383ad4e982e52142407214458ffc
Jean Honlet <jehon@users.noreply.github.com>
2017-04-04 22:21:43 +0200

    Add test about ricket consults

--- tests/unitjs/test_folder.js
+++ tests/unitjs/test_folder.js
@@ -1,28 +1,40 @@
 'use strict';
-/* global loadReference, Folder, Patient */
+/* global loadReference, Folder, Patient, OtherConsult, RicketConsult */
 
 describe('test_folder', function() {
   beforeEach(() => {
-    let json = loadReference('FolderTest.test1.json');
-    this.f = new Folder(json);
-    expect(this.f).toEqual(jasmine.any(Folder));
+    this.f1 = new Folder(loadReference('FolderTest.test1.json'));
+    expect(this.f1).toEqual(jasmine.any(Folder));
+
+    this.f6 = new Folder(loadReference('FolderTest.test6.json'));
+    expect(this.f6).toEqual(jasmine.any(Folder));
   });
 
   it("sould instanciate folder", () => {
     let f = new Folder();
-    expect(this.f).toEqual(jasmine.any(Folder));
+    expect(this.f1).toEqual(jasmine.any(Folder));
     expect(f).toEqual(jasmine.any(Folder));
   });
 
   it("should load Mock data", () => {
-    expect(this.f).toEqual(jasmine.any(Folder));
-    expect(this.f.id).toBe('1');
-    expect(this.f.getId()).toBe('1');
+    expect(this.f1).toEqual(jasmine.any(Folder));
+    expect(this.f1.id).toBe('1');
+    expect(this.f1.getId()).toBe('1');
   })
 
   it("should instanciate classes", () => {
-    console.log(this.f);
-    console.log(this.f.getPatient());
-    expect(this.f.getPatient()).toEqual(jasmine.any(Patient));
+    expect(this.f1.getPatient()).toEqual(jasmine.any(Patient));
+    expect(this.f1.getPatient().id).toBe(1);
   })
+
+  it("should query specific element (Otherconsult 1)", () => {
+    expect(this.f1.getByTypeAndId(OtherConsult, 1)).toEqual(jasmine.any(OtherConsult));
+    expect(this.f1.getByTypeAndId(OtherConsult, 1).id).toBe(1);
+  })
+
+  it("should query specific element (RicketConsult 1)", () => {
+    expect(this.f6.getByTypeAndId(RicketConsult, 1)).toEqual(jasmine.any(RicketConsult));
+    expect(this.f6.getByTypeAndId(RicketConsult, 1).id).toBe(1);
+  })
+
 })
