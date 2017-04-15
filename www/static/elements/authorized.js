09e0f82bb649dd154dc2f5114780085e788508d7
Jean Honlet <jehon@users.noreply.github.com>
2017-04-15 12:52:24 +0200

    Rebuild a jh-authorized in strict html components from scratch

---
+++ www/static/elements/authorized.js
@@ -0,0 +1,65 @@
+let JHAuthorized = (function() {
+
+  let authorizedListCB = jQuery.Callbacks();
+
+  class JHAuthorized extends HTMLElement {
+    // Object.keys(authorizedObject)
+    static setAuthorizedList(authorizedList = []) {
+      if (!authorizedList) {
+        authorizedList = [];
+      }
+      authorizedListCB.fire(authorizedList);
+    }
+
+    constructor() {
+      super();
+
+      // Authorized list
+      this.authorizedList = [];
+      authorizedListCB.add((authorizedList) => {
+        // Refresh the list and adapt()
+        this.authorizedList = authorizedList;
+        this.adapt();
+      });
+
+      // Create a shadow root
+      this.attachShadow({
+        mode: 'open'
+      });
+      this.adapt();
+    }
+
+    static get observedAttributes() {
+      return ['value', 'inversed'];
+    }
+
+    attributeChangedCallback(attributeName, oldValue, newValue, namespace) {
+      this.adapt();
+    }
+
+    adapt() {
+      // Text raw value
+      let value = this.getAttribute("value");
+
+      let inversed = this.getAttribute("inversed");
+      try {
+        inversed = JSON.parse(inversed);
+        inversed = (inversed ? true : false);
+      } catch (e) { // SyntaxError
+        inversed = false;
+      }
+
+      // XOR: thanks to http://www.howtocreate.co.uk/xor.html
+      if (this.authorizedList.indexOf(value) >= 0) {
+        this.shadowRoot.innerHTML = "<span id='securized'><slot name='securized'></slot></span>";
+      } else {
+        this.shadowRoot.innerHTML = "<span id='securized'><slot name='free'></slot></span>";
+      }
+    }
+
+  }
+
+  window.customElements.define('jh-authorized', JHAuthorized);
+
+  return JHAuthorized;
+}());
