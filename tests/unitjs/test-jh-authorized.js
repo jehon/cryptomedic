2bef37a2b67529373e165b3dc54f8d70eadd959f
Jean Honlet <jehon@users.noreply.github.com>
2017-04-17 18:00:00 +0200

    Fix initialization not working

--- tests/unitjs/test-jh-authorized.js
+++ tests/unitjs/test-jh-authorized.js
@@ -11,19 +11,21 @@ fdescribe('test-jh-authorized', function() {
   it("should be empty at the beginning", function(done) {
     testComponent("<jh-authorized value='secure'>securized content</jh-authorized>").then(el => {
       expect(el).not.toBeNull();
-      expect(el.shadowRoot.querySelector('span#securized').innerHTML.trim()).not.toContain("securized");
+      expect(el.shadowRoot.querySelector('span#securized')).toBeNull();
       JHAuthorized.setAuthorizedList();
-      expect(el.shadowRoot.querySelector('span#securized').innerHTML.trim()).not.toContain("securized");
+      expect(el.shadowRoot.querySelector('span#securized')).toBeNull();
       el.testDone();
       done();
     });
   });
 
   it("should be enabled when setting the list", function(done) {
-    testComponent("<jh-authorized value='secure'>securized content</jh-authorized>").then(el => {
+    testComponent("<jh-authorized value='secure'><span>securized content</span></jh-authorized>").then(el => {
       expect(el).not.toBeNull();
-      expect(el.shadowRoot.querySelector('span#securized').innerHTML.trim()).not.toContain("securized");
-      JHAuthorized.setAuthorizedList([ "secure", "anything", "else" ]);
+      expect(el.shadowRoot.querySelector('span#securized')).toBeNull();
+      JHAuthorized.setAuthorizedList(["secure", "anything", "else"]);
+      ShadyDOM.flush();
+      expect(el.shadowRoot.querySelector('span#securized')).not.toBeNull();
       expect(el.shadowRoot.querySelector('span#securized').innerHTML.trim()).toContain("securized");
       el.testDone();
       done();
@@ -31,8 +33,9 @@ fdescribe('test-jh-authorized', function() {
   });
 
   it("should be enabled when the list is setted before", function(done) {
+    JHAuthorized.setAuthorizedList(["secure", "anything", "else"]);
     testComponent("<jh-authorized value='secure'>securized content</jh-authorized>").then(el => {
-      JHAuthorized.setAuthorizedList([ "secure", "anything", "else" ]);
+      ShadyDOM.flush();
       expect(el.shadowRoot.querySelector('span#securized').innerHTML.trim()).toContain("securized");
       el.testDone();
       done();
@@ -41,10 +44,34 @@ fdescribe('test-jh-authorized', function() {
 
   it("should work with inversed tests", function(done) {
     testComponent("<jh-authorized inversed value='secure'>securized content</jh-authorized>").then(el => {
-      JHAuthorized.setAuthorizedList([ "secure", "anything", "else" ]);
-      expect(el.shadowRoot.querySelector('span#securized').innerHTML.trim()).not.toContain("securized");
+      JHAuthorized.setAuthorizedList(["secure", "anything", "else"]);
+      ShadyDOM.flush();
+      expect(el.shadowRoot.querySelector('span#securized')).toBeNull();
       el.testDone();
       done();
     });
   });
+
+  // it("should work inside angular", function(done) {
+  //   JHAuthorized.setAuthorizedList(["secure", "anything", "else"]);
+  //   angular
+  //     .module('ngauthorized', [])
+  //     .controller('ngauthorizedctrl', ['$scope', function($scope) {
+  //       $scope.test = function() {
+  //         console.log("test");
+  //       }
+  //     }]);
+
+  //   testComponent(`<div ng-app='ngauthorized' ng-controller='ngauthorizedctrl'>
+  //      <jh-authorized value='secure'>
+  //        <span ng-click='test()'>test me</span>
+  //      </jh-authorized>
+  //     </div>`)
+  //     .then(el => {
+  //       debugger;
+  //       // expect(el.shadowRoot.querySelector('span#securized').innerHTML.trim()).not.toContain("securized");
+  //       el.testDone();
+  //       done();
+  //     });
+  // });
 });
