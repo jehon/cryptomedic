7e6270b9a5203d8495a995a8de988ca12547b21f
Jean Honlet <jehon@users.noreply.github.com>
2017-05-06 21:36:09 +0200

    Fix typo in name

--- www/api/v1.3/app/Http/Controllers/AppointmentsController.php
+++ www/api/v1.3/app/Http/Controllers/AppointmentsController.php
@@ -4,7 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Model\Appointment;
 
-class AppoinmentsController extends ModelController {
+class AppointmentsController extends ModelController {
 	static public function getModelClass() {
 		return "App\\Model\\Appointment";
 	}
