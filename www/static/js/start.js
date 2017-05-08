248b0245e87aca86e94da7d8e0c5642dce096b68
Jean Honlet <marielineetjean@gmail.com>
2017-05-08 22:16:18 +0200

    Add comment to explain

--- www/static/js/start.js
+++ www/static/js/start.js
@@ -1,5 +1,6 @@
 
 // Also adapt package.json for adding testing the new api
+// See md5sum.php to protect old versions... and delete them when necessary !!!
 let API_VERSION='v1.3';
 
 if (!Promise || !indexedDB || !sessionStorage || !fetch) {
