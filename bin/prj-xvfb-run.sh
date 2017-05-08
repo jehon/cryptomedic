03359500329fa97ffb5c1850154a7512ff9013ff
Jean Honlet <marielineetjean@gmail.com>
2017-05-08 21:28:55 +0200

    Resist to xvfb run not present

--- bin/prj-xvfb-run.sh
+++ bin/prj-xvfb-run.sh
@@ -3,5 +3,12 @@
 if [ "$FRONT" = "1" ]; then
   "$@"
 else
-  xvfb-run --auto-servernum --server-args='-screen 0 1024x768x24 -ac +extension GLX +render -noreset' "$@"
+	# Test that the command does exists:
+	# Thanks to http://stackoverflow.com/a/677212/1954789
+	if command -v xvfb-run >/dev/null 2>&1  ; then
+  		xvfb-run --auto-servernum --server-args='-screen 0 1024x768x24 -ac +extension GLX +render -noreset' "$@"
+  	else
+  		echo "FALL BACK WITHOUT XFVB"
+  		"$@"
+  	fi
 fi
