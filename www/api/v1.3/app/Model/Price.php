eb34f96d8f0f374c2b99aefff35ed8476b7006fd
Jean Honlet <marielineetjean@gmail.com>
2017-05-18 22:42:03 +0200

    Add editable field
    The value is not calculated yet.

--- www/api/v1.3/app/Model/Price.php
+++ www/api/v1.3/app/Model/Price.php
@@ -5,5 +5,11 @@ namespace App\Model;
 class Price extends CryptomedicModel
 {
 	protected $guarded = array('id');
+    protected $appends = array('_editable');
 
+    // Transient field: http://stackoverflow.com/a/17250429/1954789
+    public function getEditableAttribute()
+    {
+        return false;
+    }
 }
