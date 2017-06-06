a472a03fb76b82c7c8588301f91ee5a9d6521711
Jean Honlet <marielineetjean@gmail.com>
2017-06-06 19:54:24 +0200

    Test formGetContent

--- www/static/js/form.js
+++ www/static/js/form.js
@@ -5,10 +5,16 @@ function formGetContent(form, prototype = {}) {
 
   // for(let i of document.querySelector(form).querySelectorAll("input:not([type=radio]), select, input[type=radio]:checked")) {
   for(let i of document.querySelector(form).querySelectorAll("[name]")) {
-    // Skip hidden input
-    if (i.clientHeight == 0) {
-      continue;
+    if (i.disabled) {
+      console.log("disabled element: ", i);
+      break;
     }
+
+    // Skip hidden input
+    // if (i.clientHeight == 0) {
+    //   console.log("hidden: ", i);
+    //   continue;
+    // }
 
     if (i.matches("[type=radio") && !i.matches("[checked]")) {
       continue;
@@ -32,19 +38,19 @@ function formGetContent(form, prototype = {}) {
       continue;
     }
 
-    switch(i.type) {
-      case "number":
-        data[name] = Number.parseInt(value);
-        break;
-      case "file":
-        // http://blog.teamtreehouse.com/uploading-files-ajax
-        // We can pass the "File" object to FormData, it will handle it for us....
-        // var file = ; //document.getElementById("fileForUpload").files[0];
-        data[name] = i.files[0];
-        break;
-      default:
-        data[name] = value;
-        break;
+    data[name] = value;
+    if (i.type) {
+      switch(i.type) {
+        case "number":
+          data[name] = Number.parseInt(value);
+          break;
+        case "file":
+          // http://blog.teamtreehouse.com/uploading-files-ajax
+          // We can pass the "File" object to FormData, it will handle it for us....
+          // var file = ; //document.getElementById("fileForUpload").files[0];
+          data[name] = i.files[0];
+          break;
+      }
     }
   }
   return data;
