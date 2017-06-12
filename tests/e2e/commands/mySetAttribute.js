ff60e2736356bfd8f67ed14fc73c8efefc47af59
Jean Honlet <jehon@users.noreply.github.com>
2017-06-12 20:32:36 +0200

    Allow form check to run with object
    This is for write-list:
    selector= [name=X]
    set value = "val" to set the value

---
+++ tests/e2e/commands/mySetAttribute.js
@@ -0,0 +1,28 @@
+
+exports.command = function(selector, attributeName, attributeValue, callback) {
+  var self = this;
+  this.waitForElementVisible(selector);
+  this.assert.visible(selector);
+  this.myScreenshot("click");
+
+  // http://stackoverflow.com/questions/38102543/when-running-nightwatch-js-test-how-can-i-get-the-name-of-browser-currently-runn
+
+  this.execute(function(selector, attributeName, attributeValue) {
+    let el = document.querySelector(selector);
+    // if (el.matches("input[mycalendar]") && attributeName == "value") {
+    //   // Problem with months?
+    //   let d = new Date(attributeValue);
+    //   attributeValue = ().toISOString().substring(0, 10);
+    //   // attributeValue = "1999-01-01";
+    // }
+    el.setAttribute(attributeName, attributeValue);
+    return true;
+  },
+  [ selector, attributeName, attributeValue ], // arguments array to be passed
+  function(result) {
+    if (typeof callback === 'function') {
+      callback.call(self, result);
+    }
+  });
+  return this;
+};
