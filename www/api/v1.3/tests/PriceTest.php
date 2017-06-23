fe42ccf29e55596ea3610b6a6e624ca48bd63473
Jean Honlet <marielineetjean@gmail.com>
2017-06-23 12:29:43 +0200

    Add Price creation

--- www/api/v1.3/tests/PriceTest.php
+++ www/api/v1.3/tests/PriceTest.php
@@ -7,6 +7,11 @@ use Illuminate\Foundation\Testing\DatabaseTransactions;
 class PriceTest extends RouteReferenceTestCase {
 	// Make Unit Tests are transactionals !
 	// use DatabaseTransactions;
+
+	protected $model = "Price";
+	protected $collection = "admin/prices";
+
+
 	public function testsUnauthenticated() {
 	    $response = $this->myRunAssertQuery(
 	        $this->getNewRequestOptionsBuilder()
@@ -28,7 +33,7 @@ class PriceTest extends RouteReferenceTestCase {
 	}
 
 
-	public function testsIndex() {
+	public function testIndex() {
 	    $json = $this->myRunAssertQuery(
 	        $this->getNewRequestOptionsBuilder()
 	         	->setUrl("admin/prices")
@@ -37,5 +42,49 @@ class PriceTest extends RouteReferenceTestCase {
 	      	);
 
 		$this->assertEquals(count($json), 3);
+		return $json;
+	}
+
+	public function testCreate() {
+		$list = $this->testIndex();
+
+	    $json = $this->myRunAssertQuery(
+	        $this->getNewRequestOptionsBuilder()
+	        	->setRole("manager")
+	         	->setUrl("admin/prices")
+	        	->setMethod("POST")
+	        	->addParams([ "pivot" => "2000-01-01" ])
+	        	->setExpected(400)
+	        	->asText()
+	        	// ->withReference()
+	      	);
+
+		$limit = date('Y-m-d', mktime(0, 0, 0, date('m'), date('d') + 4, date('Y')));
+	    $json = $this->myRunAssertQuery(
+	        $this->getNewRequestOptionsBuilder()
+	        	->setRole("manager")
+	         	->setUrl("admin/prices")
+	        	->setMethod("POST")
+	        	->addParams([ "pivot" => $limit ])
+	        	->setExpected(400)
+	        	->asText()
+	        	// ->withReference()
+	      	);
+
+		$limit = date('Y-m-d', mktime(0, 0, 0, date('m'), date('d') + 10, date('Y')));
+	    $json = $this->myRunAssertQuery(
+	        $this->getNewRequestOptionsBuilder()
+	        	->setRole("manager")
+	         	->setUrl("admin/prices")
+	        	->setMethod("POST")
+	        	->addParams([ "pivot" => $limit ])
+	      	);
+
+	    $this->assertTrue(property_exists($json, 'id'));
+	    $this->assertEquals($json->datefrom, $limit);
+	    $this->assertTrue(!property_exists($json, 'dateto'));
+	    $this->assertEquals($json->consult_field_visit, -1);
+	    $this->assertEquals($json->consult_home_visit, 150);
+	    // $this->assertEquals($json->_editable, true);
 	}
 }
