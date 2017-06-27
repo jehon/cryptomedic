28aa662b87e99223da2e5903c748c8b53b93c215
Jean Honlet <jehon@users.noreply.github.com>
2017-06-27 22:24:09 +0200

    Test for values

--- tests/e2e/tests/rw/modifyPriceListsTests.js
+++ tests/e2e/tests/rw/modifyPriceListsTests.js
@@ -11,38 +11,36 @@ module.exports = {
 
       .page.cryptomedic().tableIterator('#price_lists')
         .section("thead")
+          .row(1)
             .assert("Beginning date")
             .nextCol().assert("2016-01-03")
             .nextCol().assert("2013-01-01")
             .nextCol().assert("")
-          .nextRow(4).assert("")
+          .row(4).col(1)
+            .assert("")
             .nextCol().assert("Locked")
             .nextCol().assert("Locked")
             .nextCol().assert("Locked")
         .section("tbody")
+          .row(1).col(1)
+            .assert("consult_CDC_consultation_Bengali_Doctor")
+            .nextCol().assert("300")
+            .nextCol().assert("300")
+            .nextCol().assert("200")
+          .nextRow(4).col(1)
+            .assert("consult_field_visit")
+            .nextCol().assert("-")
+            .nextCol().assert("-")
+            .nextCol().assert("100")
+          .nextRow(7).col(1)
+            .assert("other_Other_consultation_care")
+            .nextCol().assert("free price")
+            .nextCol().assert("free price")
+            .nextCol().assert("free price")
+
         .endTable()
 
-
-      // .waitForElementVisible("input[ng-model='entryyear']")
-      // .clearValue("input[ng-model='entryyear']")
-      // .setValue("input[ng-model='entryyear']", 2014)
-      // .clearValue("input[ng-model='entryorder']")
-      // .setValue("input[ng-model='entryorder']", 103)
-      // .myClick("[ng-click='checkReference()']")
-
-      // .waitForElementPresent("#Patient_Name")
-      // .assert.containsText("#Patient_Name", "OSMAN")
-
-      // .page.cryptomedic().selectFile("Bill", 2)
-      // .page.cryptomedic().tableIterator('#paymentsList')
-      //   .section('tfoot')
-      //   .row(1).col(2).assert('Total')
-      //   .row(1).col(3).assert('138')
-      //   .endTable()
-      // ;
-
-
-      .pause(10000000)
+      // .pause(10000000)
       .end();
   },
 };
