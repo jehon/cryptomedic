eac97a8ec20a424161024845430b16083fbe2cbe
Jean Honlet <jehon@users.noreply.github.com>
2017-07-15 21:27:57 +0200

    Add setListFrom

---
+++ www/static/js/setListFrom.js
@@ -0,0 +1,21 @@
+function setListFrom(origin, target, category) {
+  console.log("SetListFrom", origin, target, category);
+  let val = document.querySelector(`input[name=${origin}]`).value;
+  if (!cryptomedic.serverSettings || !cryptomedic.serverSettings.associations) {
+    return;
+  }
+
+  let list = [];
+  if (cryptomedic.serverSettings.associations[`${category}.${val}`]) {
+    list = list.concat(cryptomedic.serverSettings.associations[`${category}.${val}`]);
+  }
+  list = list.concat(cryptomedic.serverSettings.associations[`${category}.other`]);
+
+  if (list.indexOf(current) < 0) {
+    list = [current].concat(list);
+  }
+
+  document.querySelectorAll(`input[name=${target}]`).forEach(el => {
+    console.log("Set list for ", val, " to ", list, " on ", el);
+  });
+}
