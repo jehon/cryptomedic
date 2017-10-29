297b73c65553787ae4475deca161cba2046e9efe
Jean Honlet <jehon@users.noreply.github.com>
2017-10-29 21:18:30 +0100

    Add displaying total and reacting to changes

--- tests/unitjs/edit-bill-category-test.js
+++ tests/unitjs/edit-bill-category-test.js
@@ -34,6 +34,13 @@ describe('edit-bill-category-test', function() {
         }
     ]
 
+    webDescribe("instanciate without prices", `<edit-bill-category value='${JSON.stringify(value)}' category='cat'></edit-bill-category>`, function(element) {
+        it("should instanciate empty", function() {
+            expect(element().querySelectorAll("tbody > *").length).toBe(0);
+            expect(element().getTotal()).toBe(0);
+        })
+    });
+
     webDescribe("instanciate with price and bill", `<edit-bill-category value='${JSON.stringify(value)}' price-lines='${JSON.stringify(priceCategory)}' category='cat'></edit-bill-category>`, function(element) {
         it("should instanciate", function() {
             expect(element().textContent).toContain("Price 1");
@@ -41,6 +48,17 @@ describe('edit-bill-category-test', function() {
             expect(element().textContent).not.toContain("Price 3");
             expect(element().textContent).toContain("Other");
             expect(element().getTotal()).toBe(105);
+            expect(element().querySelector("#catTotal").textContent).toContain(105);
         });
+
+        it("should react to change event", function() {
+            expect(element().getTotal()).toBe(105);
+            expect(element().querySelector("#catTotal").textContent).toContain(105);
+            let res = false;
+            let el = element().querySelector("edit-bill-line");
+            el.querySelector("input").value = 10;
+            el.fire("change", "test", element().querySelector("input"));
+            expect(element().querySelector("#catTotal").textContent).toContain(155);
+        })
     });
 });
