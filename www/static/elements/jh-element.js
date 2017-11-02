525b396111a2a8a25a987cf8852786c72b0a60ac
Jean Honlet <jehon@users.noreply.github.com>
2017-11-02 22:02:24 +0100

    Remove dead code

--- www/static/elements/jh-element.js
+++ www/static/elements/jh-element.js
@@ -59,9 +59,6 @@ let JHElement = (function() {
             attributeName = snakeToCamel(attributeName);
 
             let props = this.constructor.properties;
-            // if (this.properties) {
-            //     props = this.properties;
-            // }
             if (props && props[attributeName]) {
                 switch(props[attributeName]) {
                     case "Boolean":
