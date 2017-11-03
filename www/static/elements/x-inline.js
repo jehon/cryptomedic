d2a7431a266fc295f5f6e1a9d4514b89b4e42049
Jean Honlet <marielineetjean@gmail.com>
2017-11-03 21:46:20 +0100

    Add inline parameter

--- www/static/elements/x-inline.js
+++ www/static/elements/x-inline.js
@@ -3,18 +3,19 @@
     class XInline extends JHElement {
         static get properties() {
             return {
-                "name":  "String",
-                "type":  "String",
-                "value": "String",
-                "edit":  "Boolean"
+                "name":   "String",
+                "type":   "String",
+                "value":  "String",
+                "edit":   "Boolean",
+                "inline": "String"
             }
         }
 
         adapt() {
             if (this.edit) {
-                this.innerHTML = `<x-write name='${this.name}' type='${this.type}' value='${this.value}'></x-write>`;
+                this.innerHTML = `<x-write name='${this.name}' type='${this.type}' value='${this.value}' inline='${this.inline}'></x-write>`;
             } else {
-                this.innerHTML = `<x-read name='${this.name}' type='${this.type}' value='${this.value}'></x-read>`;
+                this.innerHTML = `<x-read name='${this.name}' type='${this.type}' value='${this.value}' inline='${this.inline}'></x-read>`;
             }
         }
     }
