ec2a2d6fe4bd2d15f575150babfb02d9efedcef3
Jean Honlet <jehon@users.noreply.github.com>
2017-11-03 22:40:33 +0100

    Propagate event

--- tests/unitjs/x-inline-test.js
+++ tests/unitjs/x-inline-test.js
@@ -39,15 +39,25 @@ describe('x-inline-test', function() {
         })
     })
 
-    webDescribe("with mode edit", `<x-inline edit name='xname' type='xtype' value='xvalue' inline='xinline'></x-inline>`, function(element) {
+    webDescribe("with mode edit", `<x-inline edit name='xname' type='numeric' value='xvalue' inline='xinline'></x-inline>`, function(element) {
         it("should be instanciated", function() {
             let xelement = element().querySelector("x-write");
             expect(xelement).not.toBeNull();
 
             expect(xelement.getAttribute('name')).toBe("xname");
-            expect(xelement.getAttribute('type')).toBe("xtype");
+            expect(xelement.getAttribute('type')).toBe("numeric");
             expect(xelement.getAttribute('value')).toBe("xvalue");
             expect(xelement.getAttribute('inline')).toBe("xinline");
+        })
+
+        it("should fire event", function() {
+            let res = false;
+            element().addEventListener("change", (event) => { res = "test" });
+            const input = element().querySelector("input");
+            input.value = 10;
+            JHElement.fireOn(input, "change", 10);
+            expect(res).toBe("test");
+            expect(element().getValue()).toBe(10);
         })
     })
 });
--- www/static/elements/x-inline.js
+++ www/static/elements/x-inline.js
@@ -14,9 +14,20 @@
         adapt() {
             if (this.edit) {
                 this.innerHTML = `<x-write name='${this.name}' type='${this.type}' value='${this.value}' inline='${this.inline}'></x-write>`;
+                let el = this.querySelector("x-write");
+                el.addEventListener("change", () => {
+                    this.fire("change", el.getValue());
+                    if (this.value != el.getValue()) {
+                        this.value = el.getValue();
+                    }
+                });
             } else {
                 this.innerHTML = `<x-read name='${this.name}' type='${this.type}' value='${this.value}' inline='${this.inline}'></x-read>`;
             }
+        }
+
+        getValue() {
+            return this.value;
         }
     }
 
