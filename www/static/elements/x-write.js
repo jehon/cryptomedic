4a910c92f01b2eb9106e5ec7f5ddc57a9b5c3c10
Jean Honlet <marielineetjean@gmail.com>
2017-11-03 22:09:15 +0100

    Extends x-write

--- www/static/elements/x-write.js
+++ www/static/elements/x-write.js
@@ -3,9 +3,10 @@
     class XWrite extends JHElement {
         static get properties() {
             return {
-                "name":  "String",
-                "type":  "String",
-                "value": "String"
+                "name":   "String",
+                "type":   "String",
+                "value":  "String",
+                "inline": "String"
             }
         }
 
@@ -15,31 +16,56 @@
                 return;
             }
 
+            let el = false;
+
             switch(this.type) {
                 case "timestamp":
-                    this.innerHTML = `<x-read name='${this.name}' type='${this.type}' value='${this.value}'></x-read>`;
+                    this.innerHTML = `<x-read name='${this.name}' type='${this.type}' value='${this.value}' ${this.inline}></x-read>`;
+                    this.getValue = () => this.value;
                     break;
                 case "boolean":
-                    // this.innerHTML = `<read-boolean name='${this.name}' value='${this.value}'></read-boolean>`;
-                    break;
-                case "list":
+                    this.innerHTML = `<input name='${this.name}' type='checkbox' ${this.value ? 'checked' : ''}/>`;
+                    el = this.querySelector('input');
+                    this.getValue = () => el.checked;
                     break;
                 case "date":
+                    this.innerHTML = `<input type='date' value='${this.value}' ${this.inline} />`;
+                    el = this.querySelector('input');
+                    this.getValue = () => el.value;
                     break;
                 case "numeric":
+                    this.innerHTML = `<input type='number' value='${this.value}' ${this.inline} />`;
+                    el = this.querySelector('input');
+                    this.getValue = () => {
+                        if (el.value === "") {
+                            return null;
+                        }
+                        return parseFloat(el.value);
+                    }
                     break;
                 case "char":
-                    // this.innerHTML = `<span name='${this.name}'>${this.value}</span>`;  
+                    this.innerHTML = `<input value='${this.value}' ${this.inline} />`;
+                    el = this.querySelector('input');
+                    this.getValue = () => el.value;
                     break;
-                case "text":
+                // case "text":
                     // this.innerHTML = `<span name='${this.name}' style='white-space: pre'>${this.value}</span>`;  
-                    break;
+                    // break;
+                // case "list":
+                //     break;
                 default:
                     console.error("Type unknown: ", this.type);
-                    this.innerHTML = `<span name='${this.name}'>unknown type: ${this.type}</span>`;
+                    this.innerHTML = `<span name='${this.name}' class='error'>unknown type: ${this.type}</span>`;
                     break;
             }
+            
+            if (el) {
+                const val = el.value;
+                el.addEventListener("change", () => this.fire("change", val));
+            }
         }
+
+        getValue() {}
     }
 
     window.customElements.define('x-write', XWrite);
