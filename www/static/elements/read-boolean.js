25de9da43922c04acfb8b1896635684ccc694f6f
Jean Honlet <marielineetjean@gmail.com>
2017-11-03 21:46:28 +0100

    Align on 4 tabs

--- www/static/elements/read-boolean.js
+++ www/static/elements/read-boolean.js
@@ -1,41 +1,41 @@
 
 let ReadBoolean = (function() {
 
-  let selfURL = document.currentScript.src
+    let selfURL = document.currentScript.src
       .replace(/\/[^/]*\/?$/, '');
 
-  class ReadBoolean extends HTMLElement {
-    constructor() {
-      super();
+    class ReadBoolean extends HTMLElement {
+        constructor() {
+            super();
 
-      // Create a shadow root
-      this.attachShadow({mode: 'open'});
-      this.shadowRoot.innerHTML = `
-          <img>
-        `;
-      this.adapt();
+            // Create a shadow root
+            this.attachShadow({mode: 'open'});
+            this.shadowRoot.innerHTML = `
+                <img>
+              `;
+            this.adapt();
+        }
+
+        static get observedAttributes() { return ['value']; }
+
+        attributeChangedCallback(attributeName, oldValue, newValue, namespace) {
+            this.adapt();
+        }
+
+        adapt() {
+            let raw = this.getAttribute("value");
+            let val = "";
+            try {
+                val = JSON.parse(raw);
+            } catch(e) { // SyntaxError
+                val = raw;
+            }
+            this.shadowRoot.querySelector('img').setAttribute('src', `${selfURL}/resources/boolean-${val?'true':'false'}.gif`)
+        }
+
     }
 
-    static get observedAttributes() { return ['value']; }
+    window.customElements.define('read-boolean', ReadBoolean);
 
-    attributeChangedCallback(attributeName, oldValue, newValue, namespace) {
-      this.adapt();
-    }
-
-    adapt() {
-      let raw = this.getAttribute("value");
-      let val = "";
-      try {
-        val = JSON.parse(raw);
-      } catch(e) { // SyntaxError
-        val = raw;
-      }
-      this.shadowRoot.querySelector('img').setAttribute('src', `${selfURL}/resources/boolean-${val?'true':'false'}.gif`)
-    }
-
-  }
-
-  window.customElements.define('read-boolean', ReadBoolean);
-
-  return ReadBoolean;
+    return ReadBoolean;
 })();
