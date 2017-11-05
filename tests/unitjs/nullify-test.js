b47a2bf792321c68bb1eb39e2d742e4bf686dec5
Jean Honlet <marielineetjean@gmail.com>
2017-11-05 22:21:02 +0100

    Add nullify test

---
+++ tests/unitjs/nullify-test.js
@@ -0,0 +1,31 @@
+
+fdescribe('nullify-test', function() {
+  it("should nullify string", function() {
+    expect(nullify("")).toEqual("");
+  });
+
+  it("should nullify string null", function() {
+    expect(nullify("null")).toEqual(null);
+  });
+
+  it("should nullify string ?", function() {
+    expect(nullify("?")).toEqual(null);
+  });
+
+  it("should nullify string undefined", function() {
+    expect(nullify("undefined")).toEqual(null);
+  });
+
+  it("should nullify string anything", function() {
+    expect(nullify("anything")).toEqual("anything");
+  });
+
+  it("should nullify int 123", function() {
+    expect(nullify(123)).toEqual(123);
+  });
+
+  it("should nullify object", function() {
+    expect(nullify({ a: 1, b: "null", c: null })).toEqual({ a: 1, b: null, c: null });
+  })
+
+})
