317588469cc24619183145ff43e0dbfae52adaf2
Jean Honlet <jehon@users.noreply.github.com>
2017-11-20 18:00:00 +0100

    Add webpack to manage bower dependancies through npm
    Update cloudydom to shadydom

---
+++ www/app.js
@@ -0,0 +1,13 @@
+
+import angular      from 'angular';
+import angularRoute from 'angular-route';
+
+// Custom Elements v1
+// import cloudydom    from 'cloudydom';
+import shadydom     from '@webcomponents/shadydom';
+
+console.log("bundle loading");
+
+window.angular = angular;
+
+console.log("bundle loaded");
