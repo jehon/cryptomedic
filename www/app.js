162083e2bf40e8336f5f97aa5921086ab4ac605a
Jean Honlet <jehon@users.noreply.github.com>
2017-11-21 07:34:59 +0100

    Push angular into bundle

--- www/app.js
+++ www/app.js
@@ -1,18 +1,17 @@
 
-import angular      from 'angular';
-import angularRoute from 'angular-route';
-
 // Custom Elements v1
-// import cloudydom    from 'cloudydom';
 import shadydom        from '@webcomponents/shadydom';
 
-import ExcellentExport from 'excellentexport';
+// Angular 1.x
+import angular         from 'angular';
+import angularRoute    from 'angular-route';
+
+// Export to excell
+import ExcellentExport from 'excellentexport';-
+
+// Clone object clone(...)
 import clone           from 'clone';
 
-console.log("bundle loading");
-
-window.angular = angular;
+window.angular         = angular;
 window.ExcellentExport = ExcellentExport;
 window.clone           = clone;
-
-console.log("bundle loaded");
