b29f8780062e9136401306f0a637b405d3d78cf7
Jean Honlet <jehon@users.noreply.github.com>
2017-11-22 07:34:00 +0100

    Reformat the code

--- www/static/controllers/ctrl_file_bill.js
+++ www/static/controllers/ctrl_file_bill.js
@@ -6,103 +6,103 @@ function ctrl_file_bill($scope) {
         window.cryptomedic.serverSettings.prices
   */
 
-  $scope.paymentEditor = new Payment();
+    $scope.paymentEditor = new Payment();
 
-  $scope.paymentsList = function() {
-    if ($scope.folder) {
-      return $scope.folder.getFilesRelatedToBill($scope.subid);
-    }
-    return [];
-  }
-
-  $scope.$watch(function() {
-    return window.cryptomedic.serverSettings.prices;
-  }, function() {
-    $scope.currentFile().calculatePriceId();
-    $scope.safeApply();
-  });
-
-  $scope.$watch('currentFile().Date', function() {
-    if ($scope.currentFile() && $scope.currentFile().calculatePriceId) {
-      $scope.currentFile().calculatePriceId(window.cryptomedic.serverSettings.prices);
-      $scope.safeApply();
-    } else {
-      $scope.safeApply();
-    }
-  });
-
-  $scope.$watch('currentFile().sl_numberOfHouseholdMembers', function() {
-    try {
-      $scope.currentFile().ratioSalary();
-    } catch (e) {
-      // Nothing to do
-    }
-  });
-
-  $scope.$watch('currentFile().sl_familySalary', function() {
-    try {
-      $scope.currentFile().ratioSalary();
-    } catch (e) {
-      // Nothing to do
-    }
-  });
-
-  $scope.actionAddPayment = function() {
-    if (!$scope.actionValidate("#paymentForm")) {
-      alert('You have errors in your data. Please correct them and try again');
-      return ;
+    $scope.paymentsList = function() {
+        if ($scope.folder) {
+          return $scope.folder.getFilesRelatedToBill($scope.subid);
+        }
+        return [];
     }
 
-    let updatedData = formGetContent("#paymentForm", new Payment());
-
-    updatedData.id = $scope.paymentEditor.id;
-    updatedData.bill_id = $scope.subid;
-    $scope.safeApply();
-
-    extractPrefsFile(updatedData);
-
-    getDataService()
-      .then(dataService => dataService.createOrSaveFile(updatedData))
-      .then(function(folder) {
-        // The data is refreshed by navigating away...
-        $scope.$emit('message', { 'level': 'success', 'text': 'The Payment has been saved.'});
-        $scope.folder = folder;
-        $scope.paymentEditor = new Payment();
+    $scope.$watch(function() {
+        return window.cryptomedic.serverSettings.prices;
+    }, function() {
+        $scope.currentFile().calculatePriceId();
         $scope.safeApply();
-      })
-  }
+    });
 
-  $scope.actionDeletePayment = function(id) {
-    getDataService()
-      .then(dataService => dataService.deleteFile($scope.folder.getByTypeAndId(Payment, id)))
-      .then(function(folder) {
-        // The data is refreshed by navigating away...
-        $scope.$emit('message', { 'level': 'success', 'text': 'The Payment has been deleted.'});
-        $scope.folder = folder;
-        $scope.paymentEditor = new Payment();
+    $scope.$watch('currentFile().Date', function() {
+        if ($scope.currentFile() && $scope.currentFile().calculatePriceId) {
+            $scope.currentFile().calculatePriceId(window.cryptomedic.serverSettings.prices);
+            $scope.safeApply();
+        } else {
+            $scope.safeApply();
+        }
+    });
+
+    $scope.$watch('currentFile().sl_numberOfHouseholdMembers', function() {
+        try {
+            $scope.currentFile().ratioSalary();
+        } catch (e) {
+            // Nothing to do
+        }
+    });
+
+    $scope.$watch('currentFile().sl_familySalary', function() {
+        try {
+            $scope.currentFile().ratioSalary();
+        } catch (e) {
+            // Nothing to do
+        }
+    });
+
+    $scope.actionAddPayment = function() {
+        if (!$scope.actionValidate("#paymentForm")) {
+            alert('You have errors in your data. Please correct them and try again');
+            return ;
+        }
+
+        let updatedData = formGetContent("#paymentForm", new Payment());
+
+        updatedData.id = $scope.paymentEditor.id;
+        updatedData.bill_id = $scope.subid;
         $scope.safeApply();
-      })
-  }
 
-  $scope.actionEditPayment = function(id) {
-    $scope.paymentEditor = new Payment($scope.folder.getByTypeAndId(Payment, id));
-  }
+        extractPrefsFile(updatedData);
 
-  $scope.getPaymentTotal = function() {
-    if (!$scope.folder) {
-      return "?";
+        getDataService()
+            .then(dataService => dataService.createOrSaveFile(updatedData))
+            .then(function(folder) {
+                // The data is refreshed by navigating away...
+                $scope.$emit('message', { 'level': 'success', 'text': 'The Payment has been saved.'});
+                $scope.folder = folder;
+                $scope.paymentEditor = new Payment();
+                $scope.safeApply();
+              })
     }
-    return $scope.folder.getFilesRelatedToBill($scope.subid).reduce((acc, file) => {
-      return acc + (file.Amount ? parseInt(file.Amount, 10) : 0)
-    }, 0);
-  }
 
-  $scope.isEmpty = function(value) {
-    if (value == "" || value == "0" || value == 0 || value == "-1" || value == -1 || value == null) {
-      return "emptyValue";
+    $scope.actionDeletePayment = function(id) {
+        getDataService()
+            .then(dataService => dataService.deleteFile($scope.folder.getByTypeAndId(Payment, id)))
+            .then(function(folder) {
+                // The data is refreshed by navigating away...
+                $scope.$emit('message', { 'level': 'success', 'text': 'The Payment has been deleted.'});
+                $scope.folder = folder;
+                $scope.paymentEditor = new Payment();
+                $scope.safeApply();
+              })
     }
-    return "";
-  }
+
+    $scope.actionEditPayment = function(id) {
+        $scope.paymentEditor = new Payment($scope.folder.getByTypeAndId(Payment, id));
+    }
+
+    $scope.getPaymentTotal = function() {
+        if (!$scope.folder) {
+            return "?";
+        }
+        return $scope.folder.getFilesRelatedToBill($scope.subid).reduce((acc, file) => {
+            return acc + (file.Amount ? parseInt(file.Amount, 10) : 0)
+        }, 0);
+    }
+
+    $scope.isEmpty = function(value) {
+        if (value == "" || value == "0" || value == 0 || value == "-1" || value == -1 || value == null) {
+            return "emptyValue";
+        }
+        return "";
+    }
 }
 
 ctrl_file_bill.$inject = [ "$scope" ];
