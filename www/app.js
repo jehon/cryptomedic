1b60cd95f8099ce92c528450669ba1c4a73204a9
Jean Honlet <jehon@users.noreply.github.com>
2017-12-05 20:40:31 +0100

    Migrate fetchfull and fetchfull-elements to npm

--- www/app.js
+++ www/app.js
@@ -12,6 +12,9 @@ import ExcellentExport  from 'excellentexport';
 // Clone object clone(...)
 import clone            from 'clone';
 
+import fetchfull        from 'fetchfull';
+import fetchfullElement from 'fetchfull-elements';
 window.angular          = angular;
 window.ExcellentExport  = ExcellentExport;
 window.clone            = clone;
+window.fetchfull        = fetchfull;
