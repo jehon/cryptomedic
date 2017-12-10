a7b68961f31299a0a0429f1e9f41ced99968ad86
Jean Honlet <jehon@users.noreply.github.com>
2017-12-10 21:47:17 +0100

    Fix reset not doing twice
    And reformat the code

--- www/static/controllers/ctrl_search.js
+++ www/static/controllers/ctrl_search.js
@@ -1,36 +1,37 @@
 function ctrl_search($scope) {
-  if (typeof($scope.params) == 'undefined') {
-    $scope.params = {};
-  }
+    if (typeof($scope.params) == 'undefined') {
+        $scope.params = {};
+    }
 
-  if (typeof($scope.listing) == 'undefined') {
-    $scope.listing = [];
-  }
+    if (typeof($scope.listing) == 'undefined') {
+        $scope.listing = [];
+    }
 
-  $scope.page = function() {
-    // TODO GUI: render the results in pages of 20 ?
-    $scope.currentPage = $scope.listing;
-  };
+    $scope.page = function() {
+        // TODO GUI: render the results in pages of 20 ?
+        $scope.currentPage = $scope.listing;
+    };
 
-  $scope.submit = function() {
-    let updatedData = formGetContent("#searchForm");
+    $scope.submit = function() {
+        let updatedData = formGetContent("#searchForm");
+        console.log(updatedData);
 
-    getDataService()
-      .then(dataService => dataService.searchForPatients(updatedData))
-      .then(function(data) {
-        $scope.listing = data;
-        $scope.page();
+        getDataService()
+            .then(dataService => dataService.searchForPatients(updatedData))
+            .then(function(data) {
+                $scope.listing = data;
+                $scope.page();
+                $scope.safeApply();
+            }, function(data) {
+                console.error(data);
+            });
+    };
+
+    $scope.reset = function() {
+        document.querySelector("#searchForm").reset();
+        $scope.listing = [];
         $scope.safeApply();
-      }, function(data) {
-        console.error(data);
-      });
-  };
-
-  $scope.reset = function() {
-    $scope.params = {};
-    $scope.listing = [];
-    $scope.safeApply();
-  };
+    };
 }
 
-ctrl_search.$inject = [ "$scope" ];
+ctrl_search.$inject = ["$scope"];
