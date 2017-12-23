c493b2cd4101aac3c99fab1fb7a39b98bef1f842
Jean Honlet <marielineet.jean@gmail.com>
2017-12-23 20:41:43 +0100

    Structure the build path

--- webpack.config.js
+++ webpack.config.js
@@ -1,20 +1,25 @@
 
-const path = require('path');
+const CleanWebpackPlugin = require('clean-webpack-plugin');
 
+const path = require('path');
 const www = path.resolve(__dirname, 'www');
+const build = "build";
 
 module.exports = {
 	entry: www + '/app.js',
 	output: {
 		path: www,
-		filename: 'bundle.js'
+		filename: `${build}/bundle.js`
 	},
+	plugins: [
+		new CleanWebpackPlugin([ build ])
+	],
 	module: {
 		loaders: [
 			{ test: /\.css$/, loader: "style-loader!css-loader" },
 			{
 				test: /\.(eot|svg|ttf|woff|woff2)$/,
-				loader: 'file-loader?name=public/files/[name].[ext]'
+				loader: `file-loader?name=${build}/[name]-[hash].[ext]`
 			}
 		]
 	}
