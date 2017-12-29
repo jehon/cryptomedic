1c07a2f3a73b4299c36e9164b163cfc259bbc37e
Jean Honlet <jehon@users.noreply.github.com>
2017-12-29 19:02:47 +0100

    Remove unused myFinallyDone

--- www/static/js/start.js
+++ www/static/js/start.js
@@ -28,10 +28,3 @@ if (!are_cookies_enabled()) {
 if (!Promise || !indexedDB || !sessionStorage || !fetch) {
   window.location.href = '/static/upgrade.html';
 }
-
-Promise.prototype.myFinallyDone = function (callback) {
-  callback = callback || function(data) { return data; };
-  return this
-    .then(callback, callback)
-    .catch(function(reason) { console.error(reason); });
-};
