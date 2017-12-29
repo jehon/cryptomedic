a69776d7046d040c6acf5f16fa81696e69486be1
Jean Honlet <marielineet.jean@gmail.com>
2017-12-29 17:01:08 +0100

    Introduce Redux

---
+++ tests/unitjs/store-test.js
@@ -0,0 +1,20 @@
+
+describe("store", function() {
+	it("should exists", function() {
+		expect(store).not.toBeUndefined();
+		expect(typeof(store.dispatch)).toBe("function");
+		expect(typeof(store.getState)).toBe("function");
+	})
+
+	it("should handle ACT_FOLDER_*", function() {
+		expect(store.getState().folder).toBeFalsy();
+		store.dispatch({ type: ACT_FOLDER_INVALIDATE });
+		expect(store.getState().folder).toBeFalsy();
+
+		store.dispatch({ type: ACT_FOLDER_STORE, payload: "blablabla" });
+		expect(store.getState().folder).toBe("blablabla");
+
+		store.dispatch({ type: ACT_FOLDER_INVALIDATE });
+		expect(store.getState().folder).toBeFalsy();
+	})
+});
