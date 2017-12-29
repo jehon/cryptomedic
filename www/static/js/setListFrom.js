43a0132476946ef460be9bee3ea1d8ab4b4721ef
Jean Honlet <marielineet.jean@gmail.com>
2017-12-29 22:10:17 +0100

    Reformat the code

--- www/static/js/setListFrom.js
+++ www/static/js/setListFrom.js
@@ -1,24 +1,22 @@
 function setListFrom(origin, target, category) {
-  let val = document.querySelector(`[name=${origin}]`).value;
-  if (!cryptomedic.serverSettings || !cryptomedic.serverSettings.associations) {
-    return;
-  }
+    if (!cryptomedic.serverSettings || !cryptomedic.serverSettings.associations) {
+        return;
+    }
+    const val = document.querySelector(`[name=${origin}]`).value;
+    let list = [];
+    if (cryptomedic.serverSettings.associations[`${category}.${val}`]) {
+        list = list.concat(cryptomedic.serverSettings.associations[`${category}.${val}`]);
+    }
+    list = list.concat(cryptomedic.serverSettings.associations[`${category}.other`]);
 
-  let list = [];
-  if (cryptomedic.serverSettings.associations[`${category}.${val}`]) {
-    list = list.concat(cryptomedic.serverSettings.associations[`${category}.${val}`]);
-  }
-  list = list.concat(cryptomedic.serverSettings.associations[`${category}.other`]);
+    if (document.querySelector(`[name=${target}]`).value) {
+        // If the current is already selected, add it to the list to keep it...
+        if (list.indexOf(current) < 0) {
+            list = [current].concat(list);
+        }
+    }
 
-  let current = document.querySelector(`[name=${target}]`).value;
-  if (current == null) {
-
-  } else {
-    if (list.indexOf(current) < 0) {
-      list = [current].concat(list);
-    }    
-  }
-  document.querySelectorAll(`[name=${target}]`).forEach(el => {
-    el.setAttribute('list', JSON.stringify(list));
-  });
+    document.querySelectorAll(`[name=${target}]`).forEach(el => {
+        el.setAttribute('list', JSON.stringify(list));
+    });
 }
