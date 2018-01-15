47e8e21dd58ff962ca7f5e8362461d40802d7bb1
Jean Honlet <jehon@users.noreply.github.com>
2018-01-15 18:00:00 +0100

    Save release_version to www/release_version.txt for Sentry

--- webpack.config.js
+++ webpack.config.js
@@ -3,8 +3,13 @@ const CleanWebpackPlugin = require('clean-webpack-plugin');
 
 const webpack = require('webpack');
 const path = require('path');
+const fs = require('fs');
 const www = path.resolve(__dirname, 'www');
 const build = "build";
+
+const released_version = (new Date()).toISOString();
+
+fs.writeFileSync(__dirname + "/www/release_version.txt", released_version);
 
 module.exports = {
 	entry: www + '/app.js',
@@ -15,7 +20,7 @@ module.exports = {
 	plugins: [
 		new CleanWebpackPlugin([ build ]),
   		new webpack.DefinePlugin({
-    		APPLICATION_BUILD_TIME: "'" + (new Date()).toISOString() + "'"
+    		APPLICATION_BUILD_TIME: "'" + released_version + "'"
   		})
   	],
 	module: {
