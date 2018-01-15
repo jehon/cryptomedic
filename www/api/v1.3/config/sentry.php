bab82e502bfab5443f715fd05735c43b63dffee2
Jean Honlet <marielineet.jean@gmail.com>
2018-01-15 12:46:24 +0100

    Save release_version to www/release_version.txt for Sentry

--- www/api/v1.3/config/sentry.php
+++ www/api/v1.3/config/sentry.php
@@ -1,10 +1,18 @@
 <?php
 
+define("RELEASE_FILE", __dir__ . "/../../../../www/release_version.txt");
+
+if (file_exists(constant("RELEASE_FILE"))) {
+	$release_version = file_get_contents(constant("RELEASE_FILE"));
+} else {
+	$release_version = "not defined";
+}
+
 return [
     'dsn' => "https://55876691584f4ef6955068b0ef4c73f5:83b12fffd6e94760b245cfb3e098062b@sentry.io/271432",
 
-    // capture release as git sha
-    // 'release' => trim(exec('git log --pretty="%h" -n1 HEAD')),
+    // capture release
+    'release' => $release_version,
 
     // Capture bindings on SQL queries
     'breadcrumbs.sql_bindings' => true,
