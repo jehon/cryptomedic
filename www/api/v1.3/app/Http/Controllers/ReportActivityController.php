1f2fe07cd5ab1768b36c3a037deb49c66b77d8c1
Jean Honlet <jehon@users.noreply.github.com>
2018-01-16 18:00:00 +0100

    Fix ActivityReport by activity type

--- www/api/v1.3/app/Http/Controllers/ReportActivityController.php
+++ www/api/v1.3/app/Http/Controllers/ReportActivityController.php
@@ -6,41 +6,44 @@ use App\Model\References;
 
 class ReportActivityController extends ReportController {
   public function buildData() {
-    $list1 = $this->runSqlWithNamedParameter("SELECT *, NOT(" . $this->getParamAsSqlFilter('when', 'Date') . ") as complementary FROM (
-      SELECT
-        bills.id as bid,
-        patients.id as pid,
-        bills.Date as Date,
-        bills.ExaminerName as ExaminerName,
-        bills.Center as Center,
-        CONCAT(patients.entryyear, '-', patients.entryorder) as patient_reference,
-        patients.Name as patient_name,
-        patients.yearofbirth,
-        patients.Sex,
-        bills.sl_familySalary,
-        bills.sl_numberOfHouseholdMembers,
-        bills.Sociallevel,
-        patients.Pathology,
-        " . Bill::getSQLAct() . " as act,
-        " . Bill::getSQLTreatment() . " as treatment,
-        " . Bill::getSQLFieldsSum(Bill::CAT_CONSULT) . " AS price_consult,
-              " . Bill::getSQLFieldsSum(Bill::CAT_MEDECINE) . " AS price_medecine,
-        " . Bill::getSQLFieldsSum(Bill::CAT_WORKSHOP) . " AS price_workshop,
-              " . Bill::getSQLFieldsSum(Bill::CAT_SURGICAL) . " AS price_surgical,
-              " . Bill::getSQLFieldsSum(Bill::CAT_OTHER) . " AS price_other,
-              bills.total_real as total_real,
-              bills.total_asked as total_asked,
-              (select sum(amount) from payments where bill_id = bills.id and " . $this->getParamAsSqlFilter("when", "payments.Date") . " ) as total_paid,
-        exists(select * from bills as b2 where b2.patient_id = bills.patient_id and b2.Date < bills.Date) as oldPatient
-      FROM bills
-          JOIN patients ON bills.patient_id = patients.id
-          JOIN prices ON bills.price_id = prices.id
+    $list1 = $this->runSqlWithNamedParameter("
+      SELECT *, 
+        NOT(" . $this->getParamAsSqlFilter('when', 'Date') . ") as complementary FROM (
+        SELECT
+          bills.id as bid,
+          patients.id as pid,
+          bills.Date as Date,
+          bills.ExaminerName as ExaminerName,
+          bills.Center as Center,
+          CONCAT(patients.entryyear, '-', patients.entryorder) as patient_reference,
+          patients.Name as patient_name,
+          patients.yearofbirth,
+          patients.Sex,
+          bills.sl_familySalary,
+          bills.sl_numberOfHouseholdMembers,
+          bills.Sociallevel,
+          patients.Pathology,
+          " . Bill::getSQLAct() . " as act,
+          " . Bill::getSQLTreatment() . " as treatment,
+          " . Bill::getSQLFieldsSum(Bill::CAT_CONSULT) . " AS price_consult,
+                " . Bill::getSQLFieldsSum(Bill::CAT_MEDECINE) . " AS price_medecine,
+          " . Bill::getSQLFieldsSum(Bill::CAT_WORKSHOP) . " AS price_workshop,
+                " . Bill::getSQLFieldsSum(Bill::CAT_SURGICAL) . " AS price_surgical,
+                " . Bill::getSQLFieldsSum(Bill::CAT_OTHER) . " AS price_other,
+                bills.total_real as total_real,
+                bills.total_asked as total_asked,
+                (select sum(amount) from payments where bill_id = bills.id and " . $this->getParamAsSqlFilter("when", "payments.Date") . " ) as total_paid,
+          exists(select * from bills as b2 where b2.patient_id = bills.patient_id and b2.Date < bills.Date) as oldPatient
+        FROM bills
+            JOIN patients ON bills.patient_id = patients.id
+            JOIN prices ON bills.price_id = prices.id
+        WHERE (1 = 1)
+          AND " . $this->getParamAsSqlFilter("center", "Center") . "
+          AND " . $this->getParamAsSqlFilter("examiner", "ExaminerName") . "
+          AND " . Bill::getActivityFilter($this->getParam("activity", "")) . "
       ) AS t
       WHERE (1 = 1)
         AND ((" . $this->getParamAsSqlFilter("when", "Date") . ") OR (total_paid > 0))
-        AND " . $this->getParamAsSqlFilter("center", "Center") . "
-        AND " . $this->getParamAsSqlFilter("examiner", "ExaminerName") . "
-        AND " . Bill::getActivityFilter($this->getParam("activity", "")) . "
       ORDER BY complementary ASC, Date ASC, bid ASC "
     );
 
