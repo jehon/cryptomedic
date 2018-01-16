a3e0d8303f8763e620d4b54555c171b1dd898d56
Jean Honlet <jehon@users.noreply.github.com>
2018-01-16 18:00:00 +0100

    Add Raj back

--- www/api/v1.3/app/Model/References.php
+++ www/api/v1.3/app/Model/References.php
@@ -220,7 +220,7 @@ References::$lists["Examiner"] = References::buildValueList([
   References::withCode("Rezaul",     "Res"),
   References::withCode("Sultana",    "Sul"),
 - References::withCode("Lipa",       "Lip"), // As of 05/2017
-  // References::withCode("Raj",        "Raj"), // As of 02/2017
+  References::withCode("Raj",        "Raj"), // As of 02/2017
   References::withCode("Juwel",      "Juwel"), // As of 01/2018
 
   // orthesis
