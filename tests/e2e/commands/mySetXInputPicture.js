7db1007e01dbd957c4a5e4dcb4c42dd6bc4c0fbe
Jean Honlet <marielineet.jean@gmail.com>
2018-01-17 20:21:29 +0100

    Migrate to x-input-picture

---
+++ tests/e2e/commands/mySetXInputPicture.js
@@ -0,0 +1,19 @@
+exports.command = function(selector, value, callback) {
+    this.waitForElementVisible(selector);
+    this.assert.visible(selector);
+
+    this.execute(function(selector, attributeName, attributeValue) {
+            let el = document.querySelector(selector);
+            el._generatePreview(XInputPicture.dataURItoBlob(value, "test.jpg"));
+            return true;
+        }, [selector, value],
+        (result) => {
+            if (typeof callback === 'function') {
+                this[callback](result);
+            }
+        });
+
+    this.pause(200);
+    
+    return this;
+};
