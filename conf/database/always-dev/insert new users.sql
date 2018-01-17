d7ffcd70c770ab1745e0cf90880ccf7b76ec55f0
Jean Honlet <marielineet.jean@gmail.com>
2018-01-17 11:53:02 +0100

    Add some users

--- conf/database/always-dev/insert new users.sql
+++ conf/database/always-dev/insert new users.sql
@@ -2,7 +2,9 @@
 UPDATE `users` SET `inExaminerList` = '1' WHERE `users`.`username` = "murshed";
 UPDATE `users` SET `inExaminerList` = '1' WHERE `users`.`username` = "thierry";
 
-INSERT INTO `users` (`id`, `created_at`, `updated_at`, `lastuser`, `username`, `name`, `password`, `email`, `codage`, `group`, `inExaminerList`, `notes`, `last_login`, `remember_token`) VALUES (NULL, NULL, '1980-01-01 00:00:00.000000', 'jehon', 'ershad', 'Ershad', NULL, NULL, 'ERS', 'cdc', '1', NULL, NULL, NULL);
+INSERT INTO `users` (`lastuser`, `username`, `name`, `codage`, `group`, `inExaminerList`) VALUES ('jehon', 'ershad', 'Ershad', 'Ers', 'cdc', '1');
+
+INSERT INTO `users` (`lastuser`, `username`, `name`, `codage`, `group`, `inExaminerList`) VALUES ('jehon', 'shetou', 'Shetou', 'She', 'cdc', '1');
 
 UPDATE `users` SET codage = "MUR" WHERE `username` = "murshed";
 UPDATE `users` SET codage = "JH"  WHERE `username` = "jehon";
