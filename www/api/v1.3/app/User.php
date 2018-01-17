0836f31f3562e61a9363d690a13d91eb7108077a
Jean Honlet <marielineet.jean@gmail.com>
2018-01-17 20:22:04 +0100

    Use guarded instead of fillable

--- www/api/v1.3/app/User.php
+++ www/api/v1.3/app/User.php
@@ -19,18 +19,15 @@ class User extends Authenticatable
     protected $table = 'users';
 
     /**
-     * The attributes that are mass assignable.
-     *
-     * @var array
-     */
-	protected $fillable = [ 'username', 'name', 'email', 'group', 'password' ];
-
-    /**
      * The attributes that should be hidden for arrays.
      *
      * @var array
      */
     protected $hidden = [
+        'password', 'remember_token',
+    ];
+
+    protected $garded = [
         'password', 'remember_token',
     ];
 
