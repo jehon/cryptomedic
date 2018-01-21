5f00f990745a5cd23e8dc62bf8969e60854c0d97
Jean Honlet <marielineet.jean@gmail.com>
2018-01-21 20:46:49 +0100

    Add todo's on x-overlay and x-waiting

--- www/static/elements/x-overlay.js
+++ www/static/elements/x-overlay.js
@@ -1,75 +1,72 @@
+// Rewrite this as the master of x-waiting
+//    and add params:
+//    - z-index
+//    - cover screen or element
+//    - show / hide "close button"
+
 (function() {
     class XOverlay extends XWaiting {
         constructor() {
             super();
             this.shadowRoot.innerHTML = `
-<div id="overlay" class="overlay">
-	<!-- Button to close the overlay navigation -->
-	<a id='close' href="javascript:void(0)" class="closebtn">&times;</a>
+			<style>
+				:host {
+					position: relative;
+				}
 
-	<!-- Overlay content -->
-	<div class="overlay-content">
-		<slot></slot>
-	</div>
-</div>
-<style>
-	/* The Overlay (background) */
-	.overlay {
-	    height: 100%;
-	    width: 100%;
-	    position: fixed;
-	    z-index: 1;
-	    left: 0;
-	    top: 0;
-	    background-color: rgb(0,0,0); /* Black fallback color */
-	    background-color: rgba(0,0,0, 0.9); /* Black w/opacity */
-	    overflow-x: hidden; /* Disable horizontal scroll */
-	    transition: 0.5s; /* 0.5 second transition effect to slide in or slide down the overlay (height or width, depending on reveal) */
-	}
+                #overlay[hidden] {
+                    display: none !important;
+                }
 
-	/* Position the content inside the overlay */
-	.overlay-content {
-	    position: relative;
-	    top: 25%; /* 25% from the top */
-	    width: 100%; /* 100% width */
-	    text-align: center; /* Centered text/links */
-	    margin-top: 30px; /* 30px top margin to avoid conflict with the close button on smaller screens */
-	}
+				#overlay {
+				    position: fixed;
+				    top: 0;
+				    left: 0;
+				    width: 100%;
+				    height: 100%;
+				    z-index: 1;
 
-	/* The navigation links inside the overlay */
-	.overlay a {
-	    padding: 8px;
-	    text-decoration: none;
-	    font-size: 36px;
-	    color: #818181;
-	    display: block; /* Display block instead of inline */
-	    transition: 0.3s; /* Transition effects on hover (color) */
-	}
+				    display: flex;
+					flex-direction: column;
+                    align-items: center; 
+					justify-content: center;
 
-	/* When you mouse over the navigation links, change their color */
-	.overlay a:hover, .overlay a:focus {
-	    color: #f1f1f1;
-	}
+				    background-color: rgba(0,0,0, 0.9); /* Black w/opacity */
+				    overflow-x: hidden;
+				    transition: 0.5s;
+				}
 
-	/* Position the close button (top right corner) */
-	.overlay .closebtn {
-	    position: absolute;
-	    top: 20px;
-	    right: 45px;
-	    font-size: 60px;
-	}
+				#close {
+				    position: absolute;
+				    top: 20px;
+				    right: 45px;
+				    font-size: 60px;
+				}
 
-	/* When the height of the screen is less than 450 pixels, change the font-size of the links and position the close button again, so they don't overlap */
-	@media screen and (max-height: 450px) {
-	    .overlay a {font-size: 20px}
-	    .overlay .closebtn {
-	        font-size: 40px;
-	        top: 15px;
-	        right: 35px;
-	    }
-	}
-</style>
-`
+				#close {
+				    text-decoration: none;
+				    color: #818181;
+				}
+
+				#close:hover, #close:focus {
+				    text-decoration: none;
+				    color: #f1f1f1;
+				}
+
+				/* When the height of the screen is less than 450 pixels, change the font-size of the links and position the close button again, so they don't overlap */
+				@media screen and (max-height: 450px) {
+				    .closebtn {
+				        font-size: 40px;
+				        top: 15px;
+				        right: 35px;
+				    }
+				}
+			</style>
+			<div id="overlay">
+				<a id='close' href="javascript:void(0)">&times;</a>
+				<slot></slot>
+			</div>
+			`;
             this.shadowRoot.querySelector("#close").addEventListener("click", () => this.free());
             this.shadowRoot.querySelector("#close").style.display = "none";
             this.free();
