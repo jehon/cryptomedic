f423520770c9cd7b257296bc7f4e1cff596ec2c9
Jean Honlet <jehon@users.noreply.github.com>
2018-02-24 21:34:12 +0100

    Extend test reset

--- conf/database/test-reset.sql
+++ conf/database/test-reset.sql
@@ -1,6 +1,9 @@
 -- autoGenerateReference
 DELETE FROM patients WHERE entryyear = 1998 and entryorder = 10000;
 
+-- createReferenceTest
+DELETE FROM patients WHERE entryyear = 1999 and entryorder = 104;
+
 -- modifyBillTest: relock the file
 UPDATE `bills` SET `updated_at` = '2014-01-01 00:00:00' WHERE `bills`.`id` = 2;
 
