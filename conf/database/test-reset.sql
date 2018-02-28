efcdcec3f7d3d74c2ec05dade1b27dd45bc30118
Jean Honlet <jehon@users.noreply.github.com>
2018-02-28 18:00:00 +0100

    Extend testing

--- conf/database/test-reset.sql
+++ conf/database/test-reset.sql
@@ -6,6 +6,7 @@ DELETE FROM patients WHERE entryyear = 1999 and entryorder = 104;
 
 -- modifyBillTest: relock the file
 UPDATE `bills` SET `updated_at` = '2014-01-01 00:00:00' WHERE `bills`.`id` = 2;
+DELETE FROM payments WHERE id > 8;
 
 -- modifyPatientTest: delete sub files
 DELETE FROM ricket_consults WHERE patient_id = 7;
