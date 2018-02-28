93ff84a10cff34f6edb99a94428bfec7047f3394
Jean Honlet <jehon@users.noreply.github.com>
2018-02-28 18:00:00 +0100

    Rework start.js to ease reading

--- www/static/js/start.js
+++ www/static/js/start.js
@@ -2,24 +2,40 @@
 // See md5sum.php to protect old versions... and delete them when necessary !!!
 let API_VERSION = 'v1.3';
 
-if (!Promise || !indexedDB || !sessionStorage || !fetch) {
-	window.location.href = '/static/upgrade.html';
-}
-
-function are_cookies_enabled() {
-	var cookieEnabled = (navigator.cookieEnabled) ? true : false;
-
-	if (typeof(navigator.cookieEnabled) == 'undefined' && !cookieEnabled) {
-		document.cookie = 'testcookie';
-		cookieEnabled = (document.cookie.indexOf('testcookie') != -1) ? true : false;
+(function() {
+	const shouldUpgrade = function(msg) {
+		window.location.href = '/static/upgrade.html';
 	}
-	return (cookieEnabled);
-}
 
-if (!are_cookies_enabled()) {
-	alert('Your cookie are disabled. Please enable them.\nVos cookies sont désactivés. Merci de les activer.');
-}
+	if (!Promise || !indexedDB || !sessionStorage || !fetch) {
+		shouldUpgrade("Promise || indexedDB || sessionStorage || fetch");
+	}
 
-if (!Promise || !indexedDB || !sessionStorage || !fetch) {
-	window.location.href = '/static/upgrade.html';
-}
+	// Test input[type=date]
+	// @See https://stackoverflow.com/a/10199306/1954789
+	(function() {
+	    var input = document.createElement('input');
+	    input.setAttribute('type','date');
+
+	    var notADateValue = 'not-a-date';
+	    input.setAttribute('value', notADateValue);
+
+	    if (input.value === notADateValue) {
+	    	shouldUpgrade("input[type=date]")
+	    }
+	})();
+
+	// Test cookies
+	(function() {
+		var cookieEnabled = (navigator.cookieEnabled) ? true : false;
+
+		if (typeof(navigator.cookieEnabled) == 'undefined' && !cookieEnabled) {
+			document.cookie = 'testcookie';
+			cookieEnabled = (document.cookie.indexOf('testcookie') != -1) ? true : false;
+		}
+		if (!cookieEnabled) {
+			alert('Your cookie are disabled. Please enable them.\nVos cookies sont désactivés. Merci de les activer.');
+			console.error("Cookies are disabled");
+		}
+	})();
+})();
