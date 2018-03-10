989e6c36592e732e3b0081b1cc0350c7a9536ca6
Jean Honlet <marielineet.jean@gmail.com>
2018-03-10 14:51:05 +0100

    Convert jh-codage into jhElement

--- www/static/elements/jh-codage.js
+++ www/static/elements/jh-codage.js
@@ -1,56 +1,51 @@
-
 (function() {
-  class JHCodage extends HTMLElement {
-    constructor() {
-      super();
-      // Create a shadow root
-      this.attachShadow({ mode: 'open' });
-      this.adapt();
+    class JHCodage extends JHElement {
+        static get properties() {
+            return { 
+                "value": "string", 
+                "translated": "string"
+            };
+        }
+
+        constructor() {
+            super();
+            // Create a shadow root
+            this.attachShadow({ mode: 'open' });
+        }
+
+        adapt() {
+            console.log("adapt: ", this._value, this._translated);
+            if (this._translated) {
+                return this._setTranslated(this._value, this._translated);
+            }
+
+            const definitions = store.getState().definitions;
+
+            if (!definitions) {
+                return this._setRaw(this._value);
+            }
+
+            if (!definitions.codes[this._value]) {
+                return this._setRaw(this._value);
+            }
+
+            return this._setTranslated(this._value, definitions.codes[this._value]);
+        }
+
+        _setTranslated(value, translated) {
+            this.setAttribute("calculated-translated", translated);
+            this.shadowRoot.innerHTML = `
+                <span id='translating' data-toggle='tooltip' data-placement='bottom' title='${value}'>
+                    <span id='translated'>${translated}</span>*
+                </span>
+            `
+        }
+
+        _setRaw(value) {
+            this.setAttribute("calculated-translated", "");
+            this.shadowRoot.innerHTML = `<span id='original'>${value}</span>`
+        }
     }
 
-    static get observedAttributes() { return [ 'value', 'translated' ]; }
-
-    attributeChangedCallback(attributeName, oldValue, newValue, namespace) {
-      this.adapt();
-    }
-
-    adapt() {
-      let value      = this.getAttribute("value");
-      let translated = this.getAttribute("translated");
-
-      if (translated) {
-        return this._setTranslated(value, translated);
-      }
-
-      const definitions = store.getState().definitions;
-
-      if (!definitions) {
-        return this._setRaw(value);
-      }
-
-      if (!definitions.codes[value]) {
-        return this._setRaw(value);
-      }
-
-      return this._setTranslated(value, definitions.codes[value]);
-    }
-
-    _setTranslated(value, translated) {
-      this.setAttribute("calculated-translated", translated);
-      this.shadowRoot.innerHTML = `
-        <span id='translating' data-toggle='tooltip' data-placement='bottom' title='${value}'>
-          <span id='translated'>${translated}</span>*
-        </span>
-      `
-    }
-
-    _setRaw(value) {
-      this.setAttribute("calculated-translated", "");
-      this.shadowRoot.innerHTML = `
-        <span id='original'>${value}</span>
-      ` 
-    }
-  }
-
-  window.customElements.define('jh-codage', JHCodage);
+    window.customElements.define('jh-codage', JHCodage);
 })();
