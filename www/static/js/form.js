243c805859dcf7ea708e4917f2e4657e633460d9
Jean Honlet <jehon@users.noreply.github.com>
2018-03-22 20:59:30 +0100

    Check for :checked
    - Reformat
    - Add logger for future debugging

--- www/static/js/form.js
+++ www/static/js/form.js
@@ -1,116 +1,126 @@
 
 function formGetContent(form, prototype = {}) {
-  let data = new prototype.constructor();
-  Object.assign(data, prototype);
+	// const log = (...args) => console.log(...args);
+	const log = () => {};
 
-  // form can be:
-  //  - css selector
-  //  - NodeList
-  //  - HTMLElement
+	let data = new prototype.constructor();
+	Object.assign(data, prototype);
 
-  let formElement = form;
-  if (typeof(form) == "string") {
-    // CSS Selector -> HTMLElement
-    formElement = document.querySelector(form);
-  }
+	// form can be:
+	//  - css selector
+	//  - NodeList
+	//  - HTMLElement
 
-  let elementsList = formElement;
-  if (formElement instanceof HTMLElement) {
-    // HTMLElement -> NodeList
-    elementsList = formElement.querySelectorAll("[name]");
-  }
+	let formElement = form;
+	if (typeof (form) == 'string') {
+		// CSS Selector -> HTMLElement
+		formElement = document.querySelector(form);
+	}
 
-  for(let i of elementsList) {
-    // Skip disabled elements
-    if (i.disabled) {
-      continue;
-    }
+	let elementsList = formElement;
+	if (formElement instanceof HTMLElement) {
+		// HTMLElement -> NodeList
+		elementsList = formElement.querySelectorAll('[name]');
+	}
 
-    if (i.offsetHeight + i.offsetWidth < 1) {
-      continue;
-    }
+	for (let i of elementsList) {
+		// Skip empty names
+		let name = i.getAttribute('name');
+		if (!name) {
+			log('no name: ', i);
+			continue;
+		}
 
-    // Skip empty names
-    let name = i.getAttribute('name');
-    if (!name) {
-      continue;
-    }
+		// Skip disabled elements
+		if (i.disabled) {
+			log('disabled: ', name);
+			continue;
+		}
 
-    // Only take the selected radio
-    if (i.matches("[type=radio") && !i.matches("[checked]")) {
-      continue;
-    }
+		if (i.offsetHeight + i.offsetWidth < 1) {
+			log('too small: ', name, i.offsetHeight, i.offsetWidth);
+			continue;
+		}
 
-    let value = i.value;
+		// Only take the selected radio
+		if (i.matches('[type=radio') && !i.matches(':checked')) {
+			log('unchecked radio: ', name, i);
+			continue;
+		}
 
-    // Skip empty values
-    if (value === "" || value == null) {
-      delete(data[name]);
-      continue;
-    }
+		let value = i.value;
 
-    // Treat some special cases
-    if (i.type) {
-      switch(i.type) {
-        case "number":
-          value = Number.parseInt(value);
-          break;
-        /* istanbul ignore next: impossible to fill in a input[type=file] element - see MSDN */
-        case "file":
-          // http://blog.teamtreehouse.com/uploading-files-ajax
-          // We can pass the "File" object to FormData, it will handle it for us....
-          if (i.data) {
-            value = i.data.data;
-          } else {
-            value = null;
-          }
-          break;
-      }
-    }
+		// Skip empty values
+		if (value === '' || value == null) {
+			log('no value: ', name, value, typeof(value));
+			delete (data[name]);
+			continue;
+		}
 
-    // Assign it
-    if (value == null) {
-      delete(data[name]);
-    } else {
-      data[name] = value;
-    }
-  }
-  return data;
+		// Treat some special cases
+		if (i.type) {
+			switch (i.type) {
+				case 'number':
+					value = Number.parseInt(value);
+					break;
+				/* istanbul ignore next: impossible to fill in a input[type=file] element - see MSDN */
+				case 'file':
+					// http://blog.teamtreehouse.com/uploading-files-ajax
+					// We can pass the 'File' object to FormData, it will handle it for us....
+					if (i.data) {
+						value = i.data.data;
+					} else {
+						value = null;
+					}
+					break;
+			}
+		}
+
+		// Assign it
+		if (value == null) {
+			log('null value: ', name);
+			delete (data[name]);
+		} else {
+			data[name] = value;
+			log('ok: ', name, value);
+		}
+	}
+	return data;
 }
 
 function formFillIn(form, object) {
-    let formElement = form;
-    if (!(form instanceof HTMLElement)) {
-        formElement = document.querySelector(form);
-    }
+	let formElement = form;
+	if (!(form instanceof HTMLElement)) {
+		formElement = document.querySelector(form);
+	}
 
-    Object.keys(object).forEach(k => {
-        formElement.querySelectorAll(`[name='${k}']`).forEach(el => el.setAttribute("value", (object[k] ? object[k] : "")));
-    });
+	Object.keys(object).forEach(k => {
+		formElement.querySelectorAll(`[name='${k}']`).forEach(el => el.setAttribute('value', (object[k] ? object[k] : '')));
+	});
 
-    formEvaluateFunctions(formElement, object);
+	formEvaluateFunctions(formElement, object);
 }
 
 function formEvaluateFunctions(formElement, object) {
-    formElement.querySelectorAll(`[function]`).forEach(el => {
-        fn = el.getAttribute('function');
-        if (fn in object) {
-            el.innerHTML = object[fn]()
-        }
-    });
+	formElement.querySelectorAll('[function]').forEach(el => {
+		const fn = el.getAttribute('function');
+		if (fn in object) {
+			el.innerHTML = object[fn]()
+		}
+	});
 }
 
 function formSwitch(form, tag, mode) {
-    let formElement = form;
-    if (!(form instanceof HTMLElement)) {
-        formElement = document.querySelector(form);
-    }
+	let formElement = form;
+	if (!(form instanceof HTMLElement)) {
+		formElement = document.querySelector(form);
+	}
 
-    formElement.querySelectorAll(`[${tag}]`).forEach(el => {
-        if (mode == "read") {
-            el.removeAttribute('edit');
-        } else {
-            el.setAttribute('edit', tag);
-        }
-    })
+	formElement.querySelectorAll(`[${tag}]`).forEach(el => {
+		if (mode == 'read') {
+			el.removeAttribute('edit');
+		} else {
+			el.setAttribute('edit', tag);
+		}
+	})
 }
