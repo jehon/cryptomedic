95419142f8ee62b5fb8c274af17dcb99fdec5bb4
Jean Honlet <jehon@users.noreply.github.com>
2018-03-22 20:59:01 +0100

    Add somes tools

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,30 +1,31 @@
 module.exports = {
-  "extends": "eslint:recommended",
-  "parserOptions": {
-    "ecmaVersion": 6,
-    "sourceType": "browser",
-    "ecmaFeatures": {
-      "impliedStrict": true
+    "env": {
+        "browser": true,
+        "es6": true
+    },
+    "extends": "eslint:recommended",
+    "parserOptions": {
+        // "sourceType": "module"
+    },
+    "rules": {
+        "indent": [
+            "error",
+			"tab",
+			{ "SwitchCase": 1 }
+        ],
+        "linebreak-style": [
+            "error",
+            "unix"
+        ],
+        "quotes": [
+            "error",
+            "single"
+        ],
+        "semi": [
+            "error",
+            "always"
+        ],
+        "no-unused-vars": [ "error", { "vars": "local" }],
+		"no-console": [ "error", { allow: [ "info", "warn", "error" ]}],
     }
-  },
-  "env": {
-    "browser": true,
-    "es6":     true,
-    "jasmine": true
-  },
-  "rules": {
-    "indent": [
-        2,
-        2,
-        { "SwitchCase": 1 }
-    ],
-    "no-console": [ "off" ],
-    "no-undef": [ "warn" ],
-    "no-unused-vars": [ "warn" ],
-  },
-  "globals": {
-    "Polymer": true,
-    "cryptomedic": true,
-    "getDataService": true
-  }
-}
+};
