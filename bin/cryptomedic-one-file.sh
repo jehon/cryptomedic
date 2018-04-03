190e4e2f3b9cb3dc9587928479d5335a553c4fd1
Jean Honlet <jehon@users.noreply.github.com>
2018-04-03 20:40:17 +0200

    Add script to upload one file

---
+++ bin/cryptomedic-one-file.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+set -e
+
+SCRIPT_DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
+PRJ_DIR=$(dirname "$SCRIPT_DIR")
+
+conf_site="prod"
+
+ftp_host=`php ${PRJ_DIR}/config.php deployment.$conf_site.ftp_host 2>/dev/null || true`
+ftp_user=`php ${PRJ_DIR}/config.php deployment.$conf_site.ftp_user 2>/dev/null || true`
+ftp_pass=`php ${PRJ_DIR}/config.php deployment.$conf_site.ftp_pass 2>/dev/null || true`
+remote_root=""
+local_root="$PRJ_DIR"
+
+if [[ "$ftp_host" = "" ]]; then
+  echo "Site not found in configuration: $conf_site"
+  exit 255
+fi
+
+FN="$1"
+
+(
+    echo "open -u $ftp_user,$ftp_pass $ftp_host";
+    echo "put '$local_root/$FN' -o '$remote_root/$FN'"
+	echo ""
+) | lftp
