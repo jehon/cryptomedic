65e7c521ddeb17e4fa187341dd16b147dd3ab35b
Jean Honlet <jehon@users.noreply.github.com>
2018-04-28 20:46:54 +0200

    Reorder lines

--- conf/database/always-dev/insert new users.sql
+++ conf/database/always-dev/insert new users.sql
@@ -2,10 +2,10 @@
 UPDATE `users` SET `inExaminerList` = '1' WHERE `users`.`username` = "murshed";
 UPDATE `users` SET `inExaminerList` = '1' WHERE `users`.`username` = "thierry";
 
-INSERT INTO `users` (`lastuser`, `username`, `name`, `codage`, `group`, `inExaminerList`) VALUES ('jehon', 'ershad', 'Ershad', 'Ers', 'cdc', '1');
-
-INSERT INTO `users` (`lastuser`, `username`, `name`, `codage`, `group`, `inExaminerList`) VALUES ('jehon', 'shetou', 'Shetou', 'She', 'cdc', '1');
-
 UPDATE `users` SET codage = "MUR" WHERE `username` = "murshed";
 UPDATE `users` SET codage = "JH"  WHERE `username` = "jehon";
 UPDATE `users` SET codage = "TCA" WHERE `username` = "thierry";
+
+INSERT INTO `users` (`lastuser`, `username`, `name`, `codage`, `group`, `inExaminerList`) VALUES ('jehon', 'ershad', 'Ershad', 'Ers', 'cdc', '1');
+INSERT INTO `users` (`lastuser`, `username`, `name`, `codage`, `group`, `inExaminerList`) VALUES ('jehon', 'shetou', 'Shetou', 'She', 'cdc', '1');
+
