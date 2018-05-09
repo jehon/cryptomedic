bb9f2ecc8fbb604e669e0077e9ad3d0d61b55ba4
Jean Honlet <jehon@users.noreply.github.com>
2018-05-09 20:34:09 +0200

    Add MarkHeader header

--- www/api/v1.3/app/Http/Kernel.php
+++ www/api/v1.3/app/Http/Kernel.php
@@ -15,7 +15,8 @@ class Kernel extends HttpKernel
      */
     protected $middleware = [
 //        \Illuminate\Foundation\Http\Middleware\CheckForMaintenanceMode::class,
-    ];
+		\App\Http\Middleware\MarkHeader::class
+	];
 
     /**
      * The application's route middleware groups.
