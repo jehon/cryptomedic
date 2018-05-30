3d48f59165b1f699cbe87100788a147746c9f1b4
Jean Honlet <jehon@users.noreply.github.com>
2018-05-31 00:04:05 +0200

    Extends testing and assertDate

--- tests/unitjs/data-test.js
+++ tests/unitjs/data-test.js
@@ -7,11 +7,12 @@ describe('tests/unit/data-test.js', function() {
 		b: 'azer',
 		c: 0,
 		d: '1',
+		obj: {},
 		nullKey: null,
 		dateNum: '2015',
 		dateNumInv: '20',
 		dateNum6: '2016-02',
-		dateObj: new Date()
+		dateObj: new Date(2016, 1, 3)
 	};
 
 	describe('dataCatch', function() {
@@ -28,6 +29,7 @@ describe('tests/unit/data-test.js', function() {
 		expect(Data(data).assertExists('b')).toBe('azer');
 		expect(Data(data).assertExists('c')).toBe(0);
 		expect(Data(data).assertExists('d')).toBe('1');
+		expect(Data(data).assertExists('obj')).toEqual({});
 	});
 
 	it('with assertNumeric', function() {
@@ -37,6 +39,7 @@ describe('tests/unit/data-test.js', function() {
 		expect(() => Data(data).assertNumeric('b')).toThrowError('Data b is not numeric(azer)');
 		expect(Data(data).assertNumeric('c')).toBe(0);
 		expect(Data(data).assertNumeric('d')).toBe(1);
+		expect(() => Data(data).assertNumericNotZero('obj')).toThrowError('Data obj is not numeric(object)');
 	});
 
 	it('with assertNumericNotZero', function() {
@@ -46,6 +49,7 @@ describe('tests/unit/data-test.js', function() {
 		expect(() => Data(data).assertNumericNotZero('b')).toThrowError('Data b is not numeric(azer)');
 		expect(() => Data(data).assertNumericNotZero('c')).toThrowError('Data c is not non-zero(0)');
 		expect(Data(data).assertNumericNotZero('d')).toBeTruthy();
+		expect(() => Data(data).assertNumericNotZero('obj')).toThrowError('Data obj is not numeric(object)');
 	});
 
 	it('with assertDate', function() {
@@ -55,8 +59,11 @@ describe('tests/unit/data-test.js', function() {
 		expect(() => Data(data).assertDate('b')).toThrowError('Data b is not a valid date(azer)');
 		expect(() => Data(data).assertDate('c')).toThrowError('Data c is not a valid date(0)');
 		expect(() => Data(data).assertDate('d')).toThrowError('Data d is not a valid date(1)');
+		expect(() => Data(data).assertDate('obj')).toThrowError('Data obj is not a valid date(object)');
 
 		expect(() => Data(data).assertDate('dateNumInv')).toThrowError('Data dateNumInv is not a valid date(20)');
-		expect(Data(data).assertNumericNotZero('d')).toBeTruthy();
+		expect(Data(data).assertDate('dateNum')).toEqual(new Date(2015, 0, 1));
+		expect(Data(data).assertDate('dateNum6')).toEqual(new Date(2016, 1, 1));
+		expect(Data(data).assertDate('dateObj')).toEqual(new Date(2016, 1, 3));
 	});
 });
