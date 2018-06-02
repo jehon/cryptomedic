70e44a00b4543182a3b0b23dc54ad40cb5aa0e0d
Jean Honlet <marielineet.jean@gmail.com>
2018-06-02 09:00:40 +0200

    Enhance bill's elements

--- www/static/elements/x-file.js
+++ www/static/elements/x-file.js
@@ -1,12 +1,13 @@
 /* global XWaiting,store */
-/* global DataMissingException */
+/* global DataMissingException, ApplicationException */
 /* global calculations */
 
 const XFile = (function() {
 	class XFile extends XWaiting {
 		static get properties() {
 			return {
-				value: 'Object'
+				value: 'Object',
+				patient: 'Object'
 			};
 		}
 
@@ -36,6 +37,72 @@ const XFile = (function() {
 		adapt() {
 			// Stub for testing
 			super.adapt();
+		}
+
+		assertData() {
+			if (!this.value) {
+				throw new ApplicationException('Data not set');
+			}
+			return this.value;
+		}
+
+		assertExists(field) {
+			const values = this.assertData();
+			if (typeof (values[field]) == 'undefined'
+				|| values[field] == null
+			) {
+				throw new DataMissingException(field, 'is undefined');
+			}
+			return values[field];
+		}
+
+		assertNumeric(field) {
+			let v = this.assertExists(field);
+			if (typeof(v) == 'number') {
+				return v;
+			}
+			if (typeof(v) == 'string') {
+				let vi = parseInt(this.value[field]);
+				if (!isNaN(vi)) {
+					return vi;
+				}
+				throw new DataMissingException(field, `is not numeric(${v})`);
+			}
+			throw new DataMissingException(field, `is not numeric(${typeof(v)})`);
+		}
+
+		assertNumericNotZero(field) {
+			const v = this.assertNumeric(field);
+			if (v !== 0) {
+				return v;
+			}
+			throw new DataMissingException(field, `is not non-zero(${v})`);
+		}
+
+		assertDate(field) {
+			let v = this.assertExists(field);
+			if (typeof(v) == 'number') {
+				v = '' + v;
+			}
+			if (typeof(v) == 'string') {
+				if (v.length < 4) {
+					throw new DataMissingException(field, `is not a valid date(${v})`);
+				}
+				var ry = parseInt(v.substring(0, 4));
+				if (isNaN(ry)) {
+					throw new DataMissingException(field, `is not a valid date(${v})`);
+				}
+				var rm = parseInt(v.substring(5, 7));
+				if (isNaN(rm)) {
+					rm = 1; // emulate january
+				}
+				return new Date(ry, rm - 1, 1);
+			}
+			if (v instanceof Date) {
+				return v;
+			}
+			throw new DataMissingException(field, `is not a valid date(${typeof(v)})`);
+			// return v;
 		}
 	}
 
