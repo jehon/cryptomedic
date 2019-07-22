dab5fe1da11feb30fa1bf49a7e6315b410e6f9f2
Jean Honlet <jehon@users.noreply.github.com>
2019-07-22 20:47:55 +0200

    Remove dead code

--- config-site.php
+++ config-site.php
@@ -28,11 +28,6 @@ if (file_exists(__DIR__ . "/../secrets.php")) {
         'ftp_host' => 'ftp.cryptomedic.org',
         'ftp_user' => $mysecrets['cryptomedic.ftp.username'],
         'ftp_pass' => $mysecrets['cryptomedic.ftp.password'],
-        // 'packages' => [
-        //   [
-        //     'extra_cmd_line' => '--exclude www/api/v1.0/storage/framework/'
-        //   ],
-        // ],
         'security_key' => $mysecrets['cryptomedic.security_key']
     ];
 }
