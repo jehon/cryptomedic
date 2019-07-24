c98b77f9711111857dc7fda950e00fac078d6028
Jean Honlet <jehon@users.noreply.github.com>
2019-07-24 17:43:25 +0200

    fix patch not working silently

--- conf/database/versions/84 price workshop other.sql
+++ conf/database/versions/84 price workshop other.sql
@@ -1,6 +1,8 @@
 
-ALTER TABLE `bills` \
+ALTER TABLE `bills`
 	ADD `workshop_other` INT(11) NOT NULL DEFAULT '0' AFTER `workshop_Extension_brace_-_AFO`;
 
-ALTER TABLE `prices` \
+ALTER TABLE `prices`
 	ADD `workshop_other` INT(11) NOT NULL DEFAULT '-1' AFTER `workshop_Extension_brace_-_AFO`;
+
+UPDATE prices SET workshop_other = 0 WHERE id = (select MAX(id) FROM (select id from prices) t);
--- www/maintenance/patch_db.php
+++ www/maintenance/patch_db.php
@@ -6,8 +6,11 @@ require_once(__DIR__ . "/../../config.php");
 require_once(__DIR__ . "/../../bin/lib/Database.php");
 
 global $myconfig;
+ob_start();
+http_response_code(500);
 
 if (!$myconfig['security']['key']) {
+	
   die("No security.admin configured");
 }
 
@@ -15,14 +18,21 @@ if ($_REQUEST['pwd'] != $myconfig['security']['key']) {
   die("No correct pwd given");
 }
 
-$db = new \Jehon\Maintenance\Database(
-    "mysql:dbname={$myconfig['database']['schema']};host={$myconfig['database']['host']}",
-    $myconfig['database']['username'],
-    $myconfig['database']['password']
-  );
+try {
+	$db = new \Jehon\Maintenance\Database(
+		"mysql:dbname={$myconfig['database']['schema']};host={$myconfig['database']['host']}",
+		$myconfig['database']['username'],
+		$myconfig['database']['password']
+	);
 
-echo "\n\nRunning versions\n";
-$db->runDirectory(__DIR__ . "/../../conf/database/versions/");
+	echo "\n\nRunning versions\n";
+	$db->runDirectory(__DIR__ . "/../../conf/database/versions/");
 
-echo "\n\nRunning always\n";
-$db->runDirectory(__DIR__ . "/../../conf/database/always/");
+	echo "\n\nRunning always\n";
+	$db->runDirectory(__DIR__ . "/../../conf/database/always/");
+	http_response_code(200);
+} catch (Exception $e) {
+	echo "Upgrade failed: " . $e->getMessage();
+	http_response_code(500);
+}
+ob_flush();
