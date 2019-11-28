aed526267a5519ad2c4d7131bf358048e106ac27
Jean Honlet <jehon@users.noreply.github.com>
2019-11-28 21:55:56 +0100

    wip: style test - check file existence

--- tests/style/screenshots-test.js
+++ tests/style/screenshots-test.js
@@ -2,28 +2,34 @@
 const path = require('path');
 const glob = require('glob');
 
-const globP = (pattern, options) => new Promise((resolve, reject) => {
-	glob(pattern, options, (err, files) => {
-		if (err) {
-			reject(err);
-		}
-		resolve(files);
-	});
-});
+const globP = (pattern, options) => {
+	return glob.sync(pattern, options);
+};
 
 const refPath = path.join(__dirname, 'references');
 const testPath = path.join(__dirname, '..', '..', 'target', 'e2e', 'browsers', 'firefox');
 
 describe('screenshots', function () {
+	let refs =     globP('*', { cwd: refPath });
+	let tests =    globP('*', { cwd: testPath });
+	let fullList = new Set([...refs, ...tests]);
+
 	it('should match screenshots', () => {
 		expect(true).toBeTruthy();
 	});
 
-	return Promise.all([ 
-		globP('*', { cwd: refPath }),
-		globP('*', { cwd: testPath }),
-	]).then(([ refs, tests ]) => {
-		console.log(refs);
-		console.log(tests);
-	});
+	for (let f of fullList) {
+		it(f, function (done) {
+			if (refs.includes(f)) {
+				if (tests.includes(f)) {
+					done();
+				} else {
+					done.fail('test is not available');
+				}
+			} else {
+				// if refs does not includes it, it is only present from 'test'
+				pending('reference is not available');
+			}
+		});
+	}
 });
