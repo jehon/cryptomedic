534f94ce9813f50272b9bddbc3ed1fc686a69e59
Jean Honlet <jehon@users.noreply.github.com>
2019-11-28 20:57:13 +0100

    wip: test-style

--- tests/style/screenshots-test.js
+++ tests/style/screenshots-test.js
@@ -1,6 +1,29 @@
 
-describe('screenshots', () => {
+const path = require('path');
+const glob = require('glob');
+
+const globP = (pattern, options) => new Promise((resolve, reject) => {
+	glob(pattern, options, (err, files) => {
+		if (err) {
+			reject(err);
+		}
+		resolve(files);
+	});
+});
+
+const refPath = path.join(__dirname, 'references');
+const testPath = path.join(__dirname, '..', '..', 'target', 'e2e', 'browsers', 'firefox');
+
+describe('screenshots', function () {
 	it('should match screenshots', () => {
 		expect(true).toBeTruthy();
+	});
+
+	return Promise.all([ 
+		globP('*', { cwd: refPath }),
+		globP('*', { cwd: testPath }),
+	]).then(([ refs, tests ]) => {
+		console.log(refs);
+		console.log(tests);
 	});
 });
