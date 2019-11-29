e98a0549ff558fe83fc6757757f02efba9903226
Jean Honlet <jehon@users.noreply.github.com>
2019-11-29 21:44:54 +0100

    wip: enhance presentation

--- tests/style/screenshots-test.js
+++ tests/style/screenshots-test.js
@@ -9,27 +9,33 @@ const globP = (pattern, options) => {
 const refPath = path.join(__dirname, 'references');
 const testPath = path.join(__dirname, '..', '..', 'target', 'e2e', 'browsers', 'firefox');
 
-describe('screenshots', function () {
-	let refs =     globP('*', { cwd: refPath });
-	let tests =    globP('*', { cwd: testPath });
-	let fullList = new Set([...refs, ...tests]);
+let refs =     globP('*', { cwd: refPath });
+let tests =    globP('*', { cwd: testPath });
+let fullList = new Set([...refs, ...tests]);
 
-	it('should match screenshots', () => {
-		expect(true).toBeTruthy();
-	});
-
-	for (let f of fullList) {
-		it(f, function (done) {
-			if (refs.includes(f)) {
-				if (tests.includes(f)) {
-					done();
-				} else {
-					done.fail('test is not available');
-				}
+for (let f of fullList) {
+	it('screenshot: ' + f, function(done) {
+		if (refs.includes(f)) {
+			if (tests.includes(f)) {
+				// 	var screenshot = new Buffer(pngString, 'base64');
+				// 	resemble(image)
+				// 	  .compareTo(screenshot)
+				// 	  .onComplete(function(data){
+				// 		if (Number(data.misMatchPercentage) <= 0.01) {
+				// 		  callback();
+				// 		} else {
+				// 		  data.getDiffImage().pack().pipe(fs.createWriteStream(image + 'diff.png'));
+				// 		  callback.fail(new Error("Screenshot '" + image+  "' differ " + data.misMatchPercentage + "%"));
+				// 		}
+				// 	  });
+				//   });
+				done();
 			} else {
-				// if refs does not includes it, it is only present from 'test'
-				pending('reference is not available');
+				done.fail('test is not available');
 			}
-		});
-	}
-});
+		} else {
+			// if refs does not includes it, it is only present from 'test'
+			pending('reference is not available');
+		}
+	});
+}
