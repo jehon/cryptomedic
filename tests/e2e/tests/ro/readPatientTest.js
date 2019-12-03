c7b1a0fbcd17c828b46b31171523317aca174412
Jean Honlet <jehon@users.noreply.github.com>
2019-12-03 21:21:30 +0100

    test: take snapshot at the end of the e2e test
    Avoid waiting wheel

--- tests/e2e/tests/ro/readPatientTest.js
+++ tests/e2e/tests/ro/readPatientTest.js
@@ -13,16 +13,15 @@ module.exports = {
 
 		// Summary
 		client.myClick('#summary');
-		client.myScreenshotReference('summary');
 		client.page.cryptomedic().tableIterator('#table_summary')
 			// Summary Bill#1
 			.row(4).assert('#2')
 			.col(4).assert('Sociallevel')
 			.endTable();
+		client.myScreenshotReference('summary');
 
 		// Nonrickect Consult
 		client.page.cryptomedic().selectFile('OtherConsult', 1);
-		client.myScreenshotReference('OtherConsult');
 		client.assert.containsText('#Date', '2007-01-10');
 		client.assert.containsText('#ExaminerName', 'Ershad');
 
@@ -37,17 +36,19 @@ module.exports = {
 		client.assert.containsText('#bmi', '16.15');
 		client.assert.containsText('#ds_bmi', '0.0 ds');
 		client.assert.not.elementPresent('#button_edit');
+		client.myScreenshotReference('OtherConsult');
 
 		client.page.cryptomedic().selectFile('Picture', 2);
-		client.myScreenshotReference('Picture');
 		client.assert.containsText('#Picture_Date', '2014-11-04');
 		client.assert.containsText('#Picture_file', '10_2014-11-06_15-32-45.JPG');
 		client.assert.containsText('#Patient_entryyear', '2000');
 		client.assert.containsText('#Patient_entryorder', '1');
 		client.assert.not.elementPresent('#button_edit');
+		client.myScreenshotReference('Picture');
 
 		client.page.cryptomedic().selectFile('Bill', 1);
 		client.assert.not.elementPresent('#button_edit');
+		client.myScreenshotReference('Bill');
 		// TODO: check bill
 	},
 	readPatient5: function(client) {
@@ -57,12 +58,12 @@ module.exports = {
 		client.assert.containsText('#Patient_Telephone', '1813247984');
 
 		client.page.cryptomedic().selectFile('ClubFoot', 1);
-		client.myScreenshotReference('ClubFoot');
 		client.waitForElementVisible('#ageAtConsultationTime');
 		client.assert.containsText('#ageAtConsultationTime', '2y0m');
 		client.assert.containsText('#ClubFoot_Treatment', 'DB splint');
 		// TODO: adapt the data and check them
 		client.assert.not.elementPresent('#button_edit');
+		client.myScreenshotReference('ClubFoot');
 
 		client.page.cryptomedic().goPatient(2001, 1);
 		client.page.cryptomedic().selectFile('RicketConsult', 3);
@@ -84,6 +85,7 @@ module.exports = {
 			.col(2).assert('Ershad')
 			.nextCol().assert(10)
 			.endTable();
+		client.myScreenshotReference('Bill');
 		client.end();
 	}
 };
