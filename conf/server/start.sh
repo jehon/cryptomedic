1cb3bbfaa8e63e3385c017d70822c33283844464
Jean Honlet <jehon@users.noreply.github.com>
2019-12-05 14:37:13 +0100

    wip: use gid

--- conf/server/start.sh
+++ conf/server/start.sh
@@ -4,25 +4,30 @@
 # export APACHE_RUN_USER=www-data
 # export APACHE_RUN_GROUP=www-data
 
-EV=/etc/apache2/envvars
-
 echo "Should run as: $HOST_UID : $HOST_GID"
 
-# Will exit ok if already exists
-groupadd -f hostgroup -g $HOST_GID
-
-if ! id -u $HOST_UID ; then
-	adduser --system --uid $HOST_UID --gid $HOST_GID hostuser
+GN=$( cat /etc/group | grep ":HOST_GID:" | cut -d ":" -f 1 )
+if [ -z "$GN" ]; then
+	GN="hostgroup"
+	echo "Creating group $GN"
+	groupadd $GN -g $HOST_GID
 fi
 
-UN="$(id -un $HOST_UID)"
-GN="hostgroup"
+if ! id -u $HOST_UID >/dev/null 2>/dev/null ; then
+	UN="hostuser"
+	echo "Creating user $UN"
+	adduser --system --uid $HOST_UID --gid $HOST_GID $UN
+else
+	UN="$(id -un $HOST_UID)"
+fi
+
+echo "Going with user $UN and group $GN"
+
+EV=/etc/apache2/envvars
 
 sed -E \
 	-e  "s/APACHE_RUN_USER=.*$/APACHE_RUN_USER=${UN}/" \
 	-e "s/APACHE_RUN_GROUP=.*$/APACHE_RUN_GROUP=${GN}/" \
 	-i "$EV"
-
-cat "$EV"
 
 apachectl -D FOREGROUND
