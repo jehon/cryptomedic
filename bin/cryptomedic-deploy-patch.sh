a15fb5800ee34eb3788dfba8794e04df29c09194
Jean Honlet <jehon@users.noreply.github.com>
2019-12-19 20:39:42 +0100

    ci: try sftp (ssh ftp)

--- bin/cryptomedic-deploy-patch.sh
+++ bin/cryptomedic-deploy-patch.sh
@@ -11,7 +11,7 @@ SCRIPT_DIR="$( dirname "${BASH_SOURCE[0]}" )"
 PRJ_DIR="$(dirname "$SCRIPT_DIR")"
 TMP="$PRJ_DIR/target/"
 
-CRYPTOMEDIC_UPLOAD_HOST="ftp.cluster003.ovh.net"
+CRYPTOMEDIC_UPLOAD_HOST="sftp://ftp.cluster003.ovh.net"
 if [ -z "$CRYPTOMEDIC_UPLOAD_USER" ]; then
     echo "Missing CRYPTOMEDIC_UPLOAD_USER" >&2
     exit 255
@@ -65,7 +65,6 @@ build_up(){
 }
 
 echo ""
-set -x
 echo "Updating md5sum.php script [for real]"
 (
     lftp_connect
