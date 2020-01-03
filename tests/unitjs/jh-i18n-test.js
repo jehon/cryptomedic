4805b92d4d0912fff5b524296c55c5ad395a6a24
Jean Honlet <jehon@users.noreply.github.com>
2020-01-03 21:59:21 +0100

    test: enrich jh-i18n

--- tests/unitjs/jh-i18n-test.js
+++ tests/unitjs/jh-i18n-test.js
@@ -2,10 +2,38 @@
 /* global webDescribe */
 
 describe('jh-i18n-test', function() {
+	webDescribe('without value', '<jh-i18n></jh-i18n>', function(element) {
+		it('should show text', function() {
+			expect(element()).not.toBeNull();
+			expect(element().innerHTML).toBe('');
+		});
+	});
+
+	webDescribe('with value=', '<jh-i18n value=""></jh-i18n>', function(element) {
+		it('should show text', function() {
+			expect(element()).not.toBeNull();
+			expect(element().innerHTML).toBe('');
+		});
+	});
+
 	webDescribe('with value=original', '<jh-i18n value=\'original\'></jh-i18n>', function(element) {
 		it('should show text', function() {
 			expect(element()).not.toBeNull();
 			expect(element().innerHTML).toBe('original');
+		});
+	});
+
+	webDescribe('with value=originalThere', '<jh-i18n value=\'originalThere\'></jh-i18n>', function(element) {
+		it('should show text', function() {
+			expect(element()).not.toBeNull();
+			expect(element().innerHTML).toBe('original There');
+		});
+	});
+
+	webDescribe('with value=123', '<jh-i18n value=\'123\'></jh-i18n>', function(element) {
+		it('should show text', function() {
+			expect(element()).not.toBeNull();
+			expect(element().innerHTML).toBe('123');
 		});
 	});
 });
