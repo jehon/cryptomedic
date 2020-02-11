927cfc5924ba14924d4b2f34480e7af6a7b17d9b
Jean Honlet <jehon@users.noreply.github.com>
2020-02-11 23:26:00 +0100

    test: first esm class (jh-i18n)

---
+++ tests/unitmjs/jh-i18n-test.js
@@ -0,0 +1,44 @@
+
+import '/base/static/jh-i18n.js';
+
+describe('jh-i18n-test', function() {
+	withHtml({ title: 'without value', html: '<jh-i18n></jh-i18n>' },
+		function(element) {
+			it('should show text', function() {
+				expect(element()).not.toBeNull();
+				expect(element().innerHTML).toBe('');
+			});
+		});
+
+	withHtml({ title: 'with value=', html: '<jh-i18n value=""></jh-i18n>' },
+		function(element) {
+			it('should show text', function() {
+				expect(element()).not.toBeNull();
+				expect(element().innerHTML).toBe('');
+			});
+		});
+
+	withHtml({ title: 'with value=original', html: '<jh-i18n value=\'original\'></jh-i18n>' },
+		function(element) {
+			it('should show text', function() {
+				expect(element()).not.toBeNull();
+				expect(element().innerHTML).toBe('original');
+			});
+		});
+
+	withHtml({ title: 'with value=originalThere', html: '<jh-i18n value=\'originalThere\'></jh-i18n>' },
+		function(element) {
+			it('should show text', function() {
+				expect(element()).not.toBeNull();
+				expect(element().innerHTML).toBe('original There');
+			});
+		});
+
+	withHtml({ title: 'with value=123', html: '<jh-i18n value=\'123\'></jh-i18n>' },
+		function(element) {
+			it('should show text', function() {
+				expect(element()).not.toBeNull();
+				expect(element().innerHTML).toBe('123');
+			});
+		});
+});
---
+++ www/static/jh-i18n.js
@@ -0,0 +1,16 @@
+
+export default class JHI18n extends HTMLElement {
+	static get observedAttributes() {
+		return [ 'value' ];
+	}
+
+	attributeChangedCallback(attributeName, oldValue, newValue) {
+		if (newValue) {
+			this.innerHTML = newValue.split(/(?=[A-Z]+[a-z])/).join(' ');
+		} else {
+			this.innerHTML = '';
+		}
+	}
+}
+
+window.customElements.define('jh-i18n', JHI18n);
