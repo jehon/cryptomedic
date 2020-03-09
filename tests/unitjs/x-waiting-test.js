cb075601b869a5c59ab8ba62ea9aec911c6ca3aa
Jean Honlet <jehon@users.noreply.github.com>
2020-03-09 15:02:04 +0100

    test: x-waiting-test working always

--- tests/unitjs/x-waiting-test.js
+++ tests/unitjs/x-waiting-test.js
@@ -1,39 +1,39 @@
 /* eslint-env jasmine */
 /* global webDescribe, JHElement */
 
-describe('tests/unit/x-waiting-test.js', function() {
-	webDescribe('initialized', '<x-waiting><div style=\'width: 200px; height: 100px; background-color: red;\'>Content</div></x-waiting>', function(element) {
-		it('should be hidden when initialized simply', function() {
+describe('tests/unit/x-waiting-test.js', function () {
+	webDescribe('initialized', '<x-waiting><div style=\'width: 200px; height: 100px; background-color: red;\'>Content</div></x-waiting>', function (element) {
+		it('should be hidden when initialized simply', function () {
 			expect(element().isBlocked()).toBeFalsy();
 			expect(element().shadowRoot.querySelector('img').offsetWidth > 0).toBeFalsy();
 		});
 
-		it('should show()', function(done) {
+		it('should show()', function (done) {
 			element().block();
 			expect(element().isBlocked()).toBeTruthy();
 			setTimeout(() => {
 				expect(element().shadowRoot.querySelector('img').offsetWidth > 0).toBeTruthy();
 				done();
-			});
+			}, 10);
 		});
 
-		it('should hide()', function() {
+		it('should hide()', function () {
 			element().free();
 			expect(element().isBlocked()).toBeFalsy();
 			expect(element().shadowRoot.querySelector('img').offsetWidth > 0).toBeFalsy();
 		});
 
-		describe('run around a promise', function() {
-			beforeEach(function() {
+		describe('run around a promise', function () {
+			beforeEach(function () {
 				jasmine.clock().install();
 			});
 
-			afterEach(function() {
+			afterEach(function () {
 				jasmine.clock().uninstall();
 			});
 
-			it('should be shown and hidden when the promise succeed', function(done) {
-				let p = new Promise(function(resolve) {
+			it('should be shown and hidden when the promise succeed', function (done) {
+				let p = new Promise(function (resolve) {
 					setTimeout(() => resolve(), 100);
 				});
 
@@ -43,12 +43,12 @@ describe('tests/unit/x-waiting-test.js', function() {
 				jasmine.clock().tick(150);
 				p2.then(() => {
 					expect(element().isBlocked()).toBeFalsy();
-					done();	
+					done();
 				});
 			});
 
-			it('should be shown and hidden if the promise fail', function(done) {
-				let p = new Promise(function(resolve, reject) {
+			it('should be shown and hidden if the promise fail', function (done) {
+				let p = new Promise(function (resolve, reject) {
 					setTimeout(() => reject(), 100);
 				});
 
@@ -58,7 +58,7 @@ describe('tests/unit/x-waiting-test.js', function() {
 				jasmine.clock().tick(150);
 				p2.catch(() => {
 					expect(element().isBlocked()).toBeFalsy();
-					done();	
+					done();
 				});
 			});
 		});
