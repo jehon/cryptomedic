f8b2012f7ebfbd6e57bd9075633319d4933af107
Jean Honlet <jehon@users.noreply.github.com>
2020-03-19 18:00:00 +0100

    wip: session in session.js

---
+++ tests/unitjs/session-test.js
@@ -0,0 +1,42 @@
+
+import { getSession, setSession, resetSession, getUsername } from '../../app/js/session.js';
+
+fdescribe('session-test', function () {
+    it("should set and reset session", async function () {
+        resetSession();
+        // The session is set before
+        setSession(1);
+        expect(await getSession()).toBe(1);
+        expect(await getSession()).toBe(1);
+        expect(await getSession()).toBe(1);
+    });
+
+    it("should set and then get the session", async function (done) {
+        // The session is set after
+        resetSession();
+        getSession().then(session => {
+            expect(session).toBe(3);
+            done();
+        })
+        setSession(3);
+    });
+
+    it("should not set twice", async function () {
+        // The session is set after
+        resetSession();
+        setSession(5);
+        expect(() => setSession(10)).toThrowError();
+
+        resetSession();
+        setSession(15);
+        expect(await getSession()).toBe(15);
+    });
+
+    it("should give info", async function () {
+        resetSession();
+        setSession({
+            username: "user"
+        });
+        expect(await getUsername()).toBe("user");
+    })
+})
