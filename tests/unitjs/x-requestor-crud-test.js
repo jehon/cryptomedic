59360edb7098a8176592496b304e049b8ef2aefd
Jean Honlet <jehon@users.noreply.github.com>
2020-03-31 16:29:04 +0200

    wip: all tests ok

--- tests/unitjs/x-requestor-crud-test.js
+++ tests/unitjs/x-requestor-crud-test.js
@@ -3,8 +3,38 @@ import '../../app/elements/x-requestor-crud.js';
 
 import { webDescribe, extractPath } from './athelpers.js';
 
+import axios from '../../app/cjs2esm/axios.js'
+import MockAdapter from '../../app/cjs2esm/axios-mock-adapter.js'
+
 describe('tests/unit/x-requestor-crud-test.js', function () {
+	var mock;
+
+	beforeEach(function () {
+		mock = new MockAdapter(axios);
+
+		mock.onGet('/object').reply(200, [1, 2]);
+		mock.onPost('/object').reply(200, "post 15");
+
+		mock.onGet('/object/15').reply(200, "get 15");
+		mock.onPut('/object/15').reply(200, "put 15");
+		mock.onDelete('/object/15').reply(200, "delete 15");
+	});
+
+	afterEach(function () {
+		mock.restore();
+	});
+
 	webDescribe('x-requestor-crud', '<x-requestor-crud relative-url=\'/object\'></x-requestor-crud>', function (element) {
+		const testRequest = function (opts, done) {
+			return element()
+				.request(opts)
+				.catch(response => {
+					console.error(response);
+					done.fail(`in test: url not found '${opts.url}'`);
+					throw response;
+				});
+		};
+
 		const buildResponse = function (result) {
 			return Promise.resolve(new Response(JSON.stringify(result), {
 				status: 200,
@@ -14,54 +44,49 @@ describe('tests/unit/x-requestor-crud-test.js', function () {
 
 		it('should list', function (done) {
 			const res = [1, 2];
-			spyOn(window, 'fetch').and.callFake((request) => {
-				expect(request.method).toBe('GET');
-				expect(extractPath(request.url)).toBe('/object?');
-				return buildResponse(res);
-			});
-			element().list().then(result => {
-				expect(result).toEqual(res);
-				done();
-			});
+			element().list()
+				.then(list => {
+					expect(list).toEqual(res);
+					done();
+				})
+				.catch(e => { console.error(e); done.fail(e); });
 		});
 
 		it('should create', function (done) {
 			const data = { a: 1 };
-			spyOn(window, 'fetch').and.callFake((request) => {
-				expect(request.method).toBe('POST');
-				expect(extractPath(request.url)).toBe('/object');
-				// TODO: check the body
-				return buildResponse();
-			});
-			element().create(data).then(done);
+			element().create(data)
+				.then(response => {
+					expect(response).toBe("post 15");
+					done();
+				})
+				.catch(e => { console.error(e); done.fail(e); });
 		});
 
 		it('should read', function (done) {
-			spyOn(window, 'fetch').and.callFake((request) => {
-				expect(request.method).toBe('GET');
-				expect(extractPath(request.url)).toBe('/object/15?');
-				return buildResponse();
-			});
-			element().read(15).then(done);
+			element().read(15)
+				.then(response => {
+					expect(response).toBe('get 15');
+					done();
+				})
+				.catch(e => { console.error(e); done.fail(e); });
 		});
 
 		it('should update', function (done) {
-			spyOn(window, 'fetch').and.callFake((request) => {
-				expect(request.method).toBe('PUT');
-				expect(extractPath(request.url)).toBe('/object/15');
-				// TODO: check the body
-				return buildResponse();
-			});
-			element().update({ id: 15 }).then(done);
+			element().update({ id: 15 })
+				.then(response => {
+					expect(response).toBe('put 15');
+					done();
+				})
+				.catch(e => { console.error(e); done.fail(e); });
 		});
 
 		it('should delete', function (done) {
-			spyOn(window, 'fetch').and.callFake((request) => {
-				expect(request.method).toBe('DELETE');
-				expect(extractPath(request.url)).toBe('/object/15');
-				return buildResponse();
-			});
-			element().delete(15).then(done);
+			element().delete(15)
+				.then(response => {
+					expect(response).toBe('delete 15');
+					done();
+				})
+				.catch(e => { console.error(e); done.fail(e); });
 		});
 	});
 });
