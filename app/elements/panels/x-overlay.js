e43b87b8db8f4c04118961edc9c1735b27e243ed
Jean Honlet <jehon@users.noreply.github.com>
2020-04-28 18:00:00 +0200

    feature: enhance the panels But not used

---
+++ app/elements/panels/x-overlay.js
@@ -0,0 +1,109 @@
+
+import XPanel from './x-panel.js';
+import { insertInSlotDefault } from '../element-helpers.js';
+
+
+export default class XOverlay extends XPanel {
+    constructor() {
+        super();
+        insertInSlotDefault(this, `
+            <style>
+                #overlay {
+                    position: absolute;
+                    top: 0;
+                    left: 0;
+                    width: 100%;
+                    height: 100%;
+                    z-index: 10;
+                    color: white;
+
+                    background-color: rgba(0.3,0.3,0.3, 0.8); /* Black w/opacity */
+                    overflow-x: hidden;
+                    transition: 0.5s;
+                    display: inline;
+                }
+
+                #overlay > slot, #overlay > span[slot-overlay] {
+                    background-color: rgba(0,0,0, 1); /* Black w/opacity */
+                    padding: 10px;
+                    border-radius: 10px;
+                }
+
+                /* Mode free */
+
+                :host #overlay {
+                    display: none;
+                }
+
+                /* Mode blocked */
+
+                :host([blocked]) #overlay {
+                    display: block;
+                }
+
+                :host([global]) #overlay {
+                    position: fixed;
+                    top: 0;
+                    left: 0;
+                    width: 100%;
+                    height: 100%;
+                }
+
+                /* Close button */
+
+				:host #close {
+					display: none;
+				}
+
+				:host([closable]) #close {
+                    display: block;
+                }
+
+				#close {
+				    position: absolute;
+				    top: 20px;
+				    right: 45px;
+				    font-size: 60px;
+
+					text-decoration: none;
+				    color: #818181;
+				}
+
+				#close:hover, #close:focus {
+				    text-decoration: none;
+				    color: #f1f1f1;
+				}
+
+				/* When the height of the screen is less than 450 pixels, change the font-size of the links and position the close button again, so they don't overlap */
+				@media screen and (max-height: 450px) {
+				    #close {
+				        font-size: 40px;
+				        top: 15px;
+				        right: 35px;
+				    }
+				}
+			</style>
+            <slot name='content'></slot>
+            <x-panel id='overlay'>
+                <a id='close' href="javascript:void(0)">&times;</a>
+                <slot name='overlay'></slot>
+            </x-panel>
+			`);
+
+        this.shadowRoot.querySelector('#close').addEventListener('click', () => this.free());
+    }
+
+    block() {
+        this.setAttribute('blocked', 'blocked');
+    }
+
+    free() {
+        this.removeAttribute('blocked');
+    }
+
+    isBlocked() {
+        return this.hasAttribute('blocked');
+    }
+}
+
+window.customElements.define('x-overlay', XOverlay);
--- tests/unitjs/x-overlay-test.js
+++ tests/unitjs/x-overlay-test.js
@@ -1,89 +1,44 @@
 
-import '../../app/elements/x-overlay.js';
+import '../../app/elements/panels/x-overlay.js';
 
 import { webDescribe } from './athelpers.js';
 
 import JHElement from '../../app/elements/jh-element.js';
 
 describe('tests/unit/x-overlay-test.js', function () {
-	webDescribe('initialized', '<x-overlay><div style=\'width: 200px; height: 100px; background-color: red\'>Content</div></x-overlay>', function (element) {
-		it('should be hidden when initialized simply', function () {
-			expect(element().isBlocked()).toBeFalsy();
-			expect(element().shadowRoot.querySelector('#overlay').offsetWidth).toBe(0);
-			expect(element().shadowRoot.querySelector('#close').offsetWidth).toBe(0);
-			expect(element().shadowRoot.querySelector('#overlay').style.zIndex).toBe('10');
-		});
+    webDescribe('initialized', '<x-overlay><div slot="content">Content</div><div slot="overlay">Overlay</div></x-overlay>', function (element) {
+        it('should be hidden when initialized simply', function () {
+            expect(element().isBlocked()).toBeFalsy();
+            expect(element().shadowRoot.querySelector('#overlay').offsetWidth).toBe(0);
+            expect(element().shadowRoot.querySelector('#close').offsetWidth).toBe(0);
+            // expect(element().shadowRoot.querySelector('#overlay').style.zIndex).toBe('10');
+        });
 
-		it('should show()', function () {
-			element().block();
-			expect(element().isBlocked()).toBeTruthy();
-			expect(element().shadowRoot.querySelector('#overlay').offsetWidth).toBeGreaterThan(0);
-		});
+        it('should show()', function () {
+            element().block();
+            expect(element().isBlocked()).toBeTruthy();
+            expect(element().shadowRoot.querySelector('#overlay').offsetWidth).toBeGreaterThan(0);
+        });
 
-		it('should hide()', function () {
-			element().free();
-			expect(element().isBlocked()).toBeFalsy();
-			expect(element().shadowRoot.querySelector('#overlay').offsetWidth).toBe(0);
-		});
+        it('should hide()', function () {
+            element().free();
+            expect(element().isBlocked()).toBeFalsy();
+            expect(element().shadowRoot.querySelector('#overlay').offsetWidth).toBe(0);
+        });
 
-		it('should be react to clicks', function () {
-			expect(element().isBlocked()).toBeFalsy();
-			element().block();
-			expect(element().isBlocked()).toBeTruthy();
-			JHElement.fireOn(element().shadowRoot.querySelector('#close'), 'click');
-			expect(element().isBlocked()).toBeFalsy();
-		});
+        it('should be react to clicks', function () {
+            expect(element().isBlocked()).toBeFalsy();
+            element().block();
+            expect(element().isBlocked()).toBeTruthy();
+            JHElement.fireOn(element().shadowRoot.querySelector('#close'), 'click');
+            expect(element().isBlocked()).toBeFalsy();
+        });
+    });
 
-		describe('run around a promise', function () {
-			beforeEach(function () {
-				jasmine.clock().install();
-			});
-
-			afterEach(function () {
-				jasmine.clock().uninstall();
-			});
-
-			it('should be shidden when the promise succeed', function (done) {
-				let p = new Promise(function (resolve) {
-					setTimeout(() => resolve(), 100);
-				});
-
-				expect(element().isBlocked()).toBeFalsy();
-				let p2 = element().aroundPromise(p);
-				expect(element().isBlocked()).toBeFalsy();
-				jasmine.clock().tick(150);
-				p2.then(() => {
-					expect(element().isBlocked()).toBeFalsy();
-					done();
-				});
-			});
-
-			it('should be shown and hidden if the promise fail', function (done) {
-				let p = new Promise(function (resolve, reject) {
-					setTimeout(() => reject(), 100);
-				});
-
-				expect(element().isBlocked()).toBeFalsy();
-				let p2 = element().aroundPromise(p);
-				expect(element().isBlocked()).toBeFalsy();
-				jasmine.clock().tick(150);
-				p2.catch(() => {
-					expect(element().isBlocked()).toBeTruthy();
-					done();
-				});
-			});
-		});
-	});
-
-	webDescribe('initialized', '<x-overlay closable z-index=123 >Content</x-overlay>', function (element) {
-		it('should handle z-index parameter', function () {
-			element().block();
-			expect(element().shadowRoot.querySelector('#overlay').style.zIndex).toBe('123');
-		});
-
-		it('should handle closable parameter', function () {
-			element().block();
-			expect(element().shadowRoot.querySelector('#close').offsetWidth).toBeGreaterThan(0);
-		});
-	});
+    webDescribe('initialized', '<x-overlay closable z-index=123 ><div slot="content">Content</div></x-overlay>', function (element) {
+        it('should handle closable parameter', function () {
+            element().block();
+            expect(element().shadowRoot.querySelector('#close').offsetWidth).toBeGreaterThan(0);
+        });
+    });
 });
