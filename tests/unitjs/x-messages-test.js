c15d4434e025e442b7edaf46c8326cd9a758abc3
Jean Honlet <jehon@users.noreply.github.com>
2020-05-02 21:18:53 +0200

    feature: x-messages

---
+++ tests/unitjs/x-messages-test.js
@@ -0,0 +1,41 @@
+
+import '../../app/elements/panels/x-messages.js';
+
+import { webDescribe } from './athelpers.js';
+
+fdescribe('tests/unit/x-messages-test.js', function () {
+    webDescribe('initialized', '<x-messages></x-messages>', function (element) {
+        beforeEach(() => {
+            element().clear();
+        });
+
+        it('should show messages', () => {
+            expect(element().querySelectorAll('div').length).toBe(0);
+
+            element().showMessages(['a', 'b']);
+            expect(element().querySelectorAll('div').length).toBe(2);
+
+            element().addMessage('c');
+            expect(element().querySelectorAll('div').length).toBe(3);
+
+            element().showMessages(['a', 'b']);
+            expect(element().querySelectorAll('div').length).toBe(2);
+        });
+
+        it('should clear', () => {
+            element().showMessages(['a', 'b']);
+            expect(element().querySelectorAll('div').length).toBe(2);
+
+            element().clear();
+            expect(element().querySelectorAll('div').length).toBe(0);
+        });
+
+        it('should show objects', () => {
+            element().addMessage('c');
+            expect(element().querySelectorAll('div').length).toBe(1);
+
+            element().addMessage({ text: 'c', level: 'danger' });
+            expect(element().querySelectorAll('div').length).toBe(2);
+        });
+    });
+});
