2af49cad785dab54acb290fb541dc9ad3a8b6a90
Jean Honlet <jehon@users.noreply.github.com>
2020-05-07 18:00:00 +0200

    test(style): add diff image

--- tests/style/screenshots-test.js
+++ tests/style/screenshots-test.js
@@ -35,15 +35,14 @@ describe('with screenshots', () => {
                     resemble(path.join(testPath, f))
                         .compareTo(path.join(refPath, f))
                         .onComplete(function (data) {
-                            // console.log(f, data);
                             const diffContent = data.misMatchPercentage;
                             const diffSize = Math.hypot(data.dimensionDifference.width, data.dimensionDifference.height);
                             expect(diffSize)
                                 .withContext('differ too much in size')
-                                .toBeLessThan(2);
+                                .toBeLessThan(1);
                             expect(diffContent)
                                 .withContext('differ too much in content')
-                                .toBeLessThan(5);
+                                .toBeLessThan(1);
                             done();
                         });
                 } else {
@@ -63,21 +62,73 @@ describe('with screenshots', () => {
         // Houston, we've had a problem...
         let res = `
             <html><body>
+            <script>
+                module = { exports: {}};
+
+                function img2data(id, w, h) {
+                    const myImgElement = document.querySelector('#' + id);
+
+                    // 1) Create a canvas, either on the page or simply in code
+                    var canvas = document.createElement('canvas');
+                    canvas.height = h;
+                    canvas.width = w;
+                    var ctx    = canvas.getContext('2d');
+                    
+                    // 2) Copy your image data into the canvas
+                    ctx.drawImage( myImgElement, 0, 0, w, h );
+                    
+                    // 3) Read your image data
+                    return ctx.getImageData(0,0,w,h).data;
+                }
+
+                function img_diff(id) {
+                    const myImgElement = document.querySelector('#i' + id + '_ref');
+                    const w = myImgElement.naturalWidth;
+                    const h = myImgElement.naturalHeight;
+
+                    const canvas = document.querySelector('#i' + id + '_fin');
+                    canvas.width = w;
+                    canvas.height = h;
+                    const diff = canvas.getContext('2d');
+                    
+                    const diffImg = diff.createImageData(w, h);
+
+                    pixelmatch(
+                        img2data('i' + id + '_ref', w, h), 
+                        img2data('i' + id + '_tst', w, h), 
+                        diffImg.data, 
+                        w, h, { threshold: 0.01 });
+                    diff.putImageData(diffImg, 0, 0);
+                }
+            </script>
+            <script src='../../node_modules/pixelmatch/index.js'></script>
             <style>
-                img {
-                    width: 45%;
+                img, canvas {
+                    width: 30%;
+                    border: solid 1px black;
                 }
                 compare {
                     border-bottom: 1px solid black;
                     margin-bottom: 5px;
                 }
-            </style>
-            \n`;
+            </style>\n`;
+
+        let id = 0;
         for (let f of problemsList) {
             res += `<div class='compare'>
-                <img src='../tests/style/references/${f}'>
-                <img src='e2e/browsers/firefox/${f}'>
-            </div>\n`;
+                <img id='i${id}_ref' src='../tests/style/references/${f}'></img>
+                <img id='i${id}_tst' src='e2e/browsers/firefox/${f}'></img>
+                <canvas id='i${id}_fin' for='${id}'></canvas>
+            </div>
+            <script>
+                window.addEventListener('load', () => {
+                    document.querySelectorAll('canvas[for]').forEach(e => {
+                        img_diff(e.getAttribute('for'));
+                    });
+                })
+            </script>
+            \n`;
+            id++;
         }
         res += '\n</body></html>';
         fs.writeFileSync('target/style.html', res);
