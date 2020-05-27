7c63e0516b055d6317457f7007abfd39d1b7a358
Jean Honlet <jehon@users.noreply.github.com>
2020-05-27 13:40:38 +0200

    feature: add onAuthorized in session
    And rework it a bit

--- app/js/session.js
+++ app/js/session.js
@@ -1,9 +1,8 @@
 
 import duix from '../../node_modules/duix/index.js';
 const SESSION = 'session';
-const USERNAME = 'username';
 
-import store, { ACT_USER_LOGIN } from './store.js';
+import store, { ACT_USER_LOGIN, ACT_USER_LOGOUT } from './store.js';
 
 // Thanks to https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Object/freeze
 function deepFreeze(object) {
@@ -19,23 +18,57 @@ function deepFreeze(object) {
     return Object.freeze(object);
 }
 
-function onWithMemory(key, cb) {
-    return duix.subscribe(key, cb, { callMeNow: true });
+/**
+ * 
+ * @param {function(any): any} valueCb - Extract the value from the session
+ * @param {function(any): any} cb - The callback that will be called with the new value
+ */
+function filterOnValue(valueCb, cb) {
+    let oldValue = Symbol('undefined'); // To make the difference between undefined and never initialized
+    return duix.subscribe(SESSION, session => {
+        let newValue = valueCb(session);
+        if (oldValue === newValue) {
+            return;
+        }
+        cb(newValue, oldValue);
+        oldValue = newValue;
+    }, {
+        callMeNow: true, /* TODO: legacy name */
+        fireImmediately: true
+    });
 }
 
-export function setSession(session = {}) {
-    if (Object.keys(session).length < 1) {
+export function setSession(session = null) {
+    if (!session || Object.keys(session).length < 1) {
         session = null;
     } else {
         deepFreeze(session);
     }
-    duix.set(USERNAME, session?.username);
+
     duix.set(SESSION, session);
-    store.dispatch({ type: ACT_USER_LOGIN, payload: session });
+
+    // TODO: legacy store
+    if (session) {
+        store.dispatch({ type: ACT_USER_LOGIN, payload: session });
+    } else {
+        store.dispatch({ type: ACT_USER_LOGOUT });
+    }
 }
+setSession();
 
 export const getSession = () => duix.get(SESSION);
-export const getUsername = () => duix.get(USERNAME);
+export const onSession = (cb) => duix.subscribe(SESSION, cb, { callMeNow: true, fireImmediately: true }); /* TODO: legacy arg name */
 
-export const onSession = (cb) => onWithMemory(SESSION, cb);
-export const onUsername = (cb) => onWithMemory(USERNAME, cb);
+/*
+ * Username
+ */
+
+export const getUsername = (session = getSession()) => session?.username;
+export const onUsername = (cb) => filterOnValue(getUsername, cb);
+
+/*
+ * Authorizations
+ */
+
+export const getAuthorized = (key, session = getSession()) => session?.authorized?.includes(key) || false;
+export const onAuthorized = (key, cb) => filterOnValue(session => getAuthorized(key, session), cb);
--- tests/unitjs/session-test.js
+++ tests/unitjs/session-test.js
@@ -1,7 +1,8 @@
 
 import {
     setSession, onSession, getSession,
-    onUsername, getUsername
+    onUsername, getUsername,
+    getAuthorized, onAuthorized
 } from '../../app/js/session.js';
 
 import { fn, loadReference } from './athelpers.js';
@@ -20,7 +21,7 @@ describe(fn(import.meta.url), function () {
         it('is empty on start', async function (done) {
             const unreg = onSession(data => {
                 expect(data).toBeNull();
-                expect(getSession()).toBeNull();
+                expect(getSession()).toBeFalsy();
                 done();
             });
             // cb is called by 'get' value, thus before sending back the unreg
@@ -29,20 +30,31 @@ describe(fn(import.meta.url), function () {
         });
 
 
-        it('should set session', async function (done) {
-            const unreg = onSession(data => {
-                if (!data) return;
-                expect(data.group).toBe("cdc");
-                expect(getSession().group).toBe("cdc");
-                done();
-            });
-            setSession(refSession);
-            unreg();
+        it('with username', function () {
+            expect(getSession()).toBeFalsy();
+            expect(getUsername()).toBeFalsy();
         });
 
-        it('with username', function () {
-            expect(getUsername()).toBeUndefined();
-        })
+        it('with auth', function () {
+            expect(getSession()).toBeFalsy();
+            expect(getAuthorized('anything')).toBeFalse();
+            expect(getAuthorized('application.open')).toBeFalse();
+        });
+
+        it('should set session', function () {
+            let sessions = [];
+            const unreg = onSession(data => sessions.push(data ? true : false));
+
+            let users = [];
+            const unregU = onUsername(data => users.push(data));
+
+            setSession(refSession);
+            unreg();
+            unregU();
+
+            expect(sessions).withContext('sessions').toEqual([false, true]);
+            expect(users).withContext('users').toEqual([undefined, "murshed"]);
+        });
     });
 
     describe('with session', function () {
@@ -51,6 +63,8 @@ describe(fn(import.meta.url), function () {
         });
 
         it('with memorized value', async function (done) {
+            expect(getSession()).not.toBeNull();
+            expect(getSession()).toBeTruthy();
             expect(getSession().group).toBe("cdc");
             const unreg = onSession(data => {
                 expect(data.group).toBe("cdc");
@@ -60,11 +74,55 @@ describe(fn(import.meta.url), function () {
         });
 
         it('with username', async function (done) {
+            expect(getSession()).toBeTruthy();
             expect(getUsername()).toBe("murshed");
             const unreg = onUsername(data => {
                 expect(data).toBe("murshed");
                 done();
             });
+            unreg();
+        });
+
+        it('with auth', function () {
+            expect(getSession()).toBeTruthy();
+            expect(getAuthorized('anything')).toBeFalsy();
+            expect(getAuthorized('application.open')).toBeTruthy();
+        });
+
+    });
+
+    describe('with auth change', function () {
+        it('should fire on change', function () {
+            let res = [];
+            setSession();
+            const unreg = onAuthorized('application.open', data => {
+                res.push(data);
+            });
+            let i = 0;
+            // Initial
+            expect(res.length).toBe(i + 1);
+            expect(res[i]).toBe(false);
+
+            setSession(refSession); // true
+            i++;
+            expect(res.length).toBe(i + 1);
+            expect(res[i]).toBe(true);
+
+            setSession(refSession); // should be skipped
+            expect(res.length).toBe(i + 1);
+            expect(res[i]).toBe(true);
+
+            refSession.authorized;
+            setSession(); // false
+            i++;
+            expect(res.length).toBe(i + 1);
+            expect(res[i]).toBe(false);
+
+            setSession(refSession); // true
+            i++;
+            expect(res.length).toBe(i + 1);
+            expect(res[i]).toBe(true);
+
             unreg();
         });
     });
