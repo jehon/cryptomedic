3ab47818be79b76b7a77fd06f9ff1a02eb0431d7
Jean Honlet <jehon@users.noreply.github.com>
2020-06-27 15:41:41 +0200

    feature: add getRouteToFolderFile

--- app/js/router.js
+++ app/js/router.js
@@ -1,26 +1,50 @@
 
 import { API_VERSION } from '../config.js';
+import FolderPage from '../models/FolderPage.js';
 
+/**
+ *
+ */
 export function getCurrentRoute() {
     return document.location.hash.substr(1);
 }
 
+/**
+ * @param hash
+ */
 export function setRoute(hash) {
     return document.location.hash = hash;
 }
 
+/**
+ * @param redirect
+ */
 export function routeToLogin(redirect = getCurrentRoute()) {
     setRoute(`/login/${redirect}`);
 }
 
+/**
+ *
+ */
 export function parseRouteLogin() {
     return {
         redirect: getCurrentRoute().replace(/^(\/+login)+\/+/, '/')
     };
 }
 
+/**
+ *
+ */
 export function parseRouteApi() {
     return {
         redirect: '/api/' + API_VERSION + getCurrentRoute().replace(/^(\/+redirect\/api)+\/+/, '/')
     };
+}
+
+/**
+ * @param {FolderPage} folderPage - the file to display
+ * @returns {string} the route
+ */
+export function getRouteToFolderFile(folderPage) {
+    return '/folder/' + folderPage.patient_id + '/file/' + folderPage.getModel() + '/' + folderPage.id;
 }
--- tests/unitjs/router-test.js
+++ tests/unitjs/router-test.js
@@ -4,6 +4,10 @@ import * as router from '../../app/js/router.js';
 import { fn } from './athelpers.js';
 import { API_VERSION } from '../../app/config.js';
 
+import Folder from '../../app/models/Folder.js';
+import { loadReference } from './athelpers.js';
+import OtherConsult from '../../app/models/OtherConsult.js';
+
 describe(fn(import.meta.url), function () {
     it('should parse login routes', function () {
         {
@@ -39,5 +43,18 @@ describe(fn(import.meta.url), function () {
     it('should parse route to api', function () {
         router.setRoute('/redirect/api/brol/test?1=2');
         expect(router.parseRouteApi().redirect).toBe(`/api/${API_VERSION}/brol/test?1=2`);
+    });
+
+    describe('folder routes', function () {
+        let f;
+        beforeEach(() => {
+            f = new Folder(loadReference('FolderTest.test1.json').folder);
+            expect(f).toEqual(jasmine.any(Folder));
+        });
+
+        it('should route to file', function () {
+            let fp = f.getByTypeAndId(OtherConsult, 1);
+            expect(router.getRouteToFolderFile(fp)).toBe('/folder/1/file/OtherConsult/1');
+        });
     });
 });
