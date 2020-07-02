7e1f0f01a4f39824002d59606d313b80735f397f
Jean Honlet <jehon@users.noreply.github.com>
2020-07-02 18:00:00 +0200

    feature: add fire (fireOn)

---
+++ app/js/fire.js
@@ -0,0 +1,12 @@
+
+/**
+ * @param {Element} target - where to start the event
+ * @param {string} name - the event name
+ * @param {*} data - to pass alongµ
+ * @returns {Event} - the fired event
+ */
+export default function fireOn(target, name, data = null) {
+    var event = new CustomEvent(name, { detail: data });
+    target.dispatchEvent(event);
+    return event;
+}
---
+++ tests/unitjs/fire-test.js
@@ -0,0 +1,16 @@
+
+import { fn } from './athelpers.js';
+import fireOn from '../../app/js/fire.js';
+
+describe(fn(import.meta.url), function () {
+    it('with default behavior', function () {
+        let ok = 0;
+        const el = document.querySelector('body');
+        el.addEventListener('test-fire-js', (/** @type {CustomEvent} */ evt) => { ok = evt.detail; });
+        fireOn(el, 'test-fire-js');
+        expect(ok).toBeNull();
+
+        fireOn(el, 'test-fire-js', 123);
+        expect(ok).toEqual(123);
+    });
+});
