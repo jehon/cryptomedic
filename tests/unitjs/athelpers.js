bc90769a9292b432ed1bdd7caf4e1ecd81913507
Jean Honlet <jehon@users.noreply.github.com>
2020-07-02 18:00:00 +0200

    chore: use fireOn from app instead of local one

--- tests/unitjs/athelpers.js
+++ tests/unitjs/athelpers.js
@@ -1,11 +1,12 @@
 
 import { API_VERSION } from '../../app/config.js';
+import _fireOn from '../../app/js/fire.js';
 
-export function fireOn(target, name, data = {}) {
-    var event = new CustomEvent(name, { detail: data });
-    target.dispatchEvent(event);
-}
+export const fireOn = _fireOn;
 
+/**
+ * @param name
+ */
 export function loadReference(name) {
     // Thanks to http://stackoverflow.com/a/27830579/1954789
     let valid_respond = readJSON('www/api/' + API_VERSION + '/tests/references/' + name);
@@ -13,12 +14,20 @@ export function loadReference(name) {
     return valid_respond;
 }
 
+/**
+ * @param url
+ */
 export function extractPath(url) {
     return url.replace(/^http.?:\/\/localhost:[0-9]+/, '');
 }
 
 /* TODO: DEPRECATED */
 /* map old version to new one */
+/**
+ * @param title
+ * @param options
+ * @param fn
+ */
 export function webDescribe(title, options, fn) {
     if (typeof (options) == 'object') {
         return withHtml(Object.assign({ title }, options), fn);
@@ -26,6 +35,9 @@ export function webDescribe(title, options, fn) {
     return withHtml({ title, html: options }, fn);
 }
 
+/**
+ * @param url
+ */
 export function fn(url) {
     return new URL(url).pathname.split('/').pop();
 }
