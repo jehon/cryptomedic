086e73c2b5f1ede7a76616184bb8c97fd6128738
Jean Honlet <jehon@users.noreply.github.com>
2020-07-04 18:33:17 +0200

    wip: implement x-graphics

---
+++ tests/unitjs/x-graphic-test.js
@@ -0,0 +1,46 @@
+
+import '../../app/elements/widgets/x-graphic.js';
+
+import { fn, loadReference } from './athelpers.js';
+import Folder from '../../app/models/Folder.js';
+
+fdescribe(fn(import.meta.url), function () {
+
+    withHtml('<x-graphic></x-graphic>', function (element) {
+        describe('without folder', function () {
+            beforeEach(() => {
+                element().folder = null;
+            });
+
+            it('should initialize', function () {
+                expect(element().innerHTML).toContain('No patient');
+            });
+        });
+
+        describe('with invalid patient', function () {
+            beforeEach(() => {
+                const folder = new Folder(loadReference('FolderTest.test1.json').folder);
+                folder.getPatient().Sex = 0;
+                element().folder = folder;
+            });
+
+            it('should initialize', function () {
+                expect(element().innerHTML).toContain('Sex of the patient is unknown');
+            });
+
+        });
+
+        describe('with folder', function () {
+            beforeEach(() => {
+                const folder = new Folder(loadReference('FolderTest.test1.json').folder);
+                expect(folder).toEqual(jasmine.any(Folder));
+                element().folder = folder;
+            });
+
+            it('should initialize', function () {
+                expect(element().innerHTML).not.toContain('No patient');
+            });
+        });
+    });
+
+});
