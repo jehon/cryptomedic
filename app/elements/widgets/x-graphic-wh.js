66a8a4b98578db0a7f95234388ecbb98b20d891c
Jean Honlet <jehon@users.noreply.github.com>
2020-07-04 18:33:17 +0200

    wip: implement x-graphics

---
+++ app/elements/widgets/x-graphic-wh.js
@@ -0,0 +1,16 @@
+
+import XGraphic from './x-graphic.js';
+import amd_stats from '../../js/amd_stats.js';
+
+class XGraphicWH extends XGraphic {
+    getImageName() { return 'wh'; }
+    getImageDimensions(sex) { return amd_stats.dimensions[`Heightcm_Weightkg_${sex}`]; }
+
+    getVariableX() { return 'Weight'; }
+    getVariableY() { return 'Height'; }
+
+    getValueX(file) { return file.Heightcm; }
+    getValueY(file) { return file.Weightkg; }
+}
+
+window.customElements.define('x-graphic-wh', XGraphicWH);
---
+++ app/elements/widgets/x-graphic.js
@@ -0,0 +1,218 @@
+
+import WithMixin from '../mixins/with-mixin.js';
+import { fromBirthDateTo } from './x-age.js';
+import { getRouteToFolderFile, setRoute } from '../../js/router.js';
+
+import createCallback from '../../js/callback.js';
+
+const hooverCallback = createCallback('hooverCallback');
+
+/**
+ * @augments HTMLElement
+ */
+export default class XGraphic extends WithMixin('folder', HTMLElement) {
+
+    /* istanbul-ignore-next */
+    onHooverUnsubscribe =  () => { }
+
+    constructor() {
+        super();
+        this.adapt();
+    }
+
+    onFolderChanged(_f) {
+        this.adapt();
+    }
+
+    connectedCallback() {
+        this.onHooverUnsubscribe = hooverCallback.onChange((uid) => {
+            this.querySelectorAll('[uid][highlighted]').forEach(e => e.removeAttribute('highlighted'));
+            this.querySelectorAll(`[uid=${uid}]`).forEach(e => e.setAttribute('highlighted', uid));
+        });
+    }
+
+    disconnectedCallback() {
+        this.onHooverUnsubscribe();
+        this.onHooverUnsubscribe = /* istanbul-ignore-next */ () => { };
+    }
+
+    getAge(file) {
+        return fromBirthDateTo(this.folder.getPatient().Yearofbirth, file.Date);
+    }
+
+    // Presentation
+    getVariableX() { return 'Age'; }
+    getVariableY() { return 'Y'; }
+    getImageName() { return 'image'; }
+    getImageDimensions(_sex) { return { top: 1, bottom: 0, left: 0, right: 1, vtop: 1, vbottom: 0, vleft: 0, vright: 1 }; }
+
+    // Data
+    getValueX(file) { return this.getAge(file); }
+    getValueY(_file) { return 0; }
+
+    displayX(file) {
+        let v = this.getValueX(file);
+        if (isNaN(v) || !v || typeof(v) != 'number') {
+            return 'Invalid ' + this.getVariableX();
+        }
+        v = Math.round(v);
+        if (v < this.getImageDimensions(this.folder?.getPatient().sexStr()).vleft) {
+            return v + ' to low';
+        }
+        if (v > this.getImageDimensions(this.folder?.getPatient().sexStr()).vright) {
+            return v + ' to high';
+        }
+        return v;
+    }
+
+    displayY(file) {
+        let v = this.getValueY(file);
+
+        if (isNaN(v)) {
+            return 'Invalid ' + this.getVariableY();
+        }
+        if (!v) {
+            return 'No ' + this.getVariableY();
+        }
+        v = Math.round(v);
+        if (v < this.getImageDimensions(this.folder?.getPatient().sexStr()).vbottom) {
+            return v + ' to low';
+        }
+        if (v > this.getImageDimensions(this.folder?.getPatient().sexStr()).vtop) {
+            return v + ' to high';
+        }
+        return v;
+    }
+
+    isValid(file) {
+        return typeof (this.displayX(file)) == 'number' && typeof (this.displayY(file)) == 'number';
+    }
+
+    getAbscisse(file) {
+        if (!this.isValid(file)) return 0;
+
+        const v = this.getValueX(file);
+        const imgDimension = this.getImageDimensions(this.folder.getPatient().sexStr());
+        const p = (v - imgDimension.vleft) / (imgDimension.vright - imgDimension.vleft);
+        return (p * (imgDimension.right - imgDimension.left) + imgDimension.left) * 100;
+    }
+
+    getOrdonnee(file) {
+        if (!this.isValid(file)) return 0;
+
+        const v = this.getValueY(file);
+        const imgDimension = this.getImageDimensions(this.folder.getPatient().sexStr());
+        const p = (v - imgDimension.vbottom) / (imgDimension.vtop - imgDimension.vbottom);
+        return (p * (imgDimension.top - imgDimension.bottom) + imgDimension.bottom) * 100;
+    }
+
+    adapt() {
+        this.innerHTML = `
+        <style>
+            /********************************************
+             *  Marks and graphics
+             */
+            .markContainer {
+                /* position: absolute -> relative to the first non-static element -> relative to 0,0 = pseudo-static */
+                position: relative;
+            }
+
+            .mark {
+                color: red;
+                position: absolute;
+                margin: 0;
+                padding: 0;
+                border: 0;
+                margin-top: -10px;
+            }
+
+            .mark > span {
+                margin-left: -50%;
+            }
+
+            .mark[highlighted] > span {
+                background-color: red;
+                color: white;
+                z-index: 100;
+            }
+
+            tr[highlighted] {
+                background-color: gray !important;
+            }
+        </style>
+        <x-group-panel title='${this.getVariableY()} / ${this.getVariableX()}'></x-group-panel>`;
+
+        const fs = this.querySelector('x-group-panel');
+
+        if (!this.folder) {
+            fs.insertAdjacentHTML('beforeend',
+                'No patient'
+            );
+            return;
+        }
+
+        if (!this.folder?.getPatient()?.sexStr()) {
+            fs.insertAdjacentHTML('beforeend',
+                'Sex of the patient is unknown'
+            );
+            return;
+        }
+
+        fs.insertAdjacentHTML('beforeend', `
+            <div class='markContainer'>
+                <img id='graph' src="/static/img/stats_${this.getImageName()}-${this.folder.getPatient().sexStr()}.jpg" width='100%'></img>
+            </div>
+            <table class='colorize datalegend'>
+                <thead>
+                    <th>Date</th>
+                    <th>${this.getVariableX()}</th>
+                    <th>${this.getVariableY()}</th>
+                    <th>Validity</th>
+                </thead>
+                <tbody></tbody>
+            </table>
+        `);
+
+        const tableElement = this.querySelector('table > tbody');
+        const imgElement = this.querySelector('.markContainer');
+
+        for (const index in this.folder.getFilesRelatedToPatient()) {
+            const file = this.folder.getFilesRelatedToPatient(index);
+            const uid = file.uid();
+            tableElement.insertAdjacentHTML('beforeend', `
+                <tr uid="${uid}" 
+                    valid="${this.isValid(file) ? 'valid' : 'invalid'}"
+                    ng-class="{ hovered: hovered == $index }"
+                >
+                    <td>${file.Date}</td>
+                    <td>${this.displayX(file)}</td>
+                    <td>${this.displayY(file)}</td>
+                    <td>${this.isValid(file) ? '✓' : '✗'}</td>
+                </tr >
+            `);
+
+            if (this.isValid(file)) {
+                imgElement.insertAdjacentHTML('beforeend', `
+                        <span
+                            uid="${uid}"
+                            class="mark"
+                            style="left: ${this.getAbscisse(file)}%; top: ${this.getOrdonnee(file)}%"
+                            ng-class="{ hovered: hovered == $index }"
+                        ><span>+</span></span>
+                    `);
+
+            }
+            this.querySelectorAll(`[uid=${uid}]`).forEach(e => e.addEventListener('click', () => {
+                setRoute(getRouteToFolderFile(file));
+            }));
+
+            this.querySelectorAll(`[uid=${uid}]`).forEach(e => e.addEventListener('mouseover', () => {
+                hooverCallback.set(uid);
+            }));
+        }
+    }
+}
+
+window.customElements.define('x-graphic', XGraphic);
+
+// TODO: hide invalid entriyes ? => tr[valid="invalid"] > td
