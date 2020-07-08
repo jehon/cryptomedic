6f6c0ca54f833e575e41ba59fbc342db10984d1f
Jean Honlet <jehon@users.noreply.github.com>
2020-07-08 07:27:43 +0200

    fix: setProperty case

--- app/js/set-property.js
+++ app/js/set-property.js
@@ -1,24 +1,25 @@
 
-import { toAttributeCase } from './string-utils.js';
+import { toAttributeCase, toPropertyCase } from './string-utils.js';
 
 /**
  * @param {Element} root - the element to start with
- * @param {string} attr - the property name to be set
+ * @param {string} name - the property name to be set
  * @param {*} value - the value to be dispatched
  * @returns {Element} the root element
  */
-export default function setPropertyOn(root, attr, value) {
-    attr = toAttributeCase(attr);
+export default function setPropertyOn(root, name, value) {
+    const attr = toAttributeCase(name);
+    const prop = toPropertyCase(name);
 
     if (root.hasAttribute('x-top')) {
         root.querySelectorAll(`[with-${attr}]`).forEach(el => {
-            el[attr] = value;
+            el[prop] = value;
         });
     }
 
     if (root.shadowRoot) {
         root.shadowRoot.querySelectorAll(`[with-${attr}]`).forEach(el => {
-            el[attr] = value;
+            el[prop] = value;
         });
     }
     return root;
--- tests/unitjs/set-property-test.js
+++ tests/unitjs/set-property-test.js
@@ -1,46 +1,62 @@
 
 import { fn } from './athelpers.js';
 import setPropertyOn from '../../app/js/set-property.js';
-import { toAttributeCase } from '../../app/js/string-utils.js';
-
-const p = toAttributeCase('test');
-
-export class XXTestPropertySet extends HTMLElement {
-    constructor() {
-        super();
-        this.attachShadow({ mode: 'open' });
-        this.shadowRoot.innerHTML = `<div id='yes' with-${p}=1></div><div id='no'></div>`;
-    }
-}
-
-window.customElements.define('xx-test-property-test', XXTestPropertySet);
 
 describe(fn(import.meta.url), function () {
     const obj = { a: 123 };
 
-    withHtml(`<div x-top="1"><div id='yes' with-${p}=1></div><div id='no'></div></div>`, function (element) {
-        it('with default behavior', function () {
-            expect(element()[p]).toBeUndefined();
+    describe('with folder', function() {
+        const p = 'folder';
 
-            setPropertyOn(element(), p, obj);
+        class XXTestPropertySet extends HTMLElement {
+            constructor() {
+                super();
+                this.attachShadow({ mode: 'open' });
+                this.shadowRoot.innerHTML = `<div id='yes' with-${p}=1></div><div id='no'></div>`;
+            }
+        }
+        window.customElements.define('xx-test-property-test', XXTestPropertySet);
 
-            expect(element()[p]).toBeUndefined();
-            expect(element().querySelector('div#yes')[p]).toBe(obj);
-            expect(element().querySelector('div#no')[p]).toBeUndefined();
+        withHtml(`<div x-top="1"><div id='yes' with-${p}=1></div><div id='no'></div></div>`, function (element) {
+            it('with default behavior', function () {
+                expect(element()[p]).toBeUndefined();
+
+                setPropertyOn(element(), p, obj);
+
+                expect(element()[p]).toBeUndefined();
+                expect(element().querySelector('div#yes')[p]).toBe(obj);
+                expect(element().querySelector('div#no')[p]).toBeUndefined();
+            });
+        });
+
+        withHtml(`<xx-test-property-test with-${p}=1></xx-test-property-test>`, function (element) {
+            it('with default behavior', function () {
+                expect(element()[p]).toBeUndefined();
+                expect(element().shadowRoot.querySelector('div#yes')[p]).toBeUndefined();
+                expect(element().shadowRoot.querySelector('div#no')[p]).toBeUndefined();
+
+                setPropertyOn(element(), p, obj);
+
+                expect(element().shadowRoot.querySelector('div#yes')[p]).toBe(obj);
+                expect(element().shadowRoot.querySelector('div#no')[p]).toBeUndefined();
+            });
         });
     });
 
-    withHtml(`<xx-test-property-test with-${p}=1></xx-test-property-test>`, function (element) {
-        it('with default behavior', function () {
-            expect(element()[p]).toBeUndefined();
-            expect(element().shadowRoot.querySelector('div#yes')[p]).toBeUndefined();
-            expect(element().shadowRoot.querySelector('div#no')[p]).toBeUndefined();
+    describe('with file-uid', function() {
+        const p = 'file-uid';
+        const prop = 'fileUid';
 
-            setPropertyOn(element(), p, obj);
+        withHtml(`<div x-top="1"><div id='yes' with-${p}=1></div><div id='no'></div></div>`, function (element) {
+            it('with default behavior', function () {
+                expect(element()[p]).toBeUndefined();
 
-            expect(element().shadowRoot.querySelector('div#yes')[p]).toBe(obj);
-            expect(element().shadowRoot.querySelector('div#no')[p]).toBeUndefined();
+                setPropertyOn(element(), p, obj);
+
+                expect(element()[p]).toBeUndefined();
+                expect(element().querySelector('div#yes')[prop]).toBe(obj);
+                expect(element().querySelector('div#no')[prop]).toBeUndefined();
+            });
         });
     });
-
 });
