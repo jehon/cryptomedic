1705dd04a454dfe635b571a30c03f14fe98624ce
Jean Honlet <jehon@users.noreply.github.com>
2020-07-12 21:21:10 +0200

    test: check the number of files included

--- tests/unitjs/athelpers.js
+++ tests/unitjs/athelpers.js
@@ -36,8 +36,20 @@ export function webDescribe(title, options, fn) {
 }
 
 /**
- * @param url
+ * Count the number of included files
+ */
+export let fnCnt = 0;
+/**
+ * @param {string} url - import.meta.url
+ * @returns {string} the filename as a test title
  */
 export function fn(url) {
+    fnCnt++;
     return new URL(url).pathname.split('/').pop();
 }
+
+afterAll(function () {
+    expect(fnCnt)
+        .withContext('#FILE_NUMBER# Number of files in athelper.js')
+        .toBe(29);
+});
