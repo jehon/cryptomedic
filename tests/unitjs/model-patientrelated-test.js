03cac045d63d0851f35c67ba75265c2049d77099
Jean Honlet <jehon@users.noreply.github.com>
2020-07-17 11:04:09 +0200

    chore: target x-patient-related to x-with-folder

--- tests/unitjs/model-patientrelated-test.js
+++ tests/unitjs/model-patientrelated-test.js
@@ -25,8 +25,6 @@ describe('PatientRelated', function () {
             expect(rc).toEqual(jasmine.anything({ id: 13 }));
             expect(rc.getPatient()).toEqual(jasmine.any(Patient));
             expect(rc.Date).toEqual('2014-01-04');
-            expect(rc.ageAtConsultTime()).toEqual(16);
-            expect(rc.ageAtConsultTime(true)).toBe('16y0m');
         });
 
         describe('with ricketConsult_13', function () {
@@ -37,20 +35,10 @@ describe('PatientRelated', function () {
 
                 expect(rc).toEqual(jasmine.any(RicketConsult));
                 expect(rc).toEqual(jasmine.anything({ id: 13 }));
-                expect(rc.ageAtConsultTime()).toBeCloseTo(16, 2);
                 expect(rc.Heightcm).toBeCloseTo(110, 1);
                 expect(rc.Weightkg).toBeCloseTo(37, 1);
                 expect(rc.bmi()).toBeCloseTo(30.57, 1);
                 expect(rc.wh()).toBeCloseTo(0.3363, 3);
-                try {
-                    rc.ds_height();
-                } catch (e) {
-                    console.error('ds_height', e, e.message, e.fileName, e.lineNumber);
-                }
-                expect(rc.ds_height()).toBeCloseTo(-9.570, 2);
-                expect(rc.ds_weight()).toBeCloseTo(-3.588, 2);
-                expect(rc.ds_weight_height()).toBeCloseTo(12.6105, 2);
-                expect(rc.ds_bmi()).toBeCloseTo(2.391, 2);
             });
         });
     });
@@ -61,13 +49,8 @@ describe('PatientRelated', function () {
             var o = new PatientRelated({});
             o.linkPatient(p);
 
-            expect(function () { o.ageAtConsultTime(); }).toThrow(new DataMissingException('Date'));
             expect(function () { o.bmi(); }).toThrow(new DataMissingException('Height'));
             expect(function () { o.wh(); }).toThrow(new DataMissingException('Height'));
-            expect(function () { o.ds_height(); }).toThrow(new DataMissingException('Date'));
-            expect(function () { o.ds_weight(); }).toThrow(new DataMissingException('Date'));
-            expect(function () { o.ds_weight_height(); }).toThrow(new DataMissingException('Height'));
-            expect(function () { o.ds_bmi(); }).toThrow(new DataMissingException('Date'));
         });
     });
 });
