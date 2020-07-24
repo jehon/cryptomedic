57275e3c4607afd037c11e1cbb9d139c25656f73
Jean Honlet <jehon@users.noreply.github.com>
2020-07-24 19:18:48 +0200

    chore: remove withHTML

--- tests/unitjs/x-i18n-test.js
+++ tests/unitjs/x-i18n-test.js
@@ -1,48 +1,35 @@
 
-import _XI18n from '../../app/elements/widgets/x-i18n.js';
 import { fn } from './athelpers.js';
-
-// TODO: use constructor instead of webDescribe
+import XI18n from '../../app/elements/widgets/x-i18n.js';
 
 describe(fn(import.meta.url), function () {
-    withHtml({ title: 'without value', html: '<x-i18n></x-i18n>' },
-        function (element) {
-            it('should show text', function () {
-                expect(element()).not.toBeNull();
-                expect(element().innerHTML).toBe('');
-            });
-        });
+    it('without value', function() {
+        const el = new XI18n();
+        expect(el.innerHTML).toBe('');
+    });
 
-    withHtml({ title: 'with value=', html: '<x-i18n value=""></x-i18n>' },
-        function (element) {
-            it('should show text', function () {
-                expect(element()).not.toBeNull();
-                expect(element().innerHTML).toBe('');
-            });
-        });
+    it('with value empty', function() {
+        const el = new XI18n();
+        el.setAttribute('value', '');
+        expect(el.innerHTML).toBe('');
+    });
 
-    withHtml({ title: 'with value=original', html: '<x-i18n value=\'original\'></x-i18n>' },
-        function (element) {
-            it('should show text', function () {
-                expect(element()).not.toBeNull();
-                expect(element().innerHTML).toBe('original');
-            });
-        });
+    it('with value "original"', function() {
+        const el = new XI18n();
+        el.setAttribute('value', 'original');
+        expect(el.innerHTML).toBe('original');
+    });
 
-    withHtml({ title: 'with value=originalThere', html: '<x-i18n value=\'originalThere\'></x-i18n>' },
-        function (element) {
-            it('should show text', function () {
-                expect(element()).not.toBeNull();
-                expect(element().innerHTML).toBe('original There');
-            });
-        });
+    it('with value "orignalThere"', function() {
+        const el = new XI18n();
+        el.setAttribute('value', 'originalThere');
+        expect(el.innerHTML).toBe('original There');
+    });
 
-    withHtml({ title: 'with value=123', html: '<x-i18n value=\'123\'></x-i18n>' },
-        function (element) {
-            it('should show text', function () {
-                expect(element()).not.toBeNull();
-                expect(element().innerHTML).toBe('123');
-            });
-        });
 
+    it('with value "123"', function() {
+        const el = new XI18n();
+        el.setAttribute('value', '123');
+        expect(el.innerHTML).toBe('123');
+    });
 });
