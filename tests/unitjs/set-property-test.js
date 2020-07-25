a0428b9f3d45f4b866b6bc9c7d9c85b447c97f65
Jean Honlet <jehon@users.noreply.github.com>
2020-07-25 07:29:55 +0200

    chore: update package-lock

--- tests/unitjs/set-property-test.js
+++ tests/unitjs/set-property-test.js
@@ -2,86 +2,77 @@
 import { fn } from './athelpers.js';
 import setPropertyOn from '../../app/js/set-property.js';
 
-class XXTestPropertySetForFolder extends HTMLElement {
-    constructor() {
-        super();
-        this.attachShadow({ mode: 'open' });
-        this.shadowRoot.innerHTML = '<div id="yes" with-folder=1></div><div id="no"></div>';
-    }
-}
-window.customElements.define('xx-test-property-for-folder-test', XXTestPropertySetForFolder);
-
 describe(fn(import.meta.url), function () {
-    const p = 'folder';
+    const pattr = 'folder';
+    const prop = pattr;
     const obj = { a: 123 };
 
+    class XXTestPropertySetForFolder extends HTMLElement {
+        constructor() {
+            super();
+            this.attachShadow({ mode: 'open' });
+            this.shadowRoot.innerHTML = `<div id="yes" with-${pattr}=1></div><div id="no"></div>`;
+        }
+    }
+    window.customElements.define('xx-test-property-for-folder-test', XXTestPropertySetForFolder);
+
     describe('with folder', function () {
-        withHtml(`<div x-top="1"><div id='yes' with-${p}=1></div><div id='no'></div></div>`, function (element) {
-            it('with default behavior', function () {
-                expect(element()[p]).toBeUndefined();
+        it('with default behavior', function () {
+            const el = document.createElement('div');
+            el.setAttribute('x-top', '1');
 
-                setPropertyOn(element(), p, obj);
+            const divY = document.createElement('div');
+            divY.setAttribute(`with-${pattr}`, '1');
+            const divN = document.createElement('div');
 
-                expect(element()[p]).toBeUndefined();
-                expect(element().querySelector('div#yes')[p]).toBe(obj);
-                expect(element().querySelector('div#no')[p]).toBeUndefined();
-            });
+            el.insertAdjacentElement('beforeend', divY);
+            el.insertAdjacentElement('beforeend', divN);
+
+            expect(el[prop]).toBeUndefined();
+
+            setPropertyOn(el, prop, obj);
+
+            expect(el[prop]).toBeUndefined();
+            expect(divY[prop]).toBe(obj);
+            expect(divN[prop]).toBeUndefined();
         });
 
-        withHtml(`<xx-test-property-for-folder-test with-${p}=1></xx-test-property-for-folder-test>`, function (element) {
-            it('with default behavior', function () {
-                expect(element()[p]).toBeUndefined();
-                expect(element().shadowRoot.querySelector('div#yes')[p]).toBeUndefined();
-                expect(element().shadowRoot.querySelector('div#no')[p]).toBeUndefined();
+        it('with default behavior', function () {
+            const el = new XXTestPropertySetForFolder();
 
-                setPropertyOn(element(), p, obj);
+            expect(el[pattr]).toBeUndefined();
 
-                expect(element().shadowRoot.querySelector('div#yes')[p]).toBe(obj);
-                expect(element().shadowRoot.querySelector('div#no')[p]).toBeUndefined();
-            });
-        });
-    });
+            setPropertyOn(el, prop, obj);
 
-    describe('with file', function () {
-        const p = 'file';
-        const prop = 'file';
-
-        withHtml(`<div x-top="1"><div id='yes' with-${p}=1></div><div id='no'></div></div>`, function (element) {
-            it('with default behavior', function () {
-                expect(element()[p]).toBeUndefined();
-
-                setPropertyOn(element(), p, obj);
-
-                expect(element()[p]).toBeUndefined();
-                expect(element().querySelector('div#yes')[prop]).toBe(obj);
-                expect(element().querySelector('div#no')[prop]).toBeUndefined();
-            });
+            expect(el[pattr]).toBeUndefined();
+            expect(el.shadowRoot.querySelector('div#yes')[prop]).toBe(obj);
+            expect(el.shadowRoot.querySelector('div#no')[prop]).toBeUndefined();
         });
     });
 
     describe('with exception in childerns', function () {
-        const p = 'file';
+        const pattr = 'file';
         const prop = 'file';
 
-        const div = document.createElement('div');
-        div.setAttribute('x-top', '1');
+        const el = document.createElement('div');
+        el.setAttribute('x-top', '1');
         const divY = document.createElement('div');
-        divY.setAttribute(`with-${p}`, '1');
+        divY.setAttribute(`with-${pattr}`, '1');
         Object.defineProperty(divY, 'file', {
             set: (_v) => { throw 'error'; },
             get: () => 'error'
         });
         const divN = document.createElement('div');
 
-        div.insertAdjacentElement('beforeend', divY);
-        div.insertAdjacentElement('beforeend', divN);
+        el.insertAdjacentElement('beforeend', divY);
+        el.insertAdjacentElement('beforeend', divN);
 
         it('with default behavior', function () {
-            expect(div[p]).toBeUndefined();
+            expect(el[prop]).toBeUndefined();
 
-            setPropertyOn(div, p, null);
+            setPropertyOn(el, prop, null);
 
-            expect(div[p]).toBeUndefined();
+            expect(el[pattr]).toBeUndefined();
             expect(divY[prop]).toBe('error');
             expect(divN[prop]).toBeUndefined();
         });
@@ -89,8 +80,6 @@ describe(fn(import.meta.url), function () {
     });
 
     describe('with exception in shadow childerns', function () {
-        const prop = p;
-
         const xxTest = new XXTestPropertySetForFolder();
         Object.defineProperty(xxTest, prop, {
             set: (_v) => { throw 'error'; },
@@ -98,7 +87,7 @@ describe(fn(import.meta.url), function () {
         });
 
         const divY = document.createElement('div');
-        divY.setAttribute(`with-${p}`, '1');
+        divY.setAttribute(`with-${pattr}`, '1');
         Object.defineProperty(divY, prop, {
             set: (_v) => { throw 'error'; },
             get: () => 'error'
@@ -109,9 +98,9 @@ describe(fn(import.meta.url), function () {
         xxTest.shadowRoot.appendChild(divN);
 
         it('with default behavior', function () {
-            setPropertyOn(xxTest, p, null);
+            setPropertyOn(xxTest, prop, null);
 
-            expect(xxTest[p]).toBe('error');
+            expect(xxTest[prop]).toBe('error');
             expect(divY[prop]).toBe('error');
             expect(divN[prop]).toBeUndefined();
         });
