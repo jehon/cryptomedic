c6cde9aaef855d8ad53211f048bcae384e0db55d
Jean Honlet <jehon@users.noreply.github.com>
2020-08-03 18:00:00 +0200

    feature: rework demo

---
+++ tests/demo/demo.js
@@ -0,0 +1,39 @@
+
+import { defineCustomElement } from '../../app/js/custom-element.js';
+
+export default class XxTest extends HTMLElement {
+    constructor() {
+        super();
+        this.attachShadow({ mode: 'open' });
+        this.shadowRoot.innerHTML = `
+            <h2>title</h2>
+            <slot></slot>
+            <div id='code'></div>
+        `;
+    }
+
+    connectedCallback() {
+        const code = this.innerHTML;
+
+        this.shadowRoot.querySelector('#code').innerHTML = code
+            .replace(/&/g, '&amp;')
+            .replace(/</g, '&lt;')
+            .replace(/>/g, '&gt;')
+            .replace(/"/g, '&quot;')
+            .replace(/'/g, '&#039;');
+
+        if (!this.hasAttribute('title')) {
+            const topEl = this.querySelector('*');
+            if (topEl) {
+                const topElName = topEl.tagName.toLowerCase();
+                this.setAttribute('title', topElName);
+            } else {
+                this.setAttribute('title', '...');
+            }
+        }
+        this.shadowRoot.querySelector('h2').innerHTML = this.getAttribute('title');
+    }
+}
+
+defineCustomElement(XxTest);
+
