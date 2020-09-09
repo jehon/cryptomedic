54ae6a3cd54b47bb21ab8c21bb6aca6b6ee9798d
Jean Honlet <jehon@users.noreply.github.com>
2020-09-09 11:37:58 +0200

    chore: migrage patient and picture to x-fff

--- www/templates/templates/patient_fiche.php
+++ www/templates/templates/patient_fiche.php
@@ -5,79 +5,64 @@
 	<div class='row'>
 		<div class="col-md-6">
 	        <x-group-panel title='General data'>
-				<table>
-					<tr ng-if="patient_id > -1">
-						<td>Entry Number</td>
-						<td><?php (new t("Patient.entryyear"))->read()->p(); ?>-<?php (new t("Patient.entryorder"))->read()->p(); ?></td>
-					</tr>
-					<tr ng-if="patient_id == -1">
-						<td>Entry Year</td>
-						<td>
-							<div class='alert alert-info'>
-								You asked the system to generate a reference for you. This reference will be generated
-								when you will save this file. I just need the year to build up the reference.
-								<?php (new t("Patient.entryyear", [ "required" => "required" ]))->write()->p(); ?>
-							</div>
-						</td>
-					</tr>
-				</table>
+				<x-fff-field ng-if="patient_id > -1" label='Entry Number'>
+					<div><?php (new t("Patient.entryyear"))->read()->p(); ?>-<?php (new t("Patient.entryorder"))->read()->p(); ?></div>
+				</x-fff-field>
+				<x-fff-field ng-if="patient_id == -1" label='Entry Year'>
+					<div>
+						<x-message level='info'>
+							You asked the system to generate a reference for you. This reference will be generated
+							when you will save this file. I just need the year to build up the reference.
+							<?php (new t("Patient.entryyear", [ "required" => "required" ]))->write()->p(); ?>
+						</x-message>
+					</div>
+				</x-fff-field>
 				<?php (new t("Patient.Name"))->tr2("Name")->p(); ?>
 				<?php (new t("Patient.Sex"))->tr2()->p(); ?>
-				<table>
-					<tr>
-						<td>Year of birth</td>
-						<td>
-							{{folder.getPatient().Yearofbirth}}
-							<div class='notModeRead'>
-								<h5>Calculate year of birth</h5>
-								<input ng-model='age.years' type='number' min='0' max='100'> years<br>
-								<input ng-model='age.months' type='number' min='-1' max='12'> months
-							</div>
-						</td>
-					</tr>
-					<tr class='notModeRead'>
-						<td>Calculate year of birth</td>
-						<td>
-						</td>
-					</tr>
-					<tr class='notModeWrite'>
-						<td>Age (today)</td>
-                        <td id='#ageToday'><x-fff-age></x-fff-age> old</td>
-					</tr>
-					<tr ng-class='{ emptyValue: !folder.getPatient().District}'>
-						<td>District</td>
-						<td>
-							<span class='notModeRead'>
-								<x-write-list value='{{folder.getPatient().District}}' name='District' list-name='Districts' nullable></x-write-list>
-							</span>
-							<span class='notModeWrite'>
-								<?php (new t("Patient.District"))->read()->p(); ?>
-							</span>
-						</td>
-					</tr>
-					<tr>
-						<td>Upazilla</td>
-						<td>
-							<span class='notModeRead'>
-								<x-write-list value='{{folder.getPatient().Upazilla}}' name='Upazilla' nullable></x-write-list>
-							</span>
-							<span class='notModeWrite'>
-								<?php (new t("Patient.Upazilla"))->read()->p(); ?>
-							</span>
-						</td>
-					</tr>
-					<tr>
-						<td>Union</td>
-						<td>
-							<span class='notModeRead'>
-								<x-write-list value='{{folder.getPatient().Union_}}' name='Union_' nullable></x-write-list>
-							</span>
-							<span class='notModeWrite'>
-								<?php (new t("Patient.Union_"))->read()->p(); ?>
-							</span>
-						</td>
-					</tr>
-				</table>
+				<x-fff-field label='Year of birth'>
+					<div>
+						{{folder.getPatient().Yearofbirth}}
+						<div class='notModeRead'>
+							<h5>Calculate year of birth</h5>
+							<div><input ng-model='age.years' type='number' min='0' max='100'> years</div>
+							<div><input ng-model='age.months' type='number' min='-1' max='12'> months</div>
+						</div>
+					</div>
+				</x-fff-field>
+				<div class='notModeRead'>Calculate year of birth</div>
+				<x-fff-field class='notModeWrite' label='Age (today)'>
+					<div id='#ageToday'><x-fff-age></x-fff-age> old</div>
+				</x-fff-field>
+				<x-fff-field field='District'>
+					<div>
+						<span class='notModeRead'>
+							<x-write-list value='{{folder.getPatient().District}}' name='District' list-name='Districts' nullable></x-write-list>
+						</span>
+						<span class='notModeWrite'>
+							<?php (new t("Patient.District"))->read()->p(); ?>
+						</span>
+					</div>
+				</x-fff-field>
+				<x-fff-field field='Upazilla'>
+					<div>
+						<span class='notModeRead'>
+							<x-write-list value='{{folder.getPatient().Upazilla}}' name='Upazilla' nullable></x-write-list>
+						</span>
+						<span class='notModeWrite'>
+							<?php (new t("Patient.Upazilla"))->read()->p(); ?>
+						</span>
+					</div>
+				</x-fff-field>
+				<x-fff-field field='Union'>
+					<div>
+						<span class='notModeRead'>
+							<x-write-list value='{{folder.getPatient().Union_}}' name='Union_' nullable></x-write-list>
+						</span>
+						<span class='notModeWrite'>
+							<?php (new t("Patient.Union_"))->read()->p(); ?>
+						</span>
+					</div>
+				</x-fff-field>
 				<?php (new t("Patient.Telephone"))->tr2()->p(); ?>
 				<?php (new t("Patient.AddressNotes"))->tr2("Adress Notes")->p(); ?>
 			</x-group-panel>
