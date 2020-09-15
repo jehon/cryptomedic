7410b446a47785f3f898696fb4ff30419ebe7377
Jean Honlet <jehon@users.noreply.github.com>
2020-09-15 21:16:29 +0200

    chore(new api): some clean up

--- www/api/v1.3/public/app/Lib/Request.php
+++ www/api/v1.3/public/app/Lib/Request.php
@@ -15,4 +15,19 @@ class Request {
         }
         return !! $_REQUEST[$name];
     }
+
+    // static function getHeader($name) {
+    //     return getallheaders()[$name];
+    // }
+
+    static function replyWith($data) {
+        header('Content-Type: application/json');
+        echo json_encode($data);
+
+        // if (self::getHeader('Accept')) {
+        //     if (preg_match('/.*text/html.*/', self::getHeader('Accept'))) {
+        //         echo "<pre>";
+        //     }
+        // }
+    }
 }
