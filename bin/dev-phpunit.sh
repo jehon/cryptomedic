85687a30086b0c7c1bf260a64f40812fa63fd67f
Jean Honlet <jehon@users.noreply.github.com>
2020-09-17 09:19:53 +0200

    build: test-api with only one context

--- bin/dev-phpunit.sh
+++ bin/dev-phpunit.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 set -e
+set -x
 
 SCRIPT_DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
 PRJ_DIR=$(dirname "$SCRIPT_DIR")
@@ -21,28 +22,78 @@ if [ "$1" == "COMMIT" ]; then
     shift
 fi
 
-for V in v* ; do
-	cd "$V" || exit 255
-	echo "Version $V ($(pwd))"
-	echo "** Laravel $V **"
-	REPORTS="$PRJ_DIR/target/php$V"
-	mkdir -p "$REPORTS"
-	chmod a+wx "$REPORTS"
-	echo "Current folder: $(pwd)"
-	./vendor/bin/phpunit  --coverage-html "$REPORTS" --coverage-xml "$REPORTS" "$@"
-	chmod -R a+wx "$REPORTS"
-	cd ..
+testOne() {
+	local T="$1"
+	local V="$2"
+	shift
+	shift
 
-	echo "** Bare $V **"
-	cd "$V/public" || exit 255
-	REPORTS="$PRJ_DIR/target/php$V-bare"
-	mkdir -p "$REPORTS"
-	chmod a+wx "$REPORTS"
-	echo "Current folder: $(pwd)"
-	./vendor/bin/phpunit  --coverage-html "$REPORTS" --coverage-xml "$REPORTS" "$@"
-	chmod -R a+wx "$REPORTS"
-	cd ..
-done
+	echo "- Type: $T version: $V"
+
+	pushd "$V" || exit 255
+	echo "Version $V ($(pwd))"
+
+	if [[ -z "$T" ]] || [[ "$T" == "laravel" ]]; then
+		if [ -f "phpunit.xml" ]; then
+			echo "** Laravel $V **"
+			REPORTS="$PRJ_DIR/target/php$V"
+			mkdir -p "$REPORTS"
+			chmod a+wx "$REPORTS"
+			./vendor/bin/phpunit  --coverage-html "$REPORTS" --coverage-xml "$REPORTS" "$@"
+			chmod -R a+wx "$REPORTS"
+		else
+			echo "** Laravel $V: skipping because no phpunit.xml **"
+		fi
+	fi
+
+	if [[ -z "$T" ]] || [[ "$T" == "bare" ]]; then
+		if [ -f "public/phpunit.xml" ]; then
+			pushd "public" || exit 255
+			echo "** Bare $V **"
+
+			REPORTS="$PRJ_DIR/target/php$V-bare"
+			mkdir -p "$REPORTS"
+			chmod a+wx "$REPORTS"
+			./vendor/bin/phpunit  --coverage-html "$REPORTS" --coverage-xml "$REPORTS" "$@"
+			chmod -R a+wx "$REPORTS"
+			popd
+		else
+			echo "** Bare $V: skipping because no phpunit.xml **"
+		fi
+	fi
+	popd
+}
+
+# 1: type (laravel/bare)
+# 2: version
+T="$1"
+V="$2"
+shift || true # If arg is missing, do not fail...
+shift || true # If arg is missing, do not fail...
+
+echo "Type: $T version: $V"
+
+if [ -n "$T" ]; then
+	if [[ -z "$V" ]]; then
+		for Vi in v* ; do
+			echo "T- Type: $T version: $V"
+			testOne "$T" "$Vi"
+		done
+	else
+		echo "TV Type: $T version: $V"
+		testOne "$T" "$V"
+	fi
+else
+	if [[ -z "$V" ]]; then
+		for Vi in v* ; do
+			echo "-- Type: $T version: $V"
+			testOne "" "$Vi"
+		done
+	else
+		echo "-V Type: $T version: $V"
+		testOne "" "$V"
+	fi
+fi
 
 cd "$PRJ_DIR"
 
