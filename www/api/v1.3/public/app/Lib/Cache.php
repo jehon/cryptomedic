8b61cf59ccd343f90938ffdea6fcb90c72e66643
Jean Honlet <jehon@users.noreply.github.com>
2020-09-18 21:57:56 +0200

    chore: inject cache generator as functions
    Use variable for cache storage
    Test Database

--- www/api/v1.3/public/app/Lib/Cache.php
+++ www/api/v1.3/public/app/Lib/Cache.php
@@ -1,31 +1,38 @@
 <?php
 
 namespace Cryptomedic\Lib;
+use Cryptomedic\Lib\Database;
+use Cryptomedic\Lib\Lists;
 
 global $myconfig;
 
 define('MY_CACHE_FILE',  constant('MY_FOLDER_TMP') . 'cache.php');
 
 class Cache {
-    static function get() {
+    static private $cache = null;
+
+    static function get(): array {
         self::load();
-        return constant('MY_CACHED');        
+        return self::$cache;
     }
 
-    static function load() {
-        if (defined('MY_CACHED')) {
+    static function load(): bool {
+        if (self::$cache) {
             return false;
         }
 
         if (!file_exists(constant('MY_CACHE_FILE'))) {
-            self::generate();
+            return self::generate();
         }
 
-        require(constant('MY_CACHE_FILE'));
+        self::$cache = unserialize(file_get_contents(constant('MY_CACHE_FILE')));
+        // require(constant('MY_CACHE_FILE'));
+
+        return true;
     }
 
     // $thread = new \Thread();
-    static function generate() {
+    static function generate(): array {
         /*
         * Initialize
         */
@@ -35,32 +42,24 @@ class Cache {
         // // Remove previously generated file
         // unlink(constant('MY_CACHE_FILE'));
 
-        /*
-        * Various generators
-        */
-
-        global $databaseStructure;
-        require(__DIR__ . '/cache-generators/database-structure.php');
-
-        global $dataListings;
-        require(__DIR__ . '/cache-generators/lists.php');
-
-        /*
-        * Write it to file
-        */
-
         // Build up the generated variable
         $cached = [
             "generated_at" => date("Y-m-d H-i-s"),
-            "dataStructure" => $databaseStructure,
-            "dataListings" => $dataListings
+            "dataStructure" => Database::generateStructureData(),
+            "dataListings" => Lists::generateLists()
         ];
 
-        // Build up the exported text
-        $export = 'define("MY_CACHED", ' . var_export($cached, true) . ');';
-
-        // Write to file
-        file_put_contents(constant('MY_CACHE_FILE'), "<?php \n $export") 
+        /*
+         * Write it to file
+         */
+        file_put_contents(constant('MY_CACHE_FILE'), serialize($cached))
             || die("Could not save data to file " . constant('MY_CACHE_FILE'));
+        
+        return $cached;
     }
 } 
+
+
+// flock($myfile,LOCK_SH);
+// $read=file_get_contents('test.txt');
+// fclose($myfile);
--- www/api/v1.3/public/app/Lib/Database.php
+++ www/api/v1.3/public/app/Lib/Database.php
@@ -5,21 +5,134 @@ namespace Cryptomedic\Lib;
 use PDO;
 use function App\Cryptomedic\Lib\Cache;
 
-function getDefinitionFor($table, $field) {
+function getDefinitionForTable($table) {
     Cache::load();
-    
+
     if (!array_key_exists($table, Cache::get()['dataStructure'])) {
         return false;
     }
-    if (!array_key_exists($field, Cache::get()['dataStructure'][$table])) {
+
+    return Cache::get()['dataStructure'][$table];
+}
+
+function getDefinitionForField($table, $field) {
+    $tableDef = getDefinitionForTable($table);
+
+    if (!$tableDef) {
         return false;
     }
-    return Cache::get()['dataStructure'][$table][$field];
+
+    if (!array_key_exists($field, $tableDef)) {
+        return false;
+    }
+
+    return $tableDef[$field];
+}
+
+function parseColumn($sqlData) {
+    $name = $sqlData['COLUMN_NAME'];
+
+    $res = [
+        'protected'  => in_array($name, [ 'id', 'created_at', 'updated_at', 'lastuser'])
+    ];
+
+    if (!$res['protected']) {
+        $res['optional']   = ($sqlData['IS_NULLABLE'] == "YES");
+
+        if (!$res['optional']) {
+            // Only mandatory fields can be insertOnly (e.g. patient_id)
+            $res['insertOnly'] = (preg_match('/^.+_id$/', $name) > 0);
+        }
+    }
+
+    // if (array_key_exists("list", $options) && $options['list']) {
+    //     $res['type'] = Database::TYPE_LIST;
+    //     $this->listing = $options['list'];
+    // } else if (array_key_exists($header, References::$model_listing)) {
+    //     // Model.Field specific list
+    //       $res['type'] = Database::TYPE_LIST;
+    //       $this->listingName = References::$model_listing[$header];
+    //       $this->listing = References::getList($this->listingName);
+    // } else if (array_key_exists("*.{$this->field}", References::$model_listing)) {
+    //     // *.Field generic list
+    //       $res['type'] = Database::TYPE_LIST;
+    //       $this->listingName = References::$model_listing["*.{$this->field}"];
+    //       $this->listing = References::getList($this->listingName);
+    // } else {
+        $res['type'] = $sqlData['DATA_TYPE'];
+
+        // Special case:
+        switch($res['type']) {
+            case "date":
+                $res['type'] = Database::TYPE_DATE;
+                break;
+            case "float":
+            case "decimal":
+            case "tinyint":
+            case "int":
+                $length = $sqlData['NUMERIC_PRECISION'];
+                if ($length == 3) {
+                    $res['type'] = Database::TYPE_BOOLEAN;
+                } else {
+                    $res['type'] = Database::TYPE_INTEGER;
+                }
+                break;
+            case "enum":
+            case "longtext":
+            case "text":
+            case "char":
+            case "varchar":
+                $res['length'] = $sqlData['CHARACTER_MAXIMUM_LENGTH'];
+                if ($res['length'] >= 800) {
+                    // Long text = blob
+                    $res['type'] = Database::TYPE_TEXT;
+                } else {
+                    $res['type'] = Database::TYPE_CHAR;
+                }
+                break;
+            case "mediumtext":
+                $res['length'] = $sqlData['CHARACTER_MAXIMUM_LENGTH'];
+                $res['type'] = Database::TYPE_TEXT;
+                break;
+            case "timestamp":
+                $res['type'] = Database::TYPE_TIMESTAMP;
+                break;
+            case "longblob":
+                $res['length'] = $sqlData['CHARACTER_MAXIMUM_LENGTH'];
+                $res['type'] = Database::TYPE_BINARY;
+                break;
+            default:
+                var_dump($sqlData);
+                throw new \Exception("Unhandled type in __construct: {$res['type']}");
+                break;
+            
+        }
+    // }
+    return $res;
 }
 
 class Database {
-    static $pdoConnection = false;
+    const TYPE_LIST         = "list";
+    const TYPE_TIMESTAMP    = "timestamp";
+    const TYPE_BOOLEAN      = "boolean";
+    const TYPE_INTEGER      = "numeric";
+    const TYPE_CHAR         = "char";
+    const TYPE_TEXT         = "text";
+    const TYPE_DATE         = "date";
+    const TYPE_BINARY       = "binary";
 
+    static private $pdoConnection = false;
+
+    static function buildSetStatement(string $table, array $data) {
+        $data = array_intersect_key($data, getDefinitionForTable($table));
+    
+        $escapedData = array_map(function($value, $key) { return "$key = " . self::$pdoConnection->quote($value); }, $data, array_keys($data));
+    
+        $sql = implode(", ", $escapedData);
+    
+        return $sql;
+    }
+    
     static function init() {
         if (Database::$pdoConnection !== false) {
             return;
@@ -48,27 +161,47 @@ class Database {
         return $res;
     }
 
-    static function insert(string $table, array $data, bool $onDuplicateKeyUpdate = false): bool {
+    static function insert(string $table, array $data): bool {
         self::init();
 
-        global $cture;
-        $validFields = array_filter(array_keys($data),
-            function ($key) use ($table) { 
-                $desc = getDefinitionFor($table, $key);
-                if ($desc === false) {
-                    return false;
-                }
-                return !$desc['protected'];
-            }        
-        );
+        $validData = [];
+        foreach($data as $key => $value) {
+            if (!getDefinitionForField($table, $key)) {
+                continue;
+            }
+            if (getDefinitionForField($table, $key)['protected']) {
+                continue;
+            }
+            $validData[$key] = $value;
+        }
+
+        $sql = self::buildSetStatement($table, $validData);
 
         // INSERT INTO table (id, name, age) VALUES(1, "A", 19) ON DUPLICATE KEY UPDATE name="A", age=19
-        $sql = "INSERT INTO $table ";
-
-        var_dump($sql, $validFields);
+        $res = self::exec("INSERT INTO $table SET $sql");
+        return ($res == 1);
     }
 
     static function update(string $table, object $data): bool {
         self::init();
+    }
+
+    static function generateStructureData(): array {
+        global $myconfig;
+
+        $databaseStructure = [];
+
+        foreach(Database::select("SELECT * FROM INFORMATION_SCHEMA.COLUMNS WHERE table_schema = '{$myconfig["database"]["schema"]}'") as $row) {
+            $sqlTable = $row['TABLE_NAME'];
+            $sqlField = $row['COLUMN_NAME'];
+
+            if (!array_key_exists($sqlTable, $databaseStructure)) {
+                $databaseStructure[$sqlTable] = [];
+            }
+
+            $databaseStructure[$sqlTable][$sqlField] = parseColumn($row);
+        }
+
+        return $databaseStructure;
     }
 }
