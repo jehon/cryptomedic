0665a1a345f4847fd5db37d754e4641307875125
Jean Honlet <jehon@users.noreply.github.com>
2020-09-20 15:59:19 +0200

    feature: use bare libs in laravel side

--- www/api/v1.3/public/app/bootstrap.php
+++ www/api/v1.3/public/app/bootstrap.php
@@ -12,7 +12,7 @@
 |
 */
 
-require __DIR__.'/../vendor/autoload.php';
+require_once(__DIR__.'/../vendor/autoload.php');
 
 global $myconfig;
 
--- www/api/v1.3/public/index.php
+++ www/api/v1.3/public/index.php
@@ -19,7 +19,7 @@
 |
 */
 
-require __DIR__.'/../bootstrap/autoload.php';
+require __DIR__ . '/../bootstrap/autoload.php';
 
 /*
 |--------------------------------------------------------------------------
@@ -33,7 +33,15 @@ require __DIR__.'/../bootstrap/autoload.php';
 |
 */
 
-$app = require_once __DIR__.'/../bootstrap/app.php';
+$app = require_once __DIR__ . '/../bootstrap/app.php';
+
+/*
+|--------------------------------------------------------------------------
+| Import my framework
+|--------------------------------------------------------------------------
+*/
+
+require_once(__DIR__ . "/app/bootstrap.php");
 
 /*
 |--------------------------------------------------------------------------
