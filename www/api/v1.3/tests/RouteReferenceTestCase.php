038c4bb6f52c8380525e0bcac85f9b0065e82d3c
Jean Honlet <jehon@users.noreply.github.com>
2021-01-10 20:07:51 +0100

    style: clean up

--- www/api/v1.3/tests/RouteReferenceTestCase.php
+++ www/api/v1.3/tests/RouteReferenceTestCase.php
@@ -13,7 +13,7 @@ class RouteReferenceTestCase extends TestCase {
 	private $params = array();
 
 	protected static function absoluteUrl($relativeUrl) {
-	  return "/api/" . basename(dirname(dirname(__FILE__))). "/" . $relativeUrl;
+		return "/api/" . basename(dirname(dirname(__FILE__))) . "/" . $relativeUrl;
 	}
 
 	public function getNewRequestOptionsBuilder() {
@@ -25,15 +25,15 @@ class RouteReferenceTestCase extends TestCase {
 			$opt = $this->getNewRequestOptionsBuilder();
 		}
 		if ($opt->getRole() !== false) {
-			
+
 			// DUPLICATE TEST USER...
 			if ($opt->getUsername()) {
-				$user = new User(['name' => 'test', 'group' => $opt->getRole(), 'username' => $opt->getUsername() ]);	
+				$user = new User(['name' => 'test', 'group' => $opt->getRole(), 'username' => $opt->getUsername()]);
 			} else {
-				$user = new User(['name' => 'test', 'group' => $opt->getRole() ]);
+				$user = new User(['name' => 'test', 'group' => $opt->getRole()]);
 			}
 
-			
+
 			$this->actingAs($user);
 		} else {
 			// $this->be(null);
@@ -66,18 +66,19 @@ class RouteReferenceTestCase extends TestCase {
 		return $json;
 	}
 
-	public function myRunAssertQueryForRoles($opt, $rolesList = [ "readonly", "cdc", "manager", "admin" ], $asUnauthenticated = true) {
+	public function myRunAssertQueryForRoles($opt, $rolesList = ["readonly", "cdc", "manager", "admin"], $asUnauthenticated = true) {
 		$json = null;
 
 		if ($asUnauthenticated) {
-			$this->myRunAssertQuery($opt->clone()
+			$this->myRunAssertQuery(
+				$opt->clone()
 					->asUnauthenticated()
 					->setExpected(401)
 					->asText()
-					);
+			);
 		}
 
-		foreach($rolesList as $r) {
+		foreach ($rolesList as $r) {
 			$json = $this->myRunAssertQuery($opt->clone()->setRole($r));
 		}
 		return $json;
@@ -89,15 +90,16 @@ class RouteReferenceTestCase extends TestCase {
 			preg_replace(
 				'/"(created_at|updated_at)":"[0-9\- :]+"/',
 				'"$1":"<timestamp>"',
-				json_encode($json))
-			);
+				json_encode($json)
+			)
+		);
 
-	 	$jsonPP = json_encode($jsonFiltered, JSON_PRETTY_PRINT);
+		$jsonPP = json_encode($jsonFiltered, JSON_PRETTY_PRINT);
 
 		/* Calculate the reference file */
- 		if ($file === null) {
+		if ($file === null) {
 			$st = debug_backtrace(DEBUG_BACKTRACE_IGNORE_ARGS);
-			while($sti = array_shift($st)) {
+			while ($sti = array_shift($st)) {
 				if ($sti['file'] == __FILE__) {
 					continue;
 				}
@@ -107,23 +109,23 @@ class RouteReferenceTestCase extends TestCase {
 				break;
 			}
 			$sti = array_shift($st); // Go to the previous call, where we have the class.method that is interresting for us
- 			$file = get_called_class() . '.' . $sti['function'] . '.json';
- 		} else {
- 			$file = $file . ".json";
- 		}
- 		$pfile = __DIR__  . "/references/" . $file;
+			$file = get_called_class() . '.' . $sti['function'] . '.json';
+		} else {
+			$file = $file . ".json";
+		}
+		$pfile = __DIR__  . "/references/" . $file;
 
- 		/* Assert or update the reference */
+		/* Assert or update the reference */
 		if (getenv("COMMIT") > 0) {
-		 	/* Read it and load $reference */
-	 		if (file_exists($pfile)) {
-	 			$reference = file_get_contents($pfile);
-	 		} else {
-	 			$reference = "";
-	 		}
+			/* Read it and load $reference */
+			if (file_exists($pfile)) {
+				$reference = file_get_contents($pfile);
+			} else {
+				$reference = "";
+			}
 
 			/* Commit to the file */
-	 		if (strcmp($reference, $jsonPP) != 0) {
+			if (strcmp($reference, $jsonPP) != 0) {
 				file_put_contents($pfile, $jsonPP);
 				echo "[$file updated]";
 			}
