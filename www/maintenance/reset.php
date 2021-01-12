f0a01fd115a94aa984626b815018ce5090a6ebc7
Jean Honlet <jehon@users.noreply.github.com>
2021-01-12 11:55:03 +0100

    fix: reset

---
+++ www/maintenance/reset.php
@@ -0,0 +1,39 @@
+<?php
+
+require_once(__DIR__ . "/../../config.php");
+
+global $myconfig;
+
+if (!$myconfig['security']['key']) {
+    http_response_code(500);
+    die("No security.admin configured");
+}
+
+if ($_REQUEST['pwd'] != $myconfig['security']['key']) {
+    http_response_code(500);
+    die("No correct pwd given");
+}
+
+function deleteFileFromGlob($file) {
+    $files = glob($file);
+    foreach ($files as $file) {
+        if (is_file($file)) {
+            echo "Removing $file\n";
+            unlink($file);
+        }
+    }
+}
+
+try {
+    http_response_code(500);
+    echo "\nRunning\n";
+
+    deleteFileFromGlob(__DIR__ . "/../api/*/bootstrap/cache/*");
+    deleteFileFromGlob(__DIR__ . "/../api/*/storage/framework/cache/cache.php");
+
+    echo "\n\nDone\n";
+    http_response_code(200);
+} catch (Exception $e) {
+    echo "Failed: " . $e->getMessage();
+    http_response_code(500);
+}
