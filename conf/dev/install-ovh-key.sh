619c653fc9b307a5d7aee5b0807478d78a1790bc
Jean Honlet <jehon@users.noreply.github.com>
2021-01-18 14:09:16 +0100

    refactor: use dev container as proxy

---
+++ conf/dev/install-ovh-key.sh
@@ -0,0 +1,29 @@
+#!/usr/bin/env bash
+
+set -e
+
+mkdir -p ~/.ssh
+
+DEPLOY_HOST=ftp.cluster003.ovh.net
+SSH_KNOWN_HOSTS=~/.ssh/known_hosts
+
+REMOTE="$( ssh-keyscan -t ssh-rsa "$DEPLOY_HOST" 2>/dev/null )";
+STORED="$( cat /setup/ovh.key )";
+
+if [ "$1" == "update" ]; then
+    ssh-keyscan -t ssh-rsa "$DEPLOY_HOST" > ovh.key
+fi
+
+if [ "$REMOTE" != "$STORED" ]; then \
+    echo "Key is updated on remote host"; \
+
+    mkdir -p ~/.ssh/
+    if grep "$DEPLOY_HOST" $SSH_KNOWN_HOSTS >/dev/null; then \
+        echo "Removing old key"; \
+        sed -i "/$(DEPLOY_HOST).*/d" $SSH_KNOWN_HOSTS ; \
+    fi
+    echo "Installing the key"
+    cat ovh.key >> $SSH_KNOWN_HOSTS
+else  \
+    echo "Key is still the same, good!"; \
+fi
---
+++ conf/dev/install-php.sh
@@ -0,0 +1,25 @@
+#!/usr/bin/env bash
+
+set -e
+
+mkdir -p /setup/tmp
+mkdir -p /setup/composer
+
+# PHP: https://www.tecmint.com/install-different-php-versions-in-ubuntu/
+
+add-apt-repository --yes ppa:ondrej/php
+apt-get install php7.3 \
+    php7.3-xdebug php7.3-curl php7.3-gd php7.3-intl php7.3-json php7.3-mbstring php7.3-xml php7.3-zip
+
+update-alternatives --set php /usr/bin/php7.3
+
+# Note: to specify composer version: | php -- --version 1.9.3
+curl https://getcomposer.org/installer \
+    --location --silent --output /setup/tmp/get-composer.sh
+
+chmod +x /setup/tmp/get-composer.sh
+
+cd /setup/composer || exit 1
+php /setup/tmp/get-composer.sh
+
+ln -s /setup/composer/composer.phar /usr/local/bin/composer
