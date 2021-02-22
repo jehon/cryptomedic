e45c8c58f5fe7bf7c9beedfabd026fa01c9e241f
Jean Honlet <jehon@users.noreply.github.com>
2021-02-22 18:00:00 +0100

    feat: add XDisplayDate

---
+++ app/elements/render/x-display-date.js
@@ -0,0 +1,19 @@
+import { defineCustomElement } from '../../js/custom-element.js';
+import date2Display from '../../js/date2Display.js';
+
+
+export default class XDisplayDate extends HTMLElement {
+    static get observedAttributes() {
+        return ['value'];
+    }
+
+    attributeChangedCallback(attributeName, _oldValue, newValue) {
+        switch (attributeName) {
+            case 'value':
+                this.innerHTML = date2Display(newValue);
+                break;
+        }
+    }
+}
+
+defineCustomElement(XDisplayDate);
