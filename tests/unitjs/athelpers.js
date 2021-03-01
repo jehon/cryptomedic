59845788f423c10b101407b584a441129dcaefbc
Jean Honlet <jehon@users.noreply.github.com>
2021-03-01 12:05:41 +0100

    chore: clean up

--- tests/unitjs/athelpers.js
+++ tests/unitjs/athelpers.js
@@ -41,18 +41,10 @@ export function webDescribe(title, options, fn) {
 /**
  * Count the number of included files
  */
-// let fnCnt = 0;
 /**
  * @param {string} url - import.meta.url
  * @returns {string} the filename as a test title
  */
 export function fn(url) {
-    // fnCnt++;
     return new URL(url).pathname.split('/').pop();
 }
-
-// afterAll(function () {
-//     expect(fnCnt)
-//         .withContext('#FILE_NUMBER# Number of files in athelper.js')
-//         .toBe(39);
-// });
