7c4929b75a191fbc04d923df847f51fc6e48ab6f
Jean Honlet <jehon@users.noreply.github.com>
2021-03-26 19:00:33 +0100

    test: enrich commands

---
+++ tests/cypress/support/commands.js
@@ -0,0 +1,48 @@
+
+//
+// https://docs.cypress.io/api/commands/screenshot#Arguments
+//
+Cypress.Commands.add('crReady',
+    () =>
+        // call another command, no need to return as it is managed
+        cy
+            .get('x-requestor')
+            .should('not.have.attr', 'running')
+);
+
+//
+// https://docs.cypress.io/api/commands/request
+//
+Cypress.Commands.add('crApi',
+    /**
+     * @param {object} options
+     * @property {string} url of the call
+     * @property {string?} method of the call
+     * @returns {*} to chain
+     */
+    (options = {}) =>
+        cy
+            .request({
+                ...options,
+                url: options.url[0] == '/' ? options.url : `/api/v1.3/${options.url}`
+            })
+);
+
+Cypress.Commands.add('crLogin',
+    (username, password = null) => {
+        cy.crApi({
+            url: 'auth/mylogin', method: 'POST', body: {
+                username: username ?? 'murshed',
+                password: password ?? 'p'
+            }
+        });
+
+        cy.visit('/build/');
+        cy.hash().should('routeStartsWith', '/home');
+        return cy.get('x-login-status #user').should('have.text', username);
+    }
+);
+
+Cypress.Commands.add('crGo',
+    (route) => cy.visit(`/build/#${route}`)
+);
