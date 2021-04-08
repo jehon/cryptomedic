62377427c6221f2738b67a0bf44f50a5c283f208
Jean Honlet <jehon@users.noreply.github.com>
2021-04-08 11:43:03 +0200

    style: add css browser support

--- .stylelintrc.js
+++ .stylelintrc.js
@@ -1,10 +1,14 @@
 module.exports = {
   "extends": "stylelint-config-standard",
   "ignoreFiles": ["www/api/**/*"],
+  "plugins": [
+    "stylelint-no-unsupported-browser-features"
+  ],
   "rules": {
     "indentation": "tab",
     "selector-type-no-unknown": [true, { "ignore": "custom-elements" }],
     "no-descending-specificity": null,
-    "declaration-empty-line-before": null
+    "declaration-empty-line-before": null,
+    "plugin/no-unsupported-browser-features": true
   }
 }
