566511c0f7ff412ec73ce4e2e8f1f556119b6fbf
Jean Honlet <jehon@users.noreply.github.com>
2021-04-21 12:01:32 +0200

    test(e2e): migrate readpatient to cypress

---
+++ tests/cypress/integration/patient-read.spec.js
@@ -0,0 +1,102 @@
+/// <reference types="Cypress" />
+
+import { goPatient, selectFile } from '../helpers/patients.js';
+import TableIterator from '../helpers/table-iterator.js';
+
+context('Actions', () => {
+    beforeEach(() => {
+        cy.crLogin('readonly');
+    });
+
+    it('read patient 2000-1', () => { // id: 1
+        goPatient(2000, 1);
+        cy.get('#Patient_Name').should('contain.text', 'rezaul islam');
+        cy.get('#Patient_Sex').should('contain.text', 'Male');
+        cy.get('#Patient_District').should('contain.text', 'Chittagong');
+        cy.get('#Patient_Pathology').should('contain.text', 'ClubFoot');
+        cy.get('#button_edit').should('not.exist');
+
+        cy.crCompareSnapshot('patient_2000_1');
+
+        cy.get('#summary').click();
+        new TableIterator('#table_summary', { xtable: false })
+            // Summary Bill#1
+            .row(5).assert('#3')
+            .col(4).assert('Sociallevel');
+
+        cy.crCompareSnapshot('patient_2000_1_summary');
+
+        selectFile('OtherConsult', 1);
+        cy.get('#Date').should('contain.text', '2007-01-10');
+        cy.get('#ExaminerName').should('contain.text', 'Ershad');
+
+        cy.get('x-ff-patient-related').should('exist');
+        cy.get('x-ff-patient-related[patient-entry-number="2000-1"]').should('exist');
+
+        cy.get('x-fff-weight-sd').should('contain.text', '0 ds');
+        cy.get('x-fff-height-sd').should('contain.text', '0 ds');
+
+        cy.get('x-fff-wh').should('contain.text', '0.22');
+        cy.get('x-fff-wh-sd').should('contain.text', 'Some data is out-of-bounds');
+
+        cy.get('x-fff-bmi').should('contain.text', '16.2');
+        cy.get('x-fff-bmi-sd').should('contain.text', '0 ds');
+
+        cy.get('#button_edit').should('not.exist');
+        cy.crCompareSnapshot('patient_2000_1_otherconsult');
+
+        selectFile('Picture', 2);
+        cy.get('#Picture_Date').should('contain.text', '2014-11-04');
+        cy.get('#Picture_file').should('contain.text', '10_2014-11-06_15-32-45.JPG');
+        cy.get('#img_file').should('be.visible');
+        cy.get('#button_edit').should('not.exist');
+        cy.crCompareSnapshot('patient_2000_1_picture');
+
+        selectFile('Bill', 1);
+        cy.get('#button_edit').should('not.exist');
+        cy.get('#Bill_Date').should('contain.text', '2011-06-09');
+        // TODO: check bill
+        cy.crCompareSnapshot('patient_2000_1_bill');
+
+        selectFile('Surgery', 5);
+        cy.get('#Surgery_ReportDiagnostic').should('contain.text', 'test');
+        cy.get('#Surgery_FollowUpComplication').should('contain.text', 'nothing');
+        cy.crCompareSnapshot('patient_2000_1_surgery');
+    });
+
+    it('read patient 2014-105', () => { // id: 5
+        goPatient(2014, 105);
+        cy.get('#Patient_Upazilla').should('contain.text', 'Ukhia');
+        cy.get('#Patient_Union_').should('contain.text', 'Jalia palong');
+        cy.get('#Patient_Telephone').should('contain.text', '1813247984');
+
+        selectFile('ClubFoot', 1);
+        cy.get('#ageAtConsultationTime').should('be.visible');
+        cy.get('#ageAtConsultationTime').should('contain.text', '2y0m');
+        cy.get('#ClubFoot_Treatment').should('contain.text', 'DB splint');
+        // TODO: adapt the data and check them
+        cy.get('#button_edit').should('not.exist');
+        cy.crCompareSnapshot('patient_2014_105_clubfoot');
+    });
+
+    it('read patient 2001-1', () => { // id: 6
+        goPatient(2001, 1);
+        selectFile('RicketConsult', 3);
+        // TODO: adapt the data and check them
+        cy.get('#button_edit').should('not.exist');
+        cy.get('#Date').should('contain.text', '2004-10-06');
+        cy.crCompareSnapshot('patient_2001_1_ricketconsult');
+    });
+
+    it('read patient 2014-103', () => { // id: 3
+        goPatient(2014, 103);
+        selectFile('Bill', 2);
+        new TableIterator('#paymentsList', { xtable: false })
+            .col(2).assert('Murshed')
+            .nextCol().assert(15)
+            .row(2)
+            .col(2).assert('Ershad')
+            .nextCol().assert(10);
+        cy.crCompareSnapshot('patient_2014_103_bill');
+    });
+});
