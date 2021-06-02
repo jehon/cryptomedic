a91053a0611a8bf385d56944a1ce606269e770dd
Jean Honlet <jehon@users.noreply.github.com>
2021-06-02 18:00:00 +0200

    refactor: use symfony error handling

--- www/api/v1.3/public/app/bootstrap.php
+++ www/api/v1.3/public/app/bootstrap.php
@@ -6,12 +6,13 @@
  * It should not be included by the Laravel part -> see shared for that.
  */
 
-require_once(__DIR__ . '/Lib/ErrorHandler.php');
-
 require_once(__DIR__ . '/shared.php');
 
-global $myconfig;
+use Symfony\Component\ErrorHandler\Debug;
+
+Debug::enable();
 
 date_default_timezone_set("GMT");
 
+global $myconfig;
 require_once(__DIR__ . '/../../../../../config.php');
