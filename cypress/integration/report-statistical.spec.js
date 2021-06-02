c798331bf8e985897424f29abd3eb384344e3d20
Jean Honlet <jehon@users.noreply.github.com>
2021-06-02 16:30:50 +0200

    build: skip some on mobile mode

--- cypress/integration/report-statistical.spec.js
+++ cypress/integration/report-statistical.spec.js
@@ -1,17 +1,20 @@
 /// <reference types="Cypress" />
 
+import configFilter from '../helpers/filter-tests.js';
 import { goReport } from '../helpers/report.js';
 
-context('Actions', () => {
-    it('should show report', () => {
-        goReport('statistical',
-            () => {
-                cy.get('[label="Period"] > x-input-list').invoke('attr', 'value', 'month');
-                cy.get('input[name="month"]').clear().type('2014-05');
-            },
-            tableIterator => tableIterator
-                .row(8).col(1).assert('Club Foots')
-                .nextCol().assert('5')
-        );
-    });
-});
+configFilter(configFilter.DESKTOP, () =>
+    context('Actions', () => {
+        it('should show report', () => {
+            goReport('statistical',
+                () => {
+                    cy.get('[label="Period"] > x-input-list').invoke('attr', 'value', 'month');
+                    cy.get('input[name="month"]').clear().type('2014-05');
+                },
+                tableIterator => tableIterator
+                    .row(8).col(1).assert('Club Foots')
+                    .nextCol().assert('5')
+            );
+        });
+    })
+);
--- cypress/integration/report-surgical.spec.js
+++ cypress/integration/report-surgical.spec.js
@@ -1,17 +1,20 @@
 /// <reference types="Cypress" />
 
+import configFilter from '../helpers/filter-tests.js';
 import { goReport } from '../helpers/report.js';
 
-context('Actions', () => {
-    it('should show report', () => {
-        goReport('surgical',
-            () => {
-                cy.get('[label="Period"] > x-input-list').invoke('attr', 'value', 'month');
-                cy.get('input[name="month"]').clear().type('2014-01');
-            },
-            tableIterator => tableIterator
-                .col(4).assert('Ukhia')
-                .nextCol().assert('2014-104')
-        );
-    });
-});
+configFilter(configFilter.DESKTOP, () =>
+    context('Actions', () => {
+        it('should show report', () => {
+            goReport('surgical',
+                () => {
+                    cy.get('[label="Period"] > x-input-list').invoke('attr', 'value', 'month');
+                    cy.get('input[name="month"]').clear().type('2014-01');
+                },
+                tableIterator => tableIterator
+                    .col(4).assert('Ukhia')
+                    .nextCol().assert('2014-104')
+            );
+        });
+    })
+);
