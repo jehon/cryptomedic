82b32414bf5af56b49f8857d057d3e5ab93af0bc
Jean Honlet <jehon@users.noreply.github.com>
2021-06-08 11:55:23 +0200

    fix: imports

--- app/main.js
+++ app/main.js
@@ -1,6 +1,12 @@
 /* istanbul ignore file */
 
+// Styles
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './css/application.css';
 
+// Elements present on the index.html:
+import '../widgets/x-user-status.js';
+import '../widgets/func/x-restricted.js';
+
+// Application
 import './controllers/mainApp.js';
