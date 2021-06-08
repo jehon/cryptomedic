b8411f0d6cc9081448d04691197747d35068cd5d
Jean Honlet <jehon@users.noreply.github.com>
2021-06-08 18:00:00 +0200

    fix: imports

--- app/main.js
+++ app/main.js
@@ -5,8 +5,8 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import './css/application.css';
 
 // Elements present on the index.html:
-import '../widgets/x-user-status.js';
-import '../widgets/func/x-restricted.js';
+import './widgets/x-user-status.js';
+import './widgets/func/x-restricted.js';
 
 // Application
 import './controllers/mainApp.js';
