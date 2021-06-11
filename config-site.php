09257ca67adba25733fe7932e7549a02a120c898
Jean Honlet <jehon@users.noreply.github.com>
2021-06-11 18:00:00 +0200

    refactor: cr-deploy-patch

--- config-site.php
+++ config-site.php
@@ -5,11 +5,5 @@ global $myconfig;
 $myconfig['dev'] = true;
 $myconfig['debug'] = true;
 
-
-if (array_key_exists("APP_ENV", $_ENV) && $_ENV["APP_ENV"] == "testing") {
-    // Phpunit testing
-    $myconfig['environment'] = 'phpunit';
-}
-
 // // this is an artificial waiting time
 // sleep(3);
