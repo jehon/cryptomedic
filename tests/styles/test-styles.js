427fe4dd665296f3ec2099cedfd878046c663c35
Jean Honlet <jehon@users.noreply.github.com>
2021-11-12 18:00:00 +0100

    refactor: styles calculated server side

--- tests/styles/test-styles.js
+++ tests/styles/test-styles.js
@@ -5,6 +5,8 @@ const path = require('path');
 const glob = require('glob');
 const resemble = require('node-resemble-js');
 const chalk = require('chalk');
+const pixelMatch = require('pixelmatch');
+const PNG = require('pngjs').PNG;
 
 const p_ok = chalk.green(' ✓ ');
 const p_warn = chalk.yellow(' ? ');
@@ -12,9 +14,19 @@ const p_ko = chalk.red('✗  ');
 
 const projectRoot = path.dirname(path.dirname(__dirname));
 const stylesRoot = path.join(projectRoot, 'tmp', 'styles');
-const referenceFolder = path.join(stylesRoot, 'references');
-const runFolder = path.join(stylesRoot, 'run');
+const referenceFolder = 'references';
+const runFolder = 'run';
+const diffFolder = 'diff';
 const referenceUpdateFolder = path.join(projectRoot, 'tests', 'styles', 'references');
+
+function inStyles(...folder) {
+    return path.join(stylesRoot, ...folder);
+}
+
+fs.mkdirSync(stylesRoot, { recursive: true });
+fs.mkdirSync(inStyles(diffFolder), { recursive: true });
+fs.mkdirSync(inStyles(diffFolder, 'desktop'), { recursive: true });
+fs.mkdirSync(inStyles(diffFolder, 'mobile'), { recursive: true });
 
 const opts = require('yargs/yargs')(process.argv.slice(2))
     .option('update', {
@@ -34,16 +46,14 @@ const MaxDiffs = {
 (async function () {
     const listOfFiles = [];
     // Add the ref
-    glob.sync('**/*.png', { cwd: referenceFolder }).map(f => listOfFiles.push({
+    glob.sync('**/*.png', { cwd: inStyles(referenceFolder) }).map(f => listOfFiles.push({
         key: f,
-        name: path.basename(f),
         ref: path.join(referenceFolder, f)
     }));
 
     // Add the run
-    glob.sync('**/*.png', { cwd: runFolder }).map(f => listOfFiles.push({
+    glob.sync('**/*.png', { cwd: inStyles(runFolder) }).map(f => listOfFiles.push({
         key: f,
-        name: path.basename(f),
         run: path.join(runFolder, f)
     }));
 
@@ -76,8 +86,8 @@ const MaxDiffs = {
             continue;
         }
         // Calculate problems for each files
-        await new Promise((resolve) => resemble(fset.run)
-            .compareTo(fset.ref)
+        await new Promise((resolve) => resemble(inStyles(fset.run))
+            .compareTo(inStyles(fset.ref))
             .onComplete(function (data) {
                 fset.compared = true;
                 const diffContent = parseFloat(data.misMatchPercentage);
@@ -86,22 +96,39 @@ const MaxDiffs = {
                 console.assert(isFinite(diffContent));
                 console.assert(isFinite(diffSize));
 
-                if (diffSize > 0) {
-                    fset.diffSize = diffSize;
-                    if (diffSize > MaxDiffs.size) {
-                        fset.problem = true;
-                        fset.problemText = `size    - ${diffSize} vs. ${MaxDiffs.size}`;
+                if (diffSize == 0 && diffContent == 0) {
+                    fs.unlinkSync(inStyles(fset.ref));
+                    // fs.unlinkSync(inStyles(fset.run));
+                } else {
+                    if (diffSize > 0) {
+                        fset.diffSize = diffSize;
+                        if (diffSize > MaxDiffs.size) {
+                            fset.problem = true;
+                            fset.problemText = `size    - ${diffSize} vs. ${MaxDiffs.size}`;
+                        }
+                    } else if (diffContent > 0) {
+                        fset.diffContent = diffContent;
+                        if (diffContent > MaxDiffs.content) {
+                            fset.problem = true;
+                            fset.problemText = `content - ${diffContent} vs. ${MaxDiffs.content}`;
+                        } else {
+                            fset.warning = true;
+                            fset.warningText = `content - ${diffContent}`;
+                        }
                     }
-                } else if (diffContent > 0) {
-                    fset.diffContent = diffContent;
-                    if (diffContent > MaxDiffs.content) {
-                        fset.problem = true;
-                        fset.problemText = `content - ${diffContent} vs. ${MaxDiffs.content}`;
-                    } else {
-                        fset.warning = true;
-                        fset.warningText = `content - ${diffContent}`;
-                    }
+
+                    // Generate the diffs
+                    fset.diff = path.join(diffFolder, fset.key);
+                    const ref = PNG.sync.read(fs.readFileSync(inStyles(fset.ref)));
+                    const run = PNG.sync.read(fs.readFileSync(inStyles(fset.run)));
+                    const { width, height } = ref;
+                    const diff = new PNG({ width, height });
+
+                    pixelMatch(ref.data, run.data, diff.data, width, height, { threshold: 0.1 });
+
+                    fs.writeFileSync(inStyles(fset.diff), PNG.sync.write(diff));
                 }
+
                 scanned++;
                 if (process.stdout.clearLine) {
                     process.stdout.clearLine(0);
@@ -117,27 +144,19 @@ const MaxDiffs = {
     }
 
     const problemsList = uniqueFiles
-        .filter(fset => fset.problem || fset.warning)
         .map(fset => {
-            if (fset.run) {
-                fset.runURL = path.relative(stylesRoot, fset.run);
-            }
-            if (fset.ref) {
-                fset.refURL = path.relative(stylesRoot, fset.ref);
+            if (fset.problem || fset.warning) {
+                if (fset.problem) {
+                    console.error(`${p_ko}: ${fset.key} ${fset.problemText}`);
+                } else {
+                    console.warn(`${p_warn}: ${fset.key} ${fset.warningText}`);
+                }
+            } else {
+                console.info(`${p_ok}: ${fset.key}`);
             }
             return fset;
-        });
-
-    // Show the problem list
-    for (const fset of uniqueFiles) {
-        if (fset.problem) {
-            console.error(`${p_ko}: ${fset.key} ${fset.problemText}`);
-        } else if (fset.warning) {
-            console.warn(`${p_warn}: ${fset.key} ${fset.warningText}`);
-        } else {
-            console.info(`${p_ok}: ${fset.key}`);
-        }
-    }
+        })
+        .filter(fset => fset.problem || fset.warning);
 
     console.info('---------------------');
 
@@ -161,7 +180,6 @@ const MaxDiffs = {
             process.exit(1);
         }
     } else {
-        fs.mkdirSync(stylesRoot, { recursive: true });
         fs.writeFileSync(path.join(stylesRoot, 'styles-problems-list.js'), 'problemsList = ' + JSON.stringify(problemsList, null, 2));
         if (problemsList.filter(fset => fset.problem).length > 0) {
             console.error(p_ko, 'some tests did fail');
