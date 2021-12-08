07b1e0b8cdee1d79628a1aa9b9f0b16a20abde57
Jean Honlet <jehon@users.noreply.github.com>
2021-12-08 18:00:00 +0100

    chore: try to remove caret with no success

--- cypress/support/compare-snapshot.js
+++ cypress/support/compare-snapshot.js
@@ -26,6 +26,19 @@ Cypress.Commands.add('crCompareSnapshot',
     (name = '') => {
         crReady();
 
+        // //
+        // // TODO: Remove caret while screenshot
+        // //
+
+        // // Escape within (https://docs.cypress.io/api/commands/within#Temporarily-escape)
+        // cy.root().closest('html').then($body => {
+        //     if ($body.find('input#no-caret').length == 0) {
+        //         // Add an input and set the focus on it
+        //         document.body.innerHTML += '<input id="no-caret" style="position: absolute; left: -1000px; top: 0px;">';
+        //     }
+        //     $body.find('input#no-caret').first().focus();
+        // });
+
         // TODO: clean-up this wait, but don't know how to do that otherwise
         cy.wait(1000); /* eslint-disable-line */
 
