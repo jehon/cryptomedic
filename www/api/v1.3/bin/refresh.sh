980ee38e9701c4b4a53f82452e3522ca9d440ff3
Jean Honlet <jehon@users.noreply.github.com>
2022-01-18 15:42:14 +0100

    chore: use CR_HTTP_HOST

--- www/api/v1.3/bin/refresh.sh
+++ www/api/v1.3/bin/refresh.sh
@@ -5,7 +5,7 @@ set -o errexit
 SCRIPT_DIR="$(dirname "${BASH_SOURCE[0]}")"
 
 # shellcheck source=/dev/null
-. "$SCRIPT_DIR/cr-lib"
+. "$SCRIPT_DIR/../../../../bin/cr-lib"
 
 # Current api dynamically calculated
 API="$(basename "$(dirname "$SCRIPT_DIR")")"
