36dcf09ede845cdf516fdaaffbd97cc5aa20ba46
Jean Honlet <jehon@users.noreply.github.com>
2022-01-19 16:52:15 +0100

    refactor: refresh

--- www/api/v1.3/bin/refresh.sh
+++ www/api/v1.3/bin/refresh.sh
@@ -5,4 +5,7 @@ set -o errexit
 # shellcheck source=../../../../bin/cr-lib
 . "$(dirname "${BASH_SOURCE[0]}")"/../../../../bin/cr-lib
 
-curl -fsSL "http://${CR_HTTP_HOST}/api/$CR_API_VERSION/routes/struct/data.php?force=1&quiet=true"
+SWD="$(dirname "${BASH_SOURCE[0]}")"
+V="$(basename "$(dirname "$SWD")")"
+
+curl -fsSL "http://${CR_HTTP_HOST}/api/$V/routes/struct/data.php?force=1&quiet=true"
