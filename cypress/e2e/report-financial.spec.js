1ce57512cb961356fe2fd7252660f6b5115f9f6f
Jean Honlet <jehon@users.noreply.github.com>
2022-12-21 11:09:49 +0100

    test(e2): the financial report

--- cypress/e2e/report-financial.spec.js
+++ cypress/e2e/report-financial.spec.js
@@ -12,7 +12,14 @@ flavorFilter(flavorFilter.DESKTOP, () =>
                     cy.get('input[name="year"]').clear().type('2014');
                 },
                 (tableIterator) => tableIterator
-                    .assert('1')
+                    .row(1).col(1).assert('2014-103')
+                    .nextCol().assert('OSMAN')
+                    .nextCol().assert('5')
+
+                    .section('tfoot')
+                    .col(4).assert(4)
+                    .nextCol(3).assert(2)
+                    .nextCol().assert(1000)
             );
         });
     })
