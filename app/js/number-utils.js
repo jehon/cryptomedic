5387dac98b6ac56f5ef15db84897e5ac586adc9e
Jean Honlet <jehon@users.noreply.github.com>
2022-12-22 16:32:22 +0100

    feat: add getFloatFrom

--- app/js/number-utils.js
+++ app/js/number-utils.js
@@ -8,3 +8,20 @@ export function nround(nbr, n) {
     const d = Math.pow(10, n);
     return Math.round(nbr * d) / d;
 }
+
+/**
+ * Parse a string
+ *
+ * @param {string|number} s to be parsed
+ * @returns {number} parsed
+ */
+export function getFloatFrom(s) {
+    if (s instanceof Number) {
+        return s;
+    }
+    const f = parseFloat(s);
+    if (isNaN(f)) {
+        return 0;
+    }
+    return f;
+}
