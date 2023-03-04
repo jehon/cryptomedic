ec57ae61dd815a901382f35379d2737fd66cf6c6
Jean Honlet <jehon@users.noreply.github.com>
2023-03-04 13:08:04 +0100

    ci: setup ovh key

--- .devcontainer/root/setup.sh
+++ .devcontainer/root/setup.sh
@@ -11,3 +11,7 @@ export DEBIAN_FRONTEND=noninteractive
 apt update
 
 apt install -y openssh-client sshpass curl coreutils diffutils
+
+mkdir -p "$HOME/.ssh/"
+chmod 755 "$HOME/.ssh/"
+cat $SWD/setup/ovh.key >> "$HOME/.ssh/known_hosts"
