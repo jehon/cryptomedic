d893b8593e97b15c7d6b8091745afd1a19d57ff8
Jean Honlet <jehon@users.noreply.github.com>
2023-04-09 10:44:30 +0200

    fix: test
    cause: bootstrap 4

--- tests/unitjs/karma.conf.js
+++ tests/unitjs/karma.conf.js
@@ -39,7 +39,7 @@ module.exports = function (config) {
       { pattern: "node_modules/bluebird/js/browser/bluebird.min.js" },
       "tests/unitjs/_global.js",
       "node_modules/karma-read-json/karma-read-json.js",
-      "node_modules/bootstrap/dist/css/bootstrap.min.css",
+      "node_modules/bootstrap4/dist/css/bootstrap.min.css",
       { pattern: "tests/unitjs/*-test.js", type: "module" },
       { pattern: "tests/unitjs/**", included: false },
       { pattern: "app/**/*", included: false },
