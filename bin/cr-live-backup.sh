7778c5015ab405eeec30fe6721e1f98878e0966d
Jean Honlet <jehon@users.noreply.github.com>
2023-05-03 09:04:46 +0200

    refactor: simplify cr-live-backup

--- bin/cr-live-backup.sh
+++ bin/cr-live-backup.sh
@@ -2,7 +2,6 @@
 
 set -o errexit
 set -o pipefail
-shopt -s nullglob
 
 if [ -z "$CRYPTOMEDIC_DEPLOY_PASSWORD" ]; then
     echo "$0 Need parameters" >&2
@@ -12,24 +11,18 @@ fi
 BACKUP_DIR="."
 if [ -n "$1" ]; then
     BACKUP_DIR="$1"
+    mkdir -p "$BACKUP_DIR"
 fi
 
-mkdir -p "$BACKUP_DIR"
-
-echo "Getting storage from $CRYPTOMEDIC_DEPLOY_HOST"
-lftp "$CRYPTOMEDIC_DEPLOY_USER:$CRYPTOMEDIC_DEPLOY_PASSWORD@$CRYPTOMEDIC_DEPLOY_HOST" \
-    -e "mirror live/storage '$BACKUP_DIR/storage'; bye"
-echo "...done"
-
 echo "Generating a new backup on remote"
-curl -fsSL --user-agent firefox "http://$CRYPTOMEDIC_HTTP_HOST/maintenance/create_db_backup.php?pwd=$CRYPTOMEDIC_HTTP_TOKEN"
+curl -fsSL "http://$CRYPTOMEDIC_HTTP_HOST/maintenance/create_db_backup.php?pwd=$CRYPTOMEDIC_HTTP_TOKEN"
 echo "- Sleeping a bit to allow the file to become visible"
 sleep 10s
 echo "...done"
 
-echo "Getting db backups (and removing them from server)"
+echo "Getting storage from $CRYPTOMEDIC_DEPLOY_HOST"
 lftp "$CRYPTOMEDIC_DEPLOY_USER:$CRYPTOMEDIC_DEPLOY_PASSWORD@$CRYPTOMEDIC_DEPLOY_HOST" \
-    -e "mirror --Remove-source-files live/backups '$BACKUP_DIR/backups'; bye"
+    -e "mirror live/ '$BACKUP_DIR/'; bye"
 echo "...done"
 
 echo "Backup finished with success"
