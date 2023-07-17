29ce6f366607d3cd7a53484eca5d7baca8e7ec5e
Jean Honlet <jehon@users.noreply.github.com>
2023-07-17 18:08:02 +0200

    fix: bridge adapt to attribute changes

--- src/app-2023/bridge.tsx
+++ src/app-2023/bridge.tsx
@@ -1,7 +1,7 @@
 // https://legacy.reactjs.org/docs/web-components.html
 
 import React from "react";
-import { createRoot } from "react-dom/client";
+import { Root, createRoot } from "react-dom/client";
 
 export function bridgeTo(
   tag: string,
@@ -11,19 +11,38 @@ export function bridgeTo(
   customElements.define(
     tag,
     class extends HTMLElement {
-      connectedCallback() {
+      #element: any
+      #root: Root
+
+      static get observedAttributes() {
+        return attributes;
+      }
+
+      constructor() {
+        super();
         const mountPoint = document.createElement("span");
         this.innerHTML = "";
         this.insertAdjacentElement("beforeend", mountPoint);
         // this.attachShadow({ mode: "open" }).appendChild(mountPoint);
 
+        this.#root = createRoot(mountPoint);
+      }
+
+      connectedCallback() {
+        this.render();
+      }
+
+      attributeChangedCallback(attributeName: string, oldValue: string, newValue: string) {
+        this.render();
+      }
+
+      render() {
         const attrs: any = {};
         for (const k of attributes) {
           attrs[k] = this.getAttribute(k) || "";
         }
-
-        const root = createRoot(mountPoint);
-        root.render(React.createElement(reactComponent, attrs));
+        this.#element = React.createElement(reactComponent, attrs);
+        this.#root.render(this.#element);
       }
     }
   );
