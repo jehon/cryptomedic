142a97c6ae99eaed14622fec3bc2b2199356d2d8
Jean Honlet <jehon@users.noreply.github.com>
2023-07-21 16:10:29 +0200

    feat(wip): cash register filtering

--- www/api/app/Http/Controllers/ReportCashRegisterController.php
+++ www/api/app/Http/Controllers/ReportCashRegisterController.php
@@ -19,7 +19,16 @@ class ReportCashRegisterController extends ReportController
 
             SUM(total_real) as total_real,
             SUM(total_asked) as total_asked,
-            SUM((SELECT SUM(Amount) FROM payments WHERE payments.bill_id = bills.id)) as paid
+            SUM((SELECT SUM(Amount) FROM payments WHERE payments.bill_id = bills.id)) as paid,
+
+            SUM(IF(false, total_real, 0)) as child_total_real,
+            SUM(IF(false, total_asked, 0)) as child_total_asked,
+            SUM(IF(false, (SELECT SUM(Amount) FROM payments WHERE payments.bill_id = bills.id), 0)) as child_paid,
+
+            SUM(IF(false, total_real, 0)) as poor_child_total_real,
+            SUM(IF(false, total_asked, 0)) as poor_child_total_asked,
+            SUM(IF(false, (SELECT SUM(Amount) FROM payments WHERE payments.bill_id = bills.id), 0)) as poor_child_paid
+
         FROM 
           bills
           LEFT JOIN patients ON patients.id = bills.patient_id
