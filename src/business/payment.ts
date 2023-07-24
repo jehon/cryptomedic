d4a118befbf0a641a37c6b611b429c21bafc8436
Jean Honlet <jehon@users.noreply.github.com>
2023-07-24 13:23:49 +0200

    chore: remove not really migrated files

--- src/business/payment.ts
+++
@@ -1,9 +0,0 @@
-/* istanbul ignore file */
-
-import FolderPage from "./folderPage";
-
-export default class Payment extends FolderPage {
-  // getModel() {
-  //   return "Payment";
-  // }
-}
--- src/business/price.ts
+++
@@ -1,9 +0,0 @@
-/* istanbul ignore file */
-
-import CRUD from "../utils/crud";
-
-export default class Price extends CRUD {
-  // static getBaseUrl() {
-  //   return "admin/prices";
-  // }
-}
