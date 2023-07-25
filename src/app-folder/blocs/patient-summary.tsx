57529a5318bb5afcc415f8c030ba2c366efe380d
Jean Honlet <jehon@users.noreply.github.com>
2023-07-25 14:00:38 +0200

    refactor: header of patient summary

--- src/app-folder/blocs/patient-summary.tsx
+++ src/app-folder/blocs/patient-summary.tsx
@@ -1,5 +1,7 @@
 import React from "react";
 import Button from "react-bootstrap/Button";
+import Accordion from "react-bootstrap/Accordion";
+import Card from "react-bootstrap/Card";
 
 import { bridgeTo } from "../../bridge";
 import Labelled from "../../widget/labelled";
@@ -15,28 +17,38 @@ export default function PatientSummary({
     return <div>No patient selected</div>;
   }
   return (
-    <tr id="summary_patient">
-      <td></td>
-      <td>
-        <Button
-          href="#/folder/{folder.getId()}/"
-          variant="outline-info"
-          style={{ width: "100%" }}
-        >
-          Patient
-        </Button>
-      </td>
-      <td></td>
-      <td>
-        <Labelled label="Entry Year">{patient.entryyear}</Labelled>
-        <Labelled label="Entry Order">{patient.entryorder}</Labelled>
-        <Labelled label="Name">{patient.Name}</Labelled>
-        <Labelled label="Year of birth">{patient.Yearofbirth}</Labelled>
-        <Labelled label="Sex">{patient.Sex}</Labelled>
-        <Labelled label="Pathology">{patient.Pathology}</Labelled>
-        <Labelled label="Other Comments">{patient.other_comments}</Labelled>
-      </td>
-    </tr>
+    <Accordion defaultActiveKey="">
+      <Card>
+        <Accordion.Toggle as={Card.Header} eventKey="0">
+          <div className="row-spread">
+            <span className="no-mobile">Patient</span>
+            <span>
+              {patient.entryyear}-{patient.entryorder}
+            </span>
+            <span className="no-mobile">{patient.Name}</span>
+            <span className="no-mobile">{patient.Yearofbirth}</span>
+          </div>
+        </Accordion.Toggle>
+        <Accordion.Collapse eventKey="0">
+          <Card.Body>
+            <Button
+              href="#/folder/{patient.getId()}/"
+              variant="outline-info"
+              style={{ width: "100%" }}
+            >
+              View the Patient
+            </Button>
+            <Labelled label="Entry Year">{patient.entryyear}</Labelled>
+            <Labelled label="Entry Order">{patient.entryorder}</Labelled>
+            <Labelled label="Name">{patient.Name}</Labelled>
+            <Labelled label="Year of birth">{patient.Yearofbirth}</Labelled>
+            <Labelled label="Sex">{patient.Sex}</Labelled>
+            <Labelled label="Pathology">{patient.Pathology}</Labelled>
+            <Labelled label="Other Comments">{patient.other_comments}</Labelled>
+          </Card.Body>
+        </Accordion.Collapse>
+      </Card>
+    </Accordion>
   );
 }
 
