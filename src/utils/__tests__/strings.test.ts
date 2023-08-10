73fbe2443f843df41ac7a47a1f6f55d9c7fa5490
Jean Honlet <jehon@users.noreply.github.com>
2023-08-10 12:13:32 +0200

    fix: migrate string utils tests

--- src/utils/__tests__/strings.test.ts
+++ src/utils/__tests__/strings.test.ts
@@ -1,6 +1,6 @@
 import { expect, test } from '@jest/globals';
 
-import { padLeftTrim, toAttributeCase, toPropertyCase } from "../strings";
+import { padLeftTrim, toAttributeCase, toTitleCase } from "../strings";
 
 // https://jestjs.io/fr/docs/expect
 test('padLeftTrim', () => {
@@ -9,18 +9,10 @@ test('padLeftTrim', () => {
     expect(padLeftTrim(15, 1)).toBe('5');
 });
 
-test("should toPropertyCase case (camel case)", function () {
-  expect(toPropertyCase("abc def")).toBe("abcDef");
-  expect(toPropertyCase("abc-def")).toBe("abcDef");
-  expect(toPropertyCase("abc_def")).toBe("abcDef");
-  expect(toPropertyCase("abc def ghi klm")).toBe("abcDefGhiKlm");
-  expect(toPropertyCase("abc-def-ghi-klm")).toBe("abcDefGhiKlm");
-  expect(toPropertyCase("abc_def_ghi_klm")).toBe("abcDefGhiKlm");
-  expect(toPropertyCase("abc def-ghi_klm")).toBe("abcDefGhiKlm");
-  expect(toPropertyCase("abc def-ghi_klm", false)).toBe("abcDefGhiKlm");
-  expect(toPropertyCase("abc def-ghi_klm", true)).toBe("AbcDefGhiKlm");
-});
-
 test("should toAttributeCase case (kebab case)", function () {
   expect(toAttributeCase("abcDefGhi")).toBe("abc-def-ghi");
+});
+
+test("should title case", function () {
+  expect(toTitleCase("abc def")).toBe("Abc Def");
 });
--- src/utils/strings.ts
+++ src/utils/strings.ts
@@ -18,19 +18,19 @@ function toUpperWordCase(text: string): string {
   return text.charAt(0).toUpperCase() + text.substring(1);
 }
 
-/**
- * (lower) camelCase (abcDefGhi - to use in object)
- *
- * @see https://en.wikipedia.org/wiki/Letter_case#Special_case_styles
- */
-export function toPropertyCase(
-  text: string,
-  firstUpper: boolean = false
-): string {
-  return _canonize(text)
-    .map((s, i) => (i > 0 || firstUpper ? toUpperWordCase(s) : s))
-    .join("");
-}
+// /**
+//  * (lower) camelCase (abcDefGhi - to use in object)
+//  *
+//  * @see https://en.wikipedia.org/wiki/Letter_case#Special_case_styles
+//  */
+// export function toPropertyCase(
+//   text: string,
+//   firstUpper: boolean = false
+// ): string {
+//   return _canonize(text)
+//     .map((s, i) => (i > 0 || firstUpper ? toUpperWordCase(s) : s))
+//     .join("");
+// }
 
 /**
  * Transform into kebab case (abc-def-ghi - to use in html)
@@ -39,4 +39,12 @@ export function toPropertyCase(
  */
 export function toAttributeCase(text: string): string {
   return _canonize(text).join("-");
+}
+
+/**
+ * @param {string} text to be transformed (abc dev ghi)
+ * @returns {string} the test in title case (Abc Def Ghi)
+ */
+export function toTitleCase(text: string): string {
+  return _canonize(text).map(toUpperWordCase).join(" ");
 }
