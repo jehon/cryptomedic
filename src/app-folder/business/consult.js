6debc71db268e3798fdc80aa3678dbba909c34b9
Jean Honlet <jehon@users.noreply.github.com>
2023-08-29 21:25:54 +0200

    fix: consults constructors

--- src/app-folder/business/consult.js
+++ src/app-folder/business/consult.js
@@ -5,6 +5,35 @@ export default class Consult extends PatientRelated {
     return "Consult";
   }
 
+  weight_kg;
+  height_cm;
+  brachial_circumference_cm;
+  treatment_evaluation;
+  treatment_finished;
+  comments;
+  suggested_for_surgery;
+
+  constructor({
+    weight_kg,
+    height_cm,
+    brachial_circumference_cm,
+    treatment_evaluation,
+    treatment_finished,
+    comments,
+    suggested_for_surgery,
+    ...others
+  } = {}) {
+    super(others);
+
+    this.weight_kg = weight_kg;
+    this.height_cm = height_cm;
+    this.brachial_circumference_cm = brachial_circumference_cm;
+    this.treatment_evaluation = treatment_evaluation;
+    this.treatment_finished = treatment_finished;
+    this.comments = comments;
+    this.suggested_for_surgery = suggested_for_surgery;
+  }
+
   validate(res) {
     res = super.validate(res);
 
--- src/app-folder/business/other-consult.js
+++ src/app-folder/business/other-consult.js
@@ -4,4 +4,42 @@ export default class OtherConsult extends Consult {
   getModel() {
     return "OtherConsult";
   }
+
+  side;
+  joints_or_bones_affected;
+  deformity;
+  articulation_mobility;
+  muscle_strength;
+  pain;
+  walk;
+  xray;
+  performed;
+  not_performed;
+
+  constructor({
+    side,
+    joints_or_bones_affected,
+    deformity,
+    articulation_mobility,
+    muscle_strength,
+    pain,
+    walk,
+    xray,
+    performed,
+    not_performed,
+    ...others
+  } = {}) {
+    super(others);
+
+    this.side = side;
+    this.joints_or_bones_affected = joints_or_bones_affected;
+    this.deformity = deformity;
+    this.articulation_mobility = articulation_mobility;
+    this.muscle_strength = muscle_strength;
+    this.pain = pain;
+    this.walk = walk;
+    this.xray = xray;
+    this.performed = performed;
+    this.not_performed = not_performed;
+  }
 }
