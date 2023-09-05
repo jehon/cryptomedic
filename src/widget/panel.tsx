441e2980871adcbf4edd04220130d8086dde953f
Jean Honlet <jehon@users.noreply.github.com>
2023-09-05 14:13:32 +0200

    chore: add tryouts

--- src/widget/panel.tsx
+++ src/widget/panel.tsx
@@ -25,6 +25,22 @@ export default function Panel(
 
   // TODO: no display if no children? does not seem to work perfectly...
 
+  // // const res = React.Children.toArray(children).filter((child) =>
+  // //   React.isValidElement(child)
+  // // );
+
+  // // const res2 = React.Children.toArray(children).filter(
+  // //   (child) =>
+  // //     !React.isValidElement(child) || (child as any as React.ReactElement).type
+  // // );
+
+  // console.log({
+  //   label,
+  //   c: React.Children.count(children),
+  //   e: children === ""
+  //   // res,
+  //   // res2
+  // });
 
   if (React.Children.count(children) === 0) {
     // Show nothing if no children...
