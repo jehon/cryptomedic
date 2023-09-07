553d5c8e4d0eb0ca7629cea7503abeb29876320c
Jean Honlet <jehon@users.noreply.github.com>
2023-09-07 15:18:08 +0200

    feat: ricket consults

--- src/app-folder/consult-ricket-summary.tsx
+++ src/app-folder/consult-ricket-summary.tsx
@@ -11,6 +11,7 @@ import TwoColumns from "../widget/two-columns";
 import Panel from "../widget/panel";
 import IO from "../widget/io";
 import { icons } from "../config";
+import { ImgSideLeft, ImgSideRight } from "../widget/images";
 import { date2HumanString, normalizeDate } from "../utils/date";
 
 export default function ConsultRicketSummary({
@@ -48,7 +49,66 @@ export default function ConsultRicketSummary({
         View
       </Button>
       <ConsultAbstractIntroduction file={file}></ConsultAbstractIntroduction>
-      <TwoColumns> </TwoColumns>
+      <TwoColumns>
+        <Panel label="Ricket Data">
+          <IO.List
+            label="Walking difficulties"
+            value={file.walking_difficulties}
+          ></IO.List>
+          <IO.List label="Pain" value={file.pain}></IO.List>
+          <IO.List
+            label="Wrist Enlargement"
+            value={file.wrist_enlargement}
+          ></IO.List>
+          <IO.List label="Rib Heading" value={file.rib_heading}></IO.List>
+        </Panel>
+      </TwoColumns>
+      <TwoColumns>
+        <Panel
+          headers={[<ImgSideRight></ImgSideRight>, <span>Right Leg</span>]}
+        >
+          <IO.List right label="Leg" value={file.right_leg}></IO.List>
+          <IO.Number
+            right
+            label="Leg Angle"
+            value={file.right_leg_angle}
+          ></IO.Number>
+          <div className="separator">Cross</div>
+          <IO.Number
+            right
+            label="Cross Right T"
+            value={file.cross_right_T}
+          ></IO.Number>
+          <IO.Number
+            right
+            label="Cross Right F"
+            value={file.cross_right_F}
+          ></IO.Number>
+          <IO.Number
+            right
+            label="IMIC Distance"
+            value={file.IMIC_distance}
+          ></IO.Number>
+          <IO.Text right label="XRay" value={file.xray}></IO.Text>
+        </Panel>
+        <Panel headers={[<ImgSideLeft></ImgSideLeft>, <span>Left leg</span>]}>
+          <IO.List left label="Leg" value={file.left_leg}></IO.List>
+          <IO.Number
+            left
+            label="Leg Angle"
+            value={file.left_leg_angle}
+          ></IO.Number>
+          <div className="separator">Cross</div>
+          <IO.Number left label="Cross T" value={file.cross_left_T}></IO.Number>
+          <IO.Number left label="Cross F" value={file.cross_left_F}></IO.Number>
+          <IO.Number
+            left
+            label="IMIC Distance"
+            value={file.IMIC_distance}
+          ></IO.Number>
+          <IO.Text left label="XRay" value={file.xray}></IO.Text>
+        </Panel>
+      </TwoColumns>
       <ConsultAbstractConclusion file={file}></ConsultAbstractConclusion>
     </Panel>
   );
