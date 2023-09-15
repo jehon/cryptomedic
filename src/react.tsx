950a2bdad255888af546db07bd53a481c27bcf17
Jean Honlet <jehon@users.noreply.github.com>
2023-09-15 10:56:37 +0200

    feat(wip): router

--- src/react.tsx
+++ src/react.tsx
@@ -1,9 +1,7 @@
+/* Styles */
 import "bootstrap4/dist/css/bootstrap.min.css";
 import "./main.css";
 import "./main-media.css";
 
-/*
- * What custom element do we need to import?
- */
-
-import "./app-folder/folder-element.tsx";
+/* Application */
+import "./app-folder/folder-element";
