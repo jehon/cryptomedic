3c0e9a0185e4693cf0d14c629f448ff1e82dde9b
Jean Honlet <jehon@users.noreply.github.com>
2023-10-01 15:18:51 +0200

    feat: full screen on click on io.image

--- src/widget/io-image.tsx
+++ src/widget/io-image.tsx
@@ -1,26 +1,24 @@
 import React, { useState } from "react";
-import IOAbstract, { IOParams } from "./io-abstract";
+import { IOParams } from "./io-abstract";
 import { Optional } from "../utils/generic-types";
 
 // For styling, see io.css
 
-export default function IOPicture(options: IOParams<Optional<string>>) {
+export default function IOPicture(props: IOParams<Optional<string>>) {
   const [top, setTop] = useState(false);
 
-  return IOAbstract<Optional<string>>(options, {
-    renderOutput: (value) => {
-      if (!value) {
-        return "No image";
-      }
+  const value = props.value;
 
-      return (
-        <img
-          className={"io-img" + top ? " fullscreen" : ""}
-          src={value}
-          alt="Content"
-          onClick={() => setTop(!top)}
-        />
-      );
-    }
-  });
+  if (!value) {
+    return "No image";
+  }
+
+  return (
+    <img
+      className={"io-img" + (top ? " fullscreen" : "")}
+      src={value}
+      alt="Content"
+      onClick={() => setTop(!top)}
+    />
+  );
 }
