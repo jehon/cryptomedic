3383fe5026b6ed8c9a0142c7bbf140ec30f120d2
Jean Honlet <jehon@users.noreply.github.com>
2023-10-08 18:40:23 +0200

    chore: remove dead code

--- src/bridge.tsx
+++ src/bridge.tsx
@@ -25,7 +25,6 @@ export function bridgeTo(
         const mountPoint = document.createElement("span");
         this.innerHTML = "";
         this.insertAdjacentElement("beforeend", mountPoint);
-        // this.attachShadow({ mode: "open" }).appendChild(mountPoint);
 
         this.#root = createRoot(mountPoint);
       }
