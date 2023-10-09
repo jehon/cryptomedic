60ad4caf79686d3773363dafe212646c65acb5cf
Jean Honlet <jehon@users.noreply.github.com>
2023-10-09 09:15:33 +0200

    feat: add error-pages

---
+++ src/widget/error-page.tsx
@@ -0,0 +1,31 @@
+import React from "react";
+import { isRouteErrorResponse, useRouteError } from "react-router-dom";
+
+// https://github.com/remix-run/react-router/discussions/9628#discussioncomment-5555901
+function errorMessage(error: unknown): string {
+  if (isRouteErrorResponse(error)) {
+    return `${error.status} ${error.statusText}`;
+  } else if (error instanceof Error) {
+    return error.message;
+  } else if (typeof error === "string") {
+    return error;
+  } else {
+    console.error(error);
+    return "Unknown error";
+  }
+}
+
+export default function ErrorPage() {
+  const error = useRouteError();
+  console.error(error);
+
+  return (
+    <div id="error-page">
+      <h1>Oops!</h1>
+      <p>Sorry, an unexpected error has occurred.</p>
+      <p>
+        <i>{errorMessage(error)}</i>
+      </p>
+    </div>
+  );
+}
