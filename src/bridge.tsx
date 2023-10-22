186ca1499b8a2c106084237503a2e59157a24f80
Jean Honlet <jehon@users.noreply.github.com>
2023-10-22 21:22:37 +0200

    feat: export the class in bridge

--- src/bridge.tsx
+++ src/bridge.tsx
@@ -10,69 +10,69 @@ export function bridgeTo(
   hardcodedAttributes: { [x: string]: any } = {},
   transferedAttributes: string[] = []
 ) {
-  customElements.define(
-    tag,
-    class extends HTMLElement {
-      #element: any;
-      #root: Root;
-      #props: ObjectMap<any> = {};
+  const el = class extends HTMLElement {
+    #element: any;
+    #root: Root;
+    #props: ObjectMap<any> = {};
 
-      static get observedAttributes() {
-        return transferedAttributes;
-      }
-
-      constructor() {
-        super();
-        const mountPoint = document.createElement("span");
-        this.innerHTML = "";
-        this.insertAdjacentElement("beforeend", mountPoint);
-
-        this.#root = createRoot(mountPoint);
-      }
-
-      connectedCallback() {
-        this.render();
-      }
-
-      attributeChangedCallback(
-        attributeName: string,
-        oldValue: string,
-        newValue: string
-      ) {
-        this.render();
-      }
-
-      set(attr: string, value: any) {
-        this.#props[attr] = value;
-        this.render();
-      }
-
-      render() {
-        const attrs: any = {};
-        for (const k of transferedAttributes) {
-          attrs[k] = this.getAttribute(k) || "";
-        }
-        this.#element = React.createElement(reactComponent, {
-          ...attrs,
-          ...hardcodedAttributes,
-          ...this.#props
-        });
-        this.#root.render(this.#element);
-      }
-
-      // //
-      // // For ctrl_folder
-      // //
-
-      // set folder(f: any) {
-      //   this.set("folder", f);
-      //   this.render();
-      // }
-
-      // set file(f: any) {
-      //   this.set("file", f);
-      //   this.render();
-      // }
+    static get observedAttributes() {
+      return transferedAttributes;
     }
-  );
+
+    constructor() {
+      super();
+      const mountPoint = document.createElement("span");
+      this.innerHTML = "";
+      this.insertAdjacentElement("beforeend", mountPoint);
+
+      this.#root = createRoot(mountPoint);
+    }
+
+    connectedCallback() {
+      this.render();
+    }
+
+    attributeChangedCallback(
+      attributeName: string,
+      oldValue: string,
+      newValue: string
+    ) {
+      this.render();
+    }
+
+    set(attr: string, value: any) {
+      this.#props[attr] = value;
+      this.render();
+    }
+
+    render() {
+      const attrs: any = {};
+      for (const k of transferedAttributes) {
+        attrs[k] = this.getAttribute(k) || "";
+      }
+      this.#element = React.createElement(reactComponent, {
+        ...attrs,
+        ...hardcodedAttributes,
+        ...this.#props
+      });
+      this.#root.render(this.#element);
+    }
+
+    // //
+    // // For ctrl_folder
+    // //
+
+    // set folder(f: any) {
+    //   this.set("folder", f);
+    //   this.render();
+    // }
+
+    // set file(f: any) {
+    //   this.set("file", f);
+    //   this.render();
+    // }
+  };
+  customElements.define(tag, el);
+
+  return el;
 }
