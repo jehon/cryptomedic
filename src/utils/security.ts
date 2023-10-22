2eb2e263a67939fe22cf79436d65fb970a0f5fb8
Jean Honlet <jehon@users.noreply.github.com>
2023-10-22 20:41:48 +0200

    refactor: security with useRequiresTransaction

---
+++ src/utils/security.ts
@@ -0,0 +1,28 @@
+import { useSyncExternalStore } from "react";
+
+import { getSession, onSession } from "../../legacy/app-old/v2/js/session";
+
+export function useRequiresTransaction(role?: string) {
+  function getSnapshot(): boolean {
+    if (!role) {
+      return true;
+    }
+
+    if (!getSession()) {
+      return false;
+    }
+
+    const authorized: string[] = (getSession() as any).authorized;
+    if (!authorized) {
+      return false;
+    }
+
+    return authorized.includes(role);
+  }
+
+  function subscribe(callback: () => void) {
+    return onSession(callback);
+  }
+
+  return useSyncExternalStore(subscribe, getSnapshot);
+}
