21893d714ff9eca34efce56bea33909064241ac9
Jean Honlet <jehon@users.noreply.github.com>
2024-01-09 09:48:05 +0100

    build: add lftp

--- .devcontainer/root/setup.sh
+++ .devcontainer/root/setup.sh
@@ -8,7 +8,7 @@ export DEBIAN_FRONTEND=noninteractive
 
 apt update
 
-apt install --yes openssh-client sshpass sshfs curl coreutils diffutils
+apt install --yes openssh-client sshpass sshfs curl coreutils diffutils lftp
 
 if [ ! -r "$HOME"/.ssh/id_rsa ]; then
     echo "Generating a ssh key"
