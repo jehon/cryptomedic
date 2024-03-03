d0c7f2fc20ed9d7961ccc2aaccccb85bb786639d
Jean Honlet <jehon@users.noreply.github.com>
2024-03-03 22:21:55 +0100

    chore: update lint

--- www/templates/templates/picture_fiche.php
+++ www/templates/templates/picture_fiche.php
@@ -14,10 +14,7 @@ t::setDefaultOption("baseExpression", "currentFile()."); ?>
 			<?php (new t("Picture.date"))->tr2()->p(); ?>
 			<?php (new t("Picture.type"))->tr2()->p(); ?>
 			<?php (new t("Picture.comments"))->tr2()->p(); ?>
-			<?php (new t("Picture.file"))
-       ->readOnly()
-       ->tr2("File")
-       ->p(); ?>
+			<?php (new t("Picture.file"))->readOnly()->tr2("File")->p(); ?>
 			<x-fff-field class='not-mode-read' label='Upload a file' ng-if="!currentFile().file" ng-class="{ error: errors.pictureRequired }">
 				<x-restricted slot='label' restricted-by='folder.edit'>
 					Upload a file
