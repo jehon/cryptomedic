659d1536760e4a698e0b50bb9f9bf74898ea6b3a
Jean Honlet <jehon@users.noreply.github.com>
2024-03-05 14:26:19 +0100

    chore: add static to proxy

--- src-ng/proxy.conf.mjs
+++ src-ng/proxy.conf.mjs
@@ -1,6 +1,6 @@
 export default [
   {
-    context: ["/api", "/built/frontend"],
+    context: ["/api", "/built/frontend", "/static"],
     target: "http://localhost:8085",
     secure: false,
     logLevel: "debug"
