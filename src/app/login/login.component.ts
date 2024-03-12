070ac8b7a887d7d1177a92bb263385b7b90ad0bc
Jean Honlet <jehon@users.noreply.github.com>
2024-03-12 14:53:44 +0100

    feat: first implem of login component

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,10 +1,40 @@
+import { CommonModule } from "@angular/common";
+import { HttpClient, HttpClientModule } from "@angular/common/http";
 import { Component } from "@angular/core";
+import { FormsModule } from "@angular/forms";
+import AuthService from "../_services/auth.service";
 
 @Component({
   standalone: true,
   selector: "app-login",
-  imports: [],
+  imports: [FormsModule, CommonModule, HttpClientModule],
   templateUrl: "./login.component.html",
-  styleUrl: "./login.component.css"
+  styleUrl: "./login.component.css",
+  providers: [HttpClient]
 })
-export class LoginComponent {}
+export class LoginComponent {
+  form: any = {
+    username: null,
+    password: null
+  };
+  isLoggedIn = false;
+  isLoginFailed = false;
+  errorMessage = "";
+  roles: string[] = [];
+
+  constructor(private authService: AuthService) {}
+
+  onSubmit(): void {
+    const { username, password } = this.form;
+    this.authService.login(username, password).subscribe({
+      next: (_data) => {
+        this.isLoginFailed = false;
+        this.isLoggedIn = true;
+      },
+      error: (err) => {
+        this.errorMessage = err.error.message;
+        this.isLoginFailed = true;
+      }
+    });
+  }
+}
