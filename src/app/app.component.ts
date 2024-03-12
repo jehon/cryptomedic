dd3352e0299204333fdf87698efd4bebef76f134
Jean Honlet <jehon@users.noreply.github.com>
2024-03-12 12:12:38 +0100

    feat: add routes and restructure a bit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,41 @@
 import { Component } from "@angular/core";
-import { RouterOutlet } from "@angular/router";
+import { RouterLink, RouterOutlet } from "@angular/router";
 
 @Component({
   selector: "app-root",
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, RouterLink],
   templateUrl: "./app.component.html",
   styleUrl: "./app.component.css"
 })
 export class AppComponent {
   title = "cryptomedic";
+  isLoggedIn = false;
+  username?: string;
+  // eventBusSub?: Subscription;
+
+  // constructor(
+  //   private authService: AuthService,
+  //   private eventBusService: EventBusService
+  // ) {}
+
+  // ngOnInit(): void {
+  //   if (this.isLoggedIn) {
+  //   }
+
+  //   this.eventBusSub = this.eventBusService.on("logout", () => {
+  //     this.logout();
+  //   });
+  // }
+
+  logout(): void {
+    //   this.authService.logout().subscribe({
+    //     next: (res) => {
+    //       window.location.reload();
+    //     },
+    //     error: (err) => {
+    //       console.log(err);
+    //     }
+    //   });
+  }
 }
---
+++ src/app/app.module.ts
@@ -0,0 +1,9 @@
+import { NgModule } from "@angular/core";
+import { FormsModule } from "@angular/forms";
+import { BrowserModule } from "@angular/platform-browser";
+
+// @see https://angular.io/guide/ngmodules
+@NgModule({
+  imports: [FormsModule, BrowserModule]
+})
+export class AppModule {}
