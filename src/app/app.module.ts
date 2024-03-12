226fc317870a2b371f9bfc27397e942e7b9cd7a0
Jean Honlet <jehon@users.noreply.github.com>
2024-03-12 12:16:52 +0100

    fix: module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,9 +1,12 @@
 import { NgModule } from "@angular/core";
 import { FormsModule } from "@angular/forms";
 import { BrowserModule } from "@angular/platform-browser";
-
+import { AppComponent } from "./app.component";
 // @see https://angular.io/guide/ngmodules
 @NgModule({
-  imports: [FormsModule, BrowserModule]
+  declarations: [AppComponent],
+  imports: [FormsModule, BrowserModule],
+  providers: [],
+  bootstrap: [AppComponent]
 })
 export class AppModule {}
