004c446fc0e2bf2d938ff28dbb19315db9874719
Jean Honlet <jehon@users.noreply.github.com>
2024-03-13 22:05:52 +0100

    feat: first interceptor

---
+++ src/app/_services/http-request-interceptor.ts
@@ -0,0 +1,29 @@
+import {
+  HTTP_INTERCEPTORS,
+  HttpEvent,
+  HttpHandler,
+  HttpHeaders,
+  HttpInterceptor,
+  HttpRequest
+} from "@angular/common/http";
+import { Injectable } from "@angular/core";
+import { Observable } from "rxjs";
+
+@Injectable()
+export class HttpRequestInterceptor<T extends undefined | unknown>
+  implements HttpInterceptor
+{
+  intercept(req: HttpRequest<T>, next: HttpHandler): Observable<HttpEvent<T>> {
+    // Auto set some options
+    req = req.clone({
+      withCredentials: true,
+      headers: new HttpHeaders({ "Content-Type": "application/json" })
+    });
+
+    return next.handle(req);
+  }
+}
+
+export const httpInterceptorProviders = [
+  { provide: HTTP_INTERCEPTORS, useClass: HttpRequestInterceptor, multi: true }
+];
--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,8 +1,13 @@
 import { APP_INITIALIZER, ApplicationConfig } from "@angular/core";
 import { provideRouter } from "@angular/router";
 
-import { HttpClient, provideHttpClient } from "@angular/common/http";
+import {
+  HTTP_INTERCEPTORS,
+  HttpClient,
+  provideHttpClient
+} from "@angular/common/http";
 import AuthService from "./_services/auth.service";
+import { HttpRequestInterceptor } from "./_services/http-request-interceptor";
 import routes from "./app.routes";
 
 export const appConfig: ApplicationConfig = {
@@ -18,6 +23,11 @@ export const appConfig: ApplicationConfig = {
       provide: APP_INITIALIZER,
       useFactory: (authService: AuthService) => () => authService.hydrate(),
       deps: [AuthService],
+      multi: true
+    },
+    {
+      provide: HTTP_INTERCEPTORS,
+      useClass: HttpRequestInterceptor,
       multi: true
     }
   ]
