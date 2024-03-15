c38f0d15675b4da7490c9c34f6aaa784b0fb8a5a
Jean Honlet <jehon@users.noreply.github.com>
2024-03-15 09:50:18 +0100

    feat: waiting wheel

--- src/app/_services/http.service.ts
+++ src/app/_services/http.service.ts
@@ -1,11 +1,44 @@
+import { HttpErrorResponse } from "@angular/common/http";
 import { Injectable } from "@angular/core";
 
-interface AppHttpError {}
+export interface HttpServiceHttpError {
+  message: string;
+  backend: boolean;
+  httpStatusCode?: number;
+}
 
 @Injectable({
   providedIn: "root"
 })
 export class HttpService {
-  errors: AppHttpError[] = [];
+  errors: HttpServiceHttpError[] = [];
   running: number = 0;
+
+  is404(error: HttpErrorResponse): boolean {
+    // Thanks to https://stackoverflow.com/questions/66824082/handling-angular-http-404-error-from-server-actually-which-represents-a-valid-re
+    if (error instanceof ErrorEvent) {
+      return false;
+    }
+
+    return error?.status === 404;
+  }
+
+  addError(error: HttpErrorResponse): HttpServiceHttpError {
+    const httpServiceHttpError: HttpServiceHttpError = {
+      message: "",
+      backend: true
+    };
+
+    if (error.error instanceof ErrorEvent) {
+      httpServiceHttpError.message = error.error.message;
+      httpServiceHttpError.backend = false;
+    } else {
+      httpServiceHttpError.message = error.message;
+      httpServiceHttpError.backend = true;
+      httpServiceHttpError.httpStatusCode = error.status;
+    }
+    this.errors.push(httpServiceHttpError);
+
+    return httpServiceHttpError;
+  }
 }
