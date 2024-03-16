331f9638682b0a2d9061cd8fce8e652c3f72a696
Jean Honlet <jehon@users.noreply.github.com>
2024-03-16 11:18:27 +0100

    feat: redirect after login

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,6 +1,7 @@
 import { CommonModule } from "@angular/common";
-import { Component } from "@angular/core";
+import { Component, OnInit } from "@angular/core";
 import { FormsModule } from "@angular/forms";
+import { Router } from "@angular/router";
 import AuthService from "../_services/auth.service";
 import BackendAuthInterface from "../_services/backend.auth";
 
@@ -11,7 +12,7 @@ import BackendAuthInterface from "../_services/backend.auth";
   templateUrl: "./login.component.html",
   styleUrl: "./login.component.css"
 })
-export class LoginComponent {
+export class LoginComponent implements OnInit {
   form = {
     username: "",
     password: ""
@@ -21,7 +22,14 @@ export class LoginComponent {
   errorMessage = "";
   data?: BackendAuthInterface;
 
-  constructor(private authService: AuthService) {}
+  constructor(
+    private router: Router,
+    private authService: AuthService
+  ) {}
+
+  ngOnInit(): void {
+    this.checkRoute();
+  }
 
   onSubmit(): void {
     const { username, password } = this.form;
@@ -30,11 +38,18 @@ export class LoginComponent {
         this.isLoginFailed = false;
         this.isLoggedIn = true;
         this.data = data;
+        this.checkRoute();
       },
       error: (err) => {
         this.errorMessage = err.error.message;
         this.isLoginFailed = true;
       }
     });
+  }
+
+  checkRoute() {
+    if (this.authService.currentUser) {
+      this.router.navigate(["/home"]);
+    }
   }
 }
