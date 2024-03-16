1730ba03cb14670f9d9268e70cae24f1608920cb
Jean Honlet <jehon@users.noreply.github.com>
2024-03-16 14:04:45 +0100

    feat: redirect to login if not authenticated

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,12 @@
 import { JsonPipe } from "@angular/common";
-import { Component } from "@angular/core";
-import { RouterLink, RouterOutlet } from "@angular/router";
+import { Component, OnInit } from "@angular/core";
+import {
+  NavigationEnd,
+  Router,
+  RouterLink,
+  RouterOutlet
+} from "@angular/router";
+import { filter } from "rxjs";
 import AuthService from "./_services/auth.service";
 import { HttpService } from "./_services/http.service";
 
@@ -11,15 +17,42 @@ import { HttpService } from "./_services/http.service";
   templateUrl: "./app.component.html",
   styleUrl: "./app.component.css"
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = "Cryptomedic";
 
   constructor(
+    public httpService: HttpService,
     public authService: AuthService,
-    public httpService: HttpService
+    private router: Router
+    // private route: ActivatedRoute
   ) {}
 
+  ngOnInit(): void {
+    this.router.events
+      .pipe(filter((e) => e instanceof NavigationEnd))
+      .subscribe(() => {
+        this.checkRoute();
+      });
+  }
+
   logout(): void {
-    this.authService.logout();
+    this.authService.logout().subscribe(() => {
+      this.checkRoute();
+    });
+  }
+
+  checkRoute() {
+    if (this.authService.currentUser) {
+      return;
+    }
+
+    const currentRoute = this.router.url;
+    if (currentRoute.startsWith("/login")) {
+      return;
+    }
+
+    this.router.navigate(["/login"], {
+      queryParams: { redirectTo: currentRoute }
+    });
   }
 }
