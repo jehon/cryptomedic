ffb344f92baf62d730a4d6f3e1e91c96fb5d26a4
Jean Honlet <jehon@users.noreply.github.com>
2024-03-19 11:29:31 +0100

    refactor: remove wget

--- conf/node/root/setup.sh
+++ conf/node/root/setup.sh
@@ -7,9 +7,9 @@ shopt -s nullglob
 export DEBIAN_FRONTEND=NONINTERACTIVE
 
 apt-get update
-apt-get install -y gnupg wget
+apt-get install -y gnupg
 
-wget --quiet --output-document=- https://dl-ssl.google.com/linux/linux_signing_key.pub | gpg --dearmor > /etc/apt/trusted.gpg.d/google-archive.gpg
+curl -fsSL https://dl-ssl.google.com/linux/linux_signing_key.pub | gpg --dearmor > /etc/apt/trusted.gpg.d/google-archive.gpg
 sh -c 'echo "deb [arch=amd64] http://dl.google.com/linux/chrome/deb/ stable main" >> /etc/apt/sources.list.d/google.list'
 
 apt-get update
