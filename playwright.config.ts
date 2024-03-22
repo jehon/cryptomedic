276a357197cbe7ce4afbd2fe29abfacbb4d6960a
Jean Honlet <jehon@users.noreply.github.com>
2024-03-22 19:31:50 +0100

    ci: reenable playwright ci specificities

--- playwright.config.ts
+++ playwright.config.ts
@@ -26,18 +26,18 @@ const config: PlaywrightTestConfig<unknown, unknown> = {
   ]
 };
 
-// if (process.env.CI) {
-//   if (config.reporter instanceof Array) {
-//     config.reporter.push(["github"]);
-//     config.reporter.push([
-//       "html",
-//       { outputFolder: "tmp/integration/playwright/report" }
-//     ]);
-//   }
-//   config.workers = 1;
+if (process.env.CI) {
+  if (config.reporter instanceof Array) {
+    config.reporter.push(["github"]);
+    config.reporter.push([
+      "html",
+      { outputFolder: "tmp/integration/playwright/report" }
+    ]);
+  }
+  config.workers = 1;
 
-//   /* Fail the build on CI if you accidentally left test.only in the source code. */
-//   config.forbidOnly = true;
-// }
+  /* Fail the build on CI if you accidentally left test.only in the source code. */
+  config.forbidOnly = true;
+}
 
 export default defineConfig(config);
