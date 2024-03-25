6b835687fc03ec4e3094d248c7765457fef77a31
Jean Honlet <jehon@users.noreply.github.com>
2024-03-25 11:27:36 +0100

    test(2e2): bad login

--- tests/e2e/login.spec.ts
+++ tests/e2e/login.spec.ts
@@ -25,3 +25,31 @@ test("login and go to home", async ({ page }) => {
   await expect(page.locator("#logout")).toBeVisible();
   await expect(page).toHaveScreenshot();
 });
+
+test("login incorrect", async ({ page }) => {
+  // See playwright.config.ts:
+  await crInit(page);
+
+  // Expect a title "to contain" a substring.
+  await expect(page).toHaveTitle(/Cryptomedic/);
+
+  await page.waitForURL(/\/login/);
+  await expect(page.locator("#current-user")).toHaveCount(0);
+  await expect(page.locator("#logout")).toHaveCount(0);
+
+  // Incorrect login
+
+  await page.getByLabel("Username").fill(LOGINS.RO);
+  await page.getByLabel("Password").fill("incorrect");
+  await page.locator(".btn-primary").click();
+
+  await expect(page.locator("#current-user")).toHaveCount(0);
+  await expect(page.locator("#logout")).toHaveCount(0);
+  await expect(page.locator("#logout")).toHaveCount(0);
+  await expect(page.getByTestId("login-error")).toBeVisible();
+
+  // Login is still possible after
+  await page.getByLabel("Password").fill(PASSWORD);
+  await page.locator(".btn-primary").click();
+  await page.waitForURL(/\/home$/);
+});
