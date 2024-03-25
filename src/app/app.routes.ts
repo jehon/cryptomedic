e98a15a53ed7ae11ab47175f0999251287774191
Jean Honlet <jehon@users.noreply.github.com>
2024-03-25 11:55:15 +0100

    feat: first patient route

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,13 +1,12 @@
 import { Routes } from "@angular/router";
 import { HomeComponent } from "./home/home.component";
 import { LoginComponent } from "./login/login.component";
+import { PatientSummaryComponent } from "./patients/patient-summary/patient-summary.component";
 
 const routes: Routes = [
-  { path: "home", component: HomeComponent },
-  // FIXME: temp debug routes
-  { path: "home2", component: HomeComponent },
-  { path: "home3", component: HomeComponent },
   { path: "login", component: LoginComponent },
+  { path: "home", component: HomeComponent },
+  { path: "patients", component: PatientSummaryComponent },
   { path: "", redirectTo: "home", pathMatch: "full" }
 ];
 
---
+++ src/app/patients/patient-summary/patient-summary.component.ts
@@ -0,0 +1,10 @@
+import { Component } from "@angular/core";
+
+@Component({
+  selector: "app-patient-summary",
+  standalone: true,
+  imports: [],
+  templateUrl: "./patient-summary.component.html",
+  styleUrl: "./patient-summary.component.css"
+})
+export class PatientSummaryComponent {}
