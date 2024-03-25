4af9da66abf0f690aea4e2779d28e481327a33ce
Jean Honlet <jehon@users.noreply.github.com>
2024-03-25 11:06:52 +0100

    build: fix 127

--- www/maintenance/versions/127 rework stats.sql
+++ www/maintenance/versions/127 rework stats.sql
@@ -3,5 +3,5 @@ TRUNCATE `server_stats`;
 ALTER TABLE `server_stats`
 DROP `last_user`,
 DROP `device`,
-ADD `counter_computer` INT NOT NULL DEFAULT 0,
-ADD `counter_mobile` INT NOT NULL DEFAULT 0;
+ADD `counter_computer` INT NOT NULL,
+ADD `counter_mobile` INT NOT NULL;
