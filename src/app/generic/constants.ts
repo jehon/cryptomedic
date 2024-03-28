7b64a5f2b88545cb1fcd0c06b90dbf2f46d99846
Jean Honlet <jehon@users.noreply.github.com>
2024-03-28 12:05:26 +0100

    feat: add constants

---
+++ src/app/generic/constants.ts
@@ -0,0 +1,13 @@
+export const icons = {
+  models: {
+    Appointment: "/static/img/model_appointment.gif",
+    Bill: "/static/img/model_bill.svg",
+    ClubFoot: "/static/img/model_consult_clubfoot.svg",
+    OtherConsult: "/static/img/model_consult_other.svg",
+    RicketConsult: "/static/img/model_consult_ricket.svg",
+    Patient: "/static/img/model_patient.gif",
+    Payment: "/static/img/payment.gif",
+    Picture: "/static/img/model_picture.svg",
+    Surgery: "/static/img/model_surgery.png"
+  }
+};
