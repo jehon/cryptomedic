177cb658f696779ad1dae6e4ef8e95a65c6fd4f9
Jean Honlet <jehon@users.noreply.github.com>
2024-03-31 22:29:26 +0200

    feat(wip): build dependencies in patient

--- src/app/patients/business/patient.ts
+++ src/app/patients/business/patient.ts
@@ -1,4 +1,5 @@
 import { Type } from "class-transformer";
+import PatientRelated from "./abstracts/patient-related";
 import Pojo from "./abstracts/pojo";
 import Appointment from "./appointment";
 
@@ -27,11 +28,50 @@ export default class Patient extends Pojo {
     return "patient";
   }
 
-  getDependencies() {
-    return [];
+  getDependencies(): PatientRelated[] {
+    return [...this.appointment].toSorted(patientRelatedOrder);
   }
 
   isLocked() {
     return false;
   }
+}
+
+export function patientRelatedOrder(
+  o1: PatientRelated,
+  o2: PatientRelated
+): number {
+  const o1First = -1;
+  const o2First = 1;
+
+  // What to do if one 'date' is missing
+  if (!o1.date && o2.date) {
+    return 20 * o1First;
+  }
+
+  if (!o2.date && o1.date) {
+    return 20 * o2First;
+  }
+
+  // Both 'date' are present
+  if (o1.date && o2.date) {
+    if (o1.date < o2.date) return 30 * o2First;
+    if (o1.date > o2.date) return 30 * o1First;
+  }
+
+  if (o1.created_at < o2.created_at) return 40 * o2First;
+  if (o1.created_at > o2.created_at) return 40 * o1First;
+
+  const o1id = parseInt(o1.id);
+  const o2id = parseInt(o2.id);
+  if (!isNaN(o1id) && !isNaN(o2id)) {
+    if (o1id > o2id) return 50 * o1First;
+    if (o1id < o2id) return 50 * o2First;
+  }
+
+  // Both 'type' are present
+  if (o1.getTechnicalName() < o2.getTechnicalName()) return 40 * o1First;
+  if (o1.getTechnicalName() > o2.getTechnicalName()) return 40 * o2First;
+
+  return 0;
 }
