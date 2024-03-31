50c3b38b59fa54e023171f1345d958493a61fa08
Jean Honlet <jehon@users.noreply.github.com>
2024-03-31 22:16:10 +0200

    chore: remove unused import

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,6 @@
 import { bootstrapApplication } from "@angular/platform-browser";
 import { AppComponent } from "./app/app.component";
 import { appConfig } from "./app/app.config";
-// For class-transformer
-import "reflect-metadata";
 
 bootstrapApplication(AppComponent, appConfig).catch((err) =>
   console.error(err)
