aaf8eddd4d42986c1b2b7e5d5358a92fe417d982
Jean Honlet <jehon@users.noreply.github.com>
2024-04-04 21:55:01 +0200

    fix: string labels

--- src/app/_helpers/strings.spec.ts
+++ src/app/_helpers/strings.spec.ts
@@ -3,6 +3,6 @@ import { technical2Human } from "./strings";
 describe("strings helpers", () => {
   it("technical2Human", () => {
     expect(technical2Human("bill")).toBe("Bill");
-    expect(technical2Human("consult-other")).toBe("Consult Other");
+    expect(technical2Human("consult_other")).toBe("Consult Other");
   });
 });
--- src/app/_helpers/strings.ts
+++ src/app/_helpers/strings.ts
@@ -1,6 +1,6 @@
 export function technical2Human(technical: string): string {
   return technical
-    .split("-")
+    .split("_")
     .map((component) => component[0].toUpperCase() + component.slice(1))
     .join(" ");
 }
