44e01e10b39caa53c46b100c665dea86446a4ff0
Jean Honlet <jehon@users.noreply.github.com>
2024-04-15 16:40:15 +0200

    chore: remove unused import

--- src/app/patients/appointment/appointment.component.ts
+++ src/app/patients/appointment/appointment.component.ts
@@ -1,4 +1,3 @@
-import { NgSwitchCase } from "@angular/common";
 import { Component, Input } from "@angular/core";
 import { IoComponent } from "../../generic/io-string/io.component";
 import Appointment from "../business/appointment";
@@ -7,9 +6,8 @@ import { FilePanelComponent } from "../file-panel/file-panel.component";
 @Component({
   selector: "app-appointment",
   standalone: true,
-  imports: [FilePanelComponent, IoComponent, NgSwitchCase],
-  templateUrl: "./appointment.component.html",
-  styleUrl: "./appointment.component.css"
+  imports: [FilePanelComponent, IoComponent],
+  templateUrl: "./appointment.component.html"
 })
 export class AppointmentComponent {
   @Input()
