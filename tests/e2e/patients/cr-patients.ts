1d149a00e0275b54b7d9c4e4bddf8d925b917c1c
Jean Honlet <jehon@users.noreply.github.com>
2024-04-18 19:35:22 +0200

    fix: test patient

--- tests/e2e/patients/cr-patients.ts
+++ tests/e2e/patients/cr-patients.ts
@@ -1,12 +1,48 @@
 import { Page } from "playwright-core";
+import { expect } from "playwright/test";
 import { LOGINS, crInit } from "../helpers/cr";
 
 export { crInit } from "../helpers/cr";
 
-export function crInitPatient(
+export async function crPatientInit(
   page: Page,
   segment: string,
   { login = LOGINS.PHYSIO }: { login?: string } = {}
 ) {
   return crInit(page, { page: segment, login: login });
+}
+
+export async function crPatientFile(
+  page: Page,
+  patient_id: string | number,
+  uuid: string = `patient.${patient_id}`
+  //   only: boolean = true
+) {
+  await crPatientInit(page, `/patients/${patient_id}#${uuid}`);
+  await expect(page.getByTestId(`${uuid}-open`)).toBeVisible();
+
+  const content = page.getByTestId(`${uuid}-content`);
+  await expect(content).toBeVisible();
+
+  const getIOContent = async (label: string) => {
+    const io = content.locator(`[label='${label}']`);
+    await expect(io).toBeVisible();
+    return io.locator(".content");
+  };
+
+  return {
+    /**
+     * If value is not defined, it is expected to be empty and invisible
+     */
+    async expectFieldValue(label: string, value?: string | number) {
+      const io = await getIOContent(label);
+      if (value) {
+        await expect(io).toBeVisible();
+        await expect(io).toHaveText("" + value);
+      } else {
+        await expect(io).not.toBeVisible();
+      }
+    },
+    async setFieldValue(_field: string, _value: string) {}
+  };
 }
--- tests/e2e/patients/read.spec.ts
+++ tests/e2e/patients/read.spec.ts
@@ -1,7 +1,22 @@
 import { test } from "@playwright/test";
-import { crInitPatient } from "./cr-patients";
+import { crPatientFile } from "./cr-patients";
 
-test("patient page", async ({ page }) => {
-  await crInitPatient(page, "/patients/1");
-  // crDebugHooks(page);
+test("patient", async ({ page }) => {
+  const panel = await crPatientFile(page, 1);
+  await panel.expectFieldValue("Entry Year", 2000);
+  await panel.expectFieldValue("Entry Order", 1);
+  await panel.expectFieldValue("Phone");
+  await panel.expectFieldValue("Pathology", "ClubFoot");
+});
+
+test("appointment", async ({ page }) => {
+  const panel = await crPatientFile(page, 1, "appointment.2");
+  await panel.expectFieldValue("Date", "2015-04-28");
+  await panel.expectFieldValue("Examiner", "Ershad");
+});
+
+test("surgery", async ({ page }) => {
+  const panel = await crPatientFile(page, 1, "surgery.5");
+  await panel.expectFieldValue("Date", "2014-01-02");
+  await panel.expectFieldValue("Diagnostic", "test");
 });
