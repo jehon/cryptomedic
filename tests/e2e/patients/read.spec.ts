450407fa59875a347c9836c2d46d16042d5eb27f
Jean Honlet <jehon@users.noreply.github.com>
2024-04-18 19:45:38 +0200

    refactor: add constant

--- tests/e2e/patients/read.spec.ts
+++ tests/e2e/patients/read.spec.ts
@@ -1,8 +1,11 @@
 import { test } from "@playwright/test";
 import { crPatientFile } from "./cr-patients";
 
+const PATIENT_ID = 1;
+// !! Manque le ClubFoot
+
 test("patient", async ({ page }) => {
-  const panel = await crPatientFile(page, 1);
+  const panel = await crPatientFile(page, PATIENT_ID);
   await panel.expectFieldValue("Entry Year", 2000);
   await panel.expectFieldValue("Entry Order", 1);
   await panel.expectFieldValue("Phone");
@@ -10,13 +13,13 @@ test("patient", async ({ page }) => {
 });
 
 test("appointment", async ({ page }) => {
-  const panel = await crPatientFile(page, 1, "appointment.2");
+  const panel = await crPatientFile(page, PATIENT_ID, "appointment.2");
   await panel.expectFieldValue("Date", "2015-04-28");
   await panel.expectFieldValue("Examiner", "Ershad");
 });
 
 test("surgery", async ({ page }) => {
-  const panel = await crPatientFile(page, 1, "surgery.5");
+  const panel = await crPatientFile(page, PATIENT_ID, "surgery.5");
   await panel.expectFieldValue("Date", "2014-01-02");
   await panel.expectFieldValue("Diagnostic", "test");
 });
