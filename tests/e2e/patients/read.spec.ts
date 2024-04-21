e6b29031eb954e71376889d355f24d0a8507a779
Jean Honlet <jehon@users.noreply.github.com>
2024-04-21 11:41:17 +0200

    test(e2e): add club foot

--- tests/e2e/patients/read.spec.ts
+++ tests/e2e/patients/read.spec.ts
@@ -24,20 +24,14 @@ test("appointment", async ({ page }) => {
 // await panel.expectFieldValue("Examiner", "Ershad");
 // });
 
-// test("consult-clubfoot", async ({ page }) => {
-//   const panel = await crPatientFile(page, PATIENT_ID, "consult-clubfoot.xxx");
-//   await panel.expectFieldValue("Date", "2015-04-28");
-//   await panel.expectFieldValue("Examiner", "Ershad");
-// });
-
 // test("consult-other", async ({ page }) => {
-//   const panel = await crPatientFile(page, PATIENT_ID, "consult-other.1");
+//   const panel = await crPatientFile(page, PATIENT_ID, "consult_other.1");
 //   await panel.expectFieldValue("Date", "2015-04-28");
 //   await panel.expectFieldValue("Examiner", "Ershad");
 // });
 
 // test("consult-ricket", async ({ page }) => {
-//   const panel = await crPatientFile(page, PATIENT_ID, "consult-ricket.13");
+//   const panel = await crPatientFile(page, PATIENT_ID, "consult_ricket.13");
 //   await panel.expectFieldValue("Date", "2015-04-28");
 //   await panel.expectFieldValue("Examiner", "Ershad");
 // });
@@ -52,4 +46,14 @@ test("surgery", async ({ page }) => {
   const panel = await crPatientFile(page, PATIENT_ID, "surgery.5");
   await panel.expectFieldValue("Date", "2014-01-02");
   await panel.expectFieldValue("Diagnostic", "test");
+});
+
+// ----------------
+// TODO: add this on 1st file
+//       and add some data to it
+test("consult-clubfoot", async ({ page }) => {
+  const panel = await crPatientFile(page, 5, "consult_clubfoot.1");
+  await panel.expectFieldValue("Date", "2015-01-10");
+  await panel.expectFieldValue("Examiner", "Ershad");
+  await panel.expectFieldValue("Age at consultation time", "2y");
 });
