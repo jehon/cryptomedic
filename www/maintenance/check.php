2e925254cdf910745b30ee74621792644da0fd17
Jean Honlet <jehon@users.noreply.github.com>
2024-05-16 20:58:50 +0200

    feat: add debug check

--- www/maintenance/check.php
+++ www/maintenance/check.php
@@ -30,3 +30,28 @@ try {
     http_response_code(503);
 }
 ob_flush();
+
+// TODO: remove this
+
+// $ip = "2001:41d0:404:200:0:0:0:2a3f";
+$ip = "135.125.106.114";
+$username = "test";
+$password = "test";
+if (in_array($_REQUEST, "ip")) {
+    $ip = $_REQUEST["ip"];
+}
+if (in_array($_REQUEST, "username")) {
+    $username = $_REQUEST["username"];
+}
+if (in_array($_REQUEST, "password")) {
+    $password = $_REQUEST["password"];
+}
+$dsn = "mysql:host=$ip;dbname=" . $myconfig["database"]["schema"];
+echo "DSN: $dsn<br>\n";
+
+try {
+    $pdo = new PDO($dsn, $username, $password);
+    echo "Connected successfully";
+} catch (PDOException $e) {
+    echo "Connection failed: " . $e->getMessage();
+}
