9c067f142079fddf7199e084d2f67f87d7064339
Jean Honlet <jehon@users.noreply.github.com>
2024-07-04 21:34:50 +0200

    style: fix order of  imports

--- legacy/react/widget/io-string.tsx
+++ legacy/react/widget/io-string.tsx
@@ -1,6 +1,6 @@
 import React from "react";
-import IOAbstract, { IOParams } from "./io-abstract";
 import { Optional } from "../utils/generic-types";
+import IOAbstract, { IOParams } from "./io-abstract";
 
 export default function IOString(options: IOParams<Optional<string>>) {
   return IOAbstract<Optional<string>>(options, {
