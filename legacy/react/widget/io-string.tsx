a24376d5d2f99eab226086ba0e0ea66d2ea8ef69
Jean Honlet <jehon@users.noreply.github.com>
2024-07-07 21:22:19 +0200

    feat: string input

--- legacy/react/widget/io-string.tsx
+++ legacy/react/widget/io-string.tsx
@@ -4,6 +4,11 @@ import IOAbstract, { IOParams } from "./io-abstract";
 
 export default function IOString(options: IOParams<Optional<string>>) {
   return IOAbstract<Optional<string>>(options, {
-    renderOutput: (value) => <div>{value}</div>
+    renderOutput: (value) => <div>{value}</div>,
+    renderInput: (value: Optional<string>) => (
+      <div>
+        <input name={options.name} defaultValue={value as string} />
+      </div>
+    )
   });
 }
