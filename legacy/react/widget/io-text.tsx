b3b419ecd00dfac1e8e4a3f69fd0dce21b80a9dd
Jean Honlet <jehon@users.noreply.github.com>
2024-07-07 22:40:54 +0200

    feat: auto-grow text area

--- legacy/react/widget/io-text.tsx
+++ legacy/react/widget/io-text.tsx
@@ -1,9 +1,24 @@
 import React from "react";
-import IOAbstract, { IOParams } from "./io-abstract";
 import { Optional } from "../utils/generic-types";
+import IOAbstract, { IOParams } from "./io-abstract";
 
 export default function IOText(options: IOParams<string>) {
+  const adjust = (element: HTMLTextAreaElement) => {
+    // const lines = element.value.split("\n").length;
+    // element.setAttribute("rows", lines);
+
+    element.style.height = "0";
+    element.style.height = "calc(" + element.scrollHeight + "px + 2em)";
+  };
+
   return IOAbstract<Optional<string>>(options, {
-    renderOutput: (value) => <pre>{value}</pre>
+    renderOutput: (value) => <pre>{value}</pre>,
+    renderInput: (value: Optional<string>) => (
+      <textarea
+        name={options.name}
+        defaultValue={value ?? ""}
+        onKeyUp={(event) => adjust(event.target as HTMLTextAreaElement)}
+      />
+    )
   });
 }
