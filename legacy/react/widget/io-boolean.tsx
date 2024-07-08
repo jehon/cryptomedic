259e33d418345a7bdb9754934df356dc541ebb6e
Jean Honlet <jehon@users.noreply.github.com>
2024-07-08 20:22:17 +0200

    feat: add io-boolean's

--- legacy/react/widget/io-boolean.tsx
+++ legacy/react/widget/io-boolean.tsx
@@ -1,9 +1,53 @@
+import React from "react";
 import { Optional } from "../utils/generic-types";
 import { ImgBooleanFalse, ImgBooleanTrue } from "./images";
 import IOAbstract, { IOParams } from "./io-abstract";
 
-export default function IOBoolean(props: IOParams<Optional<boolean>>) {
-  return IOAbstract<Optional<boolean>>(props, {
-    renderOutput: (value) => (value ? ImgBooleanTrue() : ImgBooleanFalse())
+export type StringBoolean = string | number;
+
+export default function IOBoolean(
+  options: IOParams<Optional<StringBoolean | boolean>>
+) {
+  return IOAbstract(options, {
+    renderOutput: (value) => (value ? ImgBooleanTrue() : ImgBooleanFalse()),
+    renderInput: (value) => (
+      <>
+        <div className="align">
+          <input
+            className="form-control"
+            name={options.name}
+            defaultChecked={value === true || value === 1}
+            value="1"
+            onBlur={(evt) => options.onChange && options.onChange(true)}
+            type="radio"
+          />
+          <span>Yes</span>
+        </div>
+        <div className="align">
+          <input
+            className="form-control"
+            name={options.name}
+            defaultChecked={value === false || value === 0}
+            value="0"
+            onBlur={(evt) => options.onChange && options.onChange(false)}
+            type="radio"
+          />
+          <span>No</span>
+        </div>
+        {!options.required && (
+          <div className="align">
+            <input
+              className="form-control"
+              name={options.name}
+              defaultChecked={value === null}
+              value=""
+              onBlur={(evt) => options.onChange && options.onChange(false)}
+              type="radio"
+            />
+            <span>Unknown</span>
+          </div>
+        )}
+      </>
+    )
   });
 }
