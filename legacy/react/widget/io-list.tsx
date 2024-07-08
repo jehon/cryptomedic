a100d426eb0134102742995c45ec1d9d293eab71
Jean Honlet <jehon@users.noreply.github.com>
2024-07-08 21:43:31 +0200

    feat: add io-select

--- legacy/react/widget/io-list.tsx
+++ legacy/react/widget/io-list.tsx
@@ -1,12 +1,52 @@
 import React from "react";
 import { Optional } from "../utils/generic-types";
+import { toTitleCase } from "../utils/strings";
 import { StringList } from "../utils/types";
 import IOAbstract, { IOParams } from "./io-abstract";
 
-export default function IOList(
-  options: { list?: string[] } & IOParams<Optional<StringList>>
+// In other part of the application, it could be a mapping
+// Ex: io-boolean, search panels
+export function canonizeList(
+  list: string[] | Record<string, string>
+): Record<string, string> {
+  if (Array.isArray(list)) {
+    return list.reduce((acc, k) => ({ ...acc, [k]: toTitleCase(k) }), {});
+  }
+  return list;
+}
+
+export function buildSelect(
+  list: Record<string, string>,
+  value: string,
+  name: string,
+  onChange: (val: string) => void = () => {}
 ) {
+  return (
+    <select
+      className="form-control"
+      name={name}
+      defaultValue={value ?? ""}
+      onBlur={(evt) => onChange(evt.target.value)}
+    >
+      {Object.entries(list).map(([v, k]) => (
+        <option value={v}>{k}</option>
+      ))}
+    </select>
+  );
+}
+export function buildRadios() {}
+
+export default function IOList(
+  options: { list?: string[] | Record<string, string> } & IOParams<
+    Optional<StringList>
+  >
+) {
+  // TODO: List should be mandatory and thus ?? {} not necessary anymore
+  const list: Record<string, string> = canonizeList(options.list ?? {});
+
   return IOAbstract<Optional<string>>(options, {
-    renderOutput: (value) => <div>{value}</div>
+    renderOutput: (value) => <div>{value}</div>,
+    renderInput: (value) =>
+      buildSelect(list, value ?? "", options.name ?? "", options.onChange)
   });
 }
