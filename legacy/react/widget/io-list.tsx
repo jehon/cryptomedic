3100bb4199955c785f633e624a060c0425dbe9a4
Jean Honlet <jehon@users.noreply.github.com>
2024-07-08 20:26:32 +0200

    feat: prepare io-list

--- legacy/react/widget/io-list.tsx
+++ legacy/react/widget/io-list.tsx
@@ -1,9 +1,12 @@
-import { IOParams } from "./io-abstract";
-import IOString from "./io-string";
+import React from "react";
 import { Optional } from "../utils/generic-types";
+import { StringList } from "../utils/types";
+import IOAbstract, { IOParams } from "./io-abstract";
 
 export default function IOList(
-  options: { list?: string[] } & IOParams<Optional<string>>
+  options: { list?: string[] } & IOParams<Optional<StringList>>
 ) {
-  return IOString(options);
+  return IOAbstract<Optional<string>>(options, {
+    renderOutput: (value) => <div>{value}</div>
+  });
 }
