e7b46d79e619cd2d58ada9053c1b0bba923ddfc9
Jean Honlet <jehon@users.noreply.github.com>
2024-07-08 19:59:29 +0200

    chore: remove unused code

--- legacy/react/styles/style-helpers.ts
+++ legacy/react/styles/style-helpers.ts
@@ -3,24 +3,7 @@ import React from "react";
 export const defaultWidthBox: string = "calc(min(100%, 300px))";
 export const defaultWidthScreen: string = "calc(min(100%, 800px))";
 
-// const integrities: Record<string, string> = {
-//   "5.3.1":
-//     "sha384-4bw+/aepP/YC94hEpVNVgiZdgIC5+VKNBQNGCHeKRQN+PtmoHDEXuppvnDJzQIu9"
-// };
-// const currentVersion = "5.3.1";
-
-// export function getBootstrapStyle(version = currentVersion) {
-//   if (!(version in integrities)) {
-//     console.error("Bootstrap version not found in integrities: ", version);
-//   }
-
-//   return `<link
-//     href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/css/bootstrap.min.css"
-//     integrity="${integrities[version]}"
-//     rel="stylesheet"
-//     crossorigin="anonymous"
-//     >`;
-// }
+// TODO: Remove what is not necessary
 
 // https://coryrylan.com/blog/how-to-use-web-components-with-typescript-and-react
 export type MyWebComponent<T> = Partial<
@@ -38,12 +21,3 @@ type MyCustomEvents<K extends string> = {
 export type MyWebComponentWithEvents<T, K extends string> = Partial<
   MyWebComponent<T> & MyCustomEvents<`on${K}`>
 >;
-
-// declare global {
-//   namespace JSX {
-//     interface IntrinsicElements {
-//       ['x-alert']: MyWebComponent<XAlert>;
-//       ['x-alert']: MyWebComponentWithEvents<XAlert, 'closeChange' | 'openChange'>;
-//     }
-//   }
-// }
