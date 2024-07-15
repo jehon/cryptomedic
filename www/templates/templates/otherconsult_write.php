97c5529960f7b8309536e564b1847d6127f1e83b
Jean Honlet <jehon@users.noreply.github.com>
2024-07-15 22:48:23 +0200

    refactor: push content in write templates

--- www/templates/templates/otherconsult_write.php
+++ www/templates/templates/otherconsult_write.php
@@ -1,4 +1,40 @@
 <?php
-t::setDefaultOption("writeOnly", true);
+// Example: 71154
 
-require_once str_replace("_write", "_fiche", __FILE__);
+t::setDefaultOption("writeOnly", true);
+t::setDefaultOption("baseExpression", "currentFile().");
+t::setDefaultOption("model", "OtherConsult");
+?>
+<div ng-if='errors.dateInTheFuture'>
+	<div class='alert alert-danger' id='errorDateFuture'>Error: The date can not be in the future!</div>
+</div>
+<x-two-columns>
+	<div>
+		<?php require __DIR__ . "/../helpers/consult-introduction.php"; ?>
+		<x-group-panel title='Orthopedic Data'>
+			<?php (new t("OtherConsult.side"))->tr2()->p(); ?>
+			<?php (new t("OtherConsult.joints_or_bones_affected"))->tr2()->p(); ?>
+			<?php (new t("OtherConsult.deformity"))->tr2()->p(); ?>
+			<?php (new t("OtherConsult.articulation_mobility"))->tr2()->p(); ?>
+			<?php (new t("OtherConsult.muscle_strength"))->tr2()->p(); ?>
+			<?php (new t("OtherConsult.pain"))->tr2()->p(); ?>
+			<?php (new t("OtherConsult.walk"))->tr2()->p(); ?>
+			<?php (new t("OtherConsult.xray"))->tr2()->p(); ?>
+		</x-group-panel>
+		<x-group-panel title='Conclusion'>
+			<?php (new t("OtherConsult.performed"))
+       ->tr2("Activities Performed by the Patient")
+       ->p(); ?>
+			<?php (new t("OtherConsult.not_performed"))
+       ->tr2("Activities NOT Performed by the Patient")
+       ->p(); ?>
+		</x-group-panel>
+	</div>
+	<div>
+		<x-ff-patient-related></x-ff-patient-related>
+		<x-ff-next-appointment></x-ff-next-appointment>
+		<x-group-panel title='Conclusion'>
+			<?php require __DIR__ . "/../helpers/consult-conclusion.php"; ?>
+		</x-group-panel>
+	</div>
+</x-two-columns>
--- www/templates/templates/patient_write.php
+++ www/templates/templates/patient_write.php
@@ -1,4 +1,92 @@
 <?php
 t::setDefaultOption("writeOnly", true);
+t::setDefaultOption("baseExpression", "folder.getPatient().");
+?>
+<x-two-columns>
+	<div>
+		<x-group-panel title='General data'>
+			<x-fff-field ng-if="patient_id > -1" label='Entry Number'>
+				<div><?php (new t("Patient.entry_year"))->read()->p(); ?>-<?php (new t(
+    "Patient.entry_order"
+))
+    ->read()
+    ->p(); ?></div>
+			</x-fff-field>
+			<x-fff-field ng-if="patient_id == -1" label='Entry Year'>
+				<div>
+					<x-message level='info'>
+						You asked the system to generate a reference for you. This reference will be generated
+						when you will save this file. I just need the year to build up the reference.
+						<?php (new t("Patient.entry_year", ["required" => "required"]))
+          ->write()
+          ->p(); ?>
+					</x-message>
+				</div>
+			</x-fff-field>
+			<?php (new t("Patient.name"))->tr2("Name")->p(); ?>
+			<?php (new t("Patient.sex"))->tr2()->p(); ?>
+			<x-fff-field label='Year of birth'>
+				<div>
+					{{folder.getPatient().year_of_birth}}
+					<div class='not-mode-read'>
+						<h5>Calculate year of birth</h5>
+						<div><input ng-model='age.years' type='number' min='0' max='100'> years</div>
+						<div><input ng-model='age.months' type='number' min='-1' max='12'> months</div>
+					</div>
+				</div>
+			</x-fff-field>
+			<div class='not-mode-read'>Calculate year of birth</div>
+			<x-fff-field class='not-mode-write' label='Age (today)' variable>
+				<div id='#ageToday' variable>
+					<x-fff-age></x-fff-age> old
+				</div>
+			</x-fff-field>
+			<x-fff-field field='address_district' label='District'>
+				<div>
+					<span class='not-mode-read'>
+						<x-write-list value='{{folder.getPatient().address_district}}' name='address_district' list-name='Districts' nullable></x-write-list>
+					</span>
+					<span class='not-mode-write'>
+						<?php (new t("Patient.address_district"))->read()->p(); ?>
+					</span>
+				</div>
+			</x-fff-field>
+			<x-fff-field field='address_upazilla' label='Upazilla'>
+				<div>
+					<span class='not-mode-read'>
+						<x-write-list value='{{folder.getPatient().address_upazilla}}' name='address_upazilla' nullable></x-write-list>
+					</span>
+					<span class='not-mode-write'>
+						<?php (new t("Patient.address_upazilla"))->read()->p(); ?>
+					</span>
+				</div>
+			</x-fff-field>
+			<x-fff-field field='address_union' label='Union'>
+				<div>
+					<span class='not-mode-read'>
+						<x-write-list value='{{folder.getPatient().address_union}}' name='address_union' nullable></x-write-list>
+					</span>
+					<span class='not-mode-write'>
+						<?php (new t("Patient.address_union"))->read()->p(); ?>
+					</span>
+				</div>
+			</x-fff-field>
+			<?php (new t("Patient.phone"))->tr2()->p(); ?>
+			<?php (new t("Patient.address_comments"))->tr2("Adress Notes")->p(); ?>
+		</x-group-panel>
+	</div>
+	<div>
+		<x-group-panel id='PatientPathology' ng-class='{ jserror: errors.noPathology }' title='Pathology'>
+			<?php (new t("Patient.pathology"))->tr2("Main Pathology")->p(); ?>
+			<?php (new t("Patient.comments"))->tr2("Comments")->p(); ?>
+		</x-group-panel>
+		<br />
+	</div>
+</x-two-columns>
+<jh-script>
+	document.querySelector("x-write-list[name=address_upazilla]")
+	.follow(document.querySelector("x-write-list[name=address_district]"), "district");
 
-require_once str_replace("_write", "_fiche", __FILE__);
+	document.querySelector("x-write-list[name=address_union]")
+	.follow(document.querySelector("x-write-list[name=address_upazilla]"), "upazilla");
+</jh-script>
