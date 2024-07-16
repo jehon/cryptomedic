26edf863e2062d65e5ead99654e9159c9bdb6c6d
Jean Honlet <jehon@users.noreply.github.com>
2024-07-16 10:50:18 +0200

    Revert "refactor: push content in write templates"
    This reverts commit 97c5529960f7b8309536e564b1847d6127f1e83b.

--- www/templates/templates/appointment_write.php
+++ www/templates/templates/appointment_write.php
@@ -1,26 +1,4 @@
 <?php
 t::setDefaultOption("writeOnly", true);
-t::setDefaultOption("baseExpression", "currentFile().");
-?>
-<x-two-columns ng-controller='ctrl_file_appointment'>
-	<x-group-panel title='Next appointment'>
-		<x-fff-field variable label='Today' class='not-mode-read'>
-			<div>{{today}}</div>
-		</x-fff-field>
-		<?php (new t("Appointment.purpose"))->tr2("Purpose")->p(); ?>
-		<?php (new t("Appointment.date"))->tr2("Next Appointment")->p(); ?>
-		<?php (new t("Appointment.center"))->tr2("Next Center")->p(); ?>
-		<x-fff-field>
-			<div>
-				<x-button action='Alternate' ng-click='nextMonth(3)'>Plan in 3 months</x-button>
-				<x-button action='Alternate' ng-click='nextMonth(6)'>Plan in 6 months</x-button>
-			</div>
-		</x-fff-field>
-	</x-group-panel>
-	<div>
-		<x-ff-patient-related></x-ff-patient-related>
-		<x-ff-next-appointment></x-ff-next-appointment>
-		<div>
-		</div>
-	</div>
-</x-two-columns>
+
+require_once str_replace("_write", "_fiche", __FILE__);
