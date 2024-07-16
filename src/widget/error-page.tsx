610d91496801ba50c89d7fb07a623fff0514ff82
Jean Honlet <jehon@users.noreply.github.com>
2024-07-16 21:46:52 +0200

    chore: add message in error page
    to say it is react related

--- src/widget/error-page.tsx
+++ src/widget/error-page.tsx
@@ -21,7 +21,7 @@ export default function ErrorPage() {
   return (
     <div id="error-page">
       <h1>Oops!</h1>
-      <p>Sorry, an unexpected error has occurred.</p>
+      <p>Sorry, an unexpected error has occurred (react application).</p>
       <p>
         <i>{errorMessage(error)}</i>
       </p>
