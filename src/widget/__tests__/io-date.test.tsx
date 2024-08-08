85c0d0ed255964f47dccf85572b5ba706f3f6cc1
Jean Honlet <jehon@users.noreply.github.com>
2024-08-08 21:36:32 +0200

    fix: readonly test

--- src/widget/__tests__/io-date.test.tsx
+++ src/widget/__tests__/io-date.test.tsx
@@ -3,19 +3,19 @@
  */
 
 import { expect, test } from "@jest/globals";
-import React from "react";
-import { render, screen } from "@testing-library/react";
 import "@testing-library/jest-dom/jest-globals";
+import { render, screen } from "@testing-library/react";
+import React from "react";
 
 import IODate from "../io-date";
 
 // https://jestjs.io/fr/docs/expect
 test("io-date", () => {
-  render(<IODate label="test" value={new Date(2023, 0, 2)} />);
+  render(<IODate readonly label="test" value={new Date(2023, 0, 2)} />);
   expect(screen.getByText("Jan 02, 2023")).toBeInTheDocument();
 });
 
 test("empty", () => {
-  const { container } = render(<IODate label="test" value={null} />);
+  const { container } = render(<IODate readonly label="test" value={null} />);
   expect(container).toBeEmptyDOMElement();
 });
