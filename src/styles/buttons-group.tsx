58e1dda1c5baf3a22f0d98e8f03147a8fef99741
Jean Honlet <jehon@users.noreply.github.com>
2024-08-13 08:59:34 +0200

    style: set buttons min-width

--- src/styles/buttons-group.tsx
+++ src/styles/buttons-group.tsx
@@ -7,7 +7,7 @@ export default function ButtonsGroup({
 }) {
   // https://getbootstrap.com/docs/4.0/components/button-group/
   return (
-    <div className="text-right">
+    <div className="nav justify-content-end">
       <div className="buttons btn-group" role="group">
         {children}
       </div>
