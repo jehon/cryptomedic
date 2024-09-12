0a36cbd2c7d9ea09c4fba61263c04aa0642b2489
Jean Honlet <jehon@users.noreply.github.com>
2024-09-12 16:46:55 +0200

    feat: show functions only in output mode

--- src/widget/io-function.tsx
+++ src/widget/io-function.tsx
@@ -1,10 +1,16 @@
-import IOAbstract, { IOPropsReadonly } from "./io-abstract";
+import { useContext } from "react";
+import IOAbstract, { EditContext, IOPropsReadonly } from "./io-abstract";
 
 type T = () => string;
 
 export default function IOFunction(props: IOPropsReadonly<T>) {
   if (typeof props.value != "function") {
     throw new Error(`Invalid value in IOFunction: ${props.value}}`);
+  }
+
+  const editContext = useContext(EditContext);
+  if (editContext) {
+    return <></>;
   }
 
   return IOAbstract<T>(props, {
