4a9fb69e84d4df20200a571eb8ebb40dc1ac79b7
Jean Honlet <jehon@users.noreply.github.com>
2024-09-20 13:22:43 +0200

    chore: simplify session

--- src/utils/session.js
+++ src/utils/session.js
@@ -1,35 +1,6 @@
 import duix from "duix";
+import { deepFreeze } from "./objects.js";
 const SESSION = "session";
-
-// Thanks to https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Object/freeze
-/**
- * @template {object} T
- * @param {T} object to be freezed
- * @returns {T} The object being freezed
- */
-export function deepFreeze(object) {
-  var propNames = Object.getOwnPropertyNames(object);
-  for (let name of propNames) {
-    let value = object[name];
-
-    if (value && typeof value === "object") {
-      deepFreeze(value);
-    }
-  }
-
-  return Object.freeze(object);
-}
-
-/**
- * Make a deep copy of the object
- *
- * @template {object} T
- * @param {T} object to be copied
- * @returns {T} The object being copied
- */
-export function deepCopy(object) {
-  return JSON.parse(JSON.stringify(object));
-}
 
 /**
  * @param {object} session - the object to be stored
