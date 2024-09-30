7b2f09458fa337526555e9d12677c7c5add8bb51
Jean Honlet <21492958-jeanhonlet@users.noreply.gitlab.com>
2024-09-30 14:42:00 +0200

    fix: enrich prices

--- src/business/price.ts
+++ src/business/price.ts
@@ -16,6 +16,12 @@ export default class Price extends Pojo {
 
   date_from?: StringDate;
   date_to?: StringDate;
+  social_level_threshold_1: number = 0;
+  social_level_threshold_2: number = 0;
+  social_level_threshold_3: number = 0;
+  social_level_threshold_4: number = 0;
+  social_level_threshold_5: number = 0;
+
   // We can not be more precise because
   // if not any, other fields will conflict
   [x: string]: any;
