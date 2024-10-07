3cde472fed701062d30b38a83c79c30b62aa9e2b
Jean Honlet <21492958-jeanhonlet@users.noreply.gitlab.com>
2024-10-07 09:05:56 +0200

    chore: clean up script

--- conf/database/dev/0 reset.sql
+++ conf/database/dev/0 reset.sql
@@ -3,6 +3,7 @@ USE mysql;
 -- ---------------------------------------
 -- Schema
 -- ---------------------------------------
+--
 DROP SCHEMA IF EXISTS cryptomedic;
 
 CREATE SCHEMA cryptomedic;
@@ -10,14 +11,9 @@ CREATE SCHEMA cryptomedic;
 -- ---------------------------------------
 -- Users
 -- ---------------------------------------
--- CREATE USER IF NOT EXISTS 'phpmyadmin' IDENTIFIED BY 'phpmyadmin'";
--- GRANT ALL PRIVILEGES ON *.* TO phpmyadmin";
+--
 CREATE USER IF NOT EXISTS 'mysql_cryptomedic_username' IDENTIFIED BY 'mysql_cryptomedic_password';
 
 GRANT ALL PRIVILEGES ON cryptomedic.* TO mysql_cryptomedic_username;
 
 FLUSH PRIVILEGES;
-
---  Mysql 5.7, 7.0:
--- 	ALTER USER 'root' IDENTIFIED WITH mysql_native_password BY 'password';
---  	ALTER USER 'root'@'localhost' IDENTIFIED WITH mysql_native_password BY 'password';
