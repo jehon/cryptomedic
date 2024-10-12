3f768baa0f3c96d8c5382d155499298757b5eaad
Jean Honlet <21492958-jeanhonlet@users.noreply.gitlab.com>
2024-10-12 02:33:25 +0200

    test: use fullTestRead in picture

--- tests/e2e/patients/patient-picture.spec.ts
+++ tests/e2e/patients/patient-picture.spec.ts
@@ -1,14 +1,16 @@
-import { expect, test } from "@playwright/test";
-import { crApiLogin, outputDate } from "../helpers/e2e";
-import { E2EPatient } from "./e2e-patients";
+import { test } from "@playwright/test";
+import { fullTestRead } from "../helpers/e2e-file-panel";
+import { outputDate } from "./e2e-patients";
 
-test("2000-001.picture.2", async ({ page }) => {
-  await crApiLogin(page);
-  const e2eFile = await new E2EPatient(page, 1).getFile("picture", 2).go();
-
-  await e2eFile.expectOutputValue("Date", outputDate("2014-11-04"));
-  await e2eFile.expectOutputValue("File", "10_2014-11-06_15-32-45.JPG");
-  await e2eFile.expectOutputValue("Type");
-  await expect(e2eFile.form).toHaveScreenshot();
-  await expect(e2eFile.panel).toHaveScreenshot();
-});
+test("2000-001.picture.2", ({ page }) =>
+  fullTestRead({
+    page,
+    patientId: 1,
+    fileType: "picture",
+    fileId: 2,
+    data: {
+      Date: outputDate("2014-11-04"),
+      File: "10_2014-11-06_15-32-45.JPG",
+      Type: undefined
+    }
+  }).then(() => {}));
