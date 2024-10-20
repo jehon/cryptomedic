1294d55eb4327016e6fbcb60665f476d306f226b
Jean Honlet <21492958-jeanhonlet@users.noreply.gitlab.com>
2024-10-20 13:28:06 +0200

    chore: docs it

--- src/business/consult-ricket.ts
+++ src/business/consult-ricket.ts
@@ -24,12 +24,15 @@ export default class ConsultRicket extends Consult {
   // Right
   right_leg: StringList = "";
   right_leg_angle: StringNumber = "";
+
+  // TODO: rename right_*
   cross_right_T: StringNumber = "";
   cross_right_F: StringNumber = "";
 
   // Left
   left_leg: StringList = "";
   left_leg_angle: StringNumber = "";
+  // TODO: rename left_*
   cross_left_T: StringNumber = "";
   cross_left_F: StringNumber = "";
 }
