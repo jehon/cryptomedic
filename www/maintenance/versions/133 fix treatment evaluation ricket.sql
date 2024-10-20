8e3b10d5e371a8ab9853d10132c768c9e0c3eea8
Jean Honlet <21492958-jeanhonlet@users.noreply.gitlab.com>
2024-10-20 20:01:46 +0200

    fix: treatment evaluation

---
+++ www/maintenance/versions/133 fix treatment evaluation ricket.sql
@@ -0,0 +1,5 @@
+ALTER TABLE `ricket_consults`
+CHANGE `treatment_evaluation` `treatment_evaluation` VARCHAR(128) NULL DEFAULT NULL;
+
+ALTER TABLE `club_feet`
+CHANGE `treatment_evaluation` `treatment_evaluation` VARCHAR(128) NULL DEFAULT NULL;
