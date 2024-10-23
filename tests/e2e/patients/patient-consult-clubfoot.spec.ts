f72c13ec7264c1aaa4c9a4f03731ce91ba661fc1
Jean Honlet <21492958-jeanhonlet@users.noreply.gitlab.com>
2024-10-23 20:04:40 +0200

    test(e2e): test clubfoot

--- tests/e2e/patients/patient-consult-clubfoot.spec.ts
+++ tests/e2e/patients/patient-consult-clubfoot.spec.ts
@@ -1,4 +1,8 @@
-import { ConsultFieldsConfigType, fullTest } from "../helpers/e2e-file-panel";
+import {
+  consultBasicData,
+  ConsultFieldsConfigType,
+  fullTest
+} from "../helpers/e2e-file-panel";
 
 const ctx = fullTest({
   fileType: "consult_clubfoot",
@@ -23,31 +27,31 @@ ctx.testRead({
   }
 });
 
-// ctx.testCreateDelete({
-//   patientId: 106,
-//   data: {
-//     ...consultBasicData,
-//     "Plantar Flexion Max Right": 15,
-//     "Jumping One Leg Right": "1",
-//     "Jumping One Leg Left": "2",
-//     "Run Left": "0"
-//   }
-// });
+ctx.testCreateDelete({
+  patientId: 106,
+  data: {
+    ...consultBasicData,
+    "Plantar Flexion Max Right": 15,
+    "Jumping One Leg Right": 1,
+    "Jumping One Leg Left": 2,
+    "Run Left": 0
+  }
+});
 
-// ctx.testUpdate({
-//   patientEntryOrder: "2010-06",
-//   patientId: 106,
-//   fileId: 106,
-//   dataInitial: {
-//     ...consultBasicData,
-//     "Jumping One Leg Right": 1,
-//     "Jumping One Leg Left": 2,
-//     "Run Left": 0
-//   },
-//   dataUpdated: {
-//     ...consultBasicData,
-//     "Jumping One Leg Right": 0,
-//     "Jumping One Leg Left": 1,
-//     "Run Left": 2
-//   }
-// });
+ctx.testUpdate({
+  patientEntryOrder: "2010-06",
+  patientId: 106,
+  fileId: 106,
+  dataInitial: {
+    ...consultBasicData,
+    "Jumping One Leg Right": 1,
+    "Jumping One Leg Left": 2,
+    "Run Left": 0
+  },
+  dataUpdated: {
+    ...consultBasicData,
+    "Jumping One Leg Right": 0,
+    "Jumping One Leg Left": 1,
+    "Run Left": 2
+  }
+});
