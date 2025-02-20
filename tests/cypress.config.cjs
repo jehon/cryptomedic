fc55ab03b95a53a6864bc5428a8880d1755dd962
Jean Honlet <21492958-jeanhonlet@users.noreply.gitlab.com>
2025-02-20 11:53:22 +0100

    build: simplify cypress build 2

--- tests/cypress.config.cjs
+++ tests/cypress.config.cjs
@@ -1,11 +1,21 @@
 const { defineConfig } = require("cypress");
-
 const { renameSync } = require("fs");
+const path = require("path");
 
 module.exports = defineConfig({
   viewportWidth: 1280,
   viewportHeight: 800,
   video: false,
+  videoFolder: path.join(
+    __dirname,
+    "..",
+    "tmp/integration/desktop/runtime/videos"
+  ),
+  screenshotsFolder: path.join(
+    __dirname,
+    "..",
+    "tmp/integration/desktop/runtime/screenshots"
+  ),
   e2e: {
     // Note: BaseUrl is overriden in docker-compose
     baseUrl: "http://localhost:8085",
