54dff76f3fb7cb503f1700706442d8b26bc2cc5b
Jean Honlet <21492958-jeanhonlet@users.noreply.gitlab.com>
2025-02-21 17:49:41 +0100

    build: allow cli usage

--- www/maintenance/lib/protect.php
+++ www/maintenance/lib/protect.php
@@ -4,14 +4,16 @@ require_once __DIR__ . "/../../config.php";
 
 global $myconfig;
 
-if (!$myconfig["security"]["key"]) {
-  die("No security.admin configured");
-}
+if (php_sapi_name() != "cli") {
+  if (!$myconfig["security"]["key"]) {
+    die("No security.admin configured");
+  }
 
-if (!array_key_exists("pwd", $_REQUEST)) {
-  die("Not allowed");
-}
+  if (!array_key_exists("pwd", $_REQUEST)) {
+    die("Not allowed");
+  }
 
-if ($_REQUEST["pwd"] != $myconfig["security"]["key"]) {
-  die("Not allowed");
+  if ($_REQUEST["pwd"] != $myconfig["security"]["key"]) {
+    die("Not allowed");
+  }
 }
