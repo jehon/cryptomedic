3944c97fc9ebfdb728f9563070e466abe3d70d56
Jean Honlet <jehon@users.noreply.github.com>
2025-04-20 17:24:24 +0200

    fix: type

--- src/app-patient/blocs/button-context.ts
+++ src/app-patient/blocs/button-context.ts
@@ -1,9 +1,9 @@
-import type Pojo from "../../business/abstracts/pojo";
+import type PatientRelated from "../../business/abstracts/patient-related";
 import type Folder from "../../business/folder";
 
 export type ButtonContext = {
   folder: Folder; // TODO: remove
-  staticType: typeof Pojo; // TODO: remove
+  staticType: typeof PatientRelated; // TODO: remove
   title: string;
   migrationUrlHash: string; // TODO: remove
   editMode: boolean;
