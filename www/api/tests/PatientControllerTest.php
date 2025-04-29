674c3b5505edd884fe274b4eac71932a316c4ca6
Jean Honlet <jehon@users.noreply.github.com>
2025-04-29 16:05:20 +0200

    feat: test creating a ref with entry_year

--- www/api/tests/PatientControllerTest.php
+++ www/api/tests/PatientControllerTest.php
@@ -8,7 +8,7 @@ class PatientControllerTest extends RouteReferenceTestCase {
     $response = $this->myRunAssertQuery(
         $this->getNewRequestOptionsBuilder()
           	->setUrl("patient/1")
-        	->asUnauthenticated()
+        		->asUnauthenticated()
           	->setExpected(401)
           	->asText()
       );
@@ -22,11 +22,24 @@ class PatientControllerTest extends RouteReferenceTestCase {
 		$this->assertEquals(1, $json['id']);
 	}
 
-  	public function test6() {
-	    $opt = $this->getNewRequestOptionsBuilder()
-    	  	->withReference()
-	      	->setUrl("patient/6");
-    	$json = $this->myRunAssertQueryForRoles($opt);
-    	$this->assertEquals(6, $json['id']);
-  	}
+	public function test6() {
+		$opt = $this->getNewRequestOptionsBuilder()
+				->withReference()
+				->setUrl("patient/6");
+		$json = $this->myRunAssertQueryForRoles($opt);
+		$this->assertEquals(6, $json['id']);
+	}
+
+	public function testCreateRef() {
+		$json = $this->myRunAssertQuery($this->getNewRequestOptionsBuilder()
+			->withReference([ "id", "newKey", "entry_order"])
+			->setRole("cdc")
+			->setUrl("fiche/patient")
+			->setMethod("POST")
+			->setParams([
+				"entry_year" => 2025
+		]));
+		$this->assertEquals(2025, $json["folder"][0]["record"]["entry_year"]);
+		$this->assertGreaterThanOrEqual(1000, $json["folder"][0]["record"]["entry_order"]);
+	}
 }
