f11eeb67e78099901574f70d4f39055b331fdd95
Jean Honlet <jehon@users.noreply.github.com>
2025-05-08 15:26:04 +0200

    chore: remove functions

--- src/business/abstracts/pojo.ts
+++ src/business/abstracts/pojo.ts
@@ -1,6 +1,5 @@
 import { plainToInstance } from "class-transformer";
-import { immerable, produce } from "immer";
-import { request } from "../../utils/network";
+import { immerable } from "immer";
 import { type StringDate } from "../../utils/types";
 
 export default class Pojo {
@@ -45,50 +44,4 @@ export default class Pojo {
   getParentId(): string | undefined {
     return undefined;
   }
-
-  getList<T extends Pojo>(list: keyof this): T[] {
-    return this[list] as T[];
-  }
-
-  withFile(
-    file: Pojo,
-    listName: keyof this = file.getStatic().getTechnicalName() as keyof this
-  ): this {
-    //
-    // We remove and add in one run
-    // to avoid building twice the folder
-    //
-    return produce(this.withoutFile(file, listName), (draft: this) => {
-      draft.getList(listName).push(file);
-    });
-  }
-
-  withoutFile(
-    file: Pojo,
-    listName: keyof this = file.getStatic().getTechnicalName() as keyof this
-  ): this {
-    const i = this.getList(listName).findIndex(
-      (val: Pojo) => val.uid() === file.uid()
-    );
-    if (i < 0) {
-      return this;
-    }
-
-    return produce(this, (draft: this) => {
-      draft.getList(listName).splice(i, 1);
-    });
-  }
-
-  /*********************
-   * CRUD Methods
-   */
-  static get<T extends Pojo>(id: string): Promise<T> {
-    return request({ url: [this.getTechnicalName(), id] }).then(
-      this.factory<T>
-    );
-  }
-}
-
-export function filterById(list: Pojo[], id: string) {
-  return list.filter((p) => p.id == id)[0];
 }
--- src/business/patient.test.ts
+++ src/business/patient.test.ts
@@ -1,8 +1,6 @@
 import assert from "assert";
 import test, { beforeEach } from "node:test";
 import { loadReference, RefPatient1 } from "../test-helper";
-import { filterById } from "./abstracts/pojo";
-import Appointment from "./appointment";
 import Patient from "./patient";
 
 // DELETE is tested in generate-patient.spec.ts
@@ -21,31 +19,4 @@ test("should instantiate patient", () => {
 
 test("should have loaded Mock data", () => {
   assert.equal(p.id, "1");
-});
-
-test("Copy with new file", function () {
-  assert.equal(p.id, "1");
-  const fap = filterById(p.appointment, "2");
-  assert(fap instanceof Appointment);
-  assert.equal(fap.id, 2);
-  assert.equal(fap.purpose, null);
-
-  const p2 = p.withFile(
-    Appointment.factory({
-      id: "2",
-      examiner: "test",
-      purpose: "test"
-    })
-  );
-
-  assert(p2 instanceof Patient);
-  const fap2 = filterById(p2.appointment, "2");
-  assert(fap2 instanceof Appointment);
-  assert.equal(fap2.examiner, "test");
-  assert.equal(fap2.purpose, "test");
-
-  // Initial is not changed
-  const fap3 = filterById(p.appointment, "2");
-  assert(fap3 instanceof Appointment);
-  assert.equal(fap3.purpose, null);
 });
