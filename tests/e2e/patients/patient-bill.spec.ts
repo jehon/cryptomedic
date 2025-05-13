77485871fdbb1571bf10fe1f2d22f455814bd318
Jean Honlet <jehon@users.noreply.github.com>
2025-05-13 22:10:51 +0200

    test: bill create and delete

--- tests/e2e/patients/patient-bill.spec.ts
+++ tests/e2e/patients/patient-bill.spec.ts
@@ -28,96 +28,17 @@ ctx.testRead({
   }
 });
 
-// 108
-
-// context("Actions", () => {
-//   beforeEach(() => {
-//     crLoginInBackground(crApiLogin.PHYSIO);
-
-//     crApiFolderGet(patientFilesCrud.id).then((data) => {
-//       data.folder
-//         .filter((r) => r.type != "Patient")
-//         .map((r) => crApiFicheDelete(r.type, r.id));
-//     });
-
-//     patientgo(patientFilesCrud);
-
-//     cy.get("#button_add").should("be.visible").click();
-//     cy.get("#add_bill").should("be.visible").click();
-//     cy.get("form#fileForm input").should("be.visible");
-//   });
-
-//   function setDate(val) {
-//     cy.get("#Bill_date")
-//       .should("be.visible")
-//       .invoke("attr", "value", val)
-//       .within(() => {
-//         cy.get("input").focus();
-//         cy.get("input").blur();
-//       });
-//   }
-
-//   it("add a bill without a date is an error", () => {
-//     setDate("");
-
-//     cy.get("[priceFor]").should("not.exist");
-//     cy.get("#errorNoDate").should("be.visible");
-//     cy.crCompareSnapshot("without-date");
-//   });
-
-//   // it.skip("try to set date in the future", () => {
-//   //   setDate("2100-01-01");
-//   //   cy.get("#errorDateFuture").should("be.visible");
-//   // });
-
-//   it("add a price normally", () => {
-//     setDate("2015-01-01");
-
-//     cy.get("[name=consult_give_appointment]").should("exist");
-//     cy.get("#errorNoDate").should("not.exist");
-//     cy.get("#errorDateFuture").should("not.exist");
-
-//     cy.get("#Bill_consult_home_visit").clear();
-//     cy.get("#Bill_consult_home_visit").type("1");
-
-//     cy.get("#total_calculated_raw").should("contain.text", 150);
-
-//     cy.get("#Bill_other_other_consultation_care").clear();
-//     cy.get("#Bill_other_other_consultation_care").type("15");
-
-//     cy.get("#total_calculated_raw").should("contain.text", 165);
-
-//     // Let's play with social_level
-//     cy.log("Let's play with Social Level");
-
-//     cy.get("[title=Summary]")
-//       .should("be.visible")
-//       .within(() => {
-//         cy.get("#Bill_social_level").should("contain.text", 4);
-//         cy.get("#percentage").should("contain.text", "100%");
-//         cy.get("#total_calculated_asked").should("contain.text", "165");
-//       });
-
-//     cy.get('[title="Social Data"]')
-//       .should("be.visible")
-//       .within(() => {
-//         cy.get("#Bill_sl_family_salary").clear();
-//         cy.get("#Bill_sl_family_salary").type("3000");
-
-//         cy.get("#Bill_sl_number_of_household_members").clear();
-//         cy.get("#Bill_sl_number_of_household_members").type("3");
-
-//         cy.get("x-fff-salary-ratio").should("contain.text", "1000");
-//         cy.get("#social_level_calculated").should("contain.text", 2);
-//       });
-
-//     cy.get("[title=Summary]")
-//       .should("be.visible")
-//       .within(() => {
-//         cy.get("#Bill_social_level").should("contain.text", 2);
-//         cy.get("#percentage").should("contain.text", "40%");
-//         cy.get("#total_calculated_asked").should("contain.text", "66");
-//       });
+ctx.testCreateDelete({
+  patientId: 108,
+  data: {
+    Date: "2008-02-10",
+    Examiner: "Ershad",
+    Center: "Ramu",
+    "Family Salary": "2000",
+    "Number of Household Members": "5"
+  },
+  initialIsAlreadyGood: true
+});
 
 //     // Payment
 //     cy.get("#first_payment").type("20");
