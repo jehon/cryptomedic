632d67153ac9310be70cdbde05e5bd6caf5eb469
Jean Honlet <jehon@users.noreply.github.com>
2025-05-14 11:28:44 +0200

    refactor: test

--- src/helpers.test.ts
+++ src/helpers.test.ts
@@ -1,36 +1,27 @@
 import assert from "assert";
-import Pojo from "./business/abstracts/pojo";
 import Folder from "./business/folder";
-import Patient from "./business/patient";
 
 export const RefFolder1 = {
-  name: "FolderTest.test1.json",
-  type: Folder
+  name: "FolderTest.test1.json"
 };
 // export const RefPatient1 = {
 //   name: "PatientControllerTest.test1.json",
-//   type: Patient
 // };
 
 const refRoot = "../www/api/tests/references/";
 
-export async function loadReference<T extends Pojo = Patient>({
-  name,
-  type
+export async function loadReference({
+  name
 }: {
   name: string;
-  type: { new (): Pojo };
-}): Promise<T> {
+}): Promise<Folder> {
   const valid_respond = await import(refRoot + name);
 
   if (valid_respond == null) {
     throw new Error("The reference " + name + " is empty or not found");
   }
 
-  const model =
-    type == Folder
-      ? (new Folder(valid_respond.default.folder) as unknown as T) // Can not build with factory as others
-      : ((type as typeof Pojo).factory(valid_respond.default) as T);
-  assert.ok(model instanceof type);
+  const model = new Folder(valid_respond.default.folder);
+  assert.ok(model instanceof Folder);
   return model;
 }
