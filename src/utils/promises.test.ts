ba3eefef0d25593d435000e1089d1b96b9e4f85d
Jean Honlet <jehon@users.noreply.github.com>
2025-05-16 19:27:55 +0200

    feat: add promises delay

--- src/utils/promises.test.ts
+++ src/utils/promises.test.ts
@@ -1,7 +1,11 @@
 import assert from "node:assert";
 import test from "node:test";
-import { passThrough } from "./promises";
+import { delayed, passThrough } from "./promises";
 
 test("promise", async () => {
   assert.equal(await Promise.resolve(123).then(passThrough(() => 456)), 123);
+});
+
+test("promise", async () => {
+  assert.equal(await Promise.resolve(123).then(delayed(0.1)), 123);
 });
