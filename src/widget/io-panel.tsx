7d3b74dcba053ba10b8bfaac1be8c08120df1951
Jean Honlet <jehon@users.noreply.github.com>
2025-05-20 11:19:29 +0200

    refactor(wip): merge buttons into io-panel
    Still need to simplify it

--- src/widget/io-panel.tsx
+++ src/widget/io-panel.tsx
@@ -1,12 +1,155 @@
 import { useEffect, useRef, useState } from "react";
 import { ButtonGroup } from "react-bootstrap";
+import { CrudLoader } from "../app-patient/loaders-patient";
 import type { Pojo } from "../app-patient/objects-patient";
 import { icons, type2Title, type BusinessType } from "../config";
 import { date2HumanString, normalizeDate } from "../utils/date";
-import ButtonsEdit from "./buttons-edit";
-import ButtonsView, { type ButtonContext } from "./buttons-view";
+import { passThrough } from "../utils/promises";
+import ActionButton from "./action-button";
+import ActionConfirm from "./action-confirm";
 import { EditContext } from "./io-abstract";
+import notification from "./notification";
 import Panel from "./panel";
+
+type ButtonContext<T> = {
+  apiRootUrl: string;
+  type: BusinessType;
+  file: T;
+  title: string;
+  edit: boolean;
+  onCreated: (file: T) => void;
+  onDeleted: (file: T) => void;
+  onUpdated: (file: T) => void;
+  setEdit: (edit: boolean) => void;
+};
+
+function ButtonsView<T extends Pojo>(
+  props: ButtonContext<T> & {
+    file: T;
+    onUpdated: (file: T) => void;
+    canBeLocked: boolean;
+  }
+) {
+  const crudLoader = new CrudLoader<T>(props.apiRootUrl, props.type);
+  let isLocked = false;
+  if (props.canBeLocked) {
+    if (props.file.updated_at) {
+      const dlock = new Date(props.file.updated_at);
+      dlock.setDate(dlock.getDate() + 35);
+      isLocked = dlock < new Date();
+    }
+  }
+
+  const doUnlock = () => {
+    crudLoader
+      .unlock(props.file.id!)
+      .then(notification("File unlocked"))
+      .then(passThrough(() => props.setEdit(true)))
+      .then(passThrough(props.onUpdated));
+  };
+
+  if (isLocked) {
+    return (
+      <ActionConfirm
+        style="Alternate"
+        action="Unlock"
+        discrete={true}
+        onOk={() => doUnlock()}
+        requires="folder.unlock"
+      >
+        <div>
+          Are you sure you want to unlock the File {props.title}?
+          <br />
+          Anybody will then be able to edit it.
+        </div>
+      </ActionConfirm>
+    );
+  }
+
+  if (props.edit) {
+    return <></>;
+  }
+
+  return (
+    <ActionButton
+      style="Edit"
+      onOk={() => props.setEdit(true)}
+      requires="folder.edit"
+    />
+  );
+}
+
+function ButtonsEdit<T extends Pojo>(
+  props: ButtonContext<T> & {
+    file: T;
+    canDelete: boolean;
+    onDeleted: (file: T) => void;
+    onUpdated: (file: T) => void;
+    onCreated: (file: T) => void;
+    formRef: React.RefObject<HTMLFormElement | null>;
+  }
+) {
+  const crudLoader = new CrudLoader<T>(props.apiRootUrl, props.type);
+  if (!props.edit) {
+    return <></>;
+  }
+
+  const addMode = !props.file.id;
+
+  const doDelete = () => {
+    crudLoader
+      .delete(props.file.id!)
+      .then(notification("File deleted"))
+      .then(passThrough(() => props.setEdit(false)))
+      .then(passThrough(() => props.onDeleted(props.file)));
+  };
+
+  const doCancel = () => {
+    props.setEdit(false);
+  };
+
+  const doSave = () => {
+    if (!props.formRef.current!.checkValidity()) {
+      props.formRef.current!.requestSubmit();
+      return;
+    }
+
+    const data = new FormData(props.formRef.current!);
+    if (addMode) {
+      return (
+        crudLoader
+          .create(data)
+          .then(notification("File created"))
+          // Route to the newly created file
+          .then(passThrough(() => props.setEdit(false)))
+          .then(passThrough(props.onCreated))
+      );
+    } else {
+      return crudLoader
+        .update(props.file.id!, data)
+        .then(notification("File saved"))
+        .then(passThrough(() => props.setEdit(false)))
+        .then(passThrough(props.onUpdated));
+    }
+  };
+
+  return (
+    <>
+      {props.canDelete && (
+        <ActionConfirm
+          style="Delete"
+          discrete={true}
+          onOk={doDelete}
+          requires="folder.delete"
+        >
+          <div>Are you sure you want to DELETE the File {props.title}?</div>
+        </ActionConfirm>
+      )}
+      <ActionButton style="Cancel" onOk={() => doCancel()} />
+      <ActionButton style="Confirm" action="Save" onOk={doSave} />
+    </>
+  );
+}
 
 export default function IOPanel<T extends Pojo>(props: {
   apiRootUrl: string;
