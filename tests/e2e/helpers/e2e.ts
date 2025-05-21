cb84b7d91435e9cfe64992a8c0ad4bd635e306d9
Jean Honlet <jehon@users.noreply.github.com>
2025-05-21 17:20:40 +0200

    test: add E2ECryptomedic object

--- tests/e2e/helpers/e2e.ts
+++ tests/e2e/helpers/e2e.ts
@@ -1,5 +1,7 @@
 import { expect, type Locator, type Page } from "@playwright/test";
-import { CRUD } from "../../../src/utils/network";
+import type { IndexSignature } from "../../../src/types";
+import { CRUD, type CRUDType } from "../../../src/utils/network";
+import { passThrough } from "../../../src/utils/promises";
 import crApi from "./e2e-api";
 export { outputDate } from "../../../src/utils/date";
 
@@ -79,8 +81,12 @@ export async function crExpectUrl(page: Page, r: string | RegExp) {
 }
 
 export async function crReady(page: Page): Promise<void> {
+  await expect(page, `url: ${WebBaseUrl}`).toHaveTitle(/Cryptomedic/);
+  await expect(page.getByTestId("top-level")).toBeVisible();
+
   // https://developer.mozilla.org/en-US/docs/Web/API/Document/fonts#doing_operation_after_fonts_are_loaded
   // https://github.com/microsoft/playwright/issues/28204#issuecomment-1816895791
+
   await page.evaluate(() => document.fonts.ready);
 
   // No global spinning wheel anymore
@@ -93,4 +99,91 @@ export async function crAcceptPopup(page: Page | Locator, button: string) {
   await box.getByText(button).click();
 
   await expect(box).not.toBeVisible();
+}
+
+// ************************************
+//
+// Big object for the application
+//
+// ************************************
+
+// ts-unused-exports:disable-next-line
+export class E2ECryptomedic {
+  readonly page: Page;
+
+  constructor(page: Page) {
+    this.page = page;
+  }
+
+  async reload(segment: string = ""): Promise<void> {
+    await this.page.reload();
+    await crReady(this.page);
+    if (segment) {
+      await this.goTo(segment);
+    }
+  }
+
+  api(
+    url: string,
+    options: {
+      method?: CRUDType;
+      data?: any;
+    } = {}
+  ): Promise<any> {
+    //
+    // https://playwright.dev/docs/api/class-apirequestcontext#api-request-context-post
+    //
+    // Return the response object (json)
+    //
+
+    const requestor = this.page.request as IndexSignature<any>;
+    const absoluteApiUrl = `${WebBaseUrl}/api${url}`;
+
+    return requestor[(options.method ?? CRUD.read).toLowerCase()](
+      absoluteApiUrl,
+      {
+        data: options.data ?? {}
+      }
+    )
+      .then(
+        passThrough<any>((resp) => {
+          if (resp.status() != 200) {
+            throw new Error(
+              "Server responded with invalid status: " + resp.status()
+            );
+          }
+        })
+      )
+      .then((resp: any) => resp.json());
+  }
+
+  async apiLogin(login = LOGINS.PHYSIO): Promise<void> {
+    await this.api("/auth/mylogin", {
+      method: CRUD.submit,
+      data: {
+        username: login,
+        password: PASSWORD
+      }
+    });
+    await this.page.reload();
+  }
+
+  async goTo(path: string): Promise<void> {
+    const absolutePath = `${WebBaseUrl}/built/frontend/ng1x.html?dev#${path}`;
+
+    await this.page.goto(absolutePath);
+    await crReady(this.page);
+  }
+}
+
+// ts-unused-exports:disable-next-line
+export async function startCryptomedic(
+  page: Page,
+  opts: { page?: string } = {}
+): Promise<E2ECryptomedic> {
+  const cryptomedic = new E2ECryptomedic(page);
+
+  await cryptomedic.reload(opts.page);
+
+  return cryptomedic;
 }
