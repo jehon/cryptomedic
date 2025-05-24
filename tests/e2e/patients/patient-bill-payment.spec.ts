63dabd1da623df385b6bfc9b03ec314225b5a05c
Jean Honlet <jehon@users.noreply.github.com>
2025-05-24 22:00:32 +0200

    test: remove e2e from payment

--- tests/e2e/patients/patient-bill-payment.spec.ts
+++ tests/e2e/patients/patient-bill-payment.spec.ts
@@ -1,6 +1,6 @@
 import { expect, test } from "@playwright/test";
 import { startCryptomedic } from "../helpers/e2e";
-import { E2EPatient } from "../helpers/e2e-patients";
+import { E2EIOPanel } from "../helpers/e2e-io-panel";
 
 // TODO: normalize fieldsConfig here
 test("2014-103.bill.2", async ({ page }) => {
@@ -8,13 +8,9 @@ test("2014-103.bill.2", async ({ page }) => {
   await cryptomedic.apiLogin();
   await cryptomedic.goTo(`/patient/3/bill/2`);
 
-  const e2eFile = await new E2EPatient(cryptomedic, 3).getFile({
-    fileType: "bill",
-    fileId: 2,
-    fieldsConfig: {}
-  });
+  const e2eIOPanel = new E2EIOPanel(cryptomedic.page.getByTestId(`bill.2`), {});
 
-  await e2eFile.expectOutputValue("Family Salary", 4500);
+  await e2eIOPanel.expectOutputValue("Family Salary", 4500);
   const paymentPanel = await page.getByTestId("bill.2.payments");
   await expect(paymentPanel).toBeVisible();
   const payment2 = paymentPanel.getByTestId("payment.2");
