d8fac3a574c58df33eb1d61ee2d99dda0ba2849a
Jean Honlet <jehon@users.noreply.github.com>
2025-05-26 16:38:29 +0200

    test: fix adding file

--- tests/e2e/helpers/e2e-file.ts
+++ tests/e2e/helpers/e2e-file.ts
@@ -1,21 +1,14 @@
 import test, { expect, type Page } from "@playwright/test";
-import { escapeRegExp } from "../../../src/utils/strings";
 import { outputDate, startCryptomedic } from "./e2e";
-import {
-  E2EForm,
-  type FieldsTypes,
-  type IOType,
-  type IOValue
-} from "./e2e-form";
+import { type FieldsTypes, type IOType, type IOValue } from "./e2e-form";
 import { E2EIOPanel } from "./e2e-io-panel";
-import { E2EPatient } from "./e2e-patients";
 
 type FieldConfigType = {
   type?: IOType;
   json?: string;
 };
 
-export type FieldsConfigTypeSimplified = {
+type FieldsConfigTypeSimplified = {
   [key: string]: IOType | FieldConfigType;
 };
 
@@ -67,126 +60,6 @@ function reduceFieldConfig2Form(fc?: FieldsConfigTypeSimplified): FieldsTypes {
       typeof v == "string" ? v : (v.type ?? "string")
     ])
   );
-}
-
-export class E2EFile extends E2EForm {
-  private fileBaseUrl = "";
-  protected page: Page;
-  protected type: string;
-  protected id: string;
-  protected e2ePatient: E2EPatient;
-  tmpFieldsConfig: FieldsTypes;
-
-  constructor(
-    e2ePatient: E2EPatient,
-    type: string,
-    id?: string,
-    fieldsConfig?: FieldsConfigTypeSimplified
-  ) {
-    super(
-      () => e2ePatient.cryptomedic.page.getByTestId(`${this.type}.${this.id}`),
-      reduceFieldConfig2Form(fieldsConfig)
-    );
-    this.e2ePatient = e2ePatient;
-    this.type = type;
-    this.page = e2ePatient.cryptomedic.page;
-    this.tmpFieldsConfig = reduceFieldConfig2Form(fieldsConfig);
-
-    if (id === undefined) {
-      this.id = this.e2ePatient.cryptomedic.detectId(this.type);
-    } else {
-      this.id = "" + id;
-    }
-    this.fileBaseUrl = `/patient/${this.e2ePatient.id}/${type}`;
-  }
-
-  /* ***********************************
-   *
-   * API
-   *
-   */
-
-  async apiFileUpdate(id: string, data: Record<string, string | number>) {
-    await this.e2ePatient.cryptomedic.apiCrudReset(
-      `/fiche/${this.type}`,
-      id,
-      data
-    );
-  }
-
-  /* ***********************************
-   *
-   * Generic
-   *
-   */
-
-  /* ***********************************
-   *
-   * Routes
-   *
-   */
-
-  async go(): Promise<E2EIOPanel> {
-    await this.e2ePatient.go();
-    const e2eIOPanel = new E2EIOPanel(
-      this.e2ePatient.cryptomedic.page.getByTestId(`${this.type}.${this.id}`),
-      this.tmpFieldsConfig
-    );
-    e2eIOPanel.doOpen();
-    return e2eIOPanel;
-  }
-
-  async doDelete(): Promise<this> {
-    const e2eIOPanel = new E2EIOPanel(
-      this.e2ePatient.cryptomedic.page.getByTestId(`${this.type}.${this.id}`),
-      this.tmpFieldsConfig
-    );
-    await e2eIOPanel.doDelete();
-
-    await this.e2ePatient.cryptomedic.waitForPath(
-      `/patient/${this.e2ePatient.id}/${this.type}/*`
-    );
-    return this;
-  }
-
-  async doSave(interceptAddedId: boolean = false): Promise<this> {
-    await this.expectToBeVisible();
-
-    const e2eIOPanel = new E2EIOPanel(
-      this.e2ePatient.cryptomedic.page.getByTestId(`${this.type}.${this.id}`),
-      this.tmpFieldsConfig
-    );
-    if (interceptAddedId) {
-      await e2eIOPanel.doCreate();
-      await this.e2ePatient.cryptomedic.waitForPathByRegex(
-        new RegExp(`^.*#${escapeRegExp(this.fileBaseUrl)}/[0-9]+$`)
-      );
-      this.id = this.e2ePatient.cryptomedic.detectId(this.type);
-    } else {
-      await e2eIOPanel.doSave();
-      await this.e2ePatient.cryptomedic.waitForPath(
-        `${escapeRegExp(this.fileBaseUrl)}/${this.id}`
-      );
-    }
-
-    // New id is detected, let's match it
-    await this.e2ePatient.cryptomedic.waitForPath(
-      `${escapeRegExp(this.fileBaseUrl)}/${this.id}`
-    );
-    return this;
-  }
-
-  async goEdit(): Promise<this> {
-    const e2eIOPanel = new E2EIOPanel(
-      this.e2ePatient.cryptomedic.page.getByTestId(`${this.type}.${this.id}`),
-      this.tmpFieldsConfig
-    );
-    e2eIOPanel.doEdit();
-
-    await this.e2ePatient.cryptomedic.waitForPath(`${this.fileBaseUrl}/*/edit`);
-
-    return this;
-  }
 }
 
 export function fullTest(context: {
@@ -258,7 +131,11 @@ export function fullTest(context: {
         async ({ page }) => {
           const cryptomedic = startCryptomedic(page);
           await cryptomedic.apiLogin();
-          await cryptomedic.goTo(`/patient/${options.patientId}/`);
+
+          // TODO: Use the add button
+          await cryptomedic.goTo(
+            `/patient/${options.patientId}/${context.fileType}/add`
+          );
 
           let e2eIOPanel = new E2EIOPanel(
             cryptomedic.page.getByTestId(`${context.fileType}.add`),
