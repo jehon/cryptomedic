1e809ff7ebc1dc997ef3e9e093c0e3dab6689c3b
Jean Honlet <jehon@users.noreply.github.com>
2025-05-27 17:18:30 +0200

    test: total based on api created payment

--- tests/e2e/patients/patient-bill-payment.spec.ts
+++ tests/e2e/patients/patient-bill-payment.spec.ts
@@ -2,7 +2,9 @@ import { expect, test } from "@playwright/test";
 import { startCryptomedic } from "../helpers/e2e";
 import { E2EPatient } from "../helpers/e2e-patients";
 
-// TODO: normalize fieldsConfig here
+const entryYear = "2024";
+const totalPaymentsLabel = "Payments Received (see below)";
+
 test("2014-103.bill.2", async ({ page }) => {
   const cryptomedic = await startCryptomedic(page);
   await cryptomedic.apiLogin();
@@ -16,4 +18,42 @@ test("2014-103.bill.2", async ({ page }) => {
   await expect(paymentPanel).toBeVisible();
   const payment2 = paymentPanel.getByTestId("payment.2");
   await expect(payment2).toBeVisible();
+});
+
+test("bill.payment", async ({ page }) => {
+  const cryptomedic = startCryptomedic(page);
+  await cryptomedic.apiLogin();
+  const patientId = await cryptomedic.apiCreatePatient({
+    entry_year: entryYear
+  });
+  const billId = await cryptomedic.apiCrudCreate(`/fiche/bill`, {
+    patient_id: patientId,
+    price_id: 1,
+    examiner: "Ershad"
+  });
+
+  await cryptomedic.goTo(`/patient/${patientId}/bill/${billId}`);
+  const billPanel = new E2EIOPanel(
+    cryptomedic.page.getByTestId(`bill.${billId}`),
+    {}
+  );
+  await billPanel.doOpen();
+  const paymentPanel = await cryptomedic.page.getByTestId(
+    `bill.${billId}.payments`
+  );
+
+  await expect(paymentPanel).toBeVisible();
+  await billPanel.expectOutputValue(totalPaymentsLabel, "0.0");
+
+  // API Add payment, check total
+  await cryptomedic.apiCrudCreate(`/fiche/payment`, {
+    bill_id: billId,
+    amount: 100
+  });
+
+  await cryptomedic.page.reload();
+  await expect(paymentPanel).toBeVisible();
+  await billPanel.expectOutputValue(totalPaymentsLabel, "100.0");
+
+  // GUI payment crud
 });
