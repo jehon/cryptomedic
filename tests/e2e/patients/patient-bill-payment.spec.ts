475ae36a5181c5cdd72ece951b6fa82d426b2c6e
Jean Honlet <jehon@users.noreply.github.com>
2025-05-29 12:26:33 +0200

    test: re-add simple test

---
+++ tests/e2e/patients/patient-bill-payment.spec.ts
@@ -0,0 +1,17 @@
+import { expect, test } from "@playwright/test";
+import { startCryptomedic } from "../helpers/e2e";
+import { E2EIOPanel } from "../helpers/e2e-io-panel";
+
+test("2014-103.bill.2", async ({ page }) => {
+  const cryptomedic = startCryptomedic(page);
+  await cryptomedic.apiLogin();
+  await cryptomedic.goTo(`/patient/3/bill/2`);
+
+  const e2eIOPanel = new E2EIOPanel(cryptomedic.page.getByTestId(`bill.2`), {});
+
+  await e2eIOPanel.expectOutputValue("Family Salary", 4500);
+  const paymentPanel = await page.getByTestId("bill.2.payments");
+  await expect(paymentPanel).toBeVisible();
+  const payment2 = paymentPanel.getByTestId("payment.2");
+  await expect(payment2).toBeVisible();
+});
