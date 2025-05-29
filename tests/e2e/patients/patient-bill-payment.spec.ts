36f01095f4e8c06a137abcba1003810c67054b57
Jean Honlet <jehon@users.noreply.github.com>
2025-05-29 13:13:15 +0200

    test: add bill.payment

--- tests/e2e/patients/patient-bill-payment.spec.ts
+++ tests/e2e/patients/patient-bill-payment.spec.ts
@@ -1,6 +1,13 @@
 import { expect, test } from "@playwright/test";
 import { startCryptomedic } from "../helpers/e2e";
+import { e2eDefaultDate, type FieldsTypes } from "../helpers/e2e-form";
 import { E2EIOPanel } from "../helpers/e2e-io-panel";
+
+const totalPaymentsLabel = "Payments Received (see below)";
+const paymentFieldsConfig: FieldsTypes = {
+  Date: "date",
+  Comments: "textarea"
+};
 
 test("2014-103.bill.2", async ({ page }) => {
   const cryptomedic = startCryptomedic(page);
@@ -14,4 +21,91 @@ test("2014-103.bill.2", async ({ page }) => {
   await expect(paymentPanel).toBeVisible();
   const payment2 = paymentPanel.getByTestId("payment.2");
   await expect(payment2).toBeVisible();
+});
+
+test("bill.payment", async ({ page }) => {
+  const cryptomedic = startCryptomedic(page);
+  await cryptomedic.apiLogin();
+  const patientId = await cryptomedic.apiCreatePatient();
+  const billId = await cryptomedic.apiCrudCreate(`/fiche/bill`, {
+    patient_id: patientId,
+    price_id: 1,
+    examiner: "Ershad"
+  });
+
+  await cryptomedic.goTo(`/patient/${patientId}/bill/${billId}`);
+  const billPanel = new E2EIOPanel(
+    cryptomedic.page.getByTestId(`bill.${billId}`),
+    {}
+  );
+  await billPanel.doOpen();
+  const paymentPanel = await cryptomedic.page.getByTestId(
+    `bill.${billId}.payments`
+  );
+
+  await expect(paymentPanel).toBeVisible();
+  await billPanel.expectOutputValue(totalPaymentsLabel, "0.0");
+
+  // API Add payment, check total
+  await cryptomedic.apiCrudCreate(`/fiche/payment`, {
+    bill_id: billId,
+    amount: 100
+  });
+
+  // API Add payment, check total
+  const editId = await cryptomedic.apiCrudCreate(`/fiche/payment`, {
+    bill_id: billId,
+    amount: 150
+  });
+
+  await cryptomedic.page.reload();
+  await expect(paymentPanel).toBeVisible();
+  await billPanel.expectOutputValue(totalPaymentsLabel, "250.0");
+
+  // GUI payment crud
+  {
+    await paymentPanel.getByRole("button", { name: "Add" }).click();
+    const paymentAddPanel = new E2EIOPanel(
+      paymentPanel.getByTestId("payment.add"),
+      paymentFieldsConfig
+    );
+    await paymentAddPanel.waitToBeVisible();
+    await paymentAddPanel.setAllInputValues({
+      Date: e2eDefaultDate(),
+      Amount: 500,
+      Comments: "test e2e"
+    });
+    await paymentAddPanel.doCreate();
+    await billPanel.waitToBeVisible();
+    await billPanel.expectOutputValue(totalPaymentsLabel, "750.0");
+  }
+  {
+    // GUI payment edit
+    const newData = {
+      Date: e2eDefaultDate(2),
+      Amount: "175",
+      Comments: "e2e updated"
+    };
+    const paymentEditPanel = new E2EIOPanel(
+      paymentPanel.getByTestId(`payment.${editId}`),
+      paymentFieldsConfig
+    );
+    await paymentEditPanel.doOpen();
+    await paymentEditPanel.doEdit();
+    await paymentEditPanel.expectAllInputValues({
+      Amount: "150"
+    });
+    await paymentEditPanel.setAllInputValues(newData);
+    await paymentEditPanel.doSave();
+    await paymentEditPanel.doOpen();
+    await paymentEditPanel.expectAllOutputValues(newData);
+
+    await billPanel.expectOutputValue(totalPaymentsLabel, "775.0");
+
+    // GUI payment Delete
+    await paymentEditPanel.doEdit();
+    await paymentEditPanel.doDelete();
+
+    await billPanel.expectOutputValue(totalPaymentsLabel, "600.0");
+  }
 });
