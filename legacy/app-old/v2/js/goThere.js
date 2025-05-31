394fe681a9ff229f5d1696ef8a3d3e66016ab305
Jean Honlet <jehon@users.noreply.github.com>
2025-05-31 13:02:38 +0200

    chore: remove unused files

--- legacy/app-old/v2/js/goThere.js
+++
@@ -1,7 +0,0 @@
-export default function goThere(path = "/") {
-  // console.error("Going to " + path);
-  if (!path || path == "") {
-    path = "/";
-  }
-  location.hash = path;
-}
--- legacy/app-old/v2/pages/x-page-folder.js
+++
@@ -1,7 +0,0 @@
-export default class XPageFolder extends HTMLElement {
-  constructor() {
-    super();
-  }
-}
-
-customElements.define("x-page-folder", XPageFolder);
